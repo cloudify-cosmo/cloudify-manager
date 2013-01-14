e3a6b7264035685746effe339ffed83b7e773795
Itai Frenkel <itaif@gigaspaces.com>
2013-01-14 17:31:25 +0200

    Added the ability of the mock agent to detect wrong lifecycle of service instance

--- src/test/java/org/openspaces/servicegrid/mock/MockAgent.java
+++ src/test/java/org/openspaces/servicegrid/mock/MockAgent.java
@@ -1,5 +1,8 @@
 package org.openspaces.servicegrid.mock;
 
+import java.net.URI;
+import java.util.Map;
+
 import org.openspaces.servicegrid.ImpersonatingTaskConsumer;
 import org.openspaces.servicegrid.TaskConsumer;
 import org.openspaces.servicegrid.TaskConsumerState;
@@ -9,36 +12,53 @@ import org.openspaces.servicegrid.agent.tasks.PingAgentTask;
 import org.openspaces.servicegrid.service.state.ServiceInstanceState;
 import org.openspaces.servicegrid.service.tasks.InstallServiceInstanceTask;
 import org.openspaces.servicegrid.service.tasks.StartServiceInstanceTask;
+import org.openspaces.servicegrid.streams.StreamUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 
 public class MockAgent {
 
 	private final AgentState state;
-
+	private final Map<URI, ServiceInstanceState> instancesState;
+	private ObjectMapper mapper = StreamUtils.newJsonObjectMapper();
+	
 	public MockAgent(AgentState state) {
 		this.state = state;
+		this.instancesState = Maps.newLinkedHashMap();
 	}
 
 	@ImpersonatingTaskConsumer
 	public void startServiceInstance(StartServiceInstanceTask task,
 			TaskExecutorStateModifier impersonatedStateModifier) {
+		
 		ServiceInstanceState instanceState = impersonatedStateModifier.getState();
+		Preconditions.checkState(StreamUtils.elementEquals(mapper, instancesState.get(task.getImpersonatedTarget()), instanceState));
+		
 		instanceState.setProgress(ServiceInstanceState.Progress.STARTING_INSTANCE);
 		impersonatedStateModifier.updateState(instanceState);
 		instanceState = impersonatedStateModifier.getState();
 		instanceState.setProgress(ServiceInstanceState.Progress.INSTANCE_STARTED);
 		impersonatedStateModifier.updateState(instanceState);
+		instancesState.put(task.getImpersonatedTarget(), instanceState);
 
 	}
 
 	@ImpersonatingTaskConsumer
 	public void installServiceInstance(InstallServiceInstanceTask task,
 			TaskExecutorStateModifier impersonatedStateModifier) {
+		
+		Preconditions.checkState(!instancesState.containsKey(task.getImpersonatedTarget()));
+		
 		ServiceInstanceState instanceState = impersonatedStateModifier.getState();
 		instanceState.setProgress(ServiceInstanceState.Progress.INSTALLING_INSTANCE);
 		impersonatedStateModifier.updateState(instanceState);
 		instanceState = impersonatedStateModifier.getState();
 		instanceState.setProgress(ServiceInstanceState.Progress.INSTANCE_INSTALLED);
 		impersonatedStateModifier.updateState(instanceState);
+		
+		instancesState.put(task.getImpersonatedTarget(), instanceState);
 	}
 
 	@TaskConsumer
