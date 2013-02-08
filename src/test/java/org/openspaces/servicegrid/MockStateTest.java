cfe85060d403060bbc37c8ab0204ea1d8f8bac86
Itai Frenkel <itaif@gigaspaces.com>
2013-02-08 20:18:29 +0200

    Refactoring to use StateReader and StateWriter interfaces

--- src/test/java/org/openspaces/servicegrid/MockStateTest.java
+++ src/test/java/org/openspaces/servicegrid/MockStateTest.java
@@ -9,6 +9,8 @@ import junit.framework.Assert;
 
 import org.openspaces.servicegrid.mock.MockState;
 import org.openspaces.servicegrid.state.Etag;
+import org.openspaces.servicegrid.state.StateReader;
+import org.openspaces.servicegrid.state.StateWriter;
 import org.openspaces.servicegrid.streams.StreamUtils;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,21 +23,23 @@ public class MockStateTest {
 	final ObjectMapper mapper = StreamUtils.newObjectMapper();
 	private final Logger logger = Logger.getLogger(this.getClass().getName());
 	
-	MockState state;
+	StateReader stateReader;
+	StateWriter stateWriter;
 	
 	@BeforeMethod
 	public void beforeMethod(Method method) {
-		state = new MockState();
+		stateReader = new MockState();
+		stateWriter = (StateWriter) stateReader;
 		logger.info("Starting " + method.getName());
 	}
 
 	@Test
 	public void testFirstPut() throws URISyntaxException {
 		final TaskConsumerState taskConsumerState = new TaskConsumerState();
-		final Etag etag = state.put(id, taskConsumerState, Etag.EMPTY);
+		final Etag etag = stateWriter.put(id, taskConsumerState, Etag.EMPTY);
 		Assert.assertFalse(Etag.EMPTY.equals(etag));
 		
-		assertEqualsState(taskConsumerState, state.get(id, TaskConsumerState.class).getState());
+		assertEqualsState(taskConsumerState, stateReader.get(id, TaskConsumerState.class).getState());
 	}
 
 	@Test
@@ -44,10 +48,10 @@ public class MockStateTest {
 		final TaskConsumerState taskConsumerState2 = new TaskConsumerState();
 		taskConsumerState2.setProperty("kuku", "loko");
 		
-		final Etag etag1 = state.put(id, taskConsumerState1, Etag.EMPTY);
-		final Etag etag2 = state.put(id, taskConsumerState2, etag1);
+		final Etag etag1 = stateWriter.put(id, taskConsumerState1, Etag.EMPTY);
+		final Etag etag2 = stateWriter.put(id, taskConsumerState2, etag1);
 		Assert.assertFalse(etag1.equals(etag2));
-		assertEqualsState(taskConsumerState2, state.get(id, TaskConsumerState.class).getState());
+		assertEqualsState(taskConsumerState2, stateReader.get(id, TaskConsumerState.class).getState());
 	}
 
 	@Test(expectedExceptions = IllegalArgumentException.class)
@@ -55,8 +59,8 @@ public class MockStateTest {
 		final TaskConsumerState taskConsumerState1 = new TaskConsumerState();
 		final TaskConsumerState taskConsumerState2 = new TaskConsumerState();
 		taskConsumerState2.setProperty("kuku", "loko");
-		state.put(id, taskConsumerState1, Etag.EMPTY);
-		state.put(id, taskConsumerState2, Etag.EMPTY);
+		stateWriter.put(id, taskConsumerState1, Etag.EMPTY);
+		stateWriter.put(id, taskConsumerState2, Etag.EMPTY);
 	}
 	
 	private void assertEqualsState(
@@ -67,6 +71,6 @@ public class MockStateTest {
 	
 	@Test
 	public void testEmptyGet() throws URISyntaxException {
-		Assert.assertEquals(null, state.get(id, TaskConsumerState.class));
+		Assert.assertEquals(null, stateReader.get(id, TaskConsumerState.class));
 	}
 }
