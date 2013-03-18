7a7e37656ce06cc7c718f7f6de62d9df06caa273
Niv Ingberg <niv@gigaspaces.com>
2013-03-18 13:02:53 +0200

    Extracted CloudDriver from CloudGateway.

--- orchestrator/src/main/java/org/cloudifysource/cosmo/cloud/CloudGateway.java
+++ orchestrator/src/main/java/org/cloudifysource/cosmo/cloud/CloudGateway.java
@@ -16,17 +16,9 @@ package org.cloudifysource.cosmo.cloud;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import org.jclouds.ContextBuilder;
-import org.jclouds.compute.ComputeServiceContext;
 import org.jclouds.compute.RunNodesException;
 import org.jclouds.compute.domain.ComputeMetadata;
 import org.jclouds.compute.domain.NodeMetadata;
-import org.jclouds.compute.domain.OsFamily;
-import org.jclouds.compute.domain.Template;
-import org.jclouds.ec2.domain.InstanceType;
-
-import java.util.Set;
 
 /**
  * TODO: Write a short summary of this type's roles and responsibilities.
@@ -36,87 +28,54 @@ import java.util.Set;
  */
 public class CloudGateway {
 
-    private final ComputeServiceContext context;
-    private Template defaultTemplate;
+    private final CloudDriver driver;
 
-    public static CloudGateway createEC2(String accessKeyId, String secretKey) {
-        ContextBuilder builder = ContextBuilder.newBuilder("aws-ec2")
-                .credentials(accessKeyId, secretKey);
-        final ComputeServiceContext context = builder.build(ComputeServiceContext.class);
-        return new CloudGateway(context);
-    }
-
-    public CloudGateway(ComputeServiceContext context) {
-        this.context = context;
+    public CloudGateway(CloudDriver driver) {
+        this.driver = driver;
     }
 
     public void close() {
-        context.close();
+        driver.close();
     }
 
     public NodeMetadata ensureStarted(String name) throws RunNodesException {
         final Optional<NodeMetadata> node = getByName(name);
-        if (node.isPresent()) {
-            final NodeMetadata.Status status = node.get().getStatus();
-            switch (status) {
-                case RUNNING:
-                    return node.get();
-                case SUSPENDED:
-                    context.getComputeService().resumeNode(node.get().getId());
-                    // TODO: GetByName again to refresh status.
-                    return node.get();
-                case PENDING:
-                    // TODO: wait for new node state to settle.
-                    throw new IllegalStateException("Node '" + name + "' is in PENDING status");
-                case TERMINATED:
-                    // This state is illegal since terminated nodes should be filtered in getByName()
-                    throw new IllegalStateException("Node '" + name + "' is in TERMINATED status");
-                case ERROR:
-                    throw new IllegalStateException("Node '" + name + "' is in ERROR status");
-                case UNRECOGNIZED:
-                    throw new IllegalStateException("Node '" + name + "' is in UNRECOGNIZED status");
-                default:
-                    throw new IllegalStateException("Node '" + name + "' is in unrecognized status: " + status);
-            }
-        } else {
-            return create(name);
+        if (!node.isPresent())
+            return driver.create(name);
+
+        final NodeMetadata.Status status = node.get().getStatus();
+        switch (status) {
+            case RUNNING:
+                return node.get();
+            case SUSPENDED:
+                driver.resume(node.get());
+                return driver.searchNodeById(node.get().getId()).get();
+            case PENDING:
+                // TODO: wait for new node state to settle.
+                throw new IllegalStateException("Node '" + name + "' is in PENDING status");
+            case TERMINATED:
+                // This state is illegal since terminated nodes should be filtered in getByName()
+                throw new IllegalStateException("Node '" + name + "' is in TERMINATED status");
+            case ERROR:
+                throw new IllegalStateException("Node '" + name + "' is in ERROR status");
+            case UNRECOGNIZED:
+                throw new IllegalStateException("Node '" + name + "' is in UNRECOGNIZED status");
+            default:
+                throw new IllegalStateException("Node '" + name + "' is in unrecognized status: " + status);
         }
     }
 
     public void ensureDestoryed(String name) {
         final Optional<NodeMetadata> node = getByName(name);
         if (node.isPresent())
-            destroy(node.get());
+            driver.destroy(node.get());
     }
 
     private Optional<NodeMetadata> getByName(String name) {
         Predicate<ComputeMetadata> filter = com.google.common.base.Predicates.and(
                 new GroupPredicate(name),
                 new StatusPredicate());
-        final Set<? extends NodeMetadata> nodes = context.getComputeService().listNodesDetailsMatching(filter);
-        if (nodes.isEmpty())
-            return Optional.absent();
-        return Optional.of(Iterables.getOnlyElement(nodes));
-    }
-
-    private NodeMetadata create(String name) throws RunNodesException {
-        final Set<? extends NodeMetadata> nodes =
-                context.getComputeService().createNodesInGroup(name, 1, getDefaultTemplate());
-        return Iterables.getOnlyElement(nodes);
-    }
-
-    private void destroy(NodeMetadata node) {
-        context.getComputeService().destroyNode(node.getId());
-    }
-
-    private Template getDefaultTemplate() {
-        if (defaultTemplate == null) {
-            defaultTemplate = context.getComputeService().templateBuilder()
-                    .hardwareId(InstanceType.T1_MICRO)
-                    .osFamily(OsFamily.AMZN_LINUX)
-                    .build();
-        }
-        return defaultTemplate;
+        return driver.searchSingleNode(filter);
     }
 
     /**
