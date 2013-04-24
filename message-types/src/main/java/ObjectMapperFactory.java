2f59a059e1022c70dd326dca75d2af1c650015b0
Itai Frenkel <itaif@gigaspaces.com>
2013-04-24 15:07:16 +0300

    Moved ObjectMapperFactory from message-client to message-types

--- message-types/src/main/java/ObjectMapperFactory.java
+++ message-types/src/main/java/ObjectMapperFactory.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package org.cloudifysource.cosmo.messaging;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
