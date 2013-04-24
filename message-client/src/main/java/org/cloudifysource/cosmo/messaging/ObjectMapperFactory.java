5fba85e337568f299cada01514af123ebe658536
Itai Frenkel <itaif@gigaspaces.com>
2013-04-24 17:26:11 +0300

    Moved OMF to correct package folder

--- message-client/src/main/java/org/cloudifysource/cosmo/messaging/ObjectMapperFactory.java
+++ message-client/src/main/java/org/cloudifysource/cosmo/messaging/ObjectMapperFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-
+package org.cloudifysource.cosmo.messaging;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
