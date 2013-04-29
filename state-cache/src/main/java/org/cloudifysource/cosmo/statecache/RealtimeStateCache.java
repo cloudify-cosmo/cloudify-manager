dcd8eeb71b5ea3634d6254310ae6628c13d5293c
Itai Frenkel <itaif@gigaspaces.com>
2013-04-29 10:09:20 +0300

    CLOUDIFY-1705 Fixed checkstyle warnings

--- state-cache/src/main/java/org/cloudifysource/cosmo/statecache/RealtimeStateCache.java
+++ state-cache/src/main/java/org/cloudifysource/cosmo/statecache/RealtimeStateCache.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package org.cloudifysource.cosmo.statecache;
 
 import org.cloudifysource.cosmo.messaging.consumer.MessageConsumer;
@@ -32,10 +48,9 @@ public class RealTimeStateCache {
                 @Override
                 public void onMessage(URI uri, Object message) {
                     if (message instanceof StateChangedMessage) {
-                        StateChangedMessage update = (StateChangedMessage)message;
+                        StateChangedMessage update = (StateChangedMessage) message;
                         stateCache.put(update.getResourceId(), update);
-                    }
-                    else {
+                    } else {
                         throw new IllegalArgumentException("Cannot handle message " + message);
                     }
                 }
