0c3f0399467f002dfe1534976a9e22aed963ef61
Dan Kilman <dankilman@gmail.com>
2013-04-29 13:29:12 +0300

    Extract consumer operations on the state cache to an interface with a default delegting implementation

---
+++ state-cache/src/main/java/org/cloudifysource/cosmo/statecache/DefaultStateCacheReader.java
@@ -0,0 +1,10 @@
+package org.cloudifysource.cosmo.statecache;
+
+/**
+ * TODO: Write a short summary of this type's roles and responsibilities.
+ *
+ * @author Dan Kilman
+ * @since 0.1
+ */
+public class DefaultStateCacheReader {
+}
