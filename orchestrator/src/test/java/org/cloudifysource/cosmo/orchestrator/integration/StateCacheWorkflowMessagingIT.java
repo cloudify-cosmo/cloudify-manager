5a7b198d37b4d744024fe3bf932b392cd8231109
Idan Moyal <idan@gigaspaces.com>
2013-05-02 16:54:52 +0300

    CLOUDIFY-1705 Added a resource_id semantic to resource manager participant.

--- orchestrator/src/test/java/org/cloudifysource/cosmo/orchestrator/integration/StateCacheWorkflowMessagingIT.java
+++ orchestrator/src/test/java/org/cloudifysource/cosmo/orchestrator/integration/StateCacheWorkflowMessagingIT.java
@@ -14,15 +14,17 @@
  * limitations under the License.
  *******************************************************************************/
 
-package org.cloudifysource.cosmo.statecache;
+package org.cloudifysource.cosmo.orchestrator.integration;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import org.cloudifysource.cosmo.cloud.driver.CloudDriver;
 import org.cloudifysource.cosmo.messaging.broker.MessageBrokerServer;
 import org.cloudifysource.cosmo.messaging.producer.MessageProducer;
 import org.cloudifysource.cosmo.orchestrator.workflow.RuoteRuntime;
+import org.cloudifysource.cosmo.orchestrator.workflow.RuoteWorkflow;
 import org.cloudifysource.cosmo.resource.CloudResourceProvisioner;
+import org.cloudifysource.cosmo.statecache.RealTimeStateCache;
+import org.cloudifysource.cosmo.statecache.RealTimeStateCacheConfiguration;
 import org.cloudifysource.cosmo.statecache.messages.StateChangedMessage;
 import org.mockito.Mockito;
 import org.testng.annotations.AfterMethod;
@@ -33,79 +35,52 @@ import org.testng.annotations.Test;
 
 import java.net.URI;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 
 /**
- * Tests integration of {@link StateCache} with messaging consumer.
+ * Tests integration of {@link org.cloudifysource.cosmo.statecache.StateCache} with messaging consumer.
  * @author itaif
  * @since 0.1
  */
-public class StateCacheMessagingIT {
+public class StateCacheWorkflowMessagingIT {
 
     // message broker that isolates server
     private MessageBrokerServer broker;
     private URI inputUri;
     private MessageProducer producer;
     private RealTimeStateCache cache;
-
-    @Test(timeOut = 5000)
-    public void testNodeOk() throws InterruptedException, ExecutionException {
-        final String key = "node_1";
-        final StateChangedMessage message = newStateChangedMessage(key);
-        producer.send(inputUri, message).get();
-        final CountDownLatch success = new CountDownLatch(1);
-        String subscriptionId = cache.subscribeToKeyValueStateChanges(null, null, key, newState(),
-            new StateChangeCallback() {
-                @Override
-                public void onStateChange(Object receiver, Object context, StateCache cache,
-                                          ImmutableMap<String, Object> newSnapshot) {
-                    Map<String, Object> receivedState = (Map<String, Object>) newSnapshot.get(key);
-                    if ((Boolean) receivedState.get("reachable")) {
-                        success.countDown();
-                    }
-                }
-            });
-        success.await();
-        cache.removeCallback(subscriptionId);
-    }
-
-    private StateChangedMessage newStateChangedMessage(String resourceId) {
-        final StateChangedMessage message = new StateChangedMessage();
-        message.setResourceId(resourceId);
-        message.setState(newState());
-        return message;
-    }
-
-    private Map<String, Object> newState() {
-        Map<String, Object> state = Maps.newLinkedHashMap();
-        state.put("reachable", true);
-        return state;
-    }
-
+    private RuoteRuntime runtime;
+    private CloudResourceProvisioner provisioner;
+    private URI provisionerUri;
+    private URI cacheUri;
 
     @BeforeMethod
-    @Parameters({"port" })
+    @Parameters({ "port" })
     public void startServer(@Optional("8080") int port) {
         startMessagingBroker(port);
         inputUri = URI.create("http://localhost:" + port + "/input/");
+        provisionerUri = inputUri.resolve("resource-manager");
+        cacheUri = inputUri.resolve("state");
         producer = new MessageProducer();
-        RealTimeStateCacheConfiguration config =
-                new RealTimeStateCacheConfiguration();
-        config.setMessageTopic(inputUri);
+        RealTimeStateCacheConfiguration config = new RealTimeStateCacheConfiguration();
+        config.setMessageTopic(cacheUri);
         cache = new RealTimeStateCache(config);
         cache.start();
 
         Map<String, Object> runtimeProperties = Maps.newHashMap();
         runtimeProperties.put("state_cache", cache);
+        runtimeProperties.put("broker_uri", inputUri);
+        runtimeProperties.put("message_producer", producer);
         runtime = RuoteRuntime.createRuntime(runtimeProperties);
-        provisioner = new CloudResourceProvisioner(Mockito.mock(CloudDriver.class), inputUri);
+        provisioner = new CloudResourceProvisioner(Mockito.mock(CloudDriver.class), provisionerUri);
     }
 
     @AfterMethod(alwaysRun = true)
     public void stopServer() {
         if (cache != null)
             cache.stop();
+        if (provisioner != null)
+            provisioner.stop();
         stopMessageBroker();
     }
 
@@ -119,4 +94,45 @@ public class StateCacheMessagingIT {
             broker.stop();
         }
     }
+
+    @Test(timeOut = 10000)
+    public void testMessaging() throws ExecutionException, InterruptedException {
+        final String resourceId = "node_1";
+
+        // Create radial workflow
+        final String flow =
+                "define flow\n" +
+                "  echo '--1--'\n" +
+                "  resource id: 'abcd', resource_id: \"$resource_id\", action: \"start_machine\"\n" +
+                "  echo '--2--'\n" +
+                "  state resource_id: \"$resource_id\", key: \"reachable\", value: \"true\"\n";
+        final RuoteWorkflow workflow = RuoteWorkflow.createFromString(flow, runtime);
+
+        // Execute workflow
+        final Map<String, Object> workitem = Maps.newHashMap();
+        workitem.put("resource_id", resourceId);
+        final Object workflowId = workflow.asyncExecute(workitem);
+
+        // Update state cache
+        final StateChangedMessage message = newStateChangedMessage(resourceId);
+        producer.send(cacheUri, message).get();
+
+        // Wait for workflow to end
+        runtime.waitForWorkflow(workflowId);
+    }
+
+    private StateChangedMessage newStateChangedMessage(String resourceId) {
+        final StateChangedMessage message = new StateChangedMessage();
+        message.setResourceId(resourceId);
+        message.setState(newState());
+        return message;
+    }
+
+    private Map<String, Object> newState() {
+        Map<String, Object> state = Maps.newLinkedHashMap();
+        state.put("reachable", "true");
+        return state;
+    }
+
+
 }
