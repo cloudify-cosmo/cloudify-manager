ddc849a04a12f15a2b4153241850883cb45675e2
Idan Moyal <idan@gigaspaces.com>
2013-05-09 12:48:43 +0300

    CLOUDIFY 1734 fixed checkstyle error

---
+++ orchestrator/src/test/java/org/cloudifysource/cosmo/orchestrator/workflow/RuoteExecuteTaskParticipantTest.java
@@ -0,0 +1,10 @@
+package org.cloudifysource.cosmo.orchestrator.workflow;
+
+/**
+ * TODO: Write a short summary of this type's roles and responsibilities.
+ *
+ * @author Idan Moyal
+ * @since 0.1
+ */
+public class RuoteExecuteTaskParticipantTest {
+}
