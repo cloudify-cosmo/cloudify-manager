e109edf60467656044bd508b40a29ae25637cb9c
Itai Frenkel <itaif@gigaspaces.com>
2013-05-21 16:26:22 +0300

    CLOUDIFY-1759 Removed unused imports

--- resource-monitor/src/test/java/org/cloudifysource/cosmo/monitor/ResourceMonitorConsequenceFailureTest.java
+++ resource-monitor/src/test/java/org/cloudifysource/cosmo/monitor/ResourceMonitorConsequenceFailureTest.java
@@ -20,15 +20,12 @@ import org.cloudifysource.cosmo.messaging.config.MockMessageProducerConfig;
 import org.cloudifysource.cosmo.monitor.config.MockAgentConfig;
 import org.cloudifysource.cosmo.monitor.config.ResourceMonitorServerConfig;
 import org.cloudifysource.cosmo.monitor.config.TestConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.springframework.validation.beanvalidation.BeanValidationPostProcessor;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
