c5756aeb0ab5f5987b465b79beb2b9c51146e426
Eitan Yanovsky <eitany@gigaspaces.com>
2013-06-10 14:21:20 +0300

    Adding a call which wraps an embedded jetty file server

---
+++ orchestrator/src/main/java/org/cloudifysource/cosmo/fileserver/JettyFileServer.java
@@ -0,0 +1,10 @@
+package org.cloudifysource.cosmo.fileserver;
+
+/**
+ * TODO: Write a short summary of this type's roles and responsibilities.
+ *
+ * @author Eitan Yanovsky
+ * @since 0.1
+ */
+public class JettyFileServer {
+}
---
+++ orchestrator/src/main/java/org/cloudifysource/cosmo/fileserver/config/JettyFileServerConfig.java
@@ -0,0 +1,10 @@
+package org.cloudifysource.cosmo.fileserver.config;
+
+/**
+ * TODO: Write a short summary of this type's roles and responsibilities.
+ *
+ * @author Eitan Yanovsky
+ * @since 0.1
+ */
+public class JettyFileServerConfig {
+}
