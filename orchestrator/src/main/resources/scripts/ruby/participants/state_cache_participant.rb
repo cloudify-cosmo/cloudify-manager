2e5d3ed08fbcd8f214afc05422b12d1d0aba7a5a
eitany <eitany@gigaspaces.com>
2013-07-17 15:56:58 +0300

    Make StateCacheParticipant implements StateCacheListener

--- orchestrator/src/main/resources/scripts/ruby/participants/state_cache_participant.rb
+++ orchestrator/src/main/resources/scripts/ruby/participants/state_cache_participant.rb
@@ -16,74 +16,16 @@
 
 java_import org.cloudifysource::cosmo::statecache::StateCacheListener
 
-#TODO: Collapse listener into participant in order to reduce code size
-class RuoteStateCacheListener
-  include StateCacheListener
-
-  NODE = 'node'
-  RUNTIME = 'cloudify_runtime'
-  PROPERTIES = 'properties'
-
-  @participant = nil
-  @resource_id = nil
-  @workitem = nil
-
-  def resource_id=(resource_id)
-    @resource_id = resource_id
-  end
-
-  def participant=(participant)
-    @participant = participant
-  end
-
-  def workitem=(workitem)
-    @workitem = workitem
-  end
-
-  def onResourceStateChange(snapshot)
-    matches = false
-
-    required_state = @workitem.params[StateCacheParticipant::STATE]
-
-    $logger.debug('RuoteStateChangeCallback checking state: [resource_id={}, parameters={}, snapshot={},
-required_state={}]', @resource_id, @workitem.params, snapshot, required_state)
-    required_state.each do |key, value|
-      matches = (snapshot.contains_property(@resource_id, key) and snapshot.get_property(@resource_id,
-                                                                                 key).get.to_s.eql? value.to_s)
-      break unless matches
-    end
-
-
-    if matches
-      if @workitem.fields.has_key? NODE
-        current_node = @workitem.fields[NODE]
-        state = snapshot.get_resource_properties(@resource_id)
-        node_state = Hash.new
-        state.each { |key, value| node_state[key] = value }
-        properties = current_node[PROPERTIES]
-
-        # state cache listener is always invoked by a single thread
-        # and therefore the following code is safe
-        # TODO: Consider initializing RUNTIME hashmap explicitly outside this participant and not eagerly
-        properties[RUNTIME] = Hash.new unless properties.has_key? RUNTIME
-        properties[RUNTIME][@resource_id] = node_state
-      end
-      @participant.reply(@workitem)
-      true
-    else
-      false
-    end
-  end
-
-end
-
 class StateCacheParticipant < Ruote::Participant
-
+  include StateCacheListener
 
   STATE_CACHE = 'state_cache'
   RESOURCE_ID = 'resource_id'
   LISTENER_ID = 'listener_id'
   STATE = 'state'
+  NODE = 'node'
+  RUNTIME = 'cloudify_runtime'
+  PROPERTIES = 'properties'
 
   def on_workitem
     begin
@@ -93,11 +35,7 @@ class StateCacheParticipant < Ruote::Participant
       raise "#{RESOURCE_ID} property is not set" unless defined? resource_id
       raise "#{STATE} parameter is not defined for state cache participant" unless workitem.params.has_key? STATE
 
-      listener = RuoteStateCacheListener.new
-      listener.participant = self
-      listener.resource_id = resource_id
-      listener.workitem = workitem
-      listener_id = state_cache.subscribe(resource_id, listener)
+      listener_id = state_cache.subscribe(resource_id, self)
       $logger.debug('StateCacheParticipant: subscribed with [resource_id={}, workitem={}]', resource_id, workitem)
       put(LISTENER_ID, listener_id)
     rescue Exception => e
@@ -119,6 +57,42 @@ class StateCacheParticipant < Ruote::Participant
 
   def do_not_thread
     true
+  end
+
+  def onResourceStateChange(snapshot)
+    matches = false
+    resource_id = workitem.params[RESOURCE_ID]
+    required_state = workitem.params[STATE]
+
+    $logger.debug('StateCacheParticipant onResourceStateChange called, checking state: [resource_id={}, parameters={},
+snapshot={},
+required_state={}]', resource_id, workitem.params, snapshot, required_state)
+    required_state.each do |key, value|
+      matches = (snapshot.contains_property(resource_id, key) and snapshot.get_property(resource_id,
+                                                                                        key).get.to_s.eql? value.to_s)
+      break unless matches
+    end
+
+
+    if matches
+      if workitem.fields.has_key? NODE
+        current_node = workitem.fields[NODE]
+        state = snapshot.get_resource_properties(resource_id)
+        node_state = Hash.new
+        state.each { |key, value| node_state[key] = value }
+        properties = current_node[PROPERTIES]
+
+        # state cache listener is always invoked by a single thread
+        # and therefore the following code is safe
+        # TODO: Consider initializing RUNTIME hashmap explicitly outside this participant and not eagerly
+        properties[RUNTIME] = Hash.new unless properties.has_key? RUNTIME
+        properties[RUNTIME][resource_id] = node_state
+      end
+      reply(workitem)
+      true
+    else
+      false
+    end
   end
 
 end
