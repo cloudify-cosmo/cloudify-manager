e7ee6863f5ac89644684ed9641f676c69c04a0fe
iliapolo <elip@gigaspaces.com>
2013-07-24 14:06:43 +0300

    CLOUDIFY-1911 remove duplicate process output logger

--- orchestrator/src/main/java/org/cloudifysource/cosmo/manager/process/VagrantRiemannMonitorProcess.java
+++ orchestrator/src/main/java/org/cloudifysource/cosmo/manager/process/VagrantRiemannMonitorProcess.java
@@ -21,6 +21,7 @@ import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 import org.cloudifysource.cosmo.logging.Logger;
 import org.cloudifysource.cosmo.logging.LoggerFactory;
+import org.cloudifysource.cosmo.tasks.ProcessOutputLogger;
 
 import java.io.File;
 import java.io.IOException;
