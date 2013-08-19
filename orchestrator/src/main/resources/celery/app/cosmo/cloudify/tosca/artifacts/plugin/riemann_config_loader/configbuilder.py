d912c4436b8b8d22ac8f415c1e27569648a6abc0
iliapolo <elip@gigaspaces.com>
2013-08-19 10:01:17 +0300

    CLOUDIFY-1981 added support for using rule place holders withing message

--- orchestrator/src/main/resources/celery/app/cosmo/cloudify/tosca/artifacts/plugin/riemann_config_loader/configbuilder.py
+++ orchestrator/src/main/resources/celery/app/cosmo/cloudify/tosca/artifacts/plugin/riemann_config_loader/configbuilder.py
@@ -12,15 +12,23 @@ def build_riemann_config(template, policies, rules, policies_events):
         for node_policy_name, node_policy in node_policies.items():
             node_policy_events_template = policies_events[node_policy_name]['policy']
 
-            policy_message = rule_message = ''
+            policy_message = rule_message = None
+            aggregate_message = ''
             if 'message' in policies_events[node_policy_name]:
                 policy_message = policies_events[node_policy_name]['message']
             if rules:
-                first_rule = rules.itervalues().next()
+                first_rule = rules.itervalues().next()  # assume for now we only have one rule per policy.
                 if 'message' in first_rule:
                     rule_message = first_rule['message']
 
-            aggregate_message = "{0} : {1}".format(policy_message, rule_message)
+            if policy_message:
+                aggregate_message = policy_message
+                if rule_message:
+                    aggregate_message = "{0} : {1}".format(aggregate_message, rule_message)
+            else:
+                if rule_message:
+                    aggregate_message = rule_message
+
             node_policy = build_node_policy_config(node_id,
                                                    node_policy,
                                                    rules,
@@ -39,12 +47,16 @@ def build_node_policy_config(node_id,
                              rules,
                              node_policy_events_template,
                              node_policy_name,
-                             policy_message):
+                             message):
+
+    # ugly as hell. but we currently assume one rule so its ok?
+    rule = node_policy['rules'].itervalues().next()
+    message = string.Template(message).substitute(rule['properties'])
 
     event_json = build_node_policy_event(
         node_id,
         node_policy_name,
-        node_policy['rules'], policy_message)
+        node_policy['rules'], message)
     node_policy_events = string.Template(node_policy_events_template).substitute(dict(
         event=event_json,
         node_id=node_id
@@ -62,9 +74,9 @@ def build_node_policy_config(node_id,
     return node_policy_rules[0]
 
 
-def build_node_policy_event(node_id, node_policy_name, node_policy_rules, policy_message):
+def build_node_policy_event(node_id, node_policy_name, node_policy_rules, message):
     event = {'app_id': node_id.split('.')[0], 'node_id': node_id, 'policy': node_policy_name,
-             'message': policy_message,
+             'message': message,
              'rule': node_policy_rules}
     return json.dumps(event).replace('"', '\\"')
 
