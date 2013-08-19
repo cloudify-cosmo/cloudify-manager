d729262e8474c93ebb658b133e55167634aaf36d
iliapolo <elip@gigaspaces.com>
2013-08-19 08:40:17 +0300

    CLOUDIFY-1981 added policy/rule message to riemann event description

--- orchestrator/src/main/resources/celery/app/cosmo/cloudify/tosca/artifacts/plugin/riemann_config_loader/configbuilder.py
+++ orchestrator/src/main/resources/celery/app/cosmo/cloudify/tosca/artifacts/plugin/riemann_config_loader/configbuilder.py
@@ -1,7 +1,6 @@
 __author__ = 'dank'
 
 import string
-import sys
 import json
 
 
@@ -11,12 +10,23 @@ def build_riemann_config(template, policies, rules, policies_events):
 
     for node_id, node_policies in policies.items():
         for node_policy_name, node_policy in node_policies.items():
-            node_policy_events_template = policies_events[node_policy_name]
+            node_policy_events_template = policies_events[node_policy_name]['policy']
+
+            policy_message = rule_message = ''
+            if 'message' in policies_events[node_policy_name]:
+                policy_message = policies_events[node_policy_name]['message']
+            if rules:
+                first_rule = rules.itervalues().next()
+                if 'message' in first_rule:
+                    rule_message = first_rule['message']
+
+            aggregate_message = "{0} : {1}".format(policy_message, rule_message)
             node_policy = build_node_policy_config(node_id,
                                                    node_policy,
                                                    rules,
                                                    node_policy_events_template,
-                                                   node_policy_name)
+                                                   node_policy_name,
+                                                   aggregate_message)
             policies_config.append(node_policy)
 
     return string.Template(template).substitute(dict(
@@ -28,12 +38,13 @@ def build_node_policy_config(node_id,
                              node_policy,
                              rules,
                              node_policy_events_template,
-                             node_policy_name):
+                             node_policy_name,
+                             policy_message):
 
     event_json = build_node_policy_event(
         node_id,
         node_policy_name,
-        node_policy['rules'])
+        node_policy['rules'], policy_message)
     node_policy_events = string.Template(node_policy_events_template).substitute(dict(
         event=event_json,
         node_id=node_id
@@ -41,7 +52,7 @@ def build_node_policy_config(node_id,
 
     node_policy_rules = []
     for rule in node_policy['rules'].values():
-        rule_template = rules[rule['type']]
+        rule_template = rules[rule['type']]['rule']
         rule_properties = rule['properties']
         rule_properties['node_id'] = node_id
         rule_properties['node_policy_events'] = node_policy_events
@@ -51,8 +62,10 @@ def build_node_policy_config(node_id,
     return node_policy_rules[0]
 
 
-def build_node_policy_event(node_id, node_policy_name, node_policy_rules):
-    event = {'app_id': node_id.split('.')[0], 'node_id': node_id, 'policy': node_policy_name, 'rule': node_policy_rules}
+def build_node_policy_event(node_id, node_policy_name, node_policy_rules, policy_message):
+    event = {'app_id': node_id.split('.')[0], 'node_id': node_id, 'policy': node_policy_name,
+             'message': policy_message,
+             'rule': node_policy_rules}
     return json.dumps(event).replace('"', '\\"')
 
 
