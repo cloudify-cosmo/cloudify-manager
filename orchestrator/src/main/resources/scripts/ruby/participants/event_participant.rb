3e59b4bd6972fa0940998dd82f6e435da044eb0a
Idan Moyal <idan@gigaspaces.com>
2013-08-26 10:10:42 +0300

    CLOUDIFY-2003 raise cosmo event when workflow execution ends.

--- orchestrator/src/main/resources/scripts/ruby/participants/event_participant.rb
+++ orchestrator/src/main/resources/scripts/ruby/participants/event_participant.rb
@@ -30,22 +30,8 @@ class EventParticipant < Ruote::Participant
 
       raise 'event not set' unless workitem.params.has_key? EVENT
       event = workitem.params[EVENT]
-      sub_workflow_name = workitem.sub_wf_name
-      workflow_name = workitem.wf_name
 
-      if workitem.fields.has_key? NODE
-        node = workitem.fields[NODE]
-        parts = node['id'].split('.')
-        event['node'] = parts[1]
-        event['app'] = parts[0]
-      end
-
-      if sub_workflow_name == workflow_name
-        # no need to print sub workflow if there is none
-        $user_logger.debug("[#{workflow_name}] - #{event}")
-      else
-        $user_logger.debug("[#{workflow_name}.#{sub_workflow_name}] - #{event}")
-      end
+      EventParticipant.log_event(event, workitem)
 
       reply(workitem)
 
@@ -53,6 +39,28 @@ class EventParticipant < Ruote::Participant
       log_exception(e, 'event')
       flunk(workitem, e)
     end
+  end
+
+  def self.log_event(event, workitem=nil)
+    if workitem != nil
+      sub_workflow_name = workitem.sub_wf_name
+      workflow_name = workitem.wf_name
+      if workitem.fields.has_key? NODE
+        node = workitem.fields[NODE]
+        parts = node['id'].split('.')
+        event['node'] = parts[1]
+        event['app'] = parts[0]
+      end
+      if sub_workflow_name == workflow_name
+        # no need to print sub workflow if there is none
+        $user_logger.debug("[#{workflow_name}] - #{event}")
+      else
+        $user_logger.debug("[#{workflow_name}.#{sub_workflow_name}] - #{event}")
+      end
+    else
+      $user_logger.debug("[workflow] - #{event}")
+    end
+
   end
 
 end
