316050fd456f4dd3ec3f5ef9e3465e7226b31532
iliapolo <elip@gigaspaces.com>
2013-09-02 17:11:38 +0300

    CLOUDIFY-2012 deleted python_webserver_installer from local dir and fix stup plugin according to new structure

---
+++ vagrant/test/python_webserver/plugins/stub-installer/setup.py
@@ -0,0 +1,14 @@
+__author__ = 'elip'
+
+import setuptools
+
+setuptools.setup(
+    zip_safe=False,
+    name='stub-installer',
+    version='0.1.0',
+    author='elip',
+    author_email='elip@gigaspaces.com',
+    packages=['stub_app_installer'],
+    license='LICENSE',
+    description='Stub app installer',
+)
