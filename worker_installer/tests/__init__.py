892702de6ae53cd2c63dd36afd26acf9a26ab8a0
iliapolo <elip@gigaspaces.com>
2013-09-09 10:52:26 +0300

    CLOUDIFY-2022 no real need for yield + refactor common logic to init file

--- worker_installer/tests/__init__.py
+++ worker_installer/tests/__init__.py
@@ -1,1 +1,67 @@
+import os
+import shutil
+import tempfile
+import vagrant
+from worker_installer.tasks import FabricRetryingRunner
+
 __author__ = 'idanm'
+
+VAGRANT_MACHINE_IP = "10.0.0.5"
+VAGRANT_PATH = os.path.join(tempfile.gettempdir(), "vagrant-vms")
+
+
+def get_remote_runner():
+
+    host_config = {
+        'user': 'vagrant',
+        'host': VAGRANT_MACHINE_IP,
+        'port': 22,
+        'key': '~/.vagrant.d/insecure_private_key'
+    }
+    host_string = '%(user)s@%(host)s:%(port)s' % host_config
+    key_filename = host_config['key']
+
+    return FabricRetryingRunner(key_filename=key_filename, host_string=host_string)
+
+
+def get_local_runner():
+    return FabricRetryingRunner(local=True)
+
+
+def launch_vagrant(vm_id):
+
+    print "launching a new vagrant machine to be used by test"
+
+    vagrant_file = """
+
+        Vagrant.configure("2") do |config|
+            config.vm.box = "precise64"
+            config.vm.network :private_network, ip: {0}
+            config.vm.provision :shell, :inline => "sudo ufw disable"
+        end
+
+        """.format(VAGRANT_MACHINE_IP)
+
+    v = get_vagrant(VAGRANT_PATH, vm_id)
+    with open("{0}/Vagrantfile".format(v.root), 'w') as output_file:
+        print "writing vagrant file to {0}/Vagrantfile".format(v.root)
+        output_file.write(vagrant_file)
+
+    # start the machine
+    print "calling vagrant up"
+    v.up()
+
+
+def terminate_vagrant(vm_id):
+    print "terminating vagrant machine"
+    v = get_vagrant(VAGRANT_PATH, vm_id)
+    v.destroy()
+    shutil.rmtree(v)
+
+
+def get_vagrant(vagrant_path, vm_id):
+    vm_path = os.path.join(vagrant_path, vm_id)
+    if not os.path.exists(vm_path):
+        os.makedirs(vm_path)
+    return vagrant.Vagrant(vm_path)
+
--- worker_installer/tests/test_fabric_retry_runner.py
+++ worker_installer/tests/test_fabric_retry_runner.py
@@ -1,28 +1,24 @@
-import os
 import tempfile
-import vagrant
-import shutil
+from worker_installer.tests import get_remote_runner, terminate_vagrant, launch_vagrant, get_local_runner
 
 __author__ = 'elip'
 
-from worker_installer.tasks import FabricRetryingRunner
 
-
-def run(runner):
+def _test_run(runner):
     runner.run("ls -l")
 
 
-def sudo(runner):
+def _test_sudo(runner):
     runner.sudo("ls -l")
 
 
-def put(runner):
+def _test_put(runner):
     data = "test"
     file_path = tempfile.NamedTemporaryFile().name
     runner.put(data, file_path)
 
 
-def get(runner):
+def _test_get(runner):
     data = "test"
     file_path = tempfile.NamedTemporaryFile().name
     runner.put(data, file_path)
@@ -40,10 +36,10 @@ class TestLocalRunnerCase:
 
     @classmethod
     def setup_class(cls):
-        cls.RUNNER = FabricRetryingRunner(local=True)
+        cls.RUNNER = get_local_runner()
 
     def test_run(self):
-        yield run, self.RUNNER
+        _test_run(self.RUNNER)
 
     def test_sudo(self):
         """
@@ -52,88 +48,37 @@ class TestLocalRunnerCase:
         You can only run this if you are a passwordless sudo user on the local host.
         This is the case for vagrant machines, as well as travis machines.
         """
-        yield sudo, self.RUNNER
+        _test_sudo(self.RUNNER)
 
     def test_put(self):
-        yield put, self.RUNNER
+        _test_put(self.RUNNER)
 
     def test_get(self):
-        yield get, self.RUNNER
+        _test_get(self.RUNNER)
 
 
 class TestRemoteRunnerCase:
 
-    VAGRANT_PATH = os.path.join(tempfile.gettempdir(), "vagrant-vms")
     VM_ID = "TestRemoteRunnerCase"
-    V = None
     RUNNER = None
 
     @classmethod
     def setup_class(cls):
-
-        host_config = {
-            'user': 'vagrant',
-            'host': "10.0.0.5",
-            'port': 22,
-            'key': '~/.vagrant.d/insecure_private_key'
-        }
-        host_string = '%(user)s@%(host)s:%(port)s' % host_config
-        key_filename = host_config['key']
-
-        cls.RUNNER = FabricRetryingRunner(key_filename=key_filename, host_string=host_string)
-
-        print "launching a new vagrant machine to be used by test"
-
-        vagrant_file = """
-
-        Vagrant.configure("2") do |config|
-            config.vm.box = "precise64"
-            config.vm.network :private_network, ip: "10.0.0.5"
-            config.vm.provision :shell, :inline => "sudo ufw disable"
-        end
-
-        """
-
-        cls.V = cls._get_vagrant()
-        with open("{0}/Vagrantfile".format(cls.V.root), 'w') as output_file:
-            print "writing vagrant file to {0}/Vagrantfile".format(cls.V.root)
-            output_file.write(vagrant_file)
-
-        # start the machine
-        print "calling vagrant up"
-        cls.V.up()
+        launch_vagrant(cls.VM_ID)
+        cls.RUNNER = get_remote_runner()
 
     @classmethod
     def teardown_class(cls):
-        print "terminating vagrant machine"
-        cls.V.destroy()
-        shutil.rmtree(cls.V.root)
-
-    @classmethod
-    def _get_vagrant(cls):
-        vm_path = os.path.join(cls.VAGRANT_PATH, cls.VM_ID)
-        if not os.path.exists(vm_path):
-            os.makedirs(vm_path)
-        return vagrant.Vagrant(vm_path)
-
-
-    @classmethod
-    def _status(cls, v, host_id):
-
-        # we assume a single vm vagrant file
-        v_status = v.status()
-        status = v_status.itervalues().next()
-        print "vagrant vm status is {0}".format(status)
-        return status
+        terminate_vagrant(cls.VM_ID)
 
     def test_run(self):
-        yield run, self.RUNNER
+        _test_run(self.RUNNER)
 
     def test_sudo(self):
-        yield sudo, self.RUNNER
+        _test_sudo(self.RUNNER)
 
     def test_put(self):
-        yield put, self.RUNNER
+        _test_put(self.RUNNER)
 
     def test_get(self):
-        yield get, self.RUNNER
+        _test_get(self.RUNNER)
