a067ecf94ec908c0471b3877e1c48b0b1b1320c1
iliapolo <elip@gigaspaces.com>
2013-09-11 17:16:25 +0300

    CLOUDIFY-2022 add cosmo-celery-common depenedencie to plugin installer

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ __author__ = 'elip'
 
 from setuptools import setup
 
+COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/feature/CLOUDIFY-2022-initial-commit"
+COSMO_CELERY_VERSION = "0.1.0"
+
 setup(
     name='cosmo-plugin-plugin-installer',
     version='0.1.0',
@@ -11,6 +14,11 @@ setup(
     license='LICENSE',
     description='Plugin for installing plugins into an existing celery worker',
     install_requires=[
-        "celery"
-    ]
+        "celery",
+        # we add this to allow running the plugin installer from outside of the cosmo env.
+        # this will bring the necessary dependencies
+        "cosmo-celery-common"
+    ],
+
+    dependency_links=["{0}#egg=cosmo-celery-common-{1}".format(COSMO_CELERY, COSMO_CELERY_VERSION)]
 )
