904978301a5bfe87ed8324c7b62501fd87581bd4
iliapolo <elip@gigaspaces.com>
2013-09-11 14:57:27 +0300

    CLOUDIFY-2022 added cosmo-fabric-runner to depenedencies

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
+import subprocess
+
 __author__ = 'elip'
 
 from setuptools import setup
+
+FABRIC_RUNNER = "https://github.com/CloudifySource/cosmo-fabric-runner/archive/feature/CLOUDIFY-2022-initial-commit.zip"
+
+
+def run_command(command):
+    p = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode != 0:
+        raise RuntimeError("Failed running command {0} [returncode={1}, "
+                           "output={2}, error={3}]".format(command, out, err, p.returncode))
+    return out
+
+
 setup(
     name='cosmo-plugin-agent-installer',
     version='0.1.0',
@@ -10,8 +25,11 @@ setup(
     license='LICENSE',
     description='Plugin for starting a new cosmo agent on a remote host',
     install_requires=[
-        "fabric",
+        "https://github.com/CloudifySource/cosmo-fabric-runner/archive/feature/CLOUDIFY-2022-initial-commit.zip",
         "celery"
     ]
 )
+
+
+
 
