2ec0e032666c75ac3e9ab339c8e4d14c01fda591
iliapolo <elip@gigaspaces.com>
2013-09-13 21:15:19 +0300

    CLOUDIFY-2022 added script to update management worker plugins

--- vagrant/update_worker.py
+++ vagrant/update_worker.py
@@ -1,1 +1,24 @@
+import os
+import subprocess
+
 __author__ = 'elip'
+
+DEFAULT_BRANCH = "feature/CLOUDIFY-2022-initial-commit"
+
+BRANCH = os.environ.get("COSMO_BRANCH", DEFAULT_BRANCH)
+
+
+def update_worker():
+
+    """
+    Use this method to connect to an existing management machine and update the worker with new plugins from
+    github.
+    Be sure to push your changes before running this.
+    """
+    from test import get_remote_runner
+    runner = get_remote_runner()
+    runner.run("export COSMO_BRANCH={0} "
+               "&& python2.7 /vagrant/bootstrap_lxc_manager.py --update_only=True".format(BRANCH))
+
+
+update_worker()
