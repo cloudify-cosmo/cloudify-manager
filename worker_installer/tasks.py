d3fa812fdea09b719f0ded0d772d5229b67360da
iliapolo <elip@gigaspaces.com>
2013-09-15 16:53:46 +0300

    CLOUDIFY-2022 added license and remove branch name

--- worker_installer/tasks.py
+++ worker_installer/tasks.py
@@ -1,3 +1,19 @@
+#/*******************************************************************************
+# * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *       http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+# *******************************************************************************/
+
 import logging
 
 __author__ = 'elip'
@@ -12,10 +28,6 @@ from fabric.api import hide
 from cosmo_fabric.runner import FabricRetryingRunner
 
 COSMO_APP_NAME = "cosmo"
-
-DEFAULT_BRANCH = "feature/CLOUDIFY-2022-initial-commit"
-
-BRANCH = os.environ.get("COSMO_BRANCH", DEFAULT_BRANCH)
 
 COSMO_CELERY_URL = "https://github.com/CloudifySource/cosmo-celery-common/archive/master.zip"
 
@@ -250,7 +262,6 @@ CELERYD_OPTS="\
 --broker=%(broker_url)s \
 --hostname=%(node_id)s"
 ''' % dict(env=env_string,
-           branch=DEFAULT_BRANCH,
            user=user,
            workdir=workdir,
            app=app,
