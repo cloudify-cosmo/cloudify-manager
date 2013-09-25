c1e4f5736975e5f628d9bc50d5d104a2bebcca17
iliapolo <elip@gigaspaces.com>
2013-09-25 15:47:41 +0300

    CLOUDIFY-2048 depend on develop of cosmo-celery-common

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = 'elip'
 from setuptools import setup
 
 COSMO_CELERY_VERSION = "0.1.0"
-COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/{0}".format(COSMO_CELERY_VERSION)
+COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/{0}".format("develop")
 
 setup(
     name='cosmo-plugin-plugin-installer',
