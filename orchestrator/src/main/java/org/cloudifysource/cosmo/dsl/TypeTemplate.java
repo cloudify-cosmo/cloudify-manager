d5db6cade3796f9adcd79b30732d3a1772904349
iliapolo <elip@gigaspaces.com>
2013-10-07 16:56:18 +0300

    CLOUDIFY-2073 added instances map to TypeTemplate

--- orchestrator/src/main/java/org/cloudifysource/cosmo/dsl/TypeTemplate.java
+++ orchestrator/src/main/java/org/cloudifysource/cosmo/dsl/TypeTemplate.java
@@ -18,8 +18,10 @@ package org.cloudifysource.cosmo.dsl;
 
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * A class used to represent a type template.
@@ -30,6 +32,16 @@ import java.util.List;
 public class TypeTemplate extends Type {
 
     private List<RelationshipTemplate> relationships = Lists.newArrayList();
+    private Map<String, Integer> instances = Maps.newHashMap();
+
+    public Map<String, Integer> getInstances() {
+        return instances;
+    }
+
+    public void setInstances(Map<String, Integer> instances) {
+        this.instances = instances;
+    }
+
 
     public List<RelationshipTemplate> getRelationships() {
         return relationships;
