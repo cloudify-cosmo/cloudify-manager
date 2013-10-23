55931806977a2039b807b6c96dad786163c80819
Idan Moyal <idan@gigaspaces.com>
2013-10-23 15:34:42 +0300

    CLOUDIFY-2161 fixed versions.py

--- vagrant/versions.py
+++ vagrant/versions.py
@@ -3,7 +3,7 @@ __author__ = 'elip'
 COSMO_VERSION = "develop"
 
 FABRIC_RUNNER_VERSION = "develop"
-WORKER_INSTALLER_VERSION = "CLOUDIFY-2161-inject-management-ip"
+WORKER_INSTALLER_VERSION = "feature/CLOUDIFY-2161-inject-management-ip"
 RIEMANN_LOADER_VERSION = "develop"
 VAGRANT_PROVISION_VERSION = "develop"
 OPENSTACK_PROVISION_VERSION = "develop"
