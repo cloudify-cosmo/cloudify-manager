2046f8c1a21c7c2a95120057c352e3d4b188157b
Dan Kilman <dank@gigaspaces.com>
2013-12-02 15:39:52 +0200

    CLOUDIFY-2222 remove some todos

--- manager-rest/manager_rest/blueprints_manager.py
+++ manager-rest/manager_rest/blueprints_manager.py
@@ -23,6 +23,7 @@ class BlueprintsManager(object):
 
     # TODO: call celery tasks instead of doing this directly here
     def publish_blueprint(self, dsl_location, alias_mapping_url, resources_base_url):
+        # TODO: error code if parsing fails (in one of the 2 tasks)
         plan = tasks.parse_dsl(dsl_location, alias_mapping_url, resources_base_url)
         plan = tasks.prepare_multi_instance_plan(json.loads(plan))
         new_blueprint = BlueprintState(json_plan=plan, plan=json.loads(plan))
@@ -34,7 +35,6 @@ class BlueprintsManager(object):
     # so we can parse all the workflows and see things are ok
     def validate_blueprint(self, blueprint_id):
         blueprint = self.get_blueprint(blueprint_id)
-        # TODO raise error if blueprint does not exist
         plan = blueprint.typed_plan
         response = workflow_client().validate_workflows(plan)
         # TODO raise error if error
@@ -43,7 +43,6 @@ class BlueprintsManager(object):
 
     def execute_workflow(self, blueprint_id, workflow_id):
         blueprint = self.get_blueprint(blueprint_id)
-        # TODO raise error if workflow not found
         workflow = blueprint.typed_plan['workflows'][workflow_id]
         plan = blueprint.typed_plan
         response = workflow_client().execute_workflow(workflow, plan)
@@ -59,7 +58,6 @@ class BlueprintsManager(object):
 
     def get_workflow_state(self, execution_id):
         execution = self.get_execution(execution_id)
-        # TODO raise error if execution not found
         response = workflow_client().get_workflow_status(execution.internal_workflow_id)
         execution.status = response['state']
         if execution.status == 'failed':
