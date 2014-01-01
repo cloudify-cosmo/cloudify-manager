df8621b34d0c940220d976d7ab6977c04153cfba
Idan Moyal <idan@gigaspaces.com>
2014-01-01 13:55:56 +0200

    CLOUDIFY-2285 some syntax changes to workflow 'state' command which receives now an action which can be 'get' or 'wait'

--- workflow-service/participants/node_state_participant.rb
+++ workflow-service/participants/node_state_participant.rb
@@ -18,30 +18,54 @@
 require 'rest_client'
 require 'json'
 require 'uri'
+require 'set'
 
 
+######
+# Participant getting/waiting to/for node reachable state.
+# If there's a node in context, by default node runtime state will be read from storage and injected
+# to the node in context (usually relevant when there are relationships between nodes).
+#
 class NodeStateParticipant < Ruote::Participant
 
   NODE_ID = 'node_id'
-  WAIT_UNTIL_MATCHES = 'wait_until_matches'
   MATCHES_FIELD_PARAM_NAME = 'to_f'
   MANAGER_REST_BASE_URI = 'MANAGER_REST_BASE_URI'
   REACHABLE = 'reachable'
   NODE = 'node'
+  ACTION = 'action'
+  WAIT = 'wait'
+  VALID_ACTIONS = [WAIT, 'get'].to_set
 
   def on_workitem
     begin
+      # TODO runtime-model: handle exceptions in a smart way which will fail the entire workflow if exception is not related to 'matches' logic.
       # TODO runtime-model: pass manager rest base uri as ENV variable
       ENV[MANAGER_REST_BASE_URI] = 'http://localhost:8100'
 
-      raise "#{NODE_ID} property is not set" unless workitem.params.has_key? NODE_ID
-      raise "#{REACHABLE} parameter is not defined for node state  participant" unless workitem.params.has_key? REACHABLE
+      raise "#{NODE_ID} property is not set" unless (workitem.params[NODE_ID] || '').length > 0
       raise "#{MANAGER_REST_BASE_URI} is not set in ruby env" unless ENV.has_key? MANAGER_REST_BASE_URI
+
+      action = workitem.params[ACTION] || WAIT
+      raise "invalid action: #{action} - available actions: #{VALID_ACTIONS}" unless VALID_ACTIONS.include? action
+      wait_until_matches = false
+      if action.eql? WAIT
+        wait_until_matches = true
+        raise "#{REACHABLE} parameter is not defined for node state participant" unless (workitem.params[REACHABLE] || '').length > 0
+      end
 
       base_uri = ENV[MANAGER_REST_BASE_URI]
 
       node_id = workitem.params[NODE_ID]
-      raise 'node_id is empty' unless node_id != nil and node_id.to_s.length > 0
+      current_node = workitem.fields[PreparePlanParticipant::NODE] || nil
+      result_field = workitem.params[MATCHES_FIELD_PARAM_NAME] || nil
+
+      $logger.debug('Node state participant called [context={}, action={}, node_id={}, value={}, result_field={}]',
+                    current_node['id'] || nil,
+                    action,
+                    node_id,
+                    workitem.params[REACHABLE],
+                    result_field)
 
       # TODO runtime-model: handle HTTP status codes and connection errors
       url = URI::escape("#{base_uri}/nodes/#{node_id}?reachable")
@@ -51,25 +75,26 @@ class NodeStateParticipant < Ruote::Participant
       requested_reachable_state = workitem.params[REACHABLE]
       reachable = processed[REACHABLE]
 
-      matches = requested_reachable_state.to_s.eql? reachable.to_s
+      if wait_until_matches
+        matches = requested_reachable_state.to_s.eql? reachable.to_s
+        $logger.debug('Node reachable state is: {} [matches={}]', reachable, matches)
 
-      wait_until_matches = workitem.params[WAIT_UNTIL_MATCHES] || true
+        raise "node reachable state does not match [requested=#{requested_reachable_state}, actual=#{reachable}" if wait_until_matches and not matches
 
-      raise "node reachable state does not match [requested=#{requested_reachable_state}, actual=#{reachable}" if wait_until_matches and not matches
-
-      if workitem.params.has_key? MATCHES_FIELD_PARAM_NAME
-        workitem.fields[workitem.params[MATCHES_FIELD_PARAM_NAME]] = matches
-      end
-
-      if requested_reachable_state and workitem.fields.has_key? PreparePlanParticipant::NODE
-        # TODO runtime-model: handle HTTP status codes and connection errors
-        response = RestClient.get "#{base_uri}/nodes/#{node_id}"
-        node_state = JSON.parse(response.to_str)
-        if node_state.has_key? 'runtimeInfo'
-          current_node = workitem.fields[PreparePlanParticipant::NODE]
-          properties = current_node[PreparePlanParticipant::PROPERTIES]
-          properties[PreparePlanParticipant::RUNTIME][node_id] = node_state['runtimeInfo']
+        # if there's a node in context, inject the requested node's runtime state
+        if requested_reachable_state and not current_node.nil?
+          # TODO runtime-model: handle HTTP status codes and connection errors
+          response = RestClient.get "#{base_uri}/nodes/#{node_id}"
+          node_state = JSON.parse(response.to_str)
+          if node_state.has_key? 'runtimeInfo'
+            current_node = workitem.fields[PreparePlanParticipant::NODE]
+            properties = current_node[PreparePlanParticipant::PROPERTIES]
+            properties[PreparePlanParticipant::RUNTIME][node_id] = node_state['runtimeInfo']
+          end
         end
+      elsif not result_field.nil?
+          $logger.debug('Node reachable state is set as \'{}\' workitem field [state={}]', result_field, reachable)
+          workitem.fields[result_field] = reachable
       end
 
       reply
