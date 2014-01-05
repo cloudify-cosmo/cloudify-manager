2f337e4169f3b8f01ebf37f13814a4de8adccb93
Dan Kilman <dank@gigaspaces.com>
2014-01-05 17:43:48 +0200

    CLOUDIFY-2331 add relationship unlink operation to uninstall.radial and fix broken unittests

--- workflows/tests/cosmo/connection_configurer_mock/tasks.py
+++ workflows/tests/cosmo/connection_configurer_mock/tasks.py
@@ -3,13 +3,23 @@ from time import time
 
 state = []
 
+
 @celery.task
-def configure_connection(__source_cloudify_id,
-                         __target_cloudify_id,
-                         __run_on_node_cloudify_id,
-                         __source_properties,
-                         __target_properties,
-                         **kwargs):
+def configure_connection(**kwargs):
+    append_to_state(**kwargs)
+
+
+@celery.task
+def unconfigure_connection(**kwargs):
+    append_to_state(**kwargs)
+
+
+def append_to_state(__source_cloudify_id,
+                    __target_cloudify_id,
+                    __run_on_node_cloudify_id,
+                    __source_properties,
+                    __target_properties,
+                    **kwargs):
     global state
     state.append({
         'source_id': __source_cloudify_id,
@@ -19,12 +29,6 @@ def configure_connection(__source_cloudify_id,
         'target_properties': __target_properties,
         'run_on_node_id': __run_on_node_cloudify_id
     })
-
-
-@celery.task
-def unconfigure_connection(__source_cloudify_id, __target_cloudify_id, **kwargs):
-    pass
-
 
 @celery.task
 def get_state():
