e9c50ceb672831561a1dbbba3817469d58d39ffa
Dan Kilman <dank@gigaspaces.com>
2014-01-29 17:17:02 +0200

    CLOUDIFY-2426 changes to be able to start a manager with changes not yet in develop

--- vagrant/versions.py
+++ vagrant/versions.py
@@ -3,7 +3,7 @@ __author__ = 'elip'
 COSMO_VERSION = 'develop'
 
 FABRIC_RUNNER_VERSION = 'develop'
-WORKER_INSTALLER_VERSION = 'develop'
+WORKER_INSTALLER_VERSION = 'feature/CLOUDIFY-2426-simplify-policies'
 RIEMANN_LOADER_VERSION = 'feature/feature/CLOUDIFY-2426-simplify-policies'
 VAGRANT_PROVISION_VERSION = 'develop'
 OPENSTACK_PROVISION_VERSION = 'feature/CLOUDIFY-2426-simplify-policies'
