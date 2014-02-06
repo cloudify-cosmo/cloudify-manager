e1de1b26f9196f00b91bd8221c144b24e1ece656
iliapolo <elip@gigaspaces.com>
2014-02-06 12:05:57 +0200

    CLOUDIFY-2370 added logging

--- plugin_installer/tasks.py
+++ plugin_installer/tasks.py
@@ -177,7 +177,9 @@ def get_plugin_simple_name(full_plugin_name):
 
 
 def run_command(command):
-    p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    shlex_split = shlex.split(command)
+    logger.info("Running command {0}".format(command))
+    p = subprocess.Popen(shlex_split, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     if p.returncode != 0:
         raise RuntimeError("Failed running command {0} [returncode={1}, "
