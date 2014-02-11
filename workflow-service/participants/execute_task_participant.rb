f02812322de8f605654f4cdb71d9f192b33b30dd
Idan Moyal <idan@gigaspaces.com>
2014-02-11 17:57:57 +0200

    CLOUDIFY-2454 add kv_store.tasks.put to filtered tasks

--- workflow-service/participants/execute_task_participant.rb
+++ workflow-service/participants/execute_task_participant.rb
@@ -67,12 +67,14 @@ class ExecuteTaskParticipant < Ruote::Participant
   GET_ARGUMENTS_TASK_NAME = 'plugin_installer.tasks.get_arguments'
   RESTART_CELERY_WORKER_TASK_NAME = 'worker_installer.tasks.restart'
   GET_KV_STORE_TASK_NAME = 'kv_store.tasks.get'
+  PUT_KV_STORE_TASK_NAME = 'kv_store.tasks.put'
 
   TASK_TO_FILTER = Set.new [RELOAD_RIEMANN_CONFIG_TASK_NAME,
                             VERIFY_PLUGIN_TASK_NAME,
                             RESTART_CELERY_WORKER_TASK_NAME,
                             GET_ARGUMENTS_TASK_NAME,
-                            GET_KV_STORE_TASK_NAME]
+                            GET_KV_STORE_TASK_NAME,
+                            PUT_KV_STORE_TASK_NAME]
 
   def do_not_thread
     true
