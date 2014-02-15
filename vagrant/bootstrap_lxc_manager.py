be3256b08bb5a93eb048975dfdc786c9d58bc21e
iliapolo <elip@gigaspaces.com>
2014-02-15 16:49:36 +0200

    CLOUDIFY-2506 fix invocation according to new api

--- vagrant/bootstrap_lxc_manager.py
+++ vagrant/bootstrap_lxc_manager.py
@@ -463,6 +463,7 @@ class VagrantLxcBoot:
         worker_config = {
             "user": getpass.getuser(),
             "broker": "amqp://",
+            "management": True,
             "install_vagrant": self.install_vagrant_lxc,
             "install_openstack": self.install_openstack_provisioner,
             "env": {
