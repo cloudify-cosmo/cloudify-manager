ae6bab451c1953e4e048c5f1cc699df56c88e896
iliapolo <elip@gigaspaces.com>
2014-02-16 16:50:29 +0200

    CLOUDIFY-2507 fix formatting

--- worker_installer/tasks.py
+++ worker_installer/tasks.py
@@ -239,7 +239,7 @@ def _install_celery(runner, worker_config):
 
     # expose celery work directory
     runner.sudo("sed -i '1 i'$'export {0}={1}\n' /etc/init.d/celeryd-{2}"
-                .format(CELERY_WORK_DIR_PATH_KEY, worker_config[CELERY_WORK_DIR_PATH_KEY]), worker_config["name"])
+                .format(CELERY_WORK_DIR_PATH_KEY, worker_config[CELERY_WORK_DIR_PATH_KEY], worker_config["name"]))
 
     # build initial includes
     if _is_management_node(worker_config):
