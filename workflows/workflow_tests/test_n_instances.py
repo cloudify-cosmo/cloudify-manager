d58adc5fc2c00f0a9c7a63e75fa73d528950e6b7
Dan Kilman <dank@gigaspaces.com>
2014-02-18 11:01:56 +0200

    CLOUDIFY-2511 add test with 15 nodes

--- workflows/workflow_tests/test_n_instances.py
+++ workflows/workflow_tests/test_n_instances.py
@@ -39,18 +39,18 @@ class TestMultiInstanceApplication(TestCase):
             machines_with_apps.add(host_id)
         self.assertEquals(machines, machines_with_apps)
 
-    def _test_deploy_multi_instance_many_different_hosts(self):
+    def test_deploy_multi_instance_many_different_hosts(self):
         dsl_path = resource('dsl/multi_instance_many_different_hosts.yaml')
-        deploy(dsl_path, timeout=800)
+        deploy(dsl_path)
 
         from plugins.cloudmock.tasks import get_machines
         result = self.send_task(get_machines)
         machines = set(result.get(timeout=10))
-        self.assertEquals(99, len(machines))
+        self.assertEquals(15, len(machines))
 
-        self.assertEquals(33, len(filter(lambda ma: ma.startswith('host1'),
-                                         machines)))
-        self.assertEquals(33, len(filter(lambda ma: ma.startswith('host2'),
-                                         machines)))
-        self.assertEquals(33, len(filter(lambda ma: ma.startswith('host3'),
-                                         machines)))
+        self.assertEquals(5, len(filter(lambda ma: ma.startswith('host1'),
+                                        machines)))
+        self.assertEquals(5, len(filter(lambda ma: ma.startswith('host2'),
+                                        machines)))
+        self.assertEquals(5, len(filter(lambda ma: ma.startswith('host3'),
+                                        machines)))
