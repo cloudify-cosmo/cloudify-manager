c9de65c2ff807a094c6a7efeaddf9c4cea337114
iliapolo <elip@gigaspaces.com>
2014-02-18 18:45:47 +0200

    CLOUDIFY-2514 added more mock plugins and dsl with manager plugin

---
+++ workflows/plugins/plugin_installer/tasks.py
@@ -0,0 +1,44 @@
+########
+# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+__author__ = 'idanmo'
+
+from cloudify.decorators import operation
+
+
+NON_EXISTING_OPERATIONS = ['testmockoperations.non_existent']
+
+
+@operation
+def install(ctx, plugin, **kwargs):
+    pass
+
+
+@operation
+def verify_plugin(ctx, worker_id, plugin_name, operation, throw_on_failure,
+                  **kwargs):
+    full_operation_name = "{0}.{1}".format(plugin_name, operation)
+    ctx.logger.info("full operation name is : {0}".format(full_operation_name))
+    is_non_existing = full_operation_name in NON_EXISTING_OPERATIONS
+    ctx.logger.info("is not existing : {0}".format(is_non_existing))
+    ctx.logger.info("throw on failure : {0}".format(throw_on_failure))
+    if throw_on_failure and is_non_existing:
+        raise RuntimeError("")
+    return not is_non_existing
+
+
+@operation
+def get_arguments(plugin_name, operation, **kwargs):
+    pass
