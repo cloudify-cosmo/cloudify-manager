048905235c28dbb7db95c27d5ae46d126e908cf5
iliapolo <elip@gigaspaces.com>
2014-02-23 09:11:00 +0200

    CLOUDIFY-2514 added logs

--- worker_installer/tests/test_worker_installer.py
+++ worker_installer/tests/test_worker_installer.py
@@ -36,6 +36,7 @@ def _extract_registered_plugins(broker_url, worker_name):
 
     c = Celery(broker=broker_url, backend=broker_url)
     tasks = c.control.inspect.registered(c.control.inspect())
+    print "tasks : {0}".format(tasks)
 
     # retry a few times
     attempt = 0
