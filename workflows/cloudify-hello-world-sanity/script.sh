b5756576772d4e04bc8601074cd23367a776e905
Dan Kilman <dank@gigaspaces.com>
2014-02-24 14:17:22 +0200

    CLOUDIFY-2546 update sanity

--- workflows/cloudify-hello-world-sanity/script.sh
+++ workflows/cloudify-hello-world-sanity/script.sh
@@ -1,24 +1,24 @@
-# key_path=$1
-# cloudify_agents_kp=$2
-# host_name=$3
-# management_ip=$4
-# region=$5
-# image=$6
+key_path=$1
+cloudify_agents_kp=$2
+host_name=$3
+management_ip=$4
+region=$5
+image=$6
 
-# pip install .
-
-# python sanity.py \
-#     --key_path=$key_path \
-#     --key_name=$key_name \
-#     --host_name=$host_name \
-#     --management_ip=$management_ip \
-#     --region=$region \
-#     --image=$image
+pip install .
 
 python sanity.py \
-    --key_path='~/.ssh/cloudify-agents-kp.pem' \
-    --key_name=cloudify-agents-kp \
-    --host_name=dank_hello_world_vm \
-    --management_ip=15.185.158.169 \
-    --region='az-2.region-a.geo-1' \
-    --image=67074
+    --key_path=$key_path \
+    --key_name=$key_name \
+    --host_name=$host_name \
+    --management_ip=$management_ip \
+    --region=$region \
+    --image=$image
+
+# python sanity.py \
+#     --key_path='~/.ssh/cloudify-agents-kp.pem' \
+#     --key_name=cloudify-agents-kp \
+#     --host_name=dank_hello_world_vm \
+#     --management_ip=15.185.158.169 \
+#     --region='az-2.region-a.geo-1' \
+#     --image=67074
