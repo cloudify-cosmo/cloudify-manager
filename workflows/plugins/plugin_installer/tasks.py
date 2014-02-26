447b7d7264c1340d6b8cd035200e1cbb0634e8da
iliapolo <elip@gigaspaces.com>
2014-02-26 18:04:22 +0200

    CLOUDIFY-2545 added installed plugins state in mock plugin installer

--- workflows/plugins/plugin_installer/tasks.py
+++ workflows/plugins/plugin_installer/tasks.py
@@ -20,10 +20,15 @@ from cloudify.decorators import operation
 
 NON_EXISTING_OPERATIONS = ['testmockoperations.non_existent']
 
+INSTALLED_PLUGINS = []
+
 
 @operation
 def install(ctx, plugin, **kwargs):
-    pass
+
+    global INSTALLED_PLUGINS
+    ctx.logger.info("in plugin_installer.install --> installing plugin {0}".format(plugin))
+    INSTALLED_PLUGINS.append(plugin['name'])
 
 
 @operation
@@ -39,3 +44,8 @@ def verify_plugin(ctx, worker_id, plugin_name, operation, throw_on_failure,
 @operation
 def get_arguments(plugin_name, operation, **kwargs):
     pass
+
+
+@operation
+def get_installed_plugins(**kwargs):
+    return INSTALLED_PLUGINS
