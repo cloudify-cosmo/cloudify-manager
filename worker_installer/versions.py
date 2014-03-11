5140341c21eea672f95c5dc4a0b64a079508bbaf
Dan Kilman <dankilman@gmail.com>
2014-03-11 13:46:39 +0200

    CLOUDIFY-2600 changes for running mananger in dev env

--- worker_installer/versions.py
+++ worker_installer/versions.py
@@ -16,7 +16,7 @@
 __author__ = 'elip'
 
 PLUGIN_INSTALLER_VERSION = 'develop'
-COSMO_CELERY_COMMON_VERSION = 'develop'
+COSMO_CELERY_COMMON_VERSION = 'feature/CLOUDIFY-2600-openstack-relationship-types'
 KV_STORE_VERSION = 'develop'
 RIEMANN_CONFIGURER_VERSION = 'develop'
 AGENT_INSTALLER_VERSION = 'develop'
