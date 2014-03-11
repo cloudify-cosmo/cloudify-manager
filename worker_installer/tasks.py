bda7bfe466f1aac2d776a80eb3f5492e8dd8bb10
nir0s <nir36g@gmail.com>
2014-03-11 11:25:34 +0200

    Update tasks.py

--- worker_installer/tasks.py
+++ worker_installer/tasks.py
@@ -204,7 +204,7 @@ def _install_latest_pip(runner, worker_config):
         "{0}/get-pip.py".format(worker_config['home']))
 
     runner.sudo("apt-get update -y")
-    runner.sudo("apt-get install -f)
+    runner.sudo("apt-get install -f -y)
     runner.sudo("apt-get -y install python-setuptools")
 
     runner.sudo("python {0}/get-pip.py".format(worker_config['home']))
