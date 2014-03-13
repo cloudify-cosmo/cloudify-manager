a72605b287f94c6d74f1d9f5bc3a60f8cb2e0f49
dankilman <dankilman@gmail.com>
2014-03-13 11:57:39 +0200

    CLOUDIFY-2600 restore versions

--- vagrant/bootstrap_lxc_manager.py
+++ vagrant/bootstrap_lxc_manager.py
@@ -43,7 +43,7 @@ FABRIC_RUNNER = "https://github.com/CloudifySource/" \
                 "cosmo-fabric-runner/archive/{0}.zip"\
                 .format(FABRIC_RUNNER_VERSION)
 
-WORKER_INSTALLER_VERSION = 'feature/CLOUDIFY-2600-openstack-relationship-types'
+WORKER_INSTALLER_VERSION = 'develop'
 
 WORKER_INSTALLER = "https://github.com/CloudifySource/" \
                    "cosmo-plugin-agent-installer/archive/{0}.zip" \
