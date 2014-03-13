f59a24b9cf0793e379423612a8ae44700dccf489
Ran Ziv <ran@gigaspaces.com>
2014-03-13 17:26:00 +0200

    CLOUDIFY-2655 Fixed sanity: now supports new neutron region on openstack; fixed state==started assertion; fixed blueprint comparison assertion

--- workflows/cloudify-hello-world-sanity/script.sh
+++ workflows/cloudify-hello-world-sanity/script.sh
@@ -2,8 +2,9 @@ key_path=$1
 cloudify_agents_kp=$2
 host_name=$3
 management_ip=$4
-region=$5
-image_name=$6
+image_name=$5
+security_group=$6
+management_network_name=$7
 
 pip install .
 
@@ -12,7 +13,9 @@ python sanity.py \
     --key_name=$key_name \
     --host_name=$host_name \
     --management_ip=$management_ip \
-    --image_name=$image_name
+    --image_name=$image_name \
+    --security_group=$security_group \
+    --management_network_name=$management_network_name
 
 # python sanity.py \
 #     --key_path='~/.ssh/cloudify-agents-kp.pem' \
@@ -20,3 +23,5 @@ python sanity.py \
 #     --host_name=dank_hello_world_vm \
 #     --management_ip=15.185.158.169 \
 #     --image_name='Ubuntu Precise 12.04 LTS Server 64-bit 20121026 (b)'
+#     --security_group='cloudify-sg-agents' \
+#     --management_network_name='cloudify-admin-network'
