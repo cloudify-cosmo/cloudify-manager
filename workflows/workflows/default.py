5e315e56aebd920e5ec2662c2bc4feb2755f9bfe
Dan Kilman <dankilman@gmail.com>
2014-05-13 16:23:13 +0300

    CFY-649 small refactoring

--- workflows/workflows/default.py
+++ workflows/workflows/default.py
@@ -7,30 +7,27 @@ from cloudify.workflows.tasks_graph import TaskDependencyGraph
 @workflow
 def install(ctx, **kwargs):
 
-    graph = TaskDependencyGraph()
+    graph = TaskDependencyGraph(ctx)
 
     for node in ctx.nodes:
 
         sequence = graph.sequence()
         sequence.add(
-            node.set_state('initializing', return_task=True),
-            node.set_state('creating', return_task=True),
-            node.send_event('Creating node', return_task=True),
-            node.execute_operation('cloudify.interfaces.lifecycle.create',
-                                   return_task=True),
-            node.set_state('created', return_task=True),
-            node.set_state('configuring', return_task=True),
-            node.send_event('Configuring node', return_task=True),
-            node.execute_operation('cloudify.interfaces.lifecycle.configure',
-                                   return_task=True),
-            node.set_state('configured', return_task=True),
-            node.set_state('starting', return_task=True),
-            node.send_event('Starting node', return_task=True),
-            node.execute_operation('cloudify.interfaces.lifecycle.start',
-                                   return_task=True))
+            node.set_state('initializing'),
+            node.set_state('creating'),
+            node.send_event('Creating node'),
+            node.execute_operation('cloudify.interfaces.lifecycle.create'),
+            node.set_state('created'),
+            node.set_state('configuring'),
+            node.send_event('Configuring node'),
+            node.execute_operation('cloudify.interfaces.lifecycle.configure'),
+            node.set_state('configured'),
+            node.set_state('starting'),
+            node.send_event('Starting node'),
+            node.execute_operation('cloudify.interfaces.lifecycle.start'))
         if _is_host_node(node):
             _host_post_start(node, sequence)
-        sequence.add(node.set_state('started', return_task=True))
+        sequence.add(node.set_state('started'))
 
     graph.execute()
 
@@ -41,7 +38,7 @@ def _is_host_node(node):
 
 def _wait_for_host_to_start(host_node, sequence):
         task = host_node.execute_operation(
-            'cloudify.interfaces.host.get_state', return_task=True)
+            'cloudify.interfaces.host.get_state')
         def get_node_state_handler(task):
             return task.async_result.get() is False
         task.on_success = get_node_state_handler
@@ -52,21 +49,18 @@ def _host_post_start(host_node, sequence):
     _wait_for_host_to_start(host_node, sequence)
     if host_node.properties['install_agent'] is True:
         sequence.add(
-            host_node.send_event('Installing worker', return_task=True),
+            host_node.send_event('Installing worker'),
             host_node.execute_operation(
-                'cloudify.interfaces.worker_installer.install',
-                return_task=True),
+                'cloudify.interfaces.worker_installer.install'),
             host_node.execute_operation(
-                'cloudify.interfaces.worker_installer.start',
-                return_task=True),
-            host_node.send_event('Installing plugin', return_task=True))
+                'cloudify.interfaces.worker_installer.start'),
+            host_node.send_event('Installing plugin'))
         for plugin in host_node.plugins_to_install:
             sequence.add(
                 host_node.send_event('Installing plugin: {0}'
-                                     .format(plugin['name']),
-                                     return_task=True),
+                                     .format(plugin['name'])),
                 host_node.execute_operation(
                     'cloudify.interfaces.plugin_installer.install',
-                    kwargs={'plugin': plugin}, return_task=True))
+                    kwargs={'plugin': plugin}))
         sequence.add(host_node.execute_operation(
-            'cloudify.interfaces.worker_installer.restart', return_task=True))
+            'cloudify.interfaces.worker_installer.restart'))
