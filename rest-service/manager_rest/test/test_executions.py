28015f657bb06314441a07cc831a7d8d5db4cd5b
Ran Ziv <ran@gigaspaces.com>
2014-05-24 19:01:11 +0300

    added unit and integration tests for update execution status

--- rest-service/manager_rest/test/test_executions.py
+++ rest-service/manager_rest/test/test_executions.py
@@ -49,17 +49,59 @@ class ExecutionsTestCase(BaseServerTestCase):
 
         return execution
 
-    # def test_update_execution_status(self):
-    #     (blueprint_id, deployment_id, blueprint_response,
-    #      deployment_response) = self.put_test_deployment(self.DEPLOYMENT_ID)
-    #
-    #     resource_path = '/deployments/{0}/executions'.format(deployment_id)
-    #     execution = self.post(resource_path, {
-    #         'workflowId': 'install'
-    #     }).json
-    #     get_execution_resource = '/executions/{0}'.format(execution['id'])
-    #     execution = self.get(get_execution_resource).json
-    #     self.assertEquals(, execution['status'])
+    def test_bad_update_execution_status(self):
+        (blueprint_id, deployment_id, blueprint_response,
+         deployment_response) = self.put_test_deployment(self.DEPLOYMENT_ID)
+
+        resource_path = '/deployments/{0}/executions'.format(deployment_id)
+        execution = self.post(resource_path, {
+            'workflowId': 'install'
+        }).json
+        get_execution_resource = '/executions/{0}'.format(execution['id'])
+        execution = self.get(get_execution_resource).json
+        self.assertEquals('pending', execution['status'])
+        # making a bad update request - not passing the required 'status'
+        # parameter
+        resp = self.patch('/executions/{0}'.format(execution['id']), {})
+        self.assertEquals(400, resp.status_code)
+        self.assertTrue('status' in resp.json['message'])
+
+    def test_update_execution_status(self):
+        (blueprint_id, deployment_id, blueprint_response,
+         deployment_response) = self.put_test_deployment(self.DEPLOYMENT_ID)
+
+        resource_path = '/deployments/{0}/executions'.format(deployment_id)
+        execution = self.post(resource_path, {
+            'workflowId': 'install'
+        }).json
+        get_execution_resource = '/executions/{0}'.format(execution['id'])
+        execution = self.get(get_execution_resource).json
+        self.assertEquals('pending', execution['status'])
+        execution = self.patch('/executions/{0}'.format(execution['id']),
+                               {'status': 'new-status'}).json
+        self.assertEquals('new-status', execution['status'])
+
+    def test_update_execution_status_with_error(self):
+        (blueprint_id, deployment_id, blueprint_response,
+         deployment_response) = self.put_test_deployment(self.DEPLOYMENT_ID)
+
+        resource_path = '/deployments/{0}/executions'.format(deployment_id)
+        execution = self.post(resource_path, {
+            'workflowId': 'install'
+        }).json
+        get_execution_resource = '/executions/{0}'.format(execution['id'])
+        execution = self.get(get_execution_resource).json
+        self.assertEquals('pending', execution['status'])
+        self.assertEquals('None', execution['error'])
+        execution = self.patch('/executions/{0}'.format(execution['id']),
+                               {'status': 'new-status',
+                                'error': 'some error'}).json
+        self.assertEquals('new-status', execution['status'])
+        self.assertEquals('some error', execution['error'])
+
+    def test_update_nonexistent_execution(self):
+        resp = self.patch('/executions/1234', {'status': 'new-status'})
+        self.assertEquals(404, resp.status_code)
 
     def test_cancel_execution_by_id(self):
         execution = self.test_get_execution_by_id()
