75d4fccdfed97ef9ff19f916a99ed08e54c43ae0
Ran Ziv <ran@gigaspaces.com>
2014-05-26 12:24:35 +0300

    CFY-642 modified default workflows plugin installation

--- plugins/plugin-installer/plugin_installer/tasks.py
+++ plugins/plugin-installer/plugin_installer/tasks.py
@@ -45,6 +45,14 @@ def install(ctx, plugin, **kwargs):
     'plugins' directory containing the plugin.
     """
 
+    if plugin['name'] == 'default-workflows-plugin':
+        # special handling for the default workflows plugin, as it does not
+        # currently sit on the file server and is not under a blueprint
+        # context
+        plugin['url'] = '/opt/manager/cloudify-manager-develop/workflows'
+        install_celery_plugin(plugin)
+        return
+
     if "folder" in plugin:
 
         # convert the folder into a url inside the file server
