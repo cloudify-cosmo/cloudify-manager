20a12fd31226d97e513b950d068d764842bd82aa
nir0s <nir36g@gmail.com>
2014-06-11 11:54:39 +0300

    CFY-781 now checking if distro was part of the worker config before retrieving it

--- plugins/agent-installer/worker_installer/__init__.py
+++ plugins/agent-installer/worker_installer/__init__.py
@@ -51,8 +51,9 @@ def init_worker_installer(func):
         prepare_configuration(ctx, worker_config)
         kwargs['worker_config'] = worker_config
         kwargs['runner'] = FabricRunner(ctx, worker_config)
-        kwargs['worker_config']['distro'] = \
-            get_machine_distro(kwargs['runner'])
+        if not worker_config.get('distro'):
+            kwargs['worker_config']['distro'] = \
+                get_machine_distro(kwargs['runner'])
         return func(*args, **kwargs)
     return wrapper
 
