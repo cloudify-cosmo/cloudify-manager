c3ac51a9e006640140cf45a072d459e93e09ea98
Dan Kilman <dankilman@gmail.com>
2014-06-16 21:54:21 +0300

    CFY-648 cleanup, make rest tests pass

--- rest-service/manager_rest/workflow_client.py
+++ rest-service/manager_rest/workflow_client.py
@@ -17,7 +17,7 @@ __author__ = 'dan'
 
 from manager_rest import config
 
-from manager_rest import celery_client
+from manager_rest.celery_client import celery_client as client
 
 
 class WorkflowClient(object):
@@ -28,35 +28,18 @@ class WorkflowClient(object):
                          deployment_id,
                          blueprint_id,
                          execution_id):
-        client = celery_client.celery_client()
-        plugin = workflow['plugin']
-        operation = workflow['operation']
-        properties = workflow.get('properties', {})
-        task_name = '{}.{}'.format(plugin, operation)
-        task_id = execution_id
+        task_name = '{}.{}'.format(workflow['plugin'], workflow['operation'])
         # task_queue = '{}_workflows'.format(deployment_id)
         task_queue = 'cloudify.workflows'
-        kwargs = {
-            '__cloudify_context': {
-                'workflow_id': name,
-                'blueprint_id': blueprint_id,
-                'deployment_id': deployment_id,
-                'execution_id': execution_id
-            }
-        }
-        kwargs.update(properties)
-        client.execute_task(task_name=task_name,
-                            task_queue=task_queue,
-                            task_id=task_id,
-                            kwargs=kwargs)
-
-    @staticmethod
-    def get_workflow_status(self, workflow_id):
-        raise RuntimeError('get_workflow_status')
-
-    @staticmethod
-    def get_workflows_statuses(self, workflows_ids):
-        raise RuntimeError('get_workflow_statuses')
+        kwargs = workflow.get('properties', {})
+        kwargs['__cloudify_context'] = {'workflow_id': name,
+                                        'blueprint_id': blueprint_id,
+                                        'deployment_id': deployment_id,
+                                        'execution_id': execution_id}
+        client().execute_task(task_name=task_name,
+                              task_queue=task_queue,
+                              task_id=execution_id,
+                              kwargs=kwargs)
 
     @staticmethod
     def cancel_workflow(self, workflow_id):
