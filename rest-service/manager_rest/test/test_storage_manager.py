d0397829d937e134765414378e854146a6c6a64f
Ran Ziv <ran@gigaspaces.com>
2014-06-25 17:07:26 +0300

    CFY-647 fixed unit test in storage_manager

--- rest-service/manager_rest/test/test_storage_manager.py
+++ rest-service/manager_rest/test/test_storage_manager.py
@@ -99,7 +99,7 @@ class StorageManagerTests(base_test.BaseServerTestCase):
                                 workflows={})
 
         serialized_dep = dep.to_dict()
-        self.assertEquals(6, len(serialized_dep))
+        self.assertEquals(7, len(serialized_dep))
         self.assertEquals(dep.id, serialized_dep['id'])
         self.assertEquals(dep.created_at, serialized_dep['created_at'])
         self.assertEquals(dep.updated_at, serialized_dep['updated_at'])
