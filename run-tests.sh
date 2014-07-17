2226b463c5aa5e73299064aec3a2cdf319ba3813
Idan Moyal <idan@gigaspaces.com>
2014-07-18 01:55:34 +0300

    run flake8 and integration tests separately

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -e
-
 if [ "$1" == "test-plugins" ]; then
+	set -e
 	echo "### Testing plugins..."
 	echo "### Creating agent package..."
 	mkdir -p package/linux
@@ -49,6 +48,7 @@ if [ "$1" == "test-plugins" ]; then
 	cd ..
 
 elif [ "$1" == "test-rest-service" ]; then
+	set -e
 	echo "### Testing rest-service..."
 	git clone https://github.com/cloudify-cosmo/cloudify-rest-client --depth=1
 	cd cloudify-rest-client; pip install .; cd ..
@@ -56,8 +56,40 @@ elif [ "$1" == "test-rest-service" ]; then
 	pip install nose
 	nosetests rest-service/manager_rest/test --nologcapture --nocapture
 
-#elif [ "$1" == "run-integration-tests" ]; then
+elif [ "$1" == "run-integration-tests" ]; then
+	set -e
+	echo "### Running integration tests..."
+   	sudo apt-get update && sudo apt-get install -qy python-dbus
+  	dpkg -L python-dbus
+   	sudo ln -sf /usr/lib/python2.7/dist-packages/dbus ~/env/lib/python2.7/site-packages/dbus
+   	sudo ln -sf /usr/lib/python2.7/dist-packages/_dbus_*.so ~/env/lib/python2.7/site-packages
+   	wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.deb
+   	sudo dpkg -i elasticsearch-1.0.1.deb
+   	export PATH=$PATH:/usr/share/elasticsearch/bin
+   	sudo mkdir -p /usr/share/elasticsearch/data
+   	sudo chmod 777 /usr/share/elasticsearch/data
+   	wget http://aphyr.com/riemann/riemann_0.2.2_all.deb
+   	sudo dpkg -i riemann_0.2.2_all.deb
+   	sudo touch /var/log/riemann/riemann.log
+   	sudo chmod 666 /var/log/riemann/riemann.log
+   	sudo test -d /dev/shm && sudo rm -rf /dev/shm
+   	sudo ln -Tsf /{run,dev}/shm
+   	sudo chmod 777 /dev/shm  # for celery worker
 
+   	cd workflows && pip install . && cd ..
+   	cd tests && pip install . && cd ..
 
-
+	pip install nose
+	nosetests tests/workflow_tests --nologcapture --nocapture -v
+	nosetests tests/workers_tests --nologcapture --nocapture -v
+elif [ "$1" == "flake8" ]; then
+	echo "### Running flake8..."
+	pip install flake8
+   	flake8 plugins/agent-installer/
+   	flake8 plugins/windows-agent-installer/
+   	flake8 plugins/plugin-installer/
+   	flake8 plugins/windows-plugin-installer/
+   	flake8 workflows/
+   	flake8 rest-service/
+   	flake8 tests/
 fi
