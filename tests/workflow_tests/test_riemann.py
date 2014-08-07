341cfd372ff585c6271395fc5f8ca6e42b04a3d3
Dan Kilman <dankilman@gmail.com>
2014-08-07 12:00:51 +0300

    CFY-734 continue work on test

--- tests/workflow_tests/test_riemann.py
+++ tests/workflow_tests/test_riemann.py
@@ -20,13 +20,42 @@ import bernhard
 from testenv import TestCase
 from testenv import get_resource as resource
 from testenv import deploy_application as deploy
+from testenv import undeploy_application as undeploy
 
 
 class RiemannTest(TestCase):
 
     def test_riemann(self):
         dsl_path = resource("dsl/riemann_basic.yaml")
+
+        # create deployment is the important part here
+        # we call the riemann.create task
         deployment, _ = deploy(dsl_path)
+
+        # new riemann core should be running on port 5556
+        self._send_riemann_event()
+
+        # undeploy so we can safely delete
+        undeploy(deployment.id)
+
+        # wait for ES to actually reflect the fact that the uninstall
+        # execution terminated
+        def assertion():
+            executions = self.client.executions.list(deployment.id)
+            terminated = all([e.status == 'terminated' for e in executions])
+            self.assertTrue(terminated)
+        self.do_assertions(assertion)
+
+        # we call the riemann.delete task
+        self.client.deployments.delete(deployment.id)
+
+        # riemann core should no longer be running on port 5556
+        def assertion():
+            self.assertRaises(bernhard.TransportError,
+                              self._send_riemann_event)
+        self.do_assertions(assertion)
+
+    def _send_riemann_event(self):
         client = bernhard.Client(port=5556)
         client.send({
             'service': 'testing',
