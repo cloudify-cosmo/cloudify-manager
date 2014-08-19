c9dec6c913b97a5ac5c908fcd1032261391ea14d
Ran Ziv <ran@gigaspaces.com>
2014-08-19 03:30:08 +0300

    CFY-1134 modified run-tests.sh for travis tests

--- run-tests.sh
+++ run-tests.sh
@@ -81,7 +81,7 @@ run_intergration_tests()
     sudo chmod 777 /dev/shm  # for celery worker
 
     git clone https://github.com/cloudify-cosmo/cloudify-rest-client --depth=1 -b CFY-1134-modify-runtime-properties-api
-    cd cloudify-rest-client; pip install .; cd ..
+    cd cloudify-rest-client; echo "###===### git branch is $(git branch)..."; pip install .; cd ..
     git clone https://github.com/cloudify-cosmo/cloudify-plugins-common --depth=1
     cd cloudify-plugins-common; pip install .; cd ..
 
