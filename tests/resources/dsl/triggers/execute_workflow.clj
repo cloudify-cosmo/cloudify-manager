dd5404d2b261b9bc6feb52776d22f73ec5707361
Dan Kilman <dankilman@gmail.com>
2014-08-21 16:25:55 +0300

    CFY-1046 bug fix

--- tests/resources/dsl/triggers/execute_workflow.clj
+++ tests/resources/dsl/triggers/execute_workflow.clj
@@ -1,15 +1,16 @@
 (fn execute-workflow
   [ctx]
-    (let [deployment-id       (:deployment-id ctx)
-          manager-ip          (or (System/getenv "MANAGEMENT_IP") "127.0.0.1")
-          manager-rest-port   (or (Integer/parseInt (System/getenv "MANAGER_REST_PORT")) 80)
-          base-uri            (str "http://" manager-ip ":" manager-rest-port)
-          endpoint            (str "/deployments/" deployment-id "/executions")
-          resource-uri        (str base-uri endpoint)
-          parameters          (:parameters ctx)
-          workflow            (:workflow parameters)
-          workflow-parameters (:workflow_parameters parameters)
-          body                (cheshire.core/generate-string {:workflow_id workflow
+    (let [deployment-id         (:deployment-id ctx)
+          manager-ip            (or (System/getenv "MANAGEMENT_IP") "127.0.0.1")
+          raw-manager-rest-port (or (System/getenv "MANAGER_REST_PORT") "80")
+          manager-rest-port     (Integer/parseInt raw-manager-rest-port)
+          base-uri              (str "http://" manager-ip ":" manager-rest-port)
+          endpoint              (str "/deployments/" deployment-id "/executions")
+          resource-uri          (str base-uri endpoint)
+          parameters            (:parameters ctx)
+          workflow              (:workflow parameters)
+          workflow-parameters   (:workflow_parameters parameters)
+          body                  (cheshire.core/generate-string {:workflow_id workflow
                                                               :parameters workflow-parameters})]
       (clj-http.client/post resource-uri
         {:content-type   :json
