03ab6ee6b1a92a09cd47d6d969386a06499cea8b
Dan Kilman <dankilman@gmail.com>
2014-10-19 17:54:09 +0300

    CFY-1433 fix test

--- tests/workflow_tests/test_rest_api.py
+++ tests/workflow_tests/test_rest_api.py
@@ -62,7 +62,7 @@ class RestAPITest(TestCase):
         instance = all_instances[0]
         self.assertIsNotNone(instance.id)
         self.assertEqual(self.deployment_id, instance.deployment_id)
-        self.assertIsNone(instance.runtime_properties)
+        self.assertEqual(instance.runtime_properties, {})
         self.assertEqual('uninitialized', instance.state)
 
         instance = self.client.node_instances.get(instance.id)
