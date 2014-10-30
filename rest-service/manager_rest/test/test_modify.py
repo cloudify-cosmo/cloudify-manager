bf989672f20178c0ef7dec0d394d1c3de221f758
Dan Kilman <dankilman@gmail.com>
2014-10-30 19:27:10 +0200

    CFY-1433 add test for node instance removal

--- rest-service/manager_rest/test/test_modify.py
+++ rest-service/manager_rest/test/test_modify.py
@@ -22,7 +22,7 @@ from base_test import BaseServerTestCase
 
 class ModifyTests(BaseServerTestCase):
 
-    def test_modify(self):
+    def test_modify_add_instance(self):
         _, _, _, deployment = self.put_deployment(
             deployment_id=str(uuid.uuid4()),
             blueprint_file_name='modify1.yaml')
@@ -71,6 +71,55 @@ class ModifyTests(BaseServerTestCase):
         node2_target_ids = [rel['target_id'] for rel
                             in node2_instance.relationships]
         self.assertEqual(set(node1_instance_ids), set(node2_target_ids))
+
+    def test_modify_remove_instance(self):
+        _, _, _, deployment = self.put_deployment(
+            deployment_id=str(uuid.uuid4()),
+            blueprint_file_name='modify2.yaml')
+
+        node_instances1 = self.client.node_instances.list()
+        self.assertEqual(3, len(node_instances1))
+        self._assert_number_of_instances(deployment.id, 'node1', 2, 2)
+
+        modified_nodes = {'node1': {'instances': 1}}
+        modification = self.client.deployments.modify.start(
+            deployment.id, nodes=modified_nodes)
+
+        self._assert_number_of_instances(deployment.id, 'node1', 2, 2)
+
+        self.assertEqual(modified_nodes, modification.modified_nodes)
+        node_instances2 = self.client.node_instances.list()
+        self.assertEqual(3, len(node_instances2))
+
+        initial_instance_ids = [i2.id for i2 in node_instances1]
+        new_instances = [i for i in node_instances2
+                         if i.id not in initial_instance_ids]
+        old_instances = [i for i in node_instances2
+                         if i.id in initial_instance_ids]
+        self.assertEqual(0, len(new_instances))
+        self.assertEqual(3, len(old_instances))
+
+        self.assertEqual(sorted(old_instances, key=lambda _i: _i.id),
+                         sorted(node_instances1, key=lambda _i: _i.id))
+
+        removed_and_related = modification.node_instances.removed_and_related
+        self.assertEqual(2, len(removed_and_related))
+
+        self.client.deployments.modify.finish(deployment.id, modification)
+
+        self._assert_number_of_instances(deployment.id, 'node1', 1, 2)
+
+        node_instances3 = self.client.node_instances.list()
+        self.assertEqual(2, len(node_instances3))
+
+        node1_instance_id = [i.id for i in node_instances3
+                             if i.node_id == 'node1'][0]
+        node2_instance = [i for i in node_instances3
+                          if i.node_id == 'node2'][0]
+        node2_target_ids = [rel['target_id'] for rel
+                            in node2_instance.relationships]
+        self.assertEqual(1, len(node2_target_ids))
+        self.assertEqual(node1_instance_id, node2_target_ids[0])
 
     def _assert_number_of_instances(self,
                                     deployment_id, node_id,
