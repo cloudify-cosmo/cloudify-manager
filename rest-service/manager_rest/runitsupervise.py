1daea5f74e55776ebb8a36b0a9f108199c88169b
adamlavie <adaml@gigaspaces.com>
2014-11-11 14:18:24 +0200

    CFY-1308 fixed 'cfy status'

--- rest-service/manager_rest/runitsupervise.py
+++ rest-service/manager_rest/runitsupervise.py
@@ -65,6 +65,6 @@ def get_instance_properties(name, keys=['uptime', 'status', 'pid']):
     for key in props.keys():
         if key not in keys:
             del props[key]
-    props.update('state', supervise._status2str(props['status']))
+    props.update('state', supervise.ServiceStatus._status2str(props['status']))
     out.append(props)
     return out
