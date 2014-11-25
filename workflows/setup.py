6497ccc4f614972ab5e9d9ad23620de8b436b49b
limor-gs <limor@gigaspaces.com>
2014-11-25 23:08:08 +0200

    Bump version to 3.1

--- workflows/setup.py
+++ workflows/setup.py
@@ -20,13 +20,13 @@ from setuptools import setup
 
 setup(
     name='cloudify-workflows',
-    version='3.1ga',
+    version='3.1',
     author='elip',
     author_email='elip@gigaspaces.com',
     packages=['cloudify_system_workflows'],
     license='LICENSE',
     description='Various Cloudify Workflows',
     install_requires=[
-        'cloudify-plugins-common==3.1ga',
+        'cloudify-plugins-common==3.1',
     ]
 )
