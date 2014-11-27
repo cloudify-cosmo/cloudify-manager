c2591b7e486844f564d903e6efbf5058708b4d4e
zofiaOS <zofia.ostoja-starzycka@codilime.com>
2014-11-27 14:43:51 +0000

    Added filtering out unused services in host failure

--- resources/rest-service/cloudify/policies/host_failure.clj
+++ resources/rest-service/cloudify/policies/host_failure.clj
@@ -1,7 +1,8 @@
-(where* expired?
-        (fn [event]
-          ((with {:failing_node (:node_id event)
-                  :diagnose "{{constants.HEART_BEAT_FAILURE}}"
-                  :state "{{constants.TRIGGERING_STATE}}"}
-                 downstream)
-           event)))
+(where* (is-service-name-in {%for s in service%} "{{s}}" {%endfor%})
+  (where* expired?
+          (fn [event]
+            ((with {:failing_node (:node_id event)
+                    :diagnose "{{constants.HEART_BEAT_FAILURE}}"
+                    :state "{{constants.TRIGGERING_STATE}}"}
+                   downstream)
+             event))))
