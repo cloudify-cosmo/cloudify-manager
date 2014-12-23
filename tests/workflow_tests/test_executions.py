3620aa5091ee4f1de1a49241dad9ccbc7a504fa5
ran <ran@gigaspaces.com>
2014-12-23 15:38:19 +0200

    CFY-1808 added a test for legacy cancel execution

--- tests/workflow_tests/test_executions.py
+++ tests/workflow_tests/test_executions.py
@@ -54,6 +54,15 @@ class ExecutionsTest(TestCase):
 
         self._assert_execution_cancelled(execution, deployment_id)
 
+    def test_legacy_cancel_execution(self):
+        # this tests cancellation of an execution where the workflow
+        # announces the cancel occurred by returning a value rather than by
+        # raising an error
+        execution, deployment_id = self._execute_and_cancel_execution(
+            'sleep_with_cancel_support', workflow_params={
+                'use_legacy_cancel': True}, allow_custom_params=True)
+        self._assert_execution_cancelled(execution, deployment_id)
+
     def test_cancel_execution_before_it_started(self):
         execution, deployment_id = self._execute_and_cancel_execution(
             'sleep_with_cancel_support', False, True, False)
@@ -150,7 +159,9 @@ class ExecutionsTest(TestCase):
 
     def _execute_and_cancel_execution(self, workflow_id, force=False,
                                       wait_for_termination=True,
-                                      is_wait_for_asleep_node=True):
+                                      is_wait_for_asleep_node=True,
+                                      workflow_params=None,
+                                      allow_custom_params=False):
         dsl_path = resource('dsl/sleep_workflows.yaml')
         _id = uuid.uuid1()
         blueprint_id = 'blueprint_{0}'.format(_id)
@@ -160,7 +171,8 @@ class ExecutionsTest(TestCase):
         do_retries(verify_deployment_environment_creation_complete, 30,
                    deployment_id=deployment_id)
         execution = self.client.executions.start(
-            deployment_id, workflow_id)
+            deployment_id, workflow_id, parameters=workflow_params,
+            allow_custom_parameters=allow_custom_params)
 
         node_inst_id = self.client.node_instances.list(deployment_id)[0].id
 
