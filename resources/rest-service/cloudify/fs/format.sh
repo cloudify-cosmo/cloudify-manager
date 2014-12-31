9c3458d982a222b44413fddf0f90d0a7e8ff9ce7
iliapolo <elip@gigaspaces.com>
2014-12-31 12:20:06 +0200

    CFY-1761 fix format script to access properties correctly

--- resources/rest-service/cloudify/fs/format.sh
+++ resources/rest-service/cloudify/fs/format.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-partition_number=$(ctx source node properties partition_number)
-format_after_unmount=$(ctx source node properties format_after_unmount)
+partition_number=$(ctx node properties partition_number)
+format_after_unmount=$(ctx node properties format_after_unmount)
 
-device_name=$(ctx source instance runtime-properties device_name)
+device_name=$(ctx instance runtime-properties device_name)
 
 if [ -z "${format_after_unmount}" ]; then
     ctx logger info "Removing disk partition"
