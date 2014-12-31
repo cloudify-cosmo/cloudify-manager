f4e318c32da1d940aec3691766e8d8880a8e2087
iliapolo <elip@gigaspaces.com>
2014-12-31 16:35:01 +0200

    CFY-1761 fix mount script and remove formatting

--- resources/rest-service/cloudify/fs/mount.sh
+++ resources/rest-service/cloudify/fs/mount.sh
@@ -9,3 +9,7 @@ fi
 
 ctx logger info "Mounting file system ${filesys} on ${fs_mount_path}"
 sudo mount ${filesys} ${fs_mount_path}
+
+user=$(whoami)
+ctx logger info "Changing ownership of ${fs_mount_path} to ${user}"
+sudo chown -R ${user} ${fs_mount_path}
