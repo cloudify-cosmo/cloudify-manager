76e4237bdc1b967305cf3fe476bb2d99d7d1a90b
Dan Kilman <dankilman@gmail.com>
2015-02-01 10:06:54 +0200

    CFY-2006 handle case where celery logs weren't loaded

--- tests/testenv/utils.py
+++ tests/testenv/utils.py
@@ -150,7 +150,7 @@ def verify_deployment_environment_creation_complete(deployment_id):
             or execs[0].status != Execution.TERMINATED \
             or execs[0].workflow_id != 'create_deployment_environment':
         from testenv import TestEnvironment  # avoid cyclic import
-        logs = TestEnvironment.read_celery_management_logs()
+        logs = TestEnvironment.read_celery_management_logs() or []
         logs = logs[len(logs) - 100000:]
         raise RuntimeError(
             "Expected a single execution for workflow "
