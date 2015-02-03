ed06f86b36eae441d978414a281082cf742fa6f5
adamlavie <adaml@gigaspaces.com>
2015-02-03 17:00:12 +0200

    clean fstab file on unmount

--- resources/rest-service/cloudify/fs/mount.sh
+++ resources/rest-service/cloudify/fs/mount.sh
@@ -2,7 +2,7 @@
 
 fs_mount_path=$(ctx source node properties fs_mount_path)
 filesys=$(ctx source instance runtime-properties filesys)
-fs_type=ext4
+fs_type=$(ctx source node properites fs_type)
 
 if [ ! -f ${fs_mount_path} ]; then
     sudo mkdir -p ${fs_mount_path}
@@ -14,7 +14,6 @@ sudo mount ${filesys} ${fs_mount_path}
 user=$(whoami)
 ctx logger info "Changing ownership of ${fs_mount_path} to ${user}"
 sudo chown -R ${user} ${fs_mount_path}
-# add auto flag to mount point so it would remount on reboot
-echo lalala >> lala.txt
+
+ctx logger info "Adding mount point ${fs_mount_path} to file system table"
 echo ${filesys} ${fs_mount_path} ${fs_type} auto 0 0 | sudo tee --append /etc/fstab > /dev/null
-#sudo sh -c 'echo ${filesys} ${fs_mount_path} ${fs_type} auto 0 0 >> /etc/fstab'
