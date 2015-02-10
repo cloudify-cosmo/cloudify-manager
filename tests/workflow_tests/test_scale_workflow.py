cc77dca6feaa008c0a984e3bc556fd73c11304b7
Dan Kilman <dankilman@gmail.com>
2015-02-11 00:03:08 +0200

    CFY-2043 start adding tests for scale in

--- tests/workflow_tests/test_scale_workflow.py
+++ tests/workflow_tests/test_scale_workflow.py
@@ -32,6 +32,18 @@ class TestScaleWorkflow(TestCase):
         expectations['compute']['existing']['install'] = 1
         self.deployment_assertions(expectations)
 
+    def test_compute_scale_in_compute(self):
+        expectations = self.deploy('scale4')
+        expectations['compute']['new']['install'] = 3
+        self.deployment_assertions(expectations)
+
+        expectations = self.scale(parameters={'node_id': 'compute',
+                                              'delta': -1})
+        expectations['compute']['existing']['install'] = 2
+        expectations['compute']['removed']['install'] = 1
+        expectations['compute']['removed']['uninstall'] = 1
+        self.deployment_assertions(expectations)
+
     def test_compute_scale_out_2_compute(self):
         expectations = self.deploy('scale1')
         expectations['compute']['new']['install'] = 1
@@ -41,6 +53,18 @@ class TestScaleWorkflow(TestCase):
                                               'delta': 2})
         expectations['compute']['new']['install'] = 2
         expectations['compute']['existing']['install'] = 1
+        self.deployment_assertions(expectations)
+
+    def test_compute_scale_in_2_compute(self):
+        expectations = self.deploy('scale4')
+        expectations['compute']['new']['install'] = 3
+        self.deployment_assertions(expectations)
+
+        expectations = self.scale(parameters={'node_id': 'compute',
+                                              'delta': -2})
+        expectations['compute']['existing']['install'] = 1
+        expectations['compute']['removed']['install'] = 2
+        expectations['compute']['removed']['uninstall'] = 2
         self.deployment_assertions(expectations)
 
     def test_db_contained_in_compute_scale_out_compute(self):
@@ -59,6 +83,26 @@ class TestScaleWorkflow(TestCase):
         expectations['db']['existing']['rel_install'] = 2
         self.deployment_assertions(expectations)
 
+    def test_db_contained_in_compute_scale_in_compute(self):
+        expectations = self.deploy('scale5')
+        expectations['compute']['new']['install'] = 2
+        expectations['db']['new']['install'] = 4
+        expectations['db']['new']['rel_install'] = 8
+        self.deployment_assertions(expectations)
+
+        expectations = self.scale(parameters={'node_id': 'compute',
+                                              'delta': -1})
+        expectations['compute']['existing']['install'] = 1
+        expectations['compute']['removed']['install'] = 1
+        expectations['compute']['removed']['uninstall'] = 1
+        expectations['db']['existing']['install'] = 2
+        expectations['db']['existing']['rel_install'] = 4
+        expectations['db']['removed']['install'] = 2
+        expectations['db']['removed']['uninstall'] = 2
+        expectations['db']['removed']['rel_install'] = 4
+        expectations['db']['removed']['rel_uninstall'] = 4
+        self.deployment_assertions(expectations)
+
     def test_db_contained_in_compute_scale_out_db(self):
         expectations = self.deploy('scale2')
         expectations['compute']['new']['install'] = 1
@@ -75,6 +119,26 @@ class TestScaleWorkflow(TestCase):
         expectations['db']['existing']['rel_install'] = 2
         self.deployment_assertions(expectations)
 
+    def test_db_contained_in_compute_scale_in_db(self):
+        expectations = self.deploy('scale5')
+        expectations['compute']['new']['install'] = 2
+        expectations['db']['new']['install'] = 4
+        expectations['db']['new']['rel_install'] = 8
+        self.deployment_assertions(expectations)
+
+        expectations = self.scale(parameters={'node_id': 'db',
+                                              'delta': -1})
+        expectations['compute']['existing']['install'] = 1
+        expectations['compute']['removed']['install'] = 1
+        expectations['compute']['removed']['uninstall'] = 1
+        expectations['db']['existing']['install'] = 2
+        expectations['db']['existing']['rel_install'] = 4
+        expectations['db']['removed']['install'] = 2
+        expectations['db']['removed']['uninstall'] = 2
+        expectations['db']['removed']['rel_install'] = 4
+        expectations['db']['removed']['rel_uninstall'] = 4
+        self.deployment_assertions(expectations)
+
     def test_db_contained_in_compute_scale_out_db_operate_on_db(self):
         expectations = self.deploy('scale2')
         expectations['compute']['new']['install'] = 1
@@ -89,6 +153,25 @@ class TestScaleWorkflow(TestCase):
         expectations['db']['new']['rel_install'] = 2
         expectations['db']['existing']['install'] = 1
         expectations['db']['existing']['rel_install'] = 2
+        self.deployment_assertions(expectations)
+
+    def test_db_contained_in_compute_scale_in_db_operate_on_db(self):
+        expectations = self.deploy('scale5')
+        expectations['compute']['new']['install'] = 2
+        expectations['db']['new']['install'] = 4
+        expectations['db']['new']['rel_install'] = 8
+        self.deployment_assertions(expectations)
+
+        expectations = self.scale(parameters={'node_id': 'db',
+                                              'delta': -1,
+                                              'operate_on_compute': False})
+        expectations['compute']['existing']['install'] = 2
+        expectations['db']['existing']['install'] = 2
+        expectations['db']['existing']['rel_install'] = 4
+        expectations['db']['removed']['install'] = 2
+        expectations['db']['removed']['uninstall'] = 2
+        expectations['db']['removed']['rel_install'] = 4
+        expectations['db']['removed']['rel_uninstall'] = 4
         self.deployment_assertions(expectations)
 
     def test_db_connected_to_compute_scale_out_compute(self):
@@ -124,6 +207,7 @@ class TestScaleWorkflow(TestCase):
     def setUp(self):
         super(TestScaleWorkflow, self).setUp()
         self.previous_ids = []
+        self.previous_instances = []
 
     def deployment_assertions(self, expected):
         def expected_invocations(_expectations, num_instances):
@@ -134,28 +218,42 @@ class TestScaleWorkflow(TestCase):
                 'configure': install_count / num_instances,
                 'start': install_count / num_instances
             })
+            uninstall_count = _expectations.get('uninstall') or 0
+            result.update({
+                'stop': uninstall_count / num_instances,
+                'delete': uninstall_count / num_instances,
+            })
             rel_install_count = _expectations.get('rel_install') or 0
             scale_rel_install_count = _expectations.get(
                 'scale_rel_install') or 0
             result.update({
                 'preconfigure': rel_install_count / num_instances,
                 'postconfigure': rel_install_count / num_instances,
-                'establish': rel_install_count + scale_rel_install_count /
+                'establish': (rel_install_count + scale_rel_install_count) /
                 num_instances
+            })
+            rel_uninstall_count = _expectations.get('rel_uninstall') or 0
+            result.update({
+                'unlink': rel_uninstall_count / num_instances
             })
             return result
 
         instances = self.client.node_instances.list()
+        instance_ids = [i.id for i in instances]
 
         calculated_expected = {}
         for node_id, expectations in expected.items():
             new_expectation = expectations['new']
             existing_expectation = expectations['existing']
+            removed_expectation = expectations['removed']
             node_instances = [i for i in instances if i.node_id == node_id]
             new_instances = [i for i in node_instances
                              if i.id not in self.previous_ids]
             existing_instances = [i for i in node_instances
                                   if i.id in self.previous_ids]
+            removed_instances = [i for i in self.previous_instances
+                                 if i.id not in instance_ids
+                                 and i.node_id == node_id]
             self.assertEqual(len(new_instances),
                              new_expectation.get('install') or 0,
                              'new_instances: {0}, install_expectations: {1}'
@@ -167,6 +265,12 @@ class TestScaleWorkflow(TestCase):
                              'install_expectations: {1}'
                              .format(existing_instances,
                                      existing_expectation.get('install')))
+            self.assertEqual(len(removed_instances),
+                             removed_expectation.get('uninstall') or 0,
+                             'removed_instances: {0}, '
+                             'uninstall_expectations: {1}'
+                             .format(removed_instances,
+                                     removed_expectation.get('uninstall')))
             for new_instance in new_instances:
                 calculated_expected.update({
                     new_instance.id: expected_invocations(
@@ -175,6 +279,10 @@ class TestScaleWorkflow(TestCase):
                 calculated_expected.update({
                     existing_instance.id: expected_invocations(
                         existing_expectation, len(existing_instances))})
+            for removed_instance in removed_instances:
+                calculated_expected.update({
+                    removed_instance.id: expected_invocations(
+                        removed_expectation, len(removed_instances))})
 
         invocations = self.get_plugin_data(
             'testmockoperations', self.deployment_id)[
@@ -192,21 +300,25 @@ class TestScaleWorkflow(TestCase):
         self.assertEqual(total_expected_count, len(invocations))
 
         # set state for next deployment assertion
-        self.previous_ids = [i.id for i in instances]
+        self.previous_instances = instances
+        self.previous_ids = instance_ids
 
     def expectations(self):
         return {
             'compute': {
                 'new': {},
-                'existing': {}
+                'existing': {},
+                'removed': {}
             },
             'db': {
                 'new': {},
-                'existing': {}
+                'existing': {},
+                'removed': {}
             },
             'webserver': {
                 'new': {},
-                'existing': {}
+                'existing': {},
+                'removed': {}
             }
         }
 
