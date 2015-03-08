d4bdae61e507c193b83d5402a69648634c156777
Noa Kuperberg <noakup@gmail.com>
2015-03-08 14:07:13 +0200

    removed flask_securest

--- flask_securest/flask_securest/authentication_providers/ldap.py
+++
@@ -1,9 +0,0 @@
-from flask_securest.authentication_providers.abstract_authentication_provider \
-    import AbstractAuthenticationProvider
-
-
-class LdapAuthenticator(AbstractAuthenticationProvider):
-
-    @staticmethod
-    def authenticate(user, auth_info):
-        pass
--- flask_securest/flask_securest/authentication_providers/sql.py
+++
@@ -1,9 +0,0 @@
-from flask_securest.authentication_providers.abstract_authentication_provider \
-    import AbstractAuthenticationProvider
-
-
-class SqlAuthenticator(AbstractAuthenticationProvider):
-
-    @staticmethod
-    def authenticate(user, auth_info):
-        pass
