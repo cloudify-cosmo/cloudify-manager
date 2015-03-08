e0b9201d1a63dc63f0bbd99507074c1611933f2a
Noa Kuperberg <noakup@gmail.com>
2015-03-08 14:37:02 +0200

    reverted comment

--- rest-service/manager_rest/server.py
+++ rest-service/manager_rest/server.py
@@ -35,10 +35,9 @@ from flask_securest.rest_security import (
     SECUREST_USERSTORE_IDENTIFIER_ATTRIBUTE)
 
 from manager_rest import config
-# from manager_rest import blueprints_manager
+from manager_rest import util
 from manager_rest import storage_manager
 from manager_rest import resources
-from manager_rest import utils
 from manager_rest import manager_exceptions
 from util import setup_logger
 
@@ -66,7 +65,6 @@ def setup_app():
     additional_log_handlers = [
         RotatingFileHandler(
             config.instance().rest_service_log_path,
-            '/tmp/tmplog',
             maxBytes=1024 * 1024 * 100,
             backupCount=20)
     ]
@@ -172,24 +170,25 @@ def reset_state(configuration=None):
     app = setup_app()
 
 
-def init_secure_app(app):
-    cfy_config = config.instance()
+def validate_security_config(cfy_config):
     # TODO raise better exceptions
     if not hasattr(cfy_config, 'securest_secret_key') or \
             cfy_config.securest_secret_key is None:
         raise Exception('securest_secret_key not set')
-
     if not hasattr(cfy_config, 'securest_authentication_methods') or \
             cfy_config.securest_authentication_methods is None:
         raise Exception('securest_authentication_methods not set')
-
     if not hasattr(cfy_config, 'securest_userstore_driver') or \
             cfy_config.securest_userstore_driver is None:
         raise Exception('securest_userstore_driver not set')
-
     if not hasattr(cfy_config, 'securest_userstore_identifier_attribute') or \
             cfy_config.securest_userstore_identifier_attribute is None:
         raise Exception('securest_userstore_identifier_attribute not set')
+
+
+def init_secure_app(app):
+    cfy_config = config.instance()
+    validate_security_config(cfy_config)
 
     app.config[SECUREST_SECRET_KEY] = cfy_config.securest_secret_key
     app.config[SECUREST_USERSTORE_DRIVER] = \
@@ -212,18 +211,7 @@ def register_authentication_methods(secure_app, authentication_providers):
     # Note: the order of registration is important here
     for auth_method_path in authentication_providers:
         try:
-            print '***** getting instance of: ', auth_method_path
-            auth_provider = utils.get_class_instance(auth_method_path)
-            print '----- Got class instance'
-
-            '''
-            if not hasattr(auth_provider, AUTHENTICATE_METHOD_NAME):
-                # TODO use a more specific exception type
-                raise Exception('authentication provider "{0}" does not'
-                                ' implement {1}'.format(
-                                    utils.get_runtime_class_fqn(auth_provider),
-                                    AUTHENTICATE_METHOD_NAME))
-            '''
+            auth_provider = util.get_class_instance(auth_method_path)
             secure_app.authentication_provider(auth_provider)
         except Exception as e:
             print('Failed to register authentication method: ',
