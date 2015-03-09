c7a97b28ee3d988933ae6a75a496f31084d60c5e
Anna Szpoton <anna.szpoton@codilime.com>
2015-03-09 13:09:05 +0100

    CFY-2289 Add password usage to worker installer

--- plugins/agent-installer/worker_installer/__init__.py
+++ plugins/agent-installer/worker_installer/__init__.py
@@ -163,16 +163,19 @@ def _prepare_and_validate_autoscale_params(ctx, config):
     config['max_workers'] = max_workers
 
 
-def _set_ssh_key(ctx, config):
+def _set_auth(ctx, config):
+    is_password = 'password' in config
+
     if 'key' not in config:
         if ctx.bootstrap_context.cloudify_agent.agent_key_path:
             config['key'] = ctx.bootstrap_context.cloudify_agent.agent_key_path
-        else:
+        elif not is_password:
             raise NonRecoverableError(
-                'Missing ssh key path in worker configuration '
+                'Missing password or ssh key path in worker configuration '
                 '[cloudify_agent={0}'.format(config))
 
-    if not os.path.isfile(os.path.expanduser(config['key'])):
+    if not is_password \
+            and not os.path.isfile(os.path.expanduser(config['key'])):
         raise NonRecoverableError(
             'Cannot find keypair file, expected file path was {'
             '0}'.format(config['key']))
@@ -245,7 +248,7 @@ def prepare_connection_configuration(ctx, agent_config):
         agent_config['name'] = name
     else:
         agent_config['host'] = get_machine_ip(ctx)
-        _set_ssh_key(ctx, agent_config)
+        _set_auth(ctx, agent_config)
         _set_user(ctx, agent_config)
         _set_remote_execution_port(ctx, agent_config)
         agent_config['name'] = ctx.instance.id
