2732552052f5b4b985ca961e43d41c30209987d7
Noa Kuperberg <noakup@gmail.com>
2015-03-31 16:06:03 +0300

    flake8

--- rest-service/manager_rest/test/test_security.py
+++ rest-service/manager_rest/test/test_security.py
@@ -13,10 +13,8 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
-from mock import patch
 
 from base_test import BaseServerTestCase
-from cloudify_rest_client.exceptions import CloudifyClientError
 
 
 class SecurityTestBase(BaseServerTestCase):
