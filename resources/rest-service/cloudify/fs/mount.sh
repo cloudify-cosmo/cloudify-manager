7cf64da2a2b4487cc8fa6a3875dbade953c74080
adamlavie <adaml@gigaspaces.com>
2015-05-12 21:11:26 +0300

    CFY-2727 more refactoring

--- resources/rest-service/cloudify/fs/mount.sh
+++ resources/rest-service/cloudify/fs/mount.sh
@@ -3,17 +3,21 @@
 fs_mount_path=$(ctx source node properties fs_mount_path)
 filesys=$(ctx source instance runtime-properties filesys)
 fs_type=$(ctx source node properties fs_type)
-recovery_mode=$(ctx source instance runtime-properties recovery_mode)
+recovery_enabled=$(ctx source instance runtime-properties recovery_enabled)
 
 if [ ! -d ${fs_mount_path} ]; then
     sudo mkdir -p ${fs_mount_path}
-elif which docker && [ -z ${recovery_mode} ]; then
-    docker_back=/tmp/docker_back
-    sudo mkdir -p ${docker_back}
+elif which docker; then
     sudo service docker stop
-    ctx logger info "Backing up existing docker files on ${fs_mount_path} to ${docker_back}"
-    sudo cp -a ${fs_mount_path}/. ${docker_back}
+    if [ -z ${recovery_enabled} ]; then
+        docker_back=/tmp/docker_back
+        sudo mkdir -p ${docker_back}
+        sudo service docker stop
+        ctx logger info "Backing up existing docker files on ${fs_mount_path} to ${docker_back}"
+        sudo cp -a ${fs_mount_path}/. ${docker_back}
+    fi
 fi
+
 
 ctx logger info "Mounting file system ${filesys} on ${fs_mount_path}"
 sudo mount ${filesys} ${fs_mount_path}
@@ -30,10 +34,10 @@ sudo chown -R ${user} ${fs_mount_path}
 ctx logger info "Adding mount point ${fs_mount_path} to file system table"
 echo ${filesys} ${fs_mount_path} ${fs_type} auto 0 0 | sudo tee --append /etc/fstab > /dev/null
 
-if which docker && [ -n ${recovery_mode} ]; then
-    ctx logger info "Restarting docker service"
+if which docker; then
+    ctx logger info "Starting docker service"
     sudo service docker restart
-else
-    ctx logger info "Marking this instance as mounted"
-    ctx source instance runtime-properties recovery_mode "True"
 fi
+
+ctx logger info "Marking this instance as mounted"
+ctx source instance runtime-properties recovery_enabled "True"
