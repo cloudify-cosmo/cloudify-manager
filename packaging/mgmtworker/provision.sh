b5aea7ea855527ef25304dc2a19f54e32cf7075f
opencm <limor@gigaspaces.com>
2015-11-01 14:19:10 +0200

    Bump version to 3.3rc1

--- packaging/mgmtworker/provision.sh
+++ packaging/mgmtworker/provision.sh
@@ -37,10 +37,10 @@ function upload_to_s3() {
 
 # VERSION/PRERELEASE/BUILD are exported to follow with our standard of exposing them as env vars. They are not used.
 export VERSION="3.3.0"
-export PRERELEASE="m7"
-export BUILD="277"
-CORE_TAG_NAME="3.3m7"
-PLUGINS_TAG_NAME="1.3m7"
+export PRERELEASE="rc1"
+export BUILD="278"
+CORE_TAG_NAME="3.3rc1"
+PLUGINS_TAG_NAME="1.3rc1"
 
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
