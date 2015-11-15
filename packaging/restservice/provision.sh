71d004e0713b18ebaee712fd1e2b08393de10256
limor-gs <limor@gigaspaces.com>
2015-11-15 15:47:10 +0200

    revert CFY-4129 commit

--- packaging/restservice/provision.sh
+++ packaging/restservice/provision.sh
@@ -38,7 +38,6 @@ function upload_to_s3() {
 # VERSION/PRERELEASE/BUILD are exported to follow with our standard of exposing them as env vars. They are not used.
 export VERSION="3.3.0"
 export PRERELEASE="None"
-[ PRERELEASE == "None" ] && PRERELEASE="ga"
 export BUILD="300"
 CORE_TAG_NAME="3.3"
 PLUGINS_TAG_NAME="1.3"
