1e954bb73325805220d560408d0a1578c17acc52
nirb <nirb@gigaspaces.com>
2015-11-18 14:42:05 +0200

    CFY-4067 temporarily removed events clean verification

--- workflows/cloudify_system_workflows/snapshot.py
+++ workflows/cloudify_system_workflows/snapshot.py
@@ -402,16 +402,16 @@ def _assert_clean_elasticsearch(es, log_warning=False):
         else:
             raise NonRecoverableError(message)
 
-    # check events
-    has_cloudify_events_index = es.indices.exists(index=_EVENTS_INDEX_NAME)
-    if has_cloudify_events_index:
-        events_index_name = _EVENTS_INDEX_NAME
-    else:
-        events_index_name = 'logstash-*'
-
-    res = es.count(index=events_index_name)
-    if res['count'] != '0':
-        _raise_or_log()
+    # # check events
+    # has_cloudify_events_index = es.indices.exists(index=_EVENTS_INDEX_NAME)
+    # if has_cloudify_events_index:
+    #     events_index_name = _EVENTS_INDEX_NAME
+    # else:
+    #     events_index_name = 'logstash-*'
+    #
+    # res = es.count(index=events_index_name)
+    # if res['count'] != '0':
+    #     _raise_or_log()
 
     # check storage
     storage_scan = elasticsearch.helpers.scan(es, index=_STORAGE_INDEX_NAME)
