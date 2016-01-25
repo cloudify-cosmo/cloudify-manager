090131aac0e94487a439e8de2b82181967ba7a46
opencm <t@g.com>
2016-01-25 14:29:12 +0200

    CFY-3933-Fixing-of-duplication-build-code

--- packaging/elasticsearch_curator/provision.sh
+++ packaging/elasticsearch_curator/provision.sh
@@ -19,23 +19,14 @@ function generate_checksum() {
     cd /tmp/x86_64 && md5sum=$(md5sum *.rpm) && echo $md5sum | sudo tee ${md5sum##* }.md5
 }
 
-function upload_to_s3() {
-    ###
-    # This will upload both the artifact and md5 files to the relevant bucket.
-    # Note that the bucket path is also appended the version.
-    ###
-    echo "Uploading to s3..."
-    sudo pip install s3cmd==1.5.2
-    sudo s3cmd put --force --acl-public --access_key=${AWS_ACCESS_KEY_ID} --secret_key=${AWS_ACCESS_KEY} \
-        --no-preserve --progress --human-readable-sizes --check-md5 *.rpm* s3://${AWS_S3_BUCKET_PATH}/
-}
+CORE_TAG_NAME="3.4m1"
+curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/$CORE_TAG_NAME/common/provision.sh -o ./common-provision.sh &&
+source common-provision.sh
 
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
-AWS_S3_BUCKET_PATH="gigaspaces-repository-eu/org/cloudify3/components"
+export AWS_S3_PATH="org/cloudify3/components"
 
-echo "AWS_S3_BUCKET_PATH: ${AWS_S3_BUCKET_PATH}"
-
-build_rpm
-generate_checksum
-[ -z ${AWS_ACCESS_KEY} ] || upload_to_s3
+build_rpm &&
+cd /tmp/x86_64 && create_md5 "rpm" &&
+[ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "rpm" && upload_to_s3 "md5"
--- packaging/restservice/provision.sh
+++ packaging/restservice/provision.sh
@@ -19,40 +19,14 @@ function build_rpm() {
     # sudo mv *.rpm $(ls *.rpm | sed 's|_|-|g')
 }
 
-function generate_checksum() {
-    echo "Generating md5 checksum..."
-    cd /tmp/x86_64 && md5sum=$(md5sum *.rpm) && echo $md5sum | sudo tee ${md5sum##* }.md5
-}
-
-function upload_to_s3() {
-    ###
-    # This will upload both the artifact and md5 files to the relevant bucket.
-    # Note that the bucket path is also appended the version.
-    ###
-    echo "Uploading to s3..."
-    sudo pip install s3cmd==1.5.2
-    sudo s3cmd put --force --acl-public --access_key=${AWS_ACCESS_KEY_ID} --secret_key=${AWS_ACCESS_KEY} \
-        --no-preserve --progress --human-readable-sizes --check-md5 *.rpm* s3://${AWS_S3_BUCKET_PATH}/
-}
-
 # VERSION/PRERELEASE/BUILD are exported to follow with our standard of exposing them as env vars. They are not used.
-export VERSION="3.4.0"
-export PRERELEASE="m1"
-export BUILD="390"
 CORE_TAG_NAME="3.4m1"
-PLUGINS_TAG_NAME="1.3.1"
+curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/$CORE_TAG_NAME/common/provision.sh -o ./common-provision.sh &&
+source common-provision.sh
 
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
-AWS_S3_BUCKET_PATH="gigaspaces-repository-eu/org/cloudify3/${VERSION}/${PRERELEASE}"
 
-echo "VERSION: ${VERSION}"
-echo "PRERELEASE: ${PRERELEASE}"
-echo "BUILD: ${BUILD}"
-echo "CORE_TAG_NAME: ${CORE_TAG_NAME}"
-echo "PLUGINS_TAG_NAME: ${PLUGINS_TAG_NAME}"
-echo "AWS_S3_BUCKET_PATH: ${AWS_S3_BUCKET_PATH}"
-
-build_rpm
-generate_checksum
-[ -z ${AWS_ACCESS_KEY} ] || upload_to_s3
+build_rpm &&
+cd /tmp/x86_64 && create_md5 "rpm" &&
+[ -z ${AWS_ACCESS_KEY} ] || upload_to_s3 "rpm" && upload_to_s3 "md5"
