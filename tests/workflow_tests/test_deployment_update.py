69035a7558d4833c2d76854441ab88dbf271fcdd
max-orlov <mxmrlv@gmail.com>
2016-03-07 15:39:04 +0200

    CFY-4964-support-adding-node-with-multiple-instances

--- tests/workflow_tests/test_deployment_update.py
+++ tests/workflow_tests/test_deployment_update.py
@@ -11,14 +11,16 @@
 #  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
+import os
+import shutil
 import time
+import tempfile
 
-from dsl_parser.parser import parse_from_path
 from manager_rest.models import Execution
 from testenv import TestCase
 from testenv.utils import get_resource as resource
 from testenv.utils import deploy_application as deploy
-# from manager_rest.blueprints_manager import get_blueprints_manager
+from testenv.utils import tar_blueprint
 
 
 class TestDeploymentUpdate(TestCase):
@@ -37,62 +39,101 @@ class TestDeploymentUpdate(TestCase):
                 return execution
             time.sleep(3)
 
-    def test_add_node(self):
+    def _test_add_node(self, archive_mode=False):
         """
-        add a node (type exists) which is contained in an existing node
-        - assert that both node and node instance have been created
-        - assert the node/instance relationships have been created
-        - assert the 'update' workflow has been executed and
-          all related operations were executed as well
+        Helper method which can run the test both in archive mode and in
+        blueprint mode
+        :param archive_mode: whether to archive the blueprint before the test
+        :return:
         """
+        estimated_number_of_instance = 3
+
         initial_blueprint_path = \
             resource('dsl/deployment_update/dep_up_initial.yaml')
         deployment, _ = deploy(initial_blueprint_path)
 
         new_blueprint_path = \
             resource('dsl/deployment_update/dep_up_add_node.yaml')
-        blueprint = parse_from_path(new_blueprint_path)
 
-        dep_update = \
-            self.client.deployment_updates.stage(deployment.id,
-                                                 blueprint)
-        self.client.deployment_updates.add(dep_update.id,
-                                           entity_type='node',
-                                           entity_id='site_1')
-        self.client.deployment_updates.commit(dep_update.id)
+        tempdir = tempfile.mkdtemp()
+        try:
+            if archive_mode:
+                tar_path = tar_blueprint(new_blueprint_path, tempdir)
+                dep_update = self.client.deployment_updates. \
+                    stage_archive(deployment.id, tar_path,
+                                  os.path.basename(new_blueprint_path))
+            else:
+                dep_update = \
+                    self.client.deployment_updates.stage(deployment.id,
+                                                         new_blueprint_path)
+            self.client.deployment_updates.add(dep_update.id,
+                                               entity_type='node',
+                                               entity_id='site_1')
+            self.client.deployment_updates.commit(dep_update.id)
 
-        # assert that 'update' workflow was executed
-        executions = self.client.executions.list(deployment_id=deployment.id,
-                                                 workflow_id='update')
-        execution = self._wait_for_execution(executions[0])
-        self.assertEquals('terminated', execution['status'], execution.error)
+            # assert that 'update' workflow was executed
+            executions = \
+                self.client.executions.list(deployment_id=deployment.id,
+                                            workflow_id='update')
+            execution = self._wait_for_execution(executions[0])
+            self.assertEquals('terminated', execution['status'],
+                              execution.error)
 
-        added_nodes = self.client.nodes.list(deployment_id=deployment.id,
-                                             node_id='site_1')
-        added_instances = \
-            self.client.node_instances.list(deployment_id=deployment.id,
-                                            node_id='site_1')
+            added_nodes = self.client.nodes.list(deployment_id=deployment.id,
+                                                 node_id='site_1')
+            added_instances = \
+                self.client.node_instances.list(deployment_id=deployment.id,
+                                                node_id='site_1')
 
-        # assert that node and node instance were added to storage
-        self.assertEquals(1, len(added_nodes))
-        self.assertEquals(1, len(added_instances))
+            # assert that node and node instance were added to storage
+            self.assertEquals(1, len(added_nodes))
+            self.assertEquals(estimated_number_of_instance,
+                              len(added_instances))
 
-        # assert that node has a relationship
-        node = added_nodes[0]
-        self.assertEquals(1, len(node.relationships))
-        self._assert_relationship_exists(node.relationships,
-                                         target='server')
+            # assert that node has a relationship
+            node = added_nodes[0]
+            self.assertEquals(1, len(node.relationships))
+            self._assert_relationship_exists(node.relationships,
+                                             target='server')
 
-        # assert that node instance has a relationship
-        added_instance = added_instances[0]
-        self.assertEquals(1, len(added_instance.relationships))
-        self._assert_relationship_exists(added_instance.relationships,
-                                         target='server')
+            # Checking that each node instance was indeed created with
+            # the proper relationships and ran all of the operations
+            for added_instance in added_instances:
 
-        # assert all operations in 'update' ('install') workflow
-        # are executed by making them increment a runtime property
-        self.assertDictContainsSubset({'ops_counter': '6'},
-                                      added_instance['runtime_properties'])
+                # assert that node instance has a relationship
+                self.assertEquals(1, len(added_instance.relationships))
+                self._assert_relationship_exists(added_instance.relationships,
+                                                 target='server')
+
+                # assert all operations in 'update' ('install') workflow
+                # are executed by making them increment a runtime property
+                self.assertDictContainsSubset(
+                        {'ops_counter': '6'},
+                        added_instance['runtime_properties'])
+        finally:
+            shutil.rmtree(tempdir, ignore_errors=True)
+
+    def test_add_node_bp(self):
+        """
+        add a node (type exists) which is contained in an existing node.
+        adding through the stage blueprint api.
+        - assert that both node and node instance have been created
+        - assert the node/instance relationships have been created
+        - assert the 'update' workflow has been executed and
+          all related operations were executed as well
+        """
+        self._test_add_node()
+
+    def test_add_node_archive(self):
+        """
+        add a node (type exists) which is contained in an existing node
+        adding through the stage archive api.
+        - assert that both node and node instance have been created
+        - assert the node/instance relationships have been created
+        - assert the 'update' workflow has been executed and
+          all related operations were executed as well
+        """
+        self._test_add_node(archive_mode=True)
 
     def _assert_relationship_exists(self, relationships, target,
                                     expected_type=None):
