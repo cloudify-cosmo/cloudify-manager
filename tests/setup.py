acccc917053742c556be83d1914bcaaa6a4d3abd
ran <ran@gigaspaces.com>
2016-06-01 15:53:03 +0300

    modified diamond plugin dependency to 1.3.3 in integration tests

--- tests/setup.py
+++ tests/setup.py
@@ -41,7 +41,7 @@ setup(
         'cloudify-dsl-parser==3.4a5',
         'cloudify-rest-client==3.4a5',
         'cloudify-plugins-common==3.4a5',
-        'cloudify-diamond-plugin==1.3.2',
+        'cloudify-diamond-plugin==1.3.3',
         'cloudify-script-plugin==1.4',
         'pika==0.9.14',
         'elasticsearch==1.6.0',
