e2e7e3572469886f02444b24237cd42048969027
opencm <limor@gigaspaces.com>
2016-07-03 11:39:06 +0300

    Bump version

--- packaging/manager/provision.sh
+++ packaging/manager/provision.sh
@@ -35,7 +35,7 @@ function create_resources_tar() {
 }
 
 
-CORE_TAG_NAME="3.4"
+CORE_TAG_NAME="3.4.1"
 curl https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/$CORE_TAG_NAME/common/provision.sh -o ./common-provision.sh &&
 source common-provision.sh
 
