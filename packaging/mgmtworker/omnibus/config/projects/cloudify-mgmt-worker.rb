f6acb10212da543d998cc1d1fd4dc6a2290c8322
gilzellner <gil.zellner@gmail.com>
2016-07-13 19:50:48 +0300

    fix_zlib_issue_omnibus

--- packaging/mgmtworker/omnibus/config/projects/cloudify-mgmt-worker.rb
+++ packaging/mgmtworker/omnibus/config/projects/cloudify-mgmt-worker.rb
@@ -11,6 +11,7 @@ homepage "http://getcloudify.org/"
 override :cacerts, version: '2015.10.28', source: { md5: '782dcde8f5d53b1b9e888fdf113c42b9' }
 override :pip, version: '8.1.1', source: { md5: '6b86f11841e89c8241d689956ba99ed7' }
 override :setuptools, version: '18.5', source: { md5: '533c868f01169a3085177dffe5e768bb' }
+override :zlib, version: '1.2.8', source: { md5: '44d667c142d7cda120332623eab69f40' , url: 'http://zlib.net/current/zlib-1.2.8.tar.gz'}
 
 install_dir "/opt/mgmtworker"
 
