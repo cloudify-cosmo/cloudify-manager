715bac09a3908a85c7e3cd217d837d990de36b38
opencm <limor@gigaspaces.com>
2016-09-22 12:58:26 +0300

    Bump version

--- tests/integration_tests/resources/plugins/mock-rest-plugin/setup.py
+++ tests/integration_tests/resources/plugins/mock-rest-plugin/setup.py
@@ -35,7 +35,7 @@ class InstallCommand(install):
 
 setup(
     name='mock-rest-plugin',
-    version='4.0a3',
+    version='4.0a4',
     packages=['mock_rest_plugin'],
     cmdclass={
         'install': InstallCommand,
--- workflows/setup.py
+++ workflows/setup.py
@@ -18,14 +18,14 @@ from setuptools import setup
 
 setup(
     name='cloudify-workflows',
-    version='4.0a3',
+    version='4.0a4',
     author='elip',
     author_email='elip@gigaspaces.com',
     packages=['cloudify_system_workflows'],
     license='LICENSE',
     description='Various Cloudify Workflows',
     install_requires=[
-        'cloudify-plugins-common==4.0a3',
+        'cloudify-plugins-common==4.0a4',
         'elasticsearch==1.6.0'
     ]
 )
