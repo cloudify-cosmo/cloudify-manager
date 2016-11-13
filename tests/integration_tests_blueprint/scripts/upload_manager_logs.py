dc9d8ddcb4fdd90b530da0a393624f503e5a864f
Yariv Hamamy <yariv@gigaspaces.com>
2016-11-13 14:13:11 +0200

    CFY-6100 Improve integration tests logs on s3

--- tests/integration_tests_blueprint/scripts/upload_manager_logs.py
+++ tests/integration_tests_blueprint/scripts/upload_manager_logs.py
@@ -1,37 +1,81 @@
 
 import os
 import sys
-import tinys3
+import shutil
+import tempfile
+import subprocess
+
+from manager_rest.utils import mkdirs
 
 
-def main(access_key_id, secret_access_key, bucket, logs_dir, prefix):
+S3_URI = 's3://cloudify-tests-logs/'
+LINK_PREFIX = 'http://cloudify-tests-logs.s3.amazonaws.com/'
+SKIP_FILES = ['journalctl.log']
 
-    s3 = tinys3.Connection(access_key_id, secret_access_key, bucket)
 
+def _make_links_file(build, root_dir, links):
+    for test in links:
+        lines = ['<ul>\n']
+        for link in links[test]:
+            address = LINK_PREFIX + link
+            if address.endswith('.log'):
+                address += '.txt'
+            title = os.path.join(*link.split('/')[3:])
+            lines.append('    <li><a href="{0}">{1}</a></li>\n'.format(
+                address, title))
+        lines.append('</ul>\n')
+        with open(os.path.join(root_dir, build, test, 'links.html'), 'w') as f:
+            f.writelines(lines)
+
+
+def _set_local_dir(target_dir, logs_dir, build):
+    links = {}
     for root, dirs, files in os.walk(logs_dir):
         for log_file in files:
+            if log_file in SKIP_FILES:
+                continue
             abs_path = os.path.join(root, log_file)
-            rel_path = abs_path[len(logs_dir) + 1:].split('/')
-            test_dir = '{0}-{1}-{2}'.format(prefix, rel_path[0], rel_path[1])
-            target = os.path.join('logs', test_dir, *rel_path[2:])
-            if target.endswith('.log'):
-                target += '.txt'
-            with open(abs_path, 'rb') as f:
-                s3.upload(target, f, content_type='text/plain')
+            rel_path = abs_path.replace(logs_dir, '').strip('/').split('/')
+            test_dir = '{0}-{1}-{2}'.format(build, rel_path[0], rel_path[1])
+            rel_target = os.path.join(build, test_dir, *rel_path[2:])
+            abs_target = os.path.join(target_dir, rel_target)
+            if abs_target.endswith('.log'):
+                abs_target += '.txt'
+            mkdirs(os.path.join('/', *abs_target.split('/')[:-1]))
+            shutil.copy(abs_path, abs_target)
+            links.setdefault(test_dir, []).append(rel_target)
+    return links
+
+
+def _upload_to_s3(s3_uri, local_dir):
+    subprocess.check_call([
+        'aws', 's3', 'sync', local_dir, s3_uri, '--content-type', 'text/plain'
+    ])
+
+
+def main(s3_uri, logs_dir, build):
+    tmp_dir = tempfile.mkdtemp()
+    try:
+        _make_links_file(build, tmp_dir,
+                         _set_local_dir(tmp_dir, logs_dir, build))
+        _upload_to_s3(s3_uri, tmp_dir)
+    except BaseException:
+        raise
+    finally:
+        shutil.rmtree(tmp_dir)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 6:
-        print 'Usage: upload_manager_logs.py ' \
-              'AWS_ACCESS_KEY_ID ' \
-              'AWS_SECRET_ACCESS_KEY ' \
-              'S3_BUCKET ' \
-              'LOCAL_LOGS_DIR_PATH ' \
-              'PREFIX_FOR_LOGS_DIR_ON_S3'
+
+    logs_path = os.environ.get('CFY_LOGS_PATH')
+    if not logs_path:
+        print 'The environment variable "CFY_LOGS_PATH" is not specified'
         exit(1)
 
-    main(access_key_id=sys.argv[1],
-         secret_access_key=sys.argv[2],
-         bucket=sys.argv[3],
-         logs_dir=sys.argv[4],
-         prefix=sys.argv[5])
+    if len(sys.argv) < 2:
+        print 'Usage: python upload_manager_logs.py JENKINS_BUILD_NUMBER'
+        exit(1)
+
+    main(s3_uri=S3_URI,
+         logs_dir=os.path.expanduser(logs_path),
+         build=sys.argv[1])
