98a2fb3122df7d0ba677e3292978e894f2080132
limor-gs <limor@gigaspaces.com>
2016-12-11 13:01:41 +0200

    new versioning

--- packaging/manager/provision.sh
+++ packaging/manager/provision.sh
@@ -24,10 +24,10 @@ function create_resources_tar() {
     cd /tmp
     pushd /tmp/cloudify-manager-resources
         echo "Downloading manager component packages..."
-        download_resources '/vagrant/${REPO_NAME}-'${CORE_TAG_NAME}'/packages-urls/manager-packages-blueprint.yaml'
+        download_resources '/vagrant/${REPO}-'${CORE_TAG_NAME}'/packages-urls/manager-packages-blueprint.yaml'
         pushd agents
             echo "Downloading agent packages..."
-            download_resources '/vagrant/${REPO_NAME}-'${CORE_TAG_NAME}'/packages-urls/agent-packages-blueprint.yaml'
+            download_resources '/vagrant/${REPO}-'${CORE_TAG_NAME}'/packages-urls/agent-packages-blueprint.yaml'
         popd
     popd
 
