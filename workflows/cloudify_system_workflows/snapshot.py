5e56dd23fdfee44d1f46c0e5b13a28cb89ff04ff
Javier Collado <javier@gigaspaces.com>
2017-01-30 15:45:41 +0200

    CFY-6333 Add make_zip64_archive implementation
    This is just like shutil._makezipfile with the zip64 extensions enabled.

--- workflows/cloudify_system_workflows/snapshot.py
+++ workflows/cloudify_system_workflows/snapshot.py
@@ -785,3 +785,46 @@ def restore(snapshot_id, recreate_deployments_envs, config, force, timeout,
                         .format(from_version))
     finally:
         shutil.rmtree(tempdir)
+
+
+def make_zip64_archive(zip_filename, directory):
+    """Create zip64 archive that contains all files in a directory.
+
+    zip64 is a set of extensions on top of the zip file format that allows to
+    have files larger than 2GB. This is important in snapshots where the amount
+    of data to backup might be huge.
+
+    Note that `shutil` provides a method to register new formats based on the
+    extension of the target file, since a `.zip` extension is still desired,
+    using such an extension mechanism is not an option to avoid patching the
+    already registered zip format.
+
+    In any case, this function is heavily inspired in stdlib's
+    `shutil._make_zipfile`.
+
+    :param zip_filename: Path to the zip file to be created
+    :type zip_filename: str
+    :path directory: Path to directory where all files to compress are located
+    :type directory: str
+
+    """
+    zip_context_manager = zipfile.ZipFile(
+        zip_filename,
+        'w',
+        compression=zipfile.ZIP_DEFLATED,
+        allowZip64=True,
+    )
+
+    with zip_context_manager as zip_file:
+        path = os.path.normpath(directory)
+        zip_file.write(path)
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for dirname in sorted(dirnames):
+                path = os.path.normpath(os.path.join(dirpath, dirname))
+                zip_file.write(path)
+            for filename in filenames:
+                path = os.path.normpath(os.path.join(dirpath, filename))
+                # Not sure why this check is needed,
+                # but it's in the original stdlib's implementation
+                if os.path.isfile(path):
+                    zip_file.write(path)
