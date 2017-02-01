083aa16b812ec1163a6824801c09f9c8f18a8c9f
Javier Collado <javier.collado@gmail.com>
2017-02-01 21:50:18 +0200

    More compact tox configuration (#709)
    * Use brace expansion to generate env list
    
    * Use factor conditional settings
    
    Note that this requires to separate factors with a dash instead of an
    underscore, so now the environment names are different.
    
    * Update tox environments names in run_tests scripts
    
    The environment names now use a dash instead of an underscore to follow
    tox' syntax for factors

--- run-tests.sh
+++ run-tests.sh
@@ -9,49 +9,49 @@ test_system_workflows()
 test_rest_service_v3_endpoints()
 {
     echo "### Testing rest-service endpoints with V3 client..."
-    pushd rest-service && tox -e clientV3_endpoints && popd
+    pushd rest-service && tox -e clientV3-endpoints && popd
 }
 
 test_rest_service_v3_infrastructure()
 {
     echo "### Testing rest-service infrastructure with V3 client..."
-    pushd rest-service && tox -e clientV3_infrastructure && popd
+    pushd rest-service && tox -e clientV3-infrastructure && popd
 }
 
 test_rest_service_v2_1_endpoints()
 {
     echo "### Testing rest-service endpoints with V2.1 client..."
-    pushd rest-service && tox -e clientV2_1_endpoints && popd
+    pushd rest-service && tox -e clientV2_1-endpoints && popd
 }
 
 test_rest_service_v2_1_infrastructure()
 {
     echo "### Testing rest-service infrastructure with V2.1 client..."
-    pushd rest-service && tox -e clientV2_1_infrastructure && popd
+    pushd rest-service && tox -e clientV2_1-infrastructure && popd
 }
 
 test_rest_service_v2_endpoints()
 {
     echo "### Testing rest-service endpoints with V2 client..."
-    pushd rest-service && tox -e clientV2_endpoints && popd
+    pushd rest-service && tox -e clientV2-endpoints && popd
 }
 
 test_rest_service_v2_infrastructure()
 {
     echo "### Testing rest-service infrastructure with V2 client..."
-    pushd rest-service && tox -e clientV2_infrastructure && popd
+    pushd rest-service && tox -e clientV2-infrastructure && popd
 }
 
 test_rest_service_v1_endpoints()
 {
     echo "### Testing rest-service endpoints V1 client..."
-    pushd rest-service && tox -e clientV1_endpoints && popd
+    pushd rest-service && tox -e clientV1-endpoints && popd
 }
 
 test_rest_service_v1_infrastructure()
 {
     echo "### Testing rest-service infrastructure V1 client..."
-    pushd rest-service && tox -e clientV1_infrastructure && popd
+    pushd rest-service && tox -e clientV1-infrastructure && popd
 }
 
 run_flake8()
