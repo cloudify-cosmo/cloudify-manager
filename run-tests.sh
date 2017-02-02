493039440f742650ee0adaf20f31a6e4a49cf44a
Javier Collado <javier@gigaspaces.com>
2017-02-02 06:12:29 +0200

    Do not hardcode path to tox configuration files

--- run-tests.sh
+++ run-tests.sh
@@ -1,31 +1,34 @@
 #!/bin/bash -e
 
+REST_CONFIG=./rest-service/tox.ini
+WORKFLOWS_CONFIG=./workflows/tox.ini
+
 install_dependencies() {
     echo "### Installing dependencies..."
     case $CIRCLE_NODE_INDEX in
         0)
-            tox -c ./rest-service/tox.ini -e clientV1-endpoints --notest
-            tox -c ./rest-service/tox.ini -e clientV1-infrastructure --notest
+            tox -c $REST_CONFIG -e clientV1-endpoints --notest
+            tox -c $REST_CONFIG -e clientV1-infrastructure --notest
             ;;
         1)
-            tox -c ./rest-service/tox.ini -e clientV2-endpoints --notest
+            tox -c $REST_CONFIG -e clientV2-endpoints --notest
             pip install flake8
             ;;
         2)
-            tox -c ./rest-service/tox.ini -e clientV2-infrastructure --notest
-            tox -c ./workflows/tox.ini --notest
+            tox -c $REST_CONFIG -e clientV2-infrastructure --notest
+            tox -c $WORKFLOWS_CONFIG --notest
             ;;
         3)
-            tox -c ./rest-service/tox.ini -e clientV2_1-endpoints --notest
+            tox -c $REST_CONFIG -e clientV2_1-endpoints --notest
             ;;
         4)
-            tox -c ./rest-service/tox.ini -e clientV2_1-infrastructure --notest
+            tox -c $REST_CONFIG -e clientV2_1-infrastructure --notest
             ;;
         5)
-            tox -c ./rest-service/tox.ini -e clientV3-endpoints --notest
+            tox -c $REST_CONFIG -e clientV3-endpoints --notest
             ;;
         6)
-            tox -c ./rest-service/tox.ini -e clientV3-infrastructure --notest
+            tox -c $REST_CONFIG -e clientV3-infrastructure --notest
             ;;
     esac
 }
@@ -34,28 +37,28 @@ run() {
     echo "### Running tests..."
     case $CIRCLE_NODE_INDEX in
         0)
-            tox -c ./rest-service/tox.ini -e clientV1-endpoints
-            tox -c ./rest-service/tox.ini -e clientV1-infrastructure
+            tox -c $REST_CONFIG -e clientV1-endpoints
+            tox -c $REST_CONFIG -e clientV1-infrastructure
             ;;
         1)
-            tox -c ./rest-service/tox.ini -e clientV2-endpoints
+            tox -c $REST_CONFIG -e clientV2-endpoints
             flake8 plugins/riemann-controller/ workflows/ rest-service/ tests/
             ;;
         2)
-            tox -c ./rest-service/tox.ini -e clientV2-infrastructure
-            tox -c ./workflows/tox.ini
+            tox -c $REST_CONFIG -e clientV2-infrastructure
+            tox -c $WORKFLOWS_CONFIG
             ;;
         3)
-            tox -c ./rest-service/tox.ini -e clientV2_1-endpoints
+            tox -c $REST_CONFIG -e clientV2_1-endpoints
             ;;
         4)
-            tox -c ./rest-service/tox.ini -e clientV2_1-infrastructure
+            tox -c $REST_CONFIG -e clientV2_1-infrastructure
             ;;
         5)
-            tox -c ./rest-service/tox.ini -e clientV3-endpoints
+            tox -c $REST_CONFIG -e clientV3-endpoints
             ;;
         6)
-            tox -c ./rest-service/tox.ini -e clientV3-infrastructure
+            tox -c $REST_CONFIG -e clientV3-infrastructure
             ;;
     esac
 }
