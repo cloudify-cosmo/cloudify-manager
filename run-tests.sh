f98be6fc4a3e86911b647024a03d0bd8024b8674
Javier Collado <javier@gigaspaces.com>
2017-02-23 11:34:41 +0100

    Create tox environments for caching only in first node
    When parallelism is in place, the first node is used as a reference for
    caching. Hence, it make sense to create the tox environments to be
    cached only in that node.

--- run-tests.sh
+++ run-tests.sh
@@ -9,25 +9,13 @@ install_dependencies() {
         0)
             tox -c $REST_CONFIG -e clientV1-endpoints --notest
             tox -c $REST_CONFIG -e clientV1-infrastructure --notest
-            ;;
-        1)
             tox -c $REST_CONFIG -e clientV2-endpoints --notest
             pip install flake8
-            ;;
-        2)
             tox -c $REST_CONFIG -e clientV2-infrastructure --notest
             tox -c $WORKFLOWS_CONFIG --notest
-            ;;
-        3)
             tox -c $REST_CONFIG -e clientV2_1-endpoints --notest
-            ;;
-        4)
             tox -c $REST_CONFIG -e clientV2_1-infrastructure --notest
-            ;;
-        5)
             tox -c $REST_CONFIG -e clientV3-endpoints --notest
-            ;;
-        6)
             tox -c $REST_CONFIG -e clientV3-infrastructure --notest
             ;;
     esac
@@ -36,28 +24,28 @@ install_dependencies() {
 run() {
     echo "### Running tests..."
     case $CIRCLE_NODE_INDEX in
-        0)
+        1)
             tox -c $REST_CONFIG -e clientV1-endpoints
             tox -c $REST_CONFIG -e clientV1-infrastructure
             ;;
-        1)
+        2)
             tox -c $REST_CONFIG -e clientV2-endpoints
             flake8 plugins/riemann-controller/ workflows/ rest-service/ tests/
             ;;
-        2)
+        3)
             tox -c $REST_CONFIG -e clientV2-infrastructure
             tox -c $WORKFLOWS_CONFIG
             ;;
-        3)
+        4)
             tox -c $REST_CONFIG -e clientV2_1-endpoints
             ;;
-        4)
+        5)
             tox -c $REST_CONFIG -e clientV2_1-infrastructure
             ;;
-        5)
+        6)
             tox -c $REST_CONFIG -e clientV3-endpoints
             ;;
-        6)
+        7)
             tox -c $REST_CONFIG -e clientV3-infrastructure
             ;;
     esac
