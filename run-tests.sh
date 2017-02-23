122621295423b978325a821d22d26dad8d6f6c12
Javier Collado <javier@gigaspaces.com>
2017-02-23 12:01:41 +0100

    Run flake8 in first node
    It needs to be executed in the same node where the `pip install flake8`
    is executed in the dependencies section.

--- run-tests.sh
+++ run-tests.sh
@@ -7,10 +7,10 @@ install_dependencies() {
     echo "### Installing dependencies..."
     case $CIRCLE_NODE_INDEX in
         0)
+            pip install flake8
             tox -c $REST_CONFIG -e clientV1-endpoints --notest
             tox -c $REST_CONFIG -e clientV1-infrastructure --notest
             tox -c $REST_CONFIG -e clientV2-endpoints --notest
-            pip install flake8
             tox -c $REST_CONFIG -e clientV2-infrastructure --notest
             tox -c $WORKFLOWS_CONFIG --notest
             tox -c $REST_CONFIG -e clientV2_1-endpoints --notest
@@ -24,13 +24,15 @@ install_dependencies() {
 run() {
     echo "### Running tests..."
     case $CIRCLE_NODE_INDEX in
+        0)
+            flake8 plugins/riemann-controller/ workflows/ rest-service/ tests/
+            ;;
         1)
             tox -c $REST_CONFIG -e clientV1-endpoints
             tox -c $REST_CONFIG -e clientV1-infrastructure
             ;;
         2)
             tox -c $REST_CONFIG -e clientV2-endpoints
-            flake8 plugins/riemann-controller/ workflows/ rest-service/ tests/
             ;;
         3)
             tox -c $REST_CONFIG -e clientV2-infrastructure
