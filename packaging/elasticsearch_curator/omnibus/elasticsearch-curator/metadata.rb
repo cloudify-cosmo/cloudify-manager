9f52aad28774430fdc6cf22c4da0e844728c855f
Idan Moyal <idanmo@gmail.com>
2017-02-26 15:09:53 +0200

    CFY-6092 removed es curator build (#738)

--- packaging/elasticsearch_curator/omnibus/elasticsearch-curator/metadata.rb
+++
@@ -1,2 +0,0 @@
-name "elasticsearch-curator"
-version "1.0.0"
--- packaging/elasticsearch_curator/omnibus/test/integration/cloudify/serverspec/localhost/cloudify_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-if ['centos', 'redhat'].include?(os[:family])
-
-  describe file('/opt/es-curator/embedded') do
-    it { should be_directory }
-    it { should be_readable }
-  end
-end
