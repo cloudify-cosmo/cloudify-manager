d37b7efda53b1fc1fa9e332e26ba1bb6d717ce73
opencm <limor@gigaspaces.com>
2017-03-12 17:00:34 +0200

    Bump version

--- workflows/setup.py
+++ workflows/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-workflows',
-    version='4.0a15',
+    version='4.0rc1',
     author='elip',
     author_email='elip@gigaspaces.com',
     packages=[
@@ -28,7 +28,7 @@ setup(
     license='LICENSE',
     description='Various Cloudify Workflows',
     install_requires=[
-        'cloudify-plugins-common==4.0a15',
+        'cloudify-plugins-common==4.0rc1',
         'elasticsearch==1.6.0'
     ]
 )
