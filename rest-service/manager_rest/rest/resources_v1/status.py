dfa15f6ad77d5798d9a511c2084f14a7b88cac4e
Yariv Hamamy <yariv@gigaspaces.com>
2017-03-20 10:26:14 +0200

    CFY-6560 cfy status return "Cloudify UI" instead of "Cloudify Stage"

--- rest-service/manager_rest/rest/resources_v1/status.py
+++ rest-service/manager_rest/rest/resources_v1/status.py
@@ -65,7 +65,7 @@ class Status(SecuredResource):
                             'cloudify-influxdb.service': 'InfluxDB',
                             'cloudify-rabbitmq.service': 'RabbitMQ',
                             'cloudify-riemann.service': 'Riemann',
-                            'cloudify-stage.service': 'Cloudify UI',
+                            'cloudify-stage.service': 'Cloudify Stage',
                             'logstash.service': 'Logstash',
                             'nginx.service': 'Webserver',
                             'postgresql-9.5.service': 'PostgreSQL'
