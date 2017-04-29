70971ade6d86e0a2e7c9d1b9aa0f5921dc936a90
Matt Wheeler <m@funkyhat.org>
2017-04-29 22:57:58 +0100

    Not using these after all

--- workflows/cloudify_system_workflows/snapshots/constants.py
+++ workflows/cloudify_system_workflows/snapshots/constants.py
@@ -19,7 +19,4 @@ M_SCHEMA_REVISION = 'schema_revision'
 M_HAS_CLOUDIFY_EVENTS = 'has_cloudify_events'
 ARCHIVE_CERT_DIR = 'ssl'
 
-MGMTWORKER_USER = 'mgmtworker'
-MGMTWORKER_GROUP = 'mgmtworker'
-
 SECRET_STORE_AGENT_KEY_PREFIX = 'cfyagent_key__'
