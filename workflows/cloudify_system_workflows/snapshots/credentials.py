f11499f75a87f66406952ba77a6a375db0a7374a
Matt Wheeler <m@funkyhat.org>
2017-05-05 14:21:26 +0100

    Get rid of the evidence

--- workflows/cloudify_system_workflows/snapshots/credentials.py
+++ workflows/cloudify_system_workflows/snapshots/credentials.py
@@ -196,8 +196,6 @@ def restore(tempdir, postgres):
         for orig, replace in replacements.items():
             subprocess.check_call(
                 [
-                    # 'sudo', '-u', 'cfyuser',  # TODO restore sudo when user
-                    # separation gets fixed
                     '/opt/mgmtworker/resources/cloudify/fix_snapshot_ssh_db',
                     tenant, orig, replace,
                 ],
