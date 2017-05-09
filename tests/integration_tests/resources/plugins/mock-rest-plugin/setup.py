647da95934a5695c5bd20abc7a44b6b7f6eed51d
opencm <limor@gigaspaces.com>
2017-05-09 17:46:46 +0300

    Bump version

--- tests/integration_tests/resources/plugins/mock-rest-plugin/setup.py
+++ tests/integration_tests/resources/plugins/mock-rest-plugin/setup.py
@@ -36,7 +36,7 @@ class InstallCommand(install):
 
 setup(
     name='mock-rest-plugin',
-    version='4.0.1',
+    version='4.1a1',
     packages=['mock_rest_plugin'],
     cmdclass={
         'install': InstallCommand,
