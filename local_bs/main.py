aecd2450fe2b53f6240ec387e5f81f703c42950b
Pavel Brodsky <mcouthon@gmail.com>
2017-09-11 19:01:17 +0300

    Create main installation script

---
+++ local_bs/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from pprint import pformat
+
+from .components import cli
+from .components import java
+from .components import nginx
+from .components import sanity
+from .components import manager
+from .components import logstash
+from .components import rabbitmq
+from .components import influxdb
+from .components import mgmtworker
+from .components import postgresql
+from .components import restservice
+from .components import manager_ip_setter
+
+from .components.globals import set_globals
+from .components.validations import validate_machine
+
+from .config import config
+from .logger import get_logger
+
+
+logger = get_logger('Bootstrap')
+
+
+def main():
+    logger.info('Starting Bootstrap')
+    validate_machine()
+    set_globals()
+
+    manager.run()
+    manager_ip_setter.run()
+    # TODO: Fix influxDB
+    # influxdb.run()
+    nginx.run()
+    rabbitmq.run()
+    postgresql.run()
+    java.run()
+    logstash.run()
+    restservice.run()
+    mgmtworker.run()
+    cli.run()
+
+    sanity.run()
+    logger.debug(pformat(config))
+    logger.info('Bootstrap complete!')
+
+
+if __name__ == '__main__':
+    main()
