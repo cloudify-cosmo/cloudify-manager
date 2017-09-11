7b39f90b3e2ae8a58a4fced1c46bda84f11ef6a7
Pavel Brodsky <mcouthon@gmail.com>
2017-09-11 08:33:08 +0300

    Add CLI install

---
+++ local_bs/components/cli/install.py
@@ -0,0 +1,37 @@
+from ..service_names import CLI, MANAGER
+
+from ...config import config
+from ...logger import get_logger
+
+from ...utils import common
+from ...utils.install import yum_install
+
+
+logger = get_logger(CLI)
+
+
+def _install_cli():
+    logger.info('Installing Cloudify CLI...')
+    source_url = config[CLI]['sources']['cli_source_url']
+    yum_install(source_url)
+
+
+def _configure_cli():
+    username = config[MANAGER]['security']['admin_username']
+    password = config[MANAGER]['security']['admin_password']
+
+    cmd = [
+        'cfy', 'profiles', 'use', 'localhost', '-u', username,
+        '-p', password, '-t', 'default_tenant'
+    ]
+    logger.info('Setting CLI for default user...')
+    common.run(cmd)
+
+    logger.info('Setting CLI for root user...')
+    root_cmd = ['sudo', '-u', 'root'] + cmd
+    common.run(root_cmd)
+
+
+def run():
+    _install_cli()
+    _configure_cli()
