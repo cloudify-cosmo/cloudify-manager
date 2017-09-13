ec85fb733373c6b60d68754c7d39cd223ed962b4
Pavel Brodsky <mcouthon@gmail.com>
2017-09-13 06:07:00 +0300

    Renamed project to `cfy_install`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
-    packages=['local_bs'],
+    packages=['cfy_install'],
     license='LICENSE',
     description='Local bootstrap of a cloudify manager',
     entry_points={
