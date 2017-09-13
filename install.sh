30ca291be4a288c975b38a7ae8264f10aaf1eb22
Pavel Brodsky <mcouthon@gmail.com>
2017-09-13 20:04:31 +0300

    chown /opt/cloudify-bootstrap after installing the rpm

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+echo "#############################################################"
+echo "Installing cloudify manager installer..."
+echo "#############################################################"
+
+sudo chown $USER:$USER -R /opt/cloudify-bootstrap
 pushd /opt/cloudify-bootstrap
     sudo python get-pip.py
-    rm -f get-pip.py
     sudo pip install ./cloudify-local-bootstrap
 popd
 
-echo "Cloudify installer is ready."
+echo "#############################################################"
+echo "Cloudify installer is ready!"
 echo "Edit /opt/cloudify-bootstrap/config.json, and run cfy_install"
+echo "#############################################################"
