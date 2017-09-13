f955fba38487820cf389a2086f0194a4db16c1d1
Yariv Hamamy <yariv@gigaspaces.com>
2017-09-13 17:52:56 +0300

    add utils and snapshot restore commands

--- rest-service/manager_rest/admin_cli/helptexts.py
+++ rest-service/manager_rest/admin_cli/helptexts.py
@@ -18,3 +18,23 @@ LDAP_DOMAIN = 'The LDAP domain to be used by the server'
 LDAP_IS_ACTIVE_DIRECTORY = 'Specify whether the LDAP used for authentication' \
                            ' is Active-Directory.'
 LDAP_DN_EXTRA = 'Extra LDAP DN options.'
+RESTORE_SNAPSHOT_EXCLUDE_EXISTING_DEPLOYMENTS = (
+    "Restore without recreating the currently existing deployments"
+)
+FORCE_RESTORE_ON_DIRTY_MANAGER = (
+    "Restore a snapshot on a manager where there are existing blueprints or "
+    "deployments"
+)
+RESTORE_CERTIFICATES = 'Restore the certificates from the snapshot, using ' \
+                       'them to replace the current Manager certificates. ' \
+                       'If the certificates` metadata (I.E: the Manager IP ' \
+                       'address) from the snapshot does not match the ' \
+                       'Manager metadata, the certificates cannot work on ' \
+                       'this Manager and will not be restored. In the event ' \
+                       'that the certificates have been restored, the ' \
+                       'Manager will be automatically rebooted at the end ' \
+                       'of the execution. To avoid automatic reboot, use ' \
+                       'the flag `--no-reboot` (not recommended)'
+NO_REBOOT = 'Do not perform an automatic reboot to the Manager VM after ' \
+            'restoring certificates a from snapshot (not recommended). ' \
+            'Only relevant if the `--restore-certificates` flag was supplied'
--- rest-service/manager_rest/admin_cli/main.py
+++ rest-service/manager_rest/admin_cli/main.py
@@ -1,9 +1,11 @@
 import os
 
+from flask_security import current_user
+
 from manager_rest.config import instance
 
 from . import logger, acfy
-from .commands import agents, cluster, context, ldap, ssl
+from .commands import agents, cluster, context, ldap, ssl, snapshots
 
 
 @acfy.group(name='acfy')
@@ -13,10 +15,11 @@ def _acfy(verbose):
     logger.set_global_verbosity_level(verbose)
     # TODO figure out a better way to pass this config path
     os.environ['MANAGER_REST_CONFIG_PATH'] = '/opt/manager/cloudify-rest.conf'
+    current_user.is_bootstrap_admin = True
     try:
         instance.load_configuration()
     except IOError:
-        pass
+        logger.get_logger().warn('No rest service config file')
 
 
 def _register_commands():
@@ -25,6 +28,7 @@ def _register_commands():
     _acfy.add_command(context.context)
     _acfy.add_command(ldap.ldap)
     _acfy.add_command(ssl.ssl)
+    _acfy.add_command(snapshots.snapshots)
 
 _register_commands()
 logger.configure_loggers()
