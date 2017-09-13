12fdd14283ae2e22cb83a21e4a0eb30a48add2f2
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2017-09-13 12:46:37 +0200

    add `acfy agents list`

--- rest-service/manager_rest/admin_cli/commands/agents.py
+++ rest-service/manager_rest/admin_cli/commands/agents.py
@@ -1,4 +1,8 @@
-from .. import acfy
+from .. import acfy, exceptions
+from manager_rest import celery_client
+
+
+MGMTWORKER_NAME = 'cloudify.management'
 
 
 @acfy.group(name='agents')
@@ -6,3 +10,20 @@ def agents():
     """Handle agents connected to the Manager
     """
     pass
+
+
+@agents.command('list')
+@acfy.pass_logger
+def agents_list(logger):
+    client = celery_client.get_client()
+    try:
+        registered = client.celery.control.inspect().registered()
+    except TypeError:  # in case control, or inspect is None - not connected
+        raise exceptions.CloudifyACliError(
+            'Unable to get connected RabbitMQ clients')
+
+    for name in registered:
+        worker, _, host = name.partition('@')
+        if host == MGMTWORKER_NAME:
+            continue
+        logger.info(host)
