91b3c4f68f1cc4a696adca151f0bd92698325678
Pavel Brodsky <mcouthon@gmail.com>
2017-09-20 16:16:43 +0300

    Create an exceptions module

---
+++ cfy_install/exceptions.py
@@ -0,0 +1,14 @@
+class BootstrapError(StandardError):
+    pass
+
+
+class FileError(BootstrapError):
+    pass
+
+
+class NetworkError(BootstrapError):
+    pass
+
+
+class ValidationError(BootstrapError):
+    pass
