e2cb9fdc5fb55f711a9996eac110fda95d2d2b16
Pavel Brodsky <mcouthon@gmail.com>
2017-09-20 07:54:53 +0300

    Add several commonly used strings as constants

---
+++ cfy_install/components/__init__.py
@@ -0,0 +1,16 @@
+# region Common Strings
+
+SOURCES = 'sources'
+
+SERVICE_USER = 'service_user'
+SERVICE_GROUP = 'service_group'
+
+SCRIPTS = 'scripts'
+CONFIG = 'config'
+
+HOME_DIR = 'home_dir'
+LOG_DIR = 'log_dir'
+
+VENV = 'venv'
+
+# endregion
