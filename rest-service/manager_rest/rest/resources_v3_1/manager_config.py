79e6b33a35f3cd110d3c3cce2668a568c93fa1c2
Omer Duskin <omerd@gigaspaces.com>
2017-10-01 17:16:53 +0300

    CFY-7321 don't require tenant in config api

--- rest-service/manager_rest/rest/resources_v3_1/manager_config.py
+++ rest-service/manager_rest/rest/resources_v3_1/manager_config.py
@@ -12,17 +12,16 @@
 #  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
-import json
 
 from flask import request, current_app
 
 from manager_rest import config
 from manager_rest.rest import rest_decorators
-from manager_rest.security import SecuredResource
 from manager_rest.security.authorization import authorize
+from manager_rest.security import SecuredResourceSkipTenantAuth
 
 
-class ManagerConfig(SecuredResource):
+class ManagerConfig(SecuredResourceSkipTenantAuth):
     @rest_decorators.exceptions_handled
     @authorize('manager_config_get')
     def get(self):
@@ -40,7 +39,7 @@ class ManagerConfig(SecuredResource):
         if not filter_by:
             current_app.logger.debug('Retrieving roles without a filter')
             result['authorization'] = self._authorization_config()
-        return json.dumps(result)
+        return result
 
     @staticmethod
     def _authorization_config():
