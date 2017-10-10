ac9f437b6a3a5d063091806e44419e8a3a11195d
Javier Collado <javier.collado@gmail.com>
2017-10-10 11:59:44 +0200

    CFY-7258. Set 'user' role by default in migration scripts (#997)
    * CFY-7258 Set `user` role by default to every user in a tenant
    
    * CFY-7258 Set `user` role by default to every group in a tenant
    
    * CFY-7258 Set not null constraint for the groups_tenants.role_id column

--- resources/rest-service/cloudify/migrations/versions/7aae863786af_add_role_column_groups_tenants_table.py
+++ resources/rest-service/cloudify/migrations/versions/7aae863786af_add_role_column_groups_tenants_table.py
@@ -34,6 +34,27 @@ def upgrade():
         'groups_tenants',
         ['group_id', 'tenant_id'],
     )
+    # Define tables with just the columns needed
+    # to generate the UPDATE sql expression below
+    groups_tenants = sa.table(
+        'groups_tenants',
+        sa.column('group_id', sa.Integer),
+        sa.column('role_id', sa.Integer),
+    )
+    roles = sa.table(
+        'roles',
+        sa.column('id', sa.Integer),
+        sa.column('name', sa.Text),
+    )
+    # Set 'user' role as the default for every group in a tenant
+    op.execute(
+        groups_tenants.update()
+        .values(role_id=(
+            sa.select([roles.c.id])
+            .where(roles.c.name == 'user')
+        ))
+    )
+    op.alter_column('groups_tenants', 'role_id', nullable=False)
 
 
 def downgrade():
