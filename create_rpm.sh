645a5b7466a0c865182162167237b8cc4610312a
Pavel Brodsky <mcouthon@gmail.com>
2017-10-17 14:36:52 +0300

    CLI package is now part of the single tar So it doesn't need to be downloaded manually when creating an RPM

--- create_rpm.sh
+++ create_rpm.sh
@@ -12,16 +12,10 @@ MANAGER_RESOURCES_DIR="cloudify-manager-resources"
 if [ ${COMMUNITY_EDITION} ]; then
     print_line "Working in Community edition"
 
-    CLI_PACKAGE_URL="${CLOUDIFY_RELEASE_URL}/17.10.5/release/cloudify-17.10.5~community.el6.x86_64.rpm"
-    CLI_PACKAGE="cloudify-cli-17.10.5~community.el6.x86_64.rpm"
-
     MANAGER_RESOURCES_URL="${CLOUDIFY_RELEASE_URL}/17.10.5/release/cloudify-manager-resources_17.10.5-community.tar.gz"
     MANAGER_RESOURCES_TAR="cloudify-manager-resources_17.10.5-community.tar.gz"
 else
     print_line "Working in Premium edition"
-
-    CLI_PACKAGE_URL="${CLOUDIFY_RELEASE_URL}/4.2.0/.dev1-release/cloudify-4.2.0~.dev1.el6.x86_64.rpm"
-    CLI_PACKAGE="cloudify-cli-4.2.0~.dev1.el6.x86_64.rpm"
 
     MANAGER_RESOURCES_URL="${CLOUDIFY_RELEASE_URL}/4.2.0/.dev1-release/cloudify-manager-resources_4.2.0-.dev1.tar.gz"
     MANAGER_RESOURCES_TAR="cloudify-manager-resources_4.2.0-.dev1.tar.gz"
@@ -37,30 +31,23 @@ gem install --no-ri --no-rdoc fpm
 
 mkdir -p cloudify-bootstrap
 
-print_line "Downloading cloudify manager resources tar..."
-curl ${MANAGER_RESOURCES_URL} -o ${MANAGER_RESOURCES_TAR}
+pushd cloudify-bootstrap
 
-# TODO: Remove this when the CLI is a part of the single tar
-print_line "Downloading CLI package..."
-curl ${CLI_PACKAGE_URL} -o ${CLI_PACKAGE}
+    print_line "Downloading cloudify manager resources tar..."
+    curl ${MANAGER_RESOURCES_URL} -o ${MANAGER_RESOURCES_TAR}
 
-print_line "Adding CLI package to single tar..."
-tar -xf ${MANAGER_RESOURCES_TAR}
-mv ${CLI_PACKAGE} ${MANAGER_RESOURCES_DIR}
-tar -czf ${MANAGER_RESOURCES_TAR} ${MANAGER_RESOURCES_DIR}
-rm -rf ${MANAGER_RESOURCES_DIR}
-mv ${MANAGER_RESOURCES_TAR} cloudify-bootstrap
+    print_line "Downloading local bootstrap repo..."
+    curl -L https://github.com/mcouthon/cloudify-local-bootstrap/archive/master.tar.gz | tar xz
 
-print_line "Downloading local bootstrap repo..."
-curl -L https://github.com/mcouthon/cloudify-local-bootstrap/archive/master.tar.gz | tar xz
+    # The root dir inside a Github tarball is in a repo-branch format
+    # (e.g. repo-master), so we move it inside our cloudify-bootstrap folder,
+    # with the correct name
+    mv cloudify-local-bootstrap-* cloudify-local-bootstrap
 
-# The root dir inside a Github tarball is in a repo-branch format
-# (e.g. repo-master), so we move it inside our cloudify-bootstrap folder,
-# with the correct name
-mv cloudify-local-bootstrap-* cloudify-bootstrap/cloudify-local-bootstrap
+    print_line "Getting pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
-print_line "Getting pip..."
-curl https://bootstrap.pypa.io/get-pip.py -o cloudify-bootstrap/get-pip.py
+popd
 
 print_line "Creating rpm..."
 # -s dir: Source is a directory
