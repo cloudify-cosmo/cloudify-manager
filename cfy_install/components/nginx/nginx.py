a1996f0109d1b76d590b7944edcd5ef74dd207d1
Pavel Brodsky <mcouthon@gmail.com>
2017-10-18 11:40:17 +0300

    Rewrite how the certificates are handled during the bootstrap * It is now possible to provide CA and internal certificates/keys   as inputs, as opposed to only the external ones. * Several configurations are supported:   - If both CA cert and key were provided, then internal cert/key will     be generated (unless provided as well)   - If only the CA cert was provided, then generating is impossible, so     the internal cert/key *must* be provided as well   - If neither of the CA cert/key were provided, they will be generated

--- cfy_install/components/nginx/nginx.py
+++ cfy_install/components/nginx/nginx.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, isfile
 from collections import namedtuple
 
 from .. import SOURCES, CONFIG, PRIVATE_IP, PUBLIC_IP, AGENT, SSL_INPUTS
@@ -8,7 +8,7 @@ from ..service_names import NGINX, MANAGER
 from ... import constants
 from ...config import config
 from ...logger import get_logger
-from ...exceptions import ValidationError
+from ...exceptions import ValidationError, InputError
 
 from ...utils import common
 from ...utils import certificates
@@ -22,6 +22,23 @@ CONFIG_PATH = join(constants.COMPONENTS_DIR, NGINX, CONFIG)
 UNIT_OVERRIDE_PATH = '/etc/systemd/system/nginx.service.d'
 
 logger = get_logger(NGINX)
+
+
+def _deploy_cert_and_key(cert, key, cert_dst_path, key_dst_path):
+    cert_path = config[SSL_INPUTS][cert]
+    key_path = config[SSL_INPUTS][key]
+
+    cert_deployed = False
+    key_deployed = False
+
+    if isfile(cert_path):
+        common.copy(cert_path, cert_dst_path)
+        cert_deployed = True
+    if isfile(key_path):
+        common.copy(key_path, key_dst_path)
+        key_deployed = True
+
+    return cert_deployed, key_deployed
 
 
 def _install():
@@ -39,42 +56,129 @@ def _deploy_unit_override():
     )
 
 
-def _generate_internal_certs(internal_rest_host):
-    logger.info('Creating internal certificate...')
+def _generate_internal_certs():
+    logger.info('Generating internal certificate...')
     networks = config[AGENT]['networks']
+    internal_rest_host = config[MANAGER][PRIVATE_IP]
 
     certificates.store_cert_metadata(internal_rest_host, networks)
     cert_ips = [internal_rest_host] + list(networks.values())
     certificates.generate_internal_ssl_cert(
         ips=cert_ips,
-        name=internal_rest_host
+        cn=internal_rest_host
     )
 
 
-def _generate_external_certs(internal_rest_host):
+def _generate_external_certs():
     logger.info('Creating external certificate...')
     external_rest_host = config[MANAGER][PUBLIC_IP]
+    internal_rest_host = config[MANAGER][PRIVATE_IP]
 
-    certificates.deploy_or_generate_external_ssl_cert(
+    certificates.generate_external_ssl_cert(
         ips=[external_rest_host, internal_rest_host],
-        cn=external_rest_host,
-        cert_path=config[SSL_INPUTS]['external_cert_path'],
-        key_path=config[SSL_INPUTS]['external_key_path']
+        cn=external_rest_host
     )
 
 
-def _create_certs():
+def _handle_ca_cert():
+    """
+    The user might provide both the CA key and the CA cert, or just the
+    CA cert, or nothing. It is an error to only provide the CA key.
+    If the user provided nothing, we must generate a CA cert+key.
+    :return: True if there's a CA key available (either passed or generated)
+    """
+    logger.info('Handling CA certificate...')
+    cert_deployed, key_deployed = _deploy_cert_and_key(
+        cert='ca_cert_path',
+        key='ca_key_path',
+        cert_dst_path=constants.CA_CERT_PATH,
+        key_dst_path=constants.CA_KEY_PATH
+    )
+
+    has_ca_key = key_deployed
+
+    if cert_deployed:
+        logger.info('Deployed user provided CA cert')
+    else:
+        if key_deployed:
+            raise InputError('Internal CA key provided, but the internal '
+                             'CA cert was not')
+
+        certificates.generate_ca_cert()
+        has_ca_key = True
+
+    return has_ca_key
+
+
+def _handle_internal_cert(has_ca_key):
+    """
+    The user might provide the internal cert and the internal key, or
+    neither. It is an error to only provide one of them. If the user did not
+    provide the internal cert+key, we must generate it, but we can only
+    generate it if we have a CA key (either provided or generated).
+    So it is an error to provide only the CA cert, and then not provide
+    the internal cert+key.
+    :param has_ca_key: True if there's a CA key available
+    """
+    logger.info('Handling internal certificate...')
+    cert_deployed, key_deployed = _deploy_cert_and_key(
+        cert='internal_cert_path',
+        key='internal_key_path',
+        cert_dst_path=constants.INTERNAL_CERT_PATH,
+        key_dst_path=constants.INTERNAL_KEY_PATH
+    )
+
+    if cert_deployed and key_deployed:
+        logger.info('Deployed user provided internal cert and key')
+    elif not cert_deployed and not key_deployed:
+        if has_ca_key:
+            _generate_internal_certs()
+        else:
+            raise InputError(
+                'The CA key was not provided, and neither were the internal '
+                'certificate and key. Either provide the CA key as input, '
+                'or provide the internal certificate and key as inputs'
+            )
+    else:
+        what_deployed = 'cert' if cert_deployed else 'key'
+        raise InputError(
+            'Either both the internal cert and the internal '
+            'key must be provided, or neither. Only the {0} '
+            'was provided'.format(what_deployed)
+        )
+
+
+def _handle_external_cert():
+    logger.info('Handling internal certificate...')
+    cert_deployed, key_deployed = _deploy_cert_and_key(
+        cert='external_cert_path',
+        key='external_key_path',
+        cert_dst_path=constants.EXTERNAL_CERT_PATH,
+        key_dst_path=constants.EXTERNAL_KEY_PATH
+    )
+
+    if cert_deployed and key_deployed:
+        logger.info('Deployed user provided external cert and key')
+    elif not cert_deployed and not key_deployed:
+        _generate_external_certs()
+    else:
+        what_deployed = 'cert' if cert_deployed else 'key'
+        raise InputError(
+            'Either both the external cert and the internal '
+            'key must be provided, or neither. Only the {0} '
+            'was provided'.format(what_deployed)
+        )
+
+
+def _handle_certs():
     # Needs to be run here, because openssl is required and is
     # installed by nginx
     common.remove(constants.SSL_CERTS_TARGET_DIR)
     common.mkdir(constants.SSL_CERTS_TARGET_DIR)
 
-    logger.info('Creating CA certificate...')
-    certificates.generate_ca_cert()
-
-    internal_rest_host = config[MANAGER][PRIVATE_IP]
-    _generate_internal_certs(internal_rest_host)
-    _generate_external_certs(internal_rest_host)
+    has_ca_key = _handle_ca_cert()
+    _handle_internal_cert(has_ca_key)
+    _handle_external_cert()
 
 
 def _deploy_nginx_config_files():
@@ -169,7 +273,7 @@ def _configure():
     copy_notice(NGINX)
     _deploy_unit_override()
     set_logrotate(NGINX)
-    _create_certs()
+    _handle_certs()
     _deploy_nginx_config_files()
     _start_and_verify_service()
 
