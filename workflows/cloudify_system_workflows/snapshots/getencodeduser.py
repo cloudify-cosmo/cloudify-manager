09af888e8583f5badef666428ec6909e8db953c7
Madda <nobody@nowhere.local>
2017-10-19 16:58:31 +0100

    Make API token regen work with community

--- workflows/cloudify_system_workflows/snapshots/getencodeduser.py
+++ workflows/cloudify_system_workflows/snapshots/getencodeduser.py
@@ -1,12 +1,9 @@
 from manager_rest.storage import idencoder
 
-import click
-
+import argparse
 import os
 
 
-@click.command()
-@click.option('--user_id', help='User ID to encode', type=int, required=True)
 def get_encoded_user_id(user_id):
     print(idencoder.get_encoder().encode(user_id))
 
@@ -16,4 +13,17 @@ if __name__ == '__main__':
     if config_env_var not in os.environ:
         os.environ[config_env_var] = '/opt/manager/rest-security.conf'
 
-    get_encoded_user_id()
+    # Not using click because the rest service doesn't have click in community
+    parser = argparse.ArgumentParser(
+        description='Get encoded form of user ID for API tokens.',
+    )
+    parser.add_argument(
+        '-u', '--user_id',
+        help='User ID to encode',
+        type=int,
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    get_encoded_user_id(args.user_id)
