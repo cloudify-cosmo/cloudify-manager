9126b1a3a1580597e3a991017d8f380daeea5224
Pavel Brodsky <mcouthon@gmail.com>
2017-11-14 15:27:55 +0200

    Allow loading the config separately from its creation This is in order to better catch YAML parse errors (and input errors in the future)

--- cfy_manager/config.py
+++ cfy_manager/config.py
@@ -1,10 +1,10 @@
 import yaml
 import collections
 from os.path import isfile
+from yaml.parser import ParserError
 
+from .exceptions import InputError
 from .constants import USER_CONFIG_PATH, DEFAULT_CONFIG_PATH
-
-_config = None
 
 
 def dict_merge(dct, merge_dct):
@@ -29,25 +29,39 @@ class Config(dict):
     def __init__(self, *args, **kwargs):
         super(Config, self).__init__(*args, **kwargs)
 
-        with open(DEFAULT_CONFIG_PATH, 'r') as f:
-            self.update(yaml.load(f))
+    def _load_defaults_config(self):
+        default_config = self._load_yaml(DEFAULT_CONFIG_PATH)
+        self.update(default_config)
 
+    def _load_user_config(self):
         # Allow `config.yaml` not to exist - this is normal for teardown
         if isfile(USER_CONFIG_PATH):
             # Override any default values with values from config.yaml
-            with open(USER_CONFIG_PATH, 'r') as f:
-                dict_merge(self, yaml.load(f))
+            user_config = self._load_yaml(USER_CONFIG_PATH)
+            dict_merge(self, user_config)
+
+    @staticmethod
+    def _load_yaml(path_to_yaml):
+        with open(path_to_yaml, 'r') as f:
+            try:
+                return yaml.load(f)
+            except ParserError as e:
+                raise InputError(
+                    'User config file {0} is not a properly formatted '
+                    'YAML file:\n{1}'.format(path_to_yaml, e)
+                )
+
+    def load_config(self, inputs=None):
+        self._load_defaults_config()
+        self._load_user_config()
+        self._load_inputs(inputs)
 
     def add_temp_path_to_clean(self, new_path_to_remove):
         paths_to_remove = self.setdefault('temp_paths_to_remove', [])
         paths_to_remove.append(new_path_to_remove)
 
-
-def _get_config():
-    global _config
-    if not _config:
-        _config = Config()
-    return _config
+    def _load_inputs(self, inputs):
+        pass
 
 
-config = _get_config()
+config = Config()
