7f5fde8839f91e8fb21a684f913b39b99169c1f2
opencm <limor@gigaspaces.com>
2017-11-19 12:26:08 +0200

    Bump version

--- packaging/manager/provision.sh
+++ packaging/manager/provision.sh
@@ -40,7 +40,7 @@ function create_resources_tar() {
 
 
 export CORE_TAG_NAME="4.2"
-export CORE_BRANCH="master"
+export CORE_BRANCH="4.2-build"
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
 export REPO=$3
