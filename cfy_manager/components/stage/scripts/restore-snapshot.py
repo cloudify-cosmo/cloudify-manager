d30982e316887195bf0439c65417b43729e8e2f6
Pavel Brodsky <mcouthon@gmail.com>
2017-11-26 12:38:25 +0200

    CFY-7577 Restore stage files to correct location See https://github.com/cloudify-cosmo/cloudify-manager-blueprints/commit/dc91a20a130b134f14ae548e6f3b3244a7fb9dcf

--- cfy_manager/components/stage/scripts/restore-snapshot.py
+++ cfy_manager/components/stage/scripts/restore-snapshot.py
@@ -21,9 +21,11 @@ import argparse
 HOME_DIR = "{{ stage.home_dir }}"
 
 
-def _restore(snapshot_root):
+def _restore(snapshot_root, override=False):
     for folder in ['conf', 'dist/widgets', 'dist/templates']:
         destination = os.path.join(HOME_DIR, folder)
+        if not override:
+            destination = os.path.join(destination, 'from_snapshot')
         if os.path.exists(destination):
             shutil.rmtree(destination)
         shutil.copytree(os.path.join(snapshot_root, folder), destination)
@@ -32,5 +34,10 @@ def _restore(snapshot_root):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('snapshot_root')
+    parser.add_argument(
+        '--override-existing',
+        action='store_true',
+        help='Override the existing stage files with the restored files.',
+    )
     args = parser.parse_args()
-    _restore(args.snapshot_root)
+    _restore(args.snapshot_root, override=args.override_existing)
