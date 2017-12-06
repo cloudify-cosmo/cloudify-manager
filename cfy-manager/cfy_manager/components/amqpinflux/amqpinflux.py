cfa1bf2e4a82a847d64751dff33175232ccc1e35
asdf <tehasdf@gmail.com>
2017-12-06 02:28:36 +0100

    Remove `yum install amqpinflux`

--- cfy-manager/cfy_manager/components/amqpinflux/amqpinflux.py
+++ cfy-manager/cfy_manager/components/amqpinflux/amqpinflux.py
@@ -15,7 +15,7 @@
 
 from os.path import join
 
-from .. import SOURCES, SERVICE_USER, SERVICE_GROUP
+from .. import SERVICE_USER, SERVICE_GROUP
 
 from ..service_names import AMQPINFLUX
 
@@ -24,7 +24,6 @@ from ...logger import get_logger
 
 from ...utils import common
 from ...utils.systemd import systemd
-from ...utils.install import yum_install, yum_remove
 from ...utils.files import remove_files, remove_notice, copy_notice
 from ...utils.users import (create_service_user,
                             delete_service_user,
@@ -35,11 +34,6 @@ logger = get_logger(AMQPINFLUX)
 
 HOME_DIR = join('/opt', AMQPINFLUX)
 AMQPINFLUX_VENV = join(HOME_DIR, 'env')
-
-
-def _install():
-    source_url = config[AMQPINFLUX][SOURCES]['amqpinflux_source_url']
-    yum_install(source_url)
 
 
 def _start_and_verify():
@@ -62,7 +56,6 @@ def _configure():
 
 def install():
     logger.notice('Installing AMQP-Influx...')
-    _install()
     _configure()
     logger.notice('AMQP-Influx successfully installed')
 
@@ -78,7 +71,6 @@ def remove():
     systemd.remove(AMQPINFLUX)
     remove_notice(AMQPINFLUX)
     remove_files([HOME_DIR])
-    yum_remove('cloudify-amqp-influx')
     delete_service_user(AMQPINFLUX)
     delete_group(AMQPINFLUX)
     logger.notice('AMQP-Influx successfully removed')
