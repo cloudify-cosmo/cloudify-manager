039f040f3b127269939c6a05aa836c62a885f00c
Matt Wheeler <m@funkyhat.org>
2017-12-19 21:31:17 +0000

    add upload_to_s3.py

---
+++ packaging/upload_to_s3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+import boto3
+
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+s3 = boto3.resource('s3')
+
+for file in os.listdir(THIS_DIR):
+    if file.endswith(".rpm"):
+        file_path = os.path.join(THIS_DIR, file)
+        md5sum = subprocess.check_output(['md5sum', file_path])
+        md5sum_file = file_path + '.md5'
+        with open(md5sum_file, 'w') as f:
+            f.write(md5sum)
+
+        for name in (file_path, md5sum_file):
+            s3.meta.client.upload_file(
+                name,
+                os.getenv('AWS_S3_BUCKET'),
+                )
