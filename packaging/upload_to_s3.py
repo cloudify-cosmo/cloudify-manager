68f20ff4bb552557c2c2a685c318b299581913b5
asdf <tehasdf@gmail.com>
2017-12-20 23:54:48 +0100

    use boto.client

--- packaging/upload_to_s3.py
+++ packaging/upload_to_s3.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 
 import os
 import subprocess
+from StringIO import StringIO
 
 import boto3
 
@@ -11,23 +12,20 @@ BUCKET = 'cloudify-release-eu'
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 
 for file in os.listdir(THIS_DIR):
-    if file.endswith(".rpm"):
-        file_path = os.path.join(THIS_DIR, file)
-        md5sum = subprocess.check_output(['md5sum', file_path])
-        md5sum_file = file_path + '.md5'
-        with open(md5sum_file, 'w') as f:
-            f.write(md5sum)
-        print('{0}: {1}'.format(file, md5sum))
-        version = subprocess.check_output([
-            'rpm', '-qp', '--queryformat', '%{VERSION}', file_path])
+    if not file.endswith(".rpm"):
+        continue
+    file_path = os.path.join(THIS_DIR, file)
+    version = subprocess.check_output([
+        'rpm', '-qp', '--queryformat', '%{VERSION}', file_path])
+    key = 'cloudify/{version}/build/{name}'.format(version=version, name=file)
+    md5sum = subprocess.check_output(['md5sum', file_path])
 
-        for path in (file_path, md5sum_file):
-            name = os.path.basename(path)
-            key = 'cloudify/{version}/build/{name}'.format(
-                version=version, name=name)
-            s3.meta.client.upload_file(path, BUCKET, key)
-            target_url = 'https://{0}.s3.amazonaws.com/{1}'.format(BUCKET, key)
-            print('uploaded', target_url)
+    s3.upload_file(file_path, BUCKET, key,
+                   ExtraArgs={'ACL': 'public-read'})
+    s3.upload_fileobj(StringIO(md5sum), BUCKET, key + '.md5',
+                      ExtraArgs={'ACL': 'public-read'})
+    target_url = 'https://{0}.s3.amazonaws.com/{1}'.format(BUCKET, key)
+    print('uploaded {0} ({1}): {2}'.format(file, md5sum, target_url))
