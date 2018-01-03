48953de80b2d3ca319d4b3d4b73602ae66a5d9ed
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2018-01-03 18:15:54 +0100

    CFY-7700 Translate plugin yaml urls to local urls (#1120)
    * CFY-7700 Translate plugin yaml urls to local urls
    
    * No need to pass the filename
    
    There's just one yaml file anyway
    
    * Copyright notice
    
    * Handle no import resolver spec
    
    * Tests: remove unused method
    
    * Resolver test: make mock resolver section be a dict
    
    It must be a dict for dsl-parser, and now for app_context.py as well
    There's no reason for it to have ever been a str, but we never
    actually had code that touched it, so it didn't break before
    
    * Resolver tests: only check one known key
    
    * Change resolver url
    
    Now, `plugin:cloudify-openstack-plugin:2.0.1` is the used format
    
    * Throw an error if the yaml is not found
    
    * rename functions to be "plugin_yaml", not "plugin"
    
    * str.replace(.., 1)
    
    Thanks @omerd
    
    * Fix the docstring
    
    * Use / as the version separator
    
    * Add a .strip() for the url
    
    * Better message in case plugin is not found
    
    Instead of the old:
    ```
    An error occurred on the server: 400: Invalid blueprint - Requested `Plugin` with ID `None` was not found
    ```
    we can have a better message:
    ```
    An error occurred on the server: 400: Invalid blueprint - Plugin cloudify-openstack-plugin (version: 1.0.42) not found
    ```
    
    * Use the latest version plugin available

--- rest-service/manager_rest/app_context.py
+++ rest-service/manager_rest/app_context.py
@@ -1,11 +1,37 @@
+#########
+# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+
+import os
+import glob
 from flask import current_app
+from distutils.version import LooseVersion
 
 from dsl_parser import constants
 from dsl_parser import utils as dsl_parser_utils
+from dsl_parser.import_resolver.default_import_resolver import (
+    DefaultImportResolver
+)
 
+from manager_rest import config
 from manager_rest.storage import get_storage_manager
-from manager_rest.constants import PROVIDER_CONTEXT_ID
-from manager_rest.storage.models import ProviderContext
+from manager_rest.constants import (
+    PROVIDER_CONTEXT_ID,
+    FILE_SERVER_PLUGINS_FOLDER
+)
+from manager_rest.manager_exceptions import InvalidPluginError
+from manager_rest.storage.models import ProviderContext, Plugin
 
 
 def get_parser_context(sm=None):
@@ -19,23 +45,73 @@ def get_parser_context(sm=None):
 
 
 def update_parser_context(context):
-    raw_parser_context = _extract_parser_context(context)
-    resolver = dsl_parser_utils.create_import_resolver(
-            raw_parser_context['resolver_section'])
-    validate_definitions_version = raw_parser_context[
-        'validate_definitions_version']
-    current_app.parser_context = {
-        'resolver': resolver,
-        'validate_version': validate_definitions_version
-    }
+    current_app.parser_context = _extract_parser_context(context)
 
 
 def _extract_parser_context(context):
     context = context or {}
     cloudify_section = context.get(constants.CLOUDIFY, {})
+    resolver_section = cloudify_section.get(
+        constants.IMPORT_RESOLVER_KEY) or {}
+    resolver_section.setdefault('implementation',
+                                'manager_rest.app_context:ResolverWithPlugins')
+    resolver = dsl_parser_utils.create_import_resolver(resolver_section)
     return {
-        'resolver_section': cloudify_section.get(
-                constants.IMPORT_RESOLVER_KEY),
-        'validate_definitions_version': cloudify_section.get(
-                constants.VALIDATE_DEFINITIONS_VERSION, True)
+        'resolver': resolver,
+        'validate_version': cloudify_section.get(
+            constants.VALIDATE_DEFINITIONS_VERSION, True)
     }
+
+
+class ResolverWithPlugins(DefaultImportResolver):
+    """A resolver which translates plugin-style urls to file:// urls.
+
+    The URL: `plugin:cloudify-openstack-plugin/2.0.1` will be
+    translated to: `file:///opt/manager/resources/plugins/<id>`, where <id>
+    is the id of the plugin looked up for the current tenant.
+
+    The version is optional
+    """
+    PREFIX = 'plugin:'
+
+    def fetch_import(self, import_url):
+        if self._is_plugin_url(import_url):
+            import_url = self._resolve_plugin_yaml_url(import_url)
+        return super(ResolverWithPlugins, self).fetch_import(import_url)
+
+    def _is_plugin_url(self, import_url):
+        return import_url.startswith(self.PREFIX)
+
+    def _resolve_plugin_yaml_url(self, import_url):
+        parts = import_url.replace(self.PREFIX, '', 1).strip().split('/')
+        name = parts[0]
+        version = parts[1] if len(parts) > 1 else None
+        plugin = self._find_plugin(name, version)
+        return self._make_plugin_yaml_url(plugin)
+
+    def _find_plugin(self, name, version=None):
+        filters = {'package_name': name}
+        if version is not None:
+            filters['package_version'] = version
+        sm = get_storage_manager()
+        plugins = sm.list(Plugin, filters=filters)
+        if not plugins:
+            version_message = ' (version: {0})'.format(version) \
+                if version is not None else ''
+            raise InvalidPluginError(
+                'Plugin {0}{1} not found'.format(name, version_message))
+        return max(plugins,
+                   key=lambda plugin: LooseVersion(plugin.package_version))
+
+    def _make_plugin_yaml_url(self, plugin):
+        plugin_path = os.path.join(
+            config.instance.file_server_root,
+            FILE_SERVER_PLUGINS_FOLDER,
+            plugin.id)
+        yaml_files = glob.glob(os.path.join(plugin_path, '*.yaml'))
+        if len(yaml_files) != 1:
+            raise InvalidPluginError(
+                'Plugin {0}: expected one yaml file, but found {1}'
+                .format(plugin.package_name, len(yaml_files)))
+        filename = os.path.join(plugin_path, yaml_files[0])
+        return 'file://{0}'.format(filename)
