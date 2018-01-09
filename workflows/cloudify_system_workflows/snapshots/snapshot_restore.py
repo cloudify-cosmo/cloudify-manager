7144f08fb752502b991dd0f5baa6476fd85a93e0
idob-cloudify <idob@cloudify.co>
2018-01-09 17:22:59 +0200

    fix-stage-snapshot-restore

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -310,7 +310,7 @@ class SnapshotRestore(object):
         # if this snapshot version is the same as the manager version
         # or from 4.3 onwards we support stage upgrade
         if self._snapshot_version == self._manager_version or \
-            self._snapshot_version >= V_4_3_0:
+           self._snapshot_version >= V_4_3_0:
             stage_restore_override = True
         else:
             stage_restore_override = False
