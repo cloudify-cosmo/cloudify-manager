174d75308be6d5128fae43860732a846b7202254
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2018-01-26 19:55:24 +0100

    CFY-7619 Agents RPM spec (#1136)

---
+++ packaging/agents/copy_packages.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import sys
+
+
+class WrongExtension(Exception):
+    pass
+
+
+def splitext(filename):
+    # not using os.path.splitext as it would return .gz instead of .tar.gz
+    for ext in ".tar.gz", ".exe":
+        if filename.endswith(ext):
+            return ext
+    raise WrongExtension(
+            "Unknown agent format for {0}. "
+            "Must be either tar.gz or exe".format(filename))
+
+
+def normalize_agent_name(filename):
+    return filename.split("_", 1)[0].lower()
+
+
+def normalize_names(directory, target_dir):
+    previous_targets = set()
+    for fn in os.listdir(directory):
+        try:
+            extension = splitext(fn)
+        except WrongExtension:
+            # Ignore files with extensions we don't like
+            continue
+        source = os.path.join(directory, fn)
+        target = os.path.join(target_dir, normalize_agent_name(fn) + extension)
+        print('copying {} to {}'.format(source, target))
+        if target in previous_targets:
+            raise RuntimeError(
+                    'packages normalised to same target path!', target)
+        previous_targets.add(target)
+        shutil.copy(source, target)
+
+
+normalize_names(*sys.argv[1:3])
