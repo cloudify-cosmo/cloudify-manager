d03e8085cd979f7bed60659c7a4ba015744f5273
opencm <limor@gigaspaces.com>
2018-02-01 18:59:23 +0200

    Bump version

--- packaging/restservice/provision.sh
+++ packaging/restservice/provision.sh
@@ -26,7 +26,7 @@ function build_rpm() {
 
 
 # VERSION/PRERELEASE are exported to follow with our standard of exposing them as env vars. They are not used.
-export CORE_TAG_NAME="4.3.dev1"
+export CORE_TAG_NAME="4.3"
 export CORE_BRANCH="master"
 AWS_ACCESS_KEY_ID=$1
 AWS_ACCESS_KEY=$2
--- workflows/setup.py
+++ workflows/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-workflows',
-    version='4.3.dev1',
+    version='4.3',
     author='elip',
     author_email='elip@gigaspaces.com',
     packages=[
@@ -28,7 +28,7 @@ setup(
     license='LICENSE',
     description='Various Cloudify Workflows',
     install_requires=[
-        'cloudify-plugins-common==4.3.dev1',
+        'cloudify-plugins-common==4.3',
         'elasticsearch==1.6.0',
         'retrying==1.3.3',
         'psycopg2==2.7'
