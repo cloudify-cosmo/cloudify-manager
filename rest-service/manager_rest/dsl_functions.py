1dda1e0bc42a91e85c93ef51f9c51e364ed18a06
Pavel Brodsky <mcouthon@gmail.com>
2018-03-06 19:08:49 +0200

    CFYPM-75 Implement new intrinsic function "evaluate_function"

--- rest-service/manager_rest/dsl_functions.py
+++ rest-service/manager_rest/dsl_functions.py
@@ -60,6 +60,60 @@ def get_secret_method():
     return methods['get_secret_method']
 
 
+def _evaluate_function_method(func_path, **kwargs):
+    template = """
+import json
+from tempfile import mkstemp
+
+from {module_path} import {function_name}
+
+fd, filename = mkstemp()
+
+with open(filename, 'w') as f:
+    json.dump({function_name}({kwargs}), f)
+
+print filename
+"""
+    from tempfile import mkstemp
+    from flask import current_app
+    from subprocess import check_output
+    import os
+    import json
+
+    split_path = func_path.split('.')
+    module_path = '.'.join(split_path[:-1])
+    function_name = split_path[-1]
+    kw = ', '.join(
+        '{0}={1}'.format(key, value) for key, value in kwargs.iteritems())
+
+    content = template.format(
+        module_path=module_path,
+        function_name=function_name,
+        kwargs=kw
+    )
+
+    fd, filename = mkstemp(suffix='.py')
+    os.close(fd)
+    with open(filename, 'w') as f:
+        f.write(content)
+
+    try:
+        result = check_output(
+            ['/opt/mgmtworker/env/bin/python', filename]
+        ).strip()
+    except Exception as e:
+        current_app.logger.error(e)
+        raise
+
+    # The output should be the name of the temp file with the json dump of
+    # the result
+    with open(result.strip(), 'r') as f:
+        return_val = json.load(f)
+    os.remove(filename)
+    os.remove(result)
+    return return_val
+
+
 def _get_methods(deployment_id, storage_manager):
     """Retrieve a dict of all the callbacks necessary for function evaluation
     """
@@ -78,9 +132,13 @@ def _get_methods(deployment_id, storage_manager):
     def get_secret(secret_key):
         return storage_manager.get(Secret, secret_key)
 
+    def evaluate_function(function_path, **kwargs):
+        return _evaluate_function_method(function_path, **kwargs)
+
     return dict(
         get_node_instances_method=get_node_instances,
         get_node_instance_method=get_node_instance,
         get_node_method=get_node,
-        get_secret_method=get_secret
+        get_secret_method=get_secret,
+        evaluate_function_method=evaluate_function
     )
