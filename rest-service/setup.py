4e79c1d9106f171d911954af9e352339d6ebd116
asdf <tehasdf@gmail.com>
2018-03-15 17:09:21 +0100

    cy-199

--- rest-service/setup.py
+++ rest-service/setup.py
@@ -29,13 +29,13 @@ install_requires = [
     'gunicorn==18.0',
     'PyYAML==3.10',
     'elasticsearch==1.6.0',
-    'celery==3.1.17',
     'psutil==3.3.0',
     'jsonpickle==0.9.2',
     'wagon[venv]==0.6.3',
     'python-dateutil==2.5.3',
     'voluptuous==0.9.3',
     'toolz==0.8.2',
+    'pika==0.11.2'
     'cryptography==2.1.4',
     'psycopg2-binary==2.7.4'
 ]
