603f17a76fe014728af844c8ce99ee807a8521b3
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2018-03-27 10:10:45 +0200

    fix setup.py

--- rest-service/setup.py
+++ rest-service/setup.py
@@ -35,7 +35,7 @@ install_requires = [
     'python-dateutil==2.5.3',
     'voluptuous==0.9.3',
     'toolz==0.8.2',
-    'pika==0.11.2'
+    'pika==0.11.2',
     'cryptography==2.1.4',
     'psycopg2-binary==2.7.4'
 ]
