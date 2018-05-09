02f7cf815ad9d5c3e1e5280b63cbfdde5c2fdab3
Omer Duskin <omerd@gigaspaces.com>
2018-05-09 11:41:47 +0300

    CY-330 add external authenticators folder

--- workflows/cloudify_system_workflows/snapshots/utils.py
+++ workflows/cloudify_system_workflows/snapshots/utils.py
@@ -73,6 +73,7 @@ def copy_files_between_manager_and_snapshot(archive_root,
         constants.FILE_SERVER_PLUGINS_FOLDER,
         constants.FILE_SERVER_GLOBAL_RESOURCES_FOLDER,
         constants.FILE_SERVER_TENANT_RESOURCES_FOLDER,
+        constants.FILE_SERVER_AUTHENTICATORS_FOLDER
     ]
 
     # To work with cert dir logic for archiving
