4b6ae5873e1cb738003a97236c2776866c72543e
Pavel Brodsky <mcouthon@gmail.com>
2018-05-29 11:38:58 +0300

    Rename mock-wagon-plugin to mock-plugin and move it under plugin_tests

--- tests/integration_tests/resources/dsl/plugin_tests/plugins/mock-plugin/mock_plugin/ops.py
+++ tests/integration_tests/resources/dsl/plugin_tests/plugins/mock-plugin/mock_plugin/ops.py
@@ -12,13 +12,12 @@
 #    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
-# ***************************************************************************/
+# **************************************************************************/
 
-from setuptools import setup
+from cloudify import ctx
+from cloudify.decorators import operation
 
 
-setup(
-    name='mock-wagon-plugin',
-    version='4.0a11',
-    packages=['mock_wagon_plugin'],
-)
+@operation
+def op(**_):
+    ctx.logger.info('Performing OP')
