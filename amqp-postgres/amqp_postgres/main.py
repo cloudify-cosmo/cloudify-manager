7fd7f3bebaf24c110cc584ad93f3bdba50ec093b
Pavel Brodsky <mcouthon@gmail.com>
2018-06-18 10:13:59 +0300

    CY-405 Properly setup logging for AMQP Postgres

--- amqp-postgres/amqp_postgres/main.py
+++ amqp-postgres/amqp_postgres/main.py
@@ -13,11 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ############
+import os
 
 from cloudify.amqp_client import get_client
 
+from manager_rest.app_logging import setup_logger
+from manager_rest.config import instance as config
 from manager_rest.flask_utils import setup_flask_app
-from manager_rest.config import instance as manager_config
 
 from .amqp_consumer import AMQPLogsEventsConsumer
 from .postgres_publisher import DBLogEventPublisher
@@ -25,20 +27,32 @@ from .postgres_publisher import DBLogEventPublisher
 BROKER_PORT_SSL = 5671
 BROKER_PORT_NO_SSL = 5672
 
+DEFAULT_LOG_PATH = '/var/log/cloudify/amqp-postgres/amqp_postgres.log'
+
+
+def _setup_flask_app():
+    app = setup_flask_app()
+    config.load_configuration()
+    config.rest_service_log_path = os.environ.get(
+        'LOG_PATH', DEFAULT_LOG_PATH
+    )
+    setup_logger(app.logger)
+    return app
+
 
 def main():
-    setup_flask_app()
+    app = _setup_flask_app()
 
     port = BROKER_PORT_SSL if \
-        manager_config.amqp_ca_path else BROKER_PORT_NO_SSL
+        config.amqp_ca_path else BROKER_PORT_NO_SSL
     amqp_client = get_client(
-        amqp_host=manager_config.amqp_host,
-        amqp_user=manager_config.amqp_username,
-        amqp_pass=manager_config.amqp_password,
+        amqp_host=config.amqp_host,
+        amqp_user=config.amqp_username,
+        amqp_pass=config.amqp_password,
         amqp_vhost='/',
         amqp_port=port,
-        ssl_enabled=bool(manager_config.amqp_ca_path),
-        ssl_cert_path=manager_config.amqp_ca_path
+        ssl_enabled=bool(config.amqp_ca_path),
+        ssl_cert_path=config.amqp_ca_path
     )
 
     db_publisher = DBLogEventPublisher()
@@ -47,6 +61,8 @@ def main():
     )
 
     amqp_client.add_handler(amqp_consumer)
+
+    app.logger.info('Starting consuming...')
     amqp_client.consume()
 
 
