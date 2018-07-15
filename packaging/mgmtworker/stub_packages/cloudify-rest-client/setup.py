85f8394577ffaee70cdf70450698212c9c88958f
opencm <limor@gigaspaces.com>
2018-07-15 13:42:26 +0300

    Bump version

--- packaging/mgmtworker/stub_packages/cloudify-rest-client/setup.py
+++ packaging/mgmtworker/stub_packages/cloudify-rest-client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-rest-client',
-    version='4.4',
+    version='4.5a1',
     packages=[],
     description='[DEPRECATED] A stub for the old cloudify-rest-client package',
 )
--- packaging/mgmtworker/stub_packages/cloudify-script-plugin/setup.py
+++ packaging/mgmtworker/stub_packages/cloudify-script-plugin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-script-plugin',
-    version='4.4',
+    version='4.5a1',
     packages=[],
     description='[DEPRECATED] A stub for the '
                 'old cloudify-script-plugin package',
