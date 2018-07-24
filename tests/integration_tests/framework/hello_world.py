31eb22356b3ca54558126e87ef6b61d8857d221c
tyacbovi <tyacbovi@gmail.com>
2018-07-24 16:43:10 +0300

    CY-430 : Fixing import error

--- tests/integration_tests/framework/hello_world.py
+++ tests/integration_tests/framework/hello_world.py
@@ -18,7 +18,7 @@ import shutil
 import tarfile
 
 import requests
-from pytest.mark import skip
+from pytest import mark
 
 from integration_tests.framework import utils
 from integration_tests.tests import utils as test_utils
@@ -39,7 +39,7 @@ class _HelloWorld(object):
         self.modify_blueprint_func = modify_blueprint_func
         self.skip_uninstall = skip_uninstall
 
-    @skip("Basic test frame")
+    @mark.skip("Basic test frame")
     def test_hello_world(self):
         blueprint_file = self._prepare_hello_world()
         deployment, _ = self.test_case.deploy_application(
