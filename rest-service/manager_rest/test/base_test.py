f82f492838f3f4a2843d89423f410098704d6c3e
tyacbovi <tyacbovi@gmail.com>
2018-08-29 15:17:28 +0300

    CY-470 : set the default max logs to keep to correct value

--- rest-service/manager_rest/test/base_test.py
+++ rest-service/manager_rest/test/base_test.py
@@ -327,7 +327,7 @@ class BaseServerTestCase(unittest.TestCase):
         test_config.rest_service_log_level = 'DEBUG'
         test_config.rest_service_log_path = self.rest_service_log
         test_config.rest_service_log_file_size_MB = 100,
-        test_config.rest_service_log_files_backup_count = 20
+        test_config.rest_service_log_files_backup_count = 7
         test_config.maintenance_folder = self.maintenance_mode_dir
         test_config.security_hash_salt = 'hash_salt'
         test_config.security_secret_key = 'secret_key'
