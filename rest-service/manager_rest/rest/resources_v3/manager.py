095287c2f93f1e5eca5f50fb35b518145439e347
Yariv Hamamy <yariv@gigaspaces.com>
2018-09-04 17:52:34 +0300

    CY-600 fix ldap status

--- rest-service/manager_rest/rest/resources_v3/manager.py
+++ rest-service/manager_rest/rest/resources_v3/manager.py
@@ -150,7 +150,7 @@ class LdapAuthentication(SecuredResource):
     @rest_decorators.exceptions_handled
     @authorize('ldap_status_get')
     def get(self):
-        return 'enabled' if authenticator.ldap else 'disabled'
+        return 'enabled' if config.instance.ldap_server else 'disabled'
 
     @staticmethod
     def _only_admin_in_manager():
