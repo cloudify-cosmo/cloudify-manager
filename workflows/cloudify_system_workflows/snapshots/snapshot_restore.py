44266f81b31bd3628c8444a86e2dbcbd55255ead
Adi Grabow <adigrabow@gmail.com>
2018-11-11 12:07:45 +0200

    Create REST token with the new security config file and restart REST

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -126,8 +126,10 @@ class SnapshotRestore(object):
             existing_plugins = self._get_existing_plugin_names()
 
             with Postgres(self._config) as postgres:
-                self._restore_db(postgres, schema_revision, stage_revision)
                 self._restore_files_to_manager()
+                self._restore_db(postgres, schema_revision, stage_revision)
+                self._get_new_rest_token()
+                self._restart_rest_service()
                 self._encrypt_secrets(postgres)
                 self._encrypt_rabbitmq_passwords(postgres)
                 self._restore_plugins(existing_plugins)
@@ -147,6 +149,58 @@ class SnapshotRestore(object):
         finally:
             ctx.logger.debug('Removing temp dir: {0}'.format(self._tempdir))
             shutil.rmtree(self._tempdir)
+
+    def _get_new_rest_token(self):
+        """
+        `snapshot restore` is triggered with a REST call that is authenticated
+        using security keys that are located in opt/manager/rest-security.conf.
+        During restore the rest-security.conf is changed, therefore any
+        restart of the REST service will result in authentication failure
+        (security config is loaded when the REST service starts).
+        Gunicorn restarts REST workers every 1000 calls.
+        Our solution:
+        1. At the earliest stage possible create a new valid REST token
+           using the new rest-security.conf file
+        2. Restart REST service
+        3. Continue with restore snapshot
+        (CY-767)
+        """
+        self._get_new_token()
+        new_token = self._get_token_from_file()
+        # Replace old token with new one in the workflow context, and create
+        # new REST client
+        ctx._context['rest_token'] = new_token
+        self._client = get_rest_client()
+
+    def _restart_rest_service(self):
+        restart_command = 'sudo systemctl restart cloudify-restservice'
+        utils.run(restart_command)
+        self._wait_for_rest_to_restart()
+
+    def _wait_for_rest_to_restart(self, timeout=60):
+        deadline = time.time() + timeout
+        while True:
+            time.sleep(0.5)
+            if time.time() > deadline:
+                raise NonRecoverableError(
+                    'Failed to restart cloudify-restservice.')
+            try:
+                self._client.manager.get_status()
+                break
+            except Exception:
+                pass
+
+    def _get_new_token(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        script_path = os.path.join(dir_path, 'get_new_rest_token.py')
+        command = [MANAGER_PYTHON, script_path, self._tempdir]
+        utils.run(command)
+
+    def _get_token_from_file(self):
+        new_token_path = os.path.join(self._tempdir, 'new_token')
+        with open(new_token_path, 'r') as f:
+            new_token = f.read()
+        return new_token
 
     def _should_ignore_plugin_failure(self,
                                       message):
