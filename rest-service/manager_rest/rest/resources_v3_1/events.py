a2a199f000e96212d865cdaf94ac4d3481aa7f26
Pavel Brodsky <mcouthon@gmail.com>
2018-11-15 15:41:22 +0200

    WIP

--- rest-service/manager_rest/rest/resources_v3_1/events.py
+++ rest-service/manager_rest/rest/resources_v3_1/events.py
@@ -15,34 +15,18 @@
 #
 
 from flask_restful_swagger import swagger
-from sqlalchemy import (
-    asc,
-    bindparam,
-    desc,
-    func,
-    literal_column,
-    or_ as sql_or
-)
-from toolz import dicttoolz
-from copy import deepcopy
+from sqlalchemy import literal, null
 
-from manager_rest import manager_exceptions, utils
 from manager_rest.rest import rest_decorators
-from manager_rest.rest.rest_utils import get_json_and_verify_params
 from manager_rest.security import SecuredResource
-from manager_rest.security.authorization import authorize
 from manager_rest.storage import db, get_storage_manager
-from manager_rest.storage.models_states import VisibilityState
+from manager_rest.security.authorization import authorize
 from manager_rest.rest.responses_v2 import ListResponse
 from manager_rest.rest.responses_v3_1 import LogEvent
 from manager_rest.storage.resource_models import (
-    Blueprint,
-    Deployment,
     Execution,
     Event,
     Log,
-    Node,
-    NodeInstance,
 )
 
 
@@ -56,11 +40,6 @@ class Events(SecuredResource):
     """
 
     DEFAULT_SEARCH_SIZE = 10000
-
-    type_map = {
-        'cloudify_event': Event,
-        'cloudify_log': Log
-    }
 
     @swagger.operation(
         responseclass='List[Event]',
@@ -83,27 +62,118 @@ class Events(SecuredResource):
         :rtype: dict(str)
 
         """
+        self.sm = get_storage_manager()
 
-        size = pagination.get('size', self.DEFAULT_SEARCH_SIZE)
-        offset = pagination.get('offset', 0)
+        pagination.setdefault('size', self.DEFAULT_SEARCH_SIZE)
+        pagination.setdefault('offset', 0)
 
         type_filter = filters.pop('type', ['cloudify_event'])
-        items = []
 
-        sm = get_storage_manager()
-        for elem_type in type_filter:
-            items.extend(sm.list(
-                self.type_map[elem_type],
-                include=_include,
-                filters=filters,
-                get_all_results=True
-            ).items)
+        # Only returning events
+        if type_filter == ['cloudify_event']:
+            return self.sm.list(Event, filters=filters, pagination=pagination)
 
-        total = len(items)
+        # Only returning logs
+        if type_filter == ['cloudify_log']:
+            return self.sm.list(Log, filters=filters, pagination=pagination)
 
-        items = sorted(items, key=lambda x: x.timestamp)
-        items = items[offset:offset + size]
+        # Returning both events and logs
+        execution = self._get_execution(filters)
 
+        events_query = self._get_events_query(execution)
+        logs_query = self._get_logs_query(execution)
+
+        query = events_query.union_all(logs_query)
+
+        query = self._sort_query(query, sort)
+
+        results, metadata = self._execute_query(query, pagination)
+        return ListResponse(items=results, metadata=metadata)
+
+    @staticmethod
+    def _get_events_query(execution):
+        events_query_columns = [
+            Event.timestamp.label('timestamp'),
+            Event.reported_timestamp.label('reported_timestamp'),
+            Event.message.label('message'),
+            Event.message_code.label('message_code'),
+            Event.operation.label('operation'),
+            Event.node_id.label('node_instance_id'),
+            Event.source_id.label('source_id'),
+            Event.target_id.label('target_id'),
+            Event.visibility.label('visibility'),
+
+            null().label('level'),
+            null().label('logger'),
+
+            Event.event_type.label('event_type'),
+            Event.error_causes.label('error_causes'),
+
+            literal(execution.id).label('execution_id'),
+            literal(execution.deployment_id).label('deployment_id'),
+            literal(execution.workflow_id).label('workflow_id'),
+
+            literal('cloudify_event').label('type'),
+        ]
+
+        events_query = db.session.query(*events_query_columns)
+        if execution:
+            events_query = events_query.filter(Event.execution == execution)
+
+        return events_query
+
+    @staticmethod
+    def _get_logs_query(execution):
+        logs_query_columns = [
+            Log.timestamp.label('timestamp'),
+            Log.reported_timestamp.label('reported_timestamp'),
+            Log.message.label('message'),
+            Log.message_code.label('message_code'),
+            Log.operation.label('operation'),
+            Log.node_id.label('node_instance_id'),
+            Log.source_id.label('source_id'),
+            Log.target_id.label('target_id'),
+            Log.visibility.label('visibility'),
+
+            Log.level.label('level'),
+            Log.logger.label('logger'),
+
+            null().label('event_type'),
+            null().label('error_causes'),
+
+            literal(execution.id).label('execution_id'),
+            literal(execution.deployment_id).label('deployment_id'),
+            literal(execution.workflow_id).label('workflow_id'),
+
+            literal('cloudify_log').label('type'),
+        ]
+
+        logs_query = db.session.query(*logs_query_columns)
+        if execution:
+            logs_query = logs_query.filter(Log.execution == execution)
+
+        return logs_query
+
+    def _get_execution(self, filters):
+        execution = None
+        execution_id = filters.get('execution_id')
+
+        if execution_id:
+            execution = self.sm.get(Execution, execution_id)
+
+        return execution
+
+    def _sort_query(self, query, sort):
+        if sort:
+            query = self.sm._sort_query(query, None, sort)
+        else:
+            query = query.order_by('reported_timestamp')
+        return query
+
+    def _execute_query(self, query, pagination):
+        results, total, size, offset = self.sm._paginate(query,
+                                                         pagination,
+                                                         get_all_results=True)
         metadata = {
             'pagination': {
                 'size': size,
@@ -111,5 +181,4 @@ class Events(SecuredResource):
                 'total': total
             }
         }
-
-        return ListResponse(items=items, metadata=metadata)
+        return results, metadata
