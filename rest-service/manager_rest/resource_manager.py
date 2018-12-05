a5f648e21ac84c610309fb8496af7fdd6fcd92dd
davidginzbourg <davidginzbourg@gmail.com>
2018-12-05 15:13:03 +0200

    CY-746 Handle new get_input evaluation exceptions (#1552)
    Adds support to the new exceptions that get_input can now raise.

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -989,14 +989,16 @@ class ResourceManager(object):
         try:
             deployment_plan = tasks.prepare_deployment_plan(
                 plan, get_secret_method(), inputs)
-        except parser_exceptions.MissingRequiredInputError, e:
+        except parser_exceptions.MissingRequiredInputError as e:
             raise manager_exceptions.MissingRequiredDeploymentInputError(
                 str(e))
-        except parser_exceptions.UnknownInputError, e:
+        except parser_exceptions.UnknownInputError as e:
             raise manager_exceptions.UnknownDeploymentInputError(str(e))
-        except parser_exceptions.UnknownSecretError, e:
+        except parser_exceptions.InputEvaluationError as e:
+            raise manager_exceptions.DeploymentInputEvaluationError(str(e))
+        except parser_exceptions.UnknownSecretError as e:
             raise manager_exceptions.UnknownDeploymentSecretError(str(e))
-        except parser_exceptions.UnsupportedGetSecretError, e:
+        except parser_exceptions.UnsupportedGetSecretError as e:
             raise manager_exceptions.UnsupportedDeploymentGetSecretError(
                 str(e))
 
