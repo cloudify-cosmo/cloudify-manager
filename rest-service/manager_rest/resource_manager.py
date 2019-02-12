d6c813ae4f6c5c51f6cbb172e14f3f17f545b699
idob-cloudify <33690603+idob-cloudify@users.noreply.github.com>
2019-02-12 08:56:34 +0200

    Update resource_manager.py (#1639)

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -954,7 +954,7 @@ class ResourceManager(object):
             groups=deployment_plan['groups'],
             scaling_groups=deployment_plan['scaling_groups'],
             outputs=deployment_plan['outputs'],
-            capabilities=deployment_plan['capabilities']
+            capabilities=deployment_plan.get('capabilities', {})
         )
 
     def prepare_deployment_nodes_for_storage(self,
