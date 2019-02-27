fc5fadd930bd4786c95f84f4cacdf202fece5ace
tyacbovi <tyacbovi@gmail.com>
2019-02-27 10:30:00 +0200

    Small refactor Remove not needed Move out of init file

--- cloudify_types/cloudify_types/component/component_operations.py
+++ cloudify_types/cloudify_types/component/component_operations.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2018 Cloudify Platform Ltd. All rights reserved
+# Copyright (c) 2017-2019 Cloudify Platform Ltd. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ from cloudify.decorators import operation
 
 
 from . import utils
-from . import DeploymentProxyBase
+from .component import DeploymentProxyBase
 
 
 @operation
