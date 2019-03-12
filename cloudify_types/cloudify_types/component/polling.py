6314451a0180ccb681d50910c4ef458b9a4fb97b
tyacbovi <tyacbovi@gmail.com>
2019-03-12 10:24:48 +0200

    renaming

--- cloudify_types/cloudify_types/component/polling.py
+++ cloudify_types/cloudify_types/component/polling.py
@@ -16,10 +16,17 @@ import time
 
 from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError
-from cloudify_rest_client.exceptions import CloudifyClientError
 
 from .utils import handle_client_exception
 from .constants import POLLING_INTERVAL, PAGINATION_SIZE
+
+PREDEFINED_LOG_LEVELS = {
+    'critical': 50,
+    'error': 40,
+    'warning': 30,
+    'info': 20,
+    'debug': 10
+}
 
 
 def _is_key_found(key, value, items):
@@ -73,6 +80,7 @@ def _fetch_events(client, execution_id, last_event):
     return events, full_count
 
 
+@handle_client_exception('Redirecting logs from the component failed')
 def redirect_logs(client, execution_id):
     count_events = "received_events"
 
@@ -112,15 +120,8 @@ def redirect_logs(client, execution_id):
                     message, event))
 
             level = event.get('level')
-            predefined_levels = {
-                'critical': 50,
-                'error': 40,
-                'warning': 30,
-                'info': 20,
-                'debug': 10
-            }
-            if level in predefined_levels:
-                ctx.logger.log(predefined_levels[level], message)
+            if level in PREDEFINED_LOG_LEVELS:
+                ctx.logger.log(PREDEFINED_LOG_LEVELS[level], message)
             else:
                 ctx.logger.log(20, message)
 
@@ -133,29 +134,34 @@ def redirect_logs(client, execution_id):
     ctx.instance.runtime_properties[count_events][execution_id] = last_event
 
 
-def _is_execution_ended(execution_status):
+def _is_execution_not_ended(execution_status):
     return execution_status not in ('terminated', 'failed', 'cancelled')
 
 
-def is_system_workflows_finished(client, target_deployment_id=None):
+@handle_client_exception('Checking all executions had failed')
+def is_all_executions_finished(client, deployment_id=None):
+    """
+    Checks if all system workflows or given deployment id are finished
+    running (successful not).
+
+    :param client: cloudify http client.
+    :param deployment_id: Optional, will check all it's executions
+    :return: True if all executions had ended
+    """
     offset = 0
 
     while True:
-        try:
-            executions = client.executions.list(
-                include_system_workflows=True,
-                _offset=offset,
-                _size=PAGINATION_SIZE)
-        except CloudifyClientError as ex:
-            raise NonRecoverableError(
-                'Executions list failed {0}.'.format(ex))
+        executions = client.executions.list(
+            include_system_workflows=True,
+            _offset=offset,
+            _size=PAGINATION_SIZE)
 
         for execution in executions:
             execution_status = execution.get('status')
             if (execution.get('is_system_workflow') or
-                    (target_deployment_id and
-                     target_deployment_id == execution.get('deployment_id'))):
-                if _is_execution_ended(execution_status):
+                    (deployment_id and
+                     deployment_id == execution.get('deployment_id'))):
+                if _is_execution_not_ended(execution_status):
                     return False
 
         if (executions.metadata.pagination.total <=
@@ -167,6 +173,7 @@ def is_system_workflows_finished(client, target_deployment_id=None):
     return True
 
 
+@handle_client_exception('Verifying component\' workflow state had failed')
 def is_component_workflow_at_state(client,
                                    dep_id,
                                    state,
@@ -176,21 +183,16 @@ def is_component_workflow_at_state(client,
     execution_get_args = ['status', 'workflow_id',
                           'created_at', 'ended_at', 'id']
 
-    try:
-        execution = client.executions.get(execution_id=execution_id,
-                                          _include=execution_get_args)
-        ctx.logger.debug(
-            'The execution got response for {0} is {1}'.format(dep_id,
-                                                               execution))
-
-    except CloudifyClientError as ex:
-        raise NonRecoverableError(
-            'Executions get failed {0}.'.format(ex))
+    execution = client.executions.get(execution_id=execution_id,
+                                      _include=execution_get_args)
+    ctx.logger.debug(
+        'The execution got response for {0} is {1}'.format(dep_id,
+                                                           execution))
 
     execution_id = execution.get('id')
     if log_redirect and execution_id:
         ctx.logger.debug(
-            'execution info for log_redirect is {0}'.format(execution))
+            'Execution info with log_redirect is {0}'.format(execution))
         redirect_logs(client, execution_id)
 
     execution_status = execution.get('status')
