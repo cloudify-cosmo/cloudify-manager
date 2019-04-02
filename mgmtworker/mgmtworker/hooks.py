5fe16d45e3ba32100379c61e096cd9617448db26
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-04-02 13:18:54 +0200

    Move the rest of mgmtworker-specific code

---
+++ mgmtworker/mgmtworker/hooks.py
@@ -0,0 +1,146 @@
+########
+# Copyright (c) 2019 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############
+
+import os
+import yaml
+import logging
+from distutils.version import StrictVersion
+
+from cloudify.manager import get_rest_client
+from cloudify.utils import get_admin_api_token
+from cloudify.constants import EVENTS_EXCHANGE_NAME
+
+from cloudify_agent.worker import (
+    CloudifyOperationConsumer,
+)
+
+logger = logging.getLogger('mgmtworker')
+
+
+class HookConsumer(CloudifyOperationConsumer):
+    routing_key = 'events.hooks'
+    HOOKS_CONFIG_PATH = '/opt/mgmtworker/config/hooks.conf'
+
+    def __init__(self, queue_name, registry, max_workers=5):
+        super(HookConsumer, self).__init__(queue_name,
+                                           exchange_type='topic',
+                                           registry=registry,
+                                           threadpool_size=max_workers)
+        self.queue = queue_name
+        self.exchange = EVENTS_EXCHANGE_NAME
+
+    def handle_task(self, full_task):
+        event_type = full_task['event_type']
+        hook = self._get_hook(event_type)
+        if not hook:
+            return
+        logger.info(
+            'The hook consumer received `{0}` event and the hook '
+            'implementation is: `{1}`'.format(event_type,
+                                              hook.get('implementation'))
+        )
+
+        try:
+            task = self._get_task(full_task, hook)
+            result = super(HookConsumer, self).handle_task(task)
+        except Exception as e:
+            result = {'ok': False, 'error': e.message}
+            logger.error('{0!r}, while running the hook triggered by the '
+                         'event: {1}'.format(e, event_type))
+        return result
+
+    def _get_hook(self, event_type):
+        if not os.path.exists(self.HOOKS_CONFIG_PATH):
+            logger.warn("The hook consumer received `{0}` event but the "
+                        "hooks config file doesn't exist".format(event_type))
+            return None
+
+        with open(self.HOOKS_CONFIG_PATH) as hooks_conf_file:
+            try:
+                hooks_yaml = yaml.safe_load(hooks_conf_file)
+                hooks_conf = hooks_yaml.get('hooks', {}) if hooks_yaml else {}
+            except yaml.YAMLError:
+                logger.error(
+                    "The hook consumer received `{0}` event but the hook "
+                    "config file is invalid yaml".format(event_type)
+                )
+                return None
+
+        for hook in hooks_conf:
+            if hook.get('event_type') == event_type:
+                return hook
+        logger.info("The hook consumer received `{0}` event but didn't find a "
+                    "compatible hook in the configuration".format(event_type))
+        return None
+
+    def _get_task(self, full_task, hook):
+        hook_context, operation_context = self._get_contexts(
+            full_task,
+            hook['implementation']
+        )
+        task = {
+            'cloudify_task': {
+                'kwargs': {
+                    '__cloudify_context': operation_context
+                },
+                'args': [hook_context]
+            }
+        }
+        kwargs = hook.get('inputs') or {}
+        task['cloudify_task']['kwargs'].update(kwargs)
+        return task
+
+    def _get_contexts(self, full_task, implementation):
+        hook_context = full_task['context']
+        tenant = hook_context.pop('tenant')
+        tenant_name = tenant.get('name')
+        hook_context['tenant_name'] = tenant.get('name')
+        hook_context['event_type'] = full_task['event_type']
+        hook_context['timestamp'] = full_task['timestamp']
+        hook_context['arguments'] = full_task['message']['arguments']
+        operation_context = dict(
+            type='hook',
+            tenant=tenant,
+            no_ctx_kwarg=True,
+            task_target=self.queue,
+            tenant_name=tenant_name,
+            plugin=self._get_plugin(tenant_name, implementation)
+        )
+
+        if operation_context['plugin']:
+            split_task_name = implementation.split('.')[1:]
+            operation_context['task_name'] = '.'.join(split_task_name)
+        else:
+            operation_context['task_name'] = implementation
+        return hook_context, operation_context
+
+    def _get_plugin(self, tenant_name, implementation):
+        package_name = implementation.split('.')[0]
+        filter_plugin = {'package_name': package_name}
+        admin_api_token = get_admin_api_token()
+        rest_client = get_rest_client(tenant=tenant_name,
+                                      api_token=admin_api_token)
+        plugins = rest_client.plugins.list(**filter_plugin)
+        if not plugins:
+            return {}
+
+        plugins.sort(key=lambda p: StrictVersion(p.package_version),
+                     reverse=True)
+        return {
+            'package_name': package_name,
+            'package_version': plugins[0]['package_version'],
+            'visibility': plugins[0]['visibility']
+        }
--- mgmtworker/mgmtworker/worker.py
+++ mgmtworker/mgmtworker/worker.py
@@ -17,26 +17,164 @@
 import argparse
 import logging
 
+from cloudify import dispatch
 from cloudify.logs import setup_agent_logger
+from cloudify.utils import get_admin_api_token
+from cloudify.constants import MGMTWORKER_QUEUE
+from cloudify.models_states import ExecutionState
+from cloudify.state import current_workflow_ctx, workflow_ctx
+from cloudify.manager import get_rest_client, update_execution_status
+from cloudify.amqp_client import AMQPConnection, get_client, SendHandler
 
-from cloudify.amqp_client import AMQPConnection
+from cloudify_rest_client.executions import Execution
+from cloudify_rest_client.exceptions import InvalidExecutionUpdateStatus
+
 from cloudify_agent.worker import (
     ProcessRegistry,
     CloudifyOperationConsumer,
-    CloudifyWorkflowConsumer,
-    ServiceTaskConsumer,
-    HookConsumer,
-    _setup_logger as _setup_cloudify_agent_logger
+    ServiceTaskConsumer
 )
 
+from .hooks import HookConsumer
+
+
 DEFAULT_MAX_WORKERS = 10
-logger = None
+logger = logging.getLogger('mgmtworker')
 
 
-def _setup_logger():
-    global logger
-    setup_agent_logger('mgmtworker')
-    logger = logging.getLogger('mgmtworker')
+class CloudifyWorkflowConsumer(CloudifyOperationConsumer):
+    routing_key = 'workflow'
+    handler = dispatch.WorkflowHandler
+    late_ack = True
+
+    def handle_task(self, full_task):
+        if self.is_scheduled_execution(full_task):
+            task = full_task['cloudify_task']
+            ctx = task['kwargs']['__cloudify_context']
+            self.handle_scheduled_execution(ctx['execution_id'])
+
+            if not self.can_scheduled_execution_start(ctx['execution_id'],
+                                                      ctx['tenant']['name']):
+                # Execution can't currently start running, it has been queued.
+                return
+        return super(CloudifyWorkflowConsumer, self).handle_task(full_task)
+
+    @staticmethod
+    def is_scheduled_execution(full_task):
+        """
+        If a task contains a `dead-letter-exchange` (dlx_id) information it
+        means it was scheduled
+        """
+        return True if full_task.get('dlx_id') else False
+
+    def handle_scheduled_execution(self, execution_id):
+        # This is a scheduled task. It was sent to mgmtworker queue from a
+        # temp queue using a dead-letter-exchange (dlx), need to delete them
+        self.delete_queue(execution_id + '_queue')
+        self.delete_exchange(execution_id)
+
+    @staticmethod
+    def can_scheduled_execution_start(execution_id, tenant):
+        """
+        This method checks whether or not a scheduled execution can currently
+        start running. If it can't - it changes the executions status to
+        QUEUED (so that it will automatically start running when possible)
+        """
+        api_token = get_admin_api_token()
+        tenant_client = get_rest_client(tenant=tenant, api_token=api_token)
+        if tenant_client.executions.should_start(execution_id):
+            return True
+
+        tenant_client.executions.update(execution_id, ExecutionState.QUEUED)
+        return False
+
+
+class MgmtworkerServiceTaskConsumer(ServiceTaskConsumer):
+    """ServiceTaskConsumer with additional mgmtworker-only tasks"""
+
+    service_tasks = ServiceTaskConsumer.service_tasks.copy()
+    service_tasks['cancel-workflow'] = 'cancel_workflow_task'
+
+    def __init__(self, *args, **kwargs):
+        self._workflow_registry = kwargs.pop('workflow_registry')
+        super(MgmtworkerServiceTaskConsumer, self).__init__(*args, **kwargs)
+
+    def cancel_workflow_task(self, execution_id, rest_token, tenant,
+                             execution_token):
+        logger.info('Cancelling workflow {0}'.format(execution_id))
+
+        class CancelCloudifyContext(object):
+            """A CloudifyContext that has just enough data to cancel workflows
+            """
+            def __init__(self):
+                self.tenant = tenant
+                self.tenant_name = tenant['name']
+                self.rest_token = rest_token
+                self.execution_token = execution_token
+
+        with current_workflow_ctx.push(CancelCloudifyContext()):
+            self._workflow_registry.cancel(execution_id)
+            self._cancel_agent_operations(execution_id)
+            try:
+                update_execution_status(execution_id, Execution.CANCELLED)
+            except InvalidExecutionUpdateStatus:
+                # the workflow process might have cleaned up, and marked the
+                # workflow failed or cancelled already
+                logger.info('Failed to update execution status: {0}'
+                            .format(execution_id))
+
+    def _cancel_agent_operations(self, execution_id):
+        """Send a cancel-operation task to all agents for this deployment"""
+        rest_client = get_rest_client()
+        for target in self._get_agents(rest_client, execution_id):
+            self._send_cancel_task(target, execution_id)
+
+    def _send_cancel_task(self, target, execution_id):
+        """Send a cancel-operation task to the agent given by `target`"""
+        message = {
+            'service_task': {
+                'task_name': 'cancel-operation',
+                'kwargs': {'execution_id': execution_id}
+            }
+        }
+        if target == MGMTWORKER_QUEUE:
+            client = get_client()
+        else:
+            tenant = workflow_ctx.tenant
+            client = get_client(
+                amqp_user=tenant['rabbitmq_username'],
+                amqp_pass=tenant['rabbitmq_password'],
+                amqp_vhost=tenant['rabbitmq_vhost']
+            )
+
+        handler = SendHandler(exchange=target, routing_key='service')
+        client.add_handler(handler)
+        with client:
+            handler.publish(message)
+
+    def _get_agents(self, rest_client, execution_id):
+        """Get exchange names for agents related to this execution.
+
+        Note that mgmtworker is related to all executions, since every
+        execution might have a central_deployment_agent operation.
+        """
+        yield MGMTWORKER_QUEUE
+        execution = rest_client.executions.get(execution_id)
+        node_instances = rest_client.node_instances.list(
+            deployment_id=execution.deployment_id,
+            _get_all_results=True)
+        for instance in node_instances:
+            if self._is_agent(instance):
+                yield instance.runtime_properties['cloudify_agent']['queue']
+
+    def _is_agent(self, node_instance):
+        """Does the node_instance have an agent?"""
+        # Compute nodes are hosts, so checking if host_id is the same as id
+        # is a way to check if the node instance is a Compute without
+        # querying for the actual Node
+        is_compute = node_instance.id == node_instance.host_id
+        return (is_compute and
+                'cloudify_agent' in node_instance.runtime_properties)
 
 
 def make_amqp_worker(args):
@@ -47,9 +185,9 @@ def make_amqp_worker(args):
                                   registry=operation_registry),
         CloudifyWorkflowConsumer(args.queue, args.max_workers,
                                  registry=workflow_registry),
-        ServiceTaskConsumer(args.name, args.queue, args.max_workers,
-                            operation_registry=operation_registry,
-                            workflow_registry=workflow_registry),
+        MgmtworkerServiceTaskConsumer(args.name, args.queue, args.max_workers,
+                                      operation_registry=operation_registry,
+                                      workflow_registry=workflow_registry),
     ]
 
     if args.hooks_queue:
@@ -68,8 +206,7 @@ def main():
     parser.add_argument('--hooks-queue')
     args = parser.parse_args()
 
-    _setup_logger()
-    _setup_cloudify_agent_logger('mgmtworker')
+    setup_agent_logger('mgmtworker')
 
     worker = make_amqp_worker(args)
     worker.consume()
