d7b945b0f0d731615a0a9a0547111361e2c68550
adigrabow <adigrabow@gmail.com>
2019-04-08 12:20:08 +0300

    CY-1035 Block 'snapshots' endpoint while there is no license on the Manager (#1725)

--- rest-service/manager_rest/constants.py
+++ rest-service/manager_rest/constants.py
@@ -51,15 +51,11 @@ FILE_SERVER_AUTHENTICATORS_FOLDER = 'authenticators'
 SECURITY_FILE_LOCATION = '/opt/manager/rest-security.conf'
 
 LOCAL_ADDRESS = '127.0.0.1'
-ALLOWED_MAINTENANCE_ENDPOINTS = ['maintenance',
-                                 'snapshots',
-                                 'status',
-                                 'version',
-                                 'license']
-ALLOWED_LICENSE_ENDPOINTS = ALLOWED_MAINTENANCE_ENDPOINTS + ['tokens',
-                                                             'config',
-                                                             'cluster',
-                                                             'tenants']
+ALLOWED_ENDPOINTS = ['status', 'version', 'license']
+ALLOWED_MAINTENANCE_ENDPOINTS = ALLOWED_ENDPOINTS + ['maintenance',
+                                                     'snapshots']
+ALLOWED_LICENSE_ENDPOINTS = ALLOWED_ENDPOINTS + ['tokens', 'config', 'cluster',
+                                                 'tenants']
 CLOUDIFY_AUTH_HEADER = 'Authorization'
 CLOUDIFY_AUTH_TOKEN_HEADER = 'Authentication-Token'
 BASIC_AUTH_PREFIX = 'Basic '
