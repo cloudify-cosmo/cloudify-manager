bff38d2b58236f3c3a64162d506f1203a7c13848
tyacbovi <3802463+tyacbovi@users.noreply.github.com>
2019-05-15 02:34:35 -0700

    CY-1263: Change wf cascading flag on Component-s to true (#1788)
    * Setting cascading flag to default to true
    * Changing default to true

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -647,7 +647,7 @@ class ResourceManager(object):
             wait_after_fail=wait_after_fail,
             scheduled_time=scheduled_time)
 
-        is_cascading_workflow = workflow.get('is_cascading', False)
+        is_cascading_workflow = workflow.get('is_cascading', True)
         if is_cascading_workflow:
             components_dep_ids = self._find_all_components_deployment_id(
                 deployment_id)
