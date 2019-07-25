118237703e2e6eb35f6a63663db7c4e089514815
Yoni Itzhak <yoni@cloudify.co>
2019-07-25 19:02:52 +0300

    optimizing workflow

--- rest-service/manager_rest/rest/resources_v3_1/secrets.py
+++ rest-service/manager_rest/rest/resources_v3_1/secrets.py
@@ -22,12 +22,17 @@ from cloudify.cryptography_utils import (encrypt,
                                          generate_key_using_password)
 
 from manager_rest import manager_exceptions
-from manager_rest.security.authorization import authorize
+from manager_rest.utils import current_tenant
+from manager_rest.flask_utils import get_tenant_by_name
+from manager_rest.security.authorization import (authorize,
+                                                 is_user_action_allowed)
 from manager_rest.storage import models, get_storage_manager
 from manager_rest.resource_manager import (create_secret,
+                                           update_secret,
                                            add_to_dict_values,
-                                           get_resource_manager)
-from manager_rest.security import SecuredResource, authorization
+                                           get_resource_manager,
+                                           update_imported_secret)
+from manager_rest.security import SecuredResource
 from manager_rest.rest import rest_decorators, resources_v3, rest_utils
 
 
@@ -71,7 +76,46 @@ class SecretsKey(resources_v3.SecretsKey):
         Create a new secret or update an existing secret if the flag
         update_if_exists is set to true
         """
-        return create_secret(key=key)
+        secret = self._get_secret_params(key)
+        try:
+            return create_secret(key=key, secret=secret, tenant=current_tenant)
+        except manager_exceptions.ConflictError:
+            if secret['update_if_exists']:
+                existing_secret = get_storage_manager().get(models.Secret, key)
+                return update_secret(existing_secret, secret)
+            raise
+
+    @staticmethod
+    def _get_secret_params(key):
+        rest_utils.validate_inputs({'key': key})
+        request_dict = rest_utils.get_json_and_verify_params({
+            'value': {'type': unicode}
+        })
+        update_if_exists = rest_utils.verify_and_convert_bool(
+            'update_if_exists',
+            request_dict.get('update_if_exists', False),
+        )
+        is_hidden_value = rest_utils.verify_and_convert_bool(
+            'is_hidden_value',
+            request_dict.get('is_hidden_value', False),
+        )
+        visibility_param = rest_utils.get_visibility_parameter(
+            optional=True,
+            valid_values=VisibilityState.STATES,
+        )
+        visibility = get_resource_manager().get_resource_visibility(
+            models.Secret,
+            key,
+            visibility_param
+        )
+
+        secret_params = {
+            'value': request_dict['value'],
+            'update_if_exists': update_if_exists,
+            'visibility': visibility,
+            'is_hidden_value': is_hidden_value
+        }
+        return secret_params
 
 
 class SecretsExport(SecuredResource):
@@ -122,33 +166,18 @@ class SecretsImport(SecuredResource):
     def post(self):
         response = {}
         colliding_secrets = {}
-        overridden_secrets = {}
         request_dict = self._validate_import_secrets_params()
+        tenant_map = request_dict.get('tenant_map')
+        passphrase = request_dict.get('passphrase')
         secrets_list = request_dict.get('secrets_list')
+        override_collisions = request_dict.get('override_collisions')
         existing_tenants = [tenant.name for tenant in
                             get_storage_manager().list(models.Tenant)]
-        tenant_map_dict = request_dict.get('tenant_map_dict')
-        if tenant_map_dict:
-            tenant_map_dict = self._validate_tenant_map(tenant_map_dict,
-                                                        existing_tenants)
-        passphrase = request_dict.get('passphrase')
-        override_collisions = request_dict.get('override_collisions')
-        formed_secrets_list, secrets_errors = self._format_secrets(
-                                                    secrets_list,
-                                                    tenant_map_dict,
-                                                    passphrase,
-                                                    existing_tenants,
-                                                    override_collisions)
-        for secret in formed_secrets_list:
-            collision_state, tenant_name = create_secret(
-                imported_secret=secret)
-            if collision_state == 'overridden':
-                add_to_dict_values(overridden_secrets, tenant_name,
-                                   secret['key'])
-            elif collision_state == 'collision':
-                add_to_dict_values(colliding_secrets, tenant_name,
-                                   secret['key'])
-        response['overridden_secrets'] = overridden_secrets
+        self._validate_tenant_map(tenant_map, existing_tenants)
+        secrets_errors = self._import_secrets(secrets_list, tenant_map,
+                                              passphrase, existing_tenants,
+                                              colliding_secrets,
+                                              override_collisions)
         response['colliding_secrets'] = colliding_secrets
         response['secrets_errors'] = secrets_errors
         return response
@@ -157,165 +186,144 @@ class SecretsImport(SecuredResource):
     def _validate_import_secrets_params():
         request_dict = rest_utils.get_json_and_verify_params({
             'secrets_list': {'type': list, 'optional': False},
-            'tenant_map_dict': {'type': dict, 'optional': True},
+            'tenant_map': {'type': dict, 'optional': True},
             'passphrase': {'type': unicode, 'optional': True},
             'override_collisions': {'type': bool, 'optional': False}
         })
         return request_dict
 
-    @staticmethod
-    def _format_secrets(secrets_list,
-                        tenant_map_dict,
-                        passphrase,
-                        existing_tenants,
-                        override_collisions):
-        secrets_errors_dict = {}
-        formed_secret_list = []
-        key = generate_key_using_password(passphrase) if passphrase else None
+    def _import_secrets(self, secrets_list, tenant_map, passphrase,
+                        existing_tenants, colliding_secrets, override):
+        all_secrets_errors = {}
+        encryption_key = (generate_key_using_password(passphrase) if
+                          passphrase else None)
         for i, secret in enumerate(secrets_list):
-            secret['update_if_exists'] = override_collisions
             secret_errors = {}
-            missing_attributes = SecretsImport.\
-                _validate_secret_attr(secret, secret_errors)
-            if missing_attributes:
-                secret_errors['missing secret attributes'] = missing_attributes
-            valid_encrypt, encrypted = SecretsImport._encrypted_attr(
-                secret_errors, missing_attributes, secret)
-            valid_value = True if 'value' not in secret_errors else False
-            SecretsImport._encrypt_value(valid_value, valid_encrypt,
-                                         encrypted, key, secret_errors, secret)
-            SecretsImport._is_hidden_attr(secret_errors, missing_attributes,
-                                          secret)
-            SecretsImport._tenant_name_attr(secret_errors, missing_attributes,
-                                            secret, tenant_map_dict,
-                                            existing_tenants)
-            SecretsImport._visibility_attr(secret_errors, missing_attributes,
-                                           secret)
+            missing_fields = []
+            self._validate_is_hidden_field(secret, missing_fields,
+                                           secret_errors)
+            self._validate_visibility_field(secret, missing_fields,
+                                            secret_errors)
+            self._handle_secret_tenant(secret, tenant_map, existing_tenants,
+                                       missing_fields, secret_errors)
+            self._handle_encryption(secret, encryption_key, missing_fields,
+                                    secret_errors)
+            if missing_fields:
+                secret_errors['missing secret fields'] = missing_fields
             if secret_errors:
-                secrets_errors_dict[i] = secret_errors
+                all_secrets_errors[i] = secret_errors
             else:
-                formed_secret_list.append(secret)
+                self._import_secret(secret, colliding_secrets, override)
+        return all_secrets_errors
 
-        return formed_secret_list, secrets_errors_dict
+    def _validate_is_hidden_field(self, secret, missing_fields, secret_errors):
+        if self._is_missing_field(secret, 'is_hidden_value', missing_fields):
+            return
+        is_hidden_value = self._validate_boolean(secret, 'is_hidden_value')
+        if is_hidden_value is None:
+            secret_errors['is_hidden_value'] = 'Not boolean'
+        else:
+            secret['is_hidden_value'] = is_hidden_value
 
-    @staticmethod
-    def _validate_tenant_name(secret, tenant_map_dict, existing_tenants):
-        tenant_name_exists = True
-        tenant_name_authorized = True
+    def _validate_visibility_field(self, secret, missing_fields,
+                                   secret_errors):
+        if self._is_missing_field(secret, 'visibility', missing_fields):
+            return
+        visibility = secret['visibility']
+        if visibility not in ['private', 'tenant', 'global']:
+            secret_errors['visibility'] = 'Not a valid visibility'
+
+    def _handle_secret_tenant(self, secret, tenant_map, existing_tenants,
+                              missing_fields, secret_errors):
+        if self._is_missing_field(secret, 'tenant_name', missing_fields):
+            return
         tenant_name = secret['tenant_name']
-        if tenant_map_dict and (tenant_name in tenant_map_dict):
-            tenant_name = tenant_map_dict[tenant_name]
+        if tenant_map and tenant_name in tenant_map:
+            secret['tenant_name'] = tenant_map[tenant_name]
+        self._validate_tenant_action(secret['tenant_name'], existing_tenants,
+                                     secret_errors)
+
+    @staticmethod
+    def _validate_tenant_action(tenant_name, existing_tenants, secret_errors):
+        """
+        Ensure the tenant exists and that the current user is authorized to
+        perform the `secret_create` action
+        """
+
         if tenant_name not in existing_tenants:
-            tenant_name_exists = False
-        if tenant_name_exists and not authorization.is_user_action_allowed(
-                'secret_create', tenant_name):
-            tenant_name_authorized = False
+            secret_errors['tenant_name'] = 'The tenant `{0}` was not' \
+                                           ' found'.format(str(tenant_name))
+        elif not is_user_action_allowed('secret_create', tenant_name):
+            secret_errors['tenant_name'] =\
+                'User `{0}` is not permitted to perform the action ' \
+                '`secrets_create` in tenant: ' \
+                '`{1}`'.format(current_user.username, str(tenant_name))
 
-        return tenant_name_exists, tenant_name_authorized, tenant_name
+    def _handle_encryption(self, secret, encryption_key,
+                           missing_fields, secret_errors):
+        """
+        Asserts 'encrypted' field is boolean, and decrypts the secrets' value
+        when encrypted == True
+        """
+        if (self._is_missing_field(secret, 'encrypted', missing_fields) or
+                self._is_missing_field(secret, 'value', missing_fields)):
+            return
+        is_encrypted = self._validate_boolean(secret, 'encrypted')
+        if is_encrypted is None:
+            secret_errors['encrypted'] = 'Not boolean'
+        elif is_encrypted:
+            self._decrypt_value(secret, encryption_key, secret_errors)
 
     @staticmethod
-    def _validate_bool_attr(secret, attr):
-        valid = True
+    def _decrypt_value(secret, encryption_key, secret_errors):
+        if encryption_key:
+            secret['value'] = decrypt(secret['value'], encryption_key)
+        else:
+            secret_errors['No passphrase'] = 'The secret is encrypted ' \
+                                             'but no passphrase was given'
+
+    def _import_secret(self, secret, colliding_secrets, override_collisions):
         try:
-            attr = rest_utils.verify_and_convert_bool('', secret[attr])
+            tenant = get_tenant_by_name(secret['tenant_name'])
+            create_secret(key=secret['key'], secret=secret, tenant=tenant)
+        except manager_exceptions.ConflictError:
+            if override_collisions:
+                existing_secret = self._get_secret_object(secret)
+                update_imported_secret(existing_secret, secret)
+            add_to_dict_values(colliding_secrets, secret['tenant_name'],
+                               secret['key'])
+
+    @staticmethod
+    def _get_secret_object(secret):
+        return models.Secret.query.filter_by(
+            id=secret['key'],
+            tenant_name=secret['tenant_name']).first()
+
+    @staticmethod
+    def _is_missing_field(secret, field, missing_fields):
+        value = secret[field] if field in secret.keys() else None
+        if (field not in secret.keys() or value is None or
+                (isinstance(value, unicode) and not value.strip())):
+            missing_fields.append(field)
+            return True
+        return False
+
+    @staticmethod
+    def _validate_boolean(secret, field):
+        try:
+            field = rest_utils.verify_and_convert_bool('', secret[field])
         except manager_exceptions.BadParametersError:
-            valid = False
-        return valid, attr
+            return None
+        return field
 
     @staticmethod
-    def _validate_tenant_map(tenant_map, existing_tenants_list):
-        not_existing_tenants = []
-        for origin_tenant, destination_tenant in tenant_map.iteritems():
-            if destination_tenant not in existing_tenants_list:
-                not_existing_tenants.append(destination_tenant)
-            if origin_tenant == destination_tenant:
-                del tenant_map[origin_tenant]
-        not_exist_msg = '{0} tenants do not exist'.format(not_existing_tenants)
-        if not_existing_tenants:
-            raise manager_exceptions.BadParametersError(not_exist_msg)
-        return tenant_map
-
-    @staticmethod
-    def _validate_secret_attr(secret, secret_errors):
-        secret_attributes = ['key', 'value', 'is_hidden_value', 'tenant_name',
-                             'visibility', 'encrypted']
-        missing_attr = rest_utils.verify_dict_params(secret, secret_attributes)
-        for attr in secret_attributes:
-            if attr not in ['is_hidden_value', 'encrypted']:
-                SecretsImport._attr_not_empty(secret_errors, missing_attr,
-                                              secret, attr)
-        return missing_attr
-
-    @staticmethod
-    def _encrypted_attr(secret_errors, missing_attributes, secret):
-        valid_encrypt = False
-        encrypted = False
-        if 'encrypted' not in missing_attributes:
-            valid_encrypt, encrypted = SecretsImport. \
-                _validate_bool_attr(secret, 'encrypted')
-            if not valid_encrypt:
-                secret_errors['encrypted'] = 'Not boolean'
-        return valid_encrypt, encrypted
-
-    @staticmethod
-    def _value_attr(secret_errors, missing_attributes, secret):
-        return SecretsImport._attr_not_empty(secret_errors, missing_attributes,
-                                             secret, 'value')
-
-    @staticmethod
-    def _attr_not_empty(secret_errors, missing_attributes, secret, attr):
-        not_empty_attr = True
-        if attr not in missing_attributes:
-            if (not secret[attr]) or not secret[attr].strip():
-                secret_errors[attr] = 'Empty'
-                not_empty_attr = False
-        return not_empty_attr
-
-    @staticmethod
-    def _encrypt_value(valid_value, valid_encrypt, encrypted, key,
-                       secret_errors, secret):
-        if valid_value and valid_encrypt:
-            if encrypted and key:
-                secret['value'] = decrypt(secret['value'], key)
-            elif encrypted and not key:
-                secret_errors['No passphrase'] = 'The secret is encrypted ' \
-                                                 'but no passphrase was given'
-
-    @staticmethod
-    def _is_hidden_attr(secret_errors, missing_attributes, secret):
-        if 'is_hidden_value' not in missing_attributes:
-            valid_is_hidden_value, is_hidden_value = SecretsImport. \
-                _validate_bool_attr(secret, 'is_hidden_value')
-            if not valid_is_hidden_value:
-                secret_errors['is_hidden_value'] = 'Not boolean'
-            else:
-                secret['is_hidden_value'] = is_hidden_value
-
-    @staticmethod
-    def _tenant_name_attr(secret_errors, missing_attributes, secret,
-                          tenant_map_dict, existing_tenants):
-        if 'tenant_name' not in missing_attributes and ('tenant_name'
-                                                        not in secret_errors):
-            tenant_name_exists, tenant_name_authorized, tenant_name = \
-                SecretsImport._validate_tenant_name(secret,
-                                                    tenant_map_dict,
-                                                    existing_tenants)
-            not_auth_msg = 'User `{0}` is not permitted to perform the' \
-                           ' action secrets_create in the tenant: {1}'.\
-                format(current_user.username, str(tenant_name))
-            not_exist_msg = 'The tenant `{0}` was not found '.\
-                format(str(tenant_name))
-            if tenant_name_authorized and tenant_name_exists:
-                secret['tenant_name'] = tenant_name
-            elif not tenant_name_authorized:
-                secret_errors['tenant_name'] = not_auth_msg
-            elif not tenant_name_exists:
-                secret_errors['tenant_name'] = not_exist_msg
-
-    @staticmethod
-    def _visibility_attr(secret_errors, missing_attributes, secret):
-        if 'visibility' not in missing_attributes and ('visibility' not in
-                                                       secret_errors):
-            visibility = secret['visibility']
-            if visibility not in ['private', 'tenant', 'global']:
-                secret_errors['visibility'] = 'Not a valid visibility'
+    def _validate_tenant_map(tenant_map, existing_tenants):
+        if not tenant_map:
+            return
+        destination_tenants = set(tenant_map.values())
+        missing_tenants = destination_tenants.difference(existing_tenants)
+        if missing_tenants:
+            missing_tenants = [str(tenant) for tenant in missing_tenants]
+            raise manager_exceptions.BadParametersError(
+                'The following destination tenants do not exist on the '
+                'Manager: {0}'.format(missing_tenants))
