baf47a3f5cc6b73a4c0ae40c63c5b31e90bb44d7
Yoni Itzhak <yoni@cloudify.co>
2019-08-14 14:01:56 +0300

    CY-1559 hide credentials in logs (#1899)
    * CY-1559 hide credentials in logs

--- workflows/cloudify_system_workflows/snapshots/postgres.py
+++ workflows/cloudify_system_workflows/snapshots/postgres.py
@@ -49,7 +49,7 @@ class Postgres(object):
     _COMPOSER_TABLES_TO_EXCLUDE = ['"SequelizeMeta"']
 
     def __init__(self, config):
-        ctx.logger.debug('Init Postgres config: {0}'.format(config))
+        self._print_postgres_config(config)
         self._bin_dir = config.postgresql_bin_path
         self._db_name = config.postgresql_db_name
         self._host = config.postgresql_host
@@ -75,7 +75,8 @@ class Postgres(object):
         # Don't change admin user during the restore or the workflow will
         # fail to correctly execute (the admin user update query reverts it
         # to the one from before the restore)
-        self._append_dump(dump_file, self._get_admin_user_update_query())
+        query, protected_query = self._get_admin_user_update_query()
+        self._append_dump(dump_file, query, protected_query)
 
         self._restore_dump(dump_file, self._db_name)
 
@@ -174,13 +175,16 @@ class Postgres(object):
         self._append_dump(dump_file, delete_current_execution_query)
 
     def _get_admin_user_update_query(self):
-        """Return a query that updates the admin user in the DB
+        """Returns a tuple of (query, print_query):
+        query - updates the admin user in the DB and
+        protected_query - hides the credentials for the logs file
         """
         username, password = self._get_admin_credentials()
-        return "UPDATE users " \
-               "SET username='{0}', password='{1}' " \
-               "WHERE id=0;" \
-               .format(username, password)
+        base_query = "UPDATE users " \
+                     "SET username='{0}', password='{1}' " \
+                     "WHERE id=0;"
+        return (base_query.format(username, password),
+                base_query.format('*'*8, '*'*8))
 
     def _get_execution_restore_query(self):
         """Return a query that creates an execution to the DB with the ID (and
@@ -236,7 +240,7 @@ class Postgres(object):
         admin user, the provider context and the current execution
         """
         queries = self._get_clear_tables_queries(preserve_defaults=True)
-        queries.append(self._get_admin_user_update_query())
+        queries.append(self._get_admin_user_update_query()[0])
         if not self.current_execution_date:
             self.init_current_execution_data()
         queries.append(self._get_execution_restore_query())
@@ -336,8 +340,13 @@ class Postgres(object):
         run_shell(command)
 
     @staticmethod
-    def _append_dump(dump_file, query):
-        ctx.logger.debug('Adding to end of dump: {0}'.format(query))
+    def _append_dump(dump_file, query, protected_query=None):
+        """
+        `protected_query` is the same string as `query` only that it hides
+        sensitive information, e.g. username and password.
+        """
+        print_query = protected_query or query
+        ctx.logger.debug('Adding to end of dump: {0}'.format(print_query))
         with open(dump_file, 'a') as f:
             f.write('\n{0}\n'.format(query))
 
@@ -512,3 +521,12 @@ class Postgres(object):
     def restore_license_from_dump(self, tmp_dir):
         dump_file = os.path.join(tmp_dir, LICENSE_DUMP_FILE)
         self._restore_dump(dump_file, self._db_name, table='licenses')
+
+    @staticmethod
+    def _print_postgres_config(config):
+        postgres_password, postgres_username = config.postgresql_password, \
+                                               config.postgresql_username
+        config.postgresql_password = config.postgresql_username = '********'
+        ctx.logger.debug('Init Postgres config: {0}'.format(config))
+        config.postgresql_password, config.postgresql_username = \
+            postgres_password, postgres_username
--- workflows/cloudify_system_workflows/snapshots/utils.py
+++ workflows/cloudify_system_workflows/snapshots/utils.py
@@ -34,20 +34,18 @@ PYTHON_MANAGER_ENV = '/opt/manager/env/bin/python'
 SCHEMA_SCRIPT = '/opt/manager/resources/cloudify/migrations/schema.py'
 
 
-class DictToAttributes(object):
-    def __init__(self, dic):
-        self._dict = dic
+class DictToAttributes(dict):
+    def __init__(self, dictionary):
+        super(DictToAttributes, self).__init__(dictionary)
 
-    def __getattr__(self, name):
-        return self._dict[name]
+    def __getattr__(self, key):
+        return super(DictToAttributes, self).__getitem__(key)
+
+    def __setattr__(self, key, value):
+        super(DictToAttributes, self).__setitem__(key, value)
 
     def __str__(self):
-        try:
-            # try to convert to json,
-            # may fail on UTF-8 and stuff, don't sweat on it..
-            return json.dumps(self._dict)
-        except Exception:
-            return self._dict
+        return json.dumps(self)
 
 
 def copy_files_between_manager_and_snapshot(archive_root,
