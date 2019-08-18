0e07a9af0a4742c3122ba4ac9a01ce72773c8d0e
Isaac Shabtay <isaac@shabtay.com>
2019-08-17 18:04:50 -0700

    CYBL-888: set debug message's level to debug, and actually print the execution ID

--- cloudify_types/cloudify_types/component/polling.py
+++ cloudify_types/cloudify_types/component/polling.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 import time
 
 from cloudify import ctx
@@ -20,14 +21,6 @@ from cloudify.exceptions import NonRecoverableError
 from cloudify_types.utils import handle_client_exception
 
 from .constants import POLLING_INTERVAL, PAGINATION_SIZE, EXECUTIONS_TIMEOUT
-
-PREDEFINED_LOG_LEVELS = {
-    'critical': 50,
-    'error': 40,
-    'warning': 30,
-    'info': 20,
-    'debug': 10
-}
 
 
 def poll_with_timeout(pollster,
@@ -39,7 +32,7 @@ def poll_with_timeout(pollster,
     timeout = float('infinity') if timeout == -1 else timeout
     current_time = time.time()
 
-    ctx.logger.debug('Pooling with timeout {0} seconds'.format(timeout))
+    ctx.logger.debug('Pooling with timeout of {0} seconds'.format(timeout))
 
     while time.time() <= current_time + timeout:
         if pollster() != expected_result:
@@ -104,21 +97,27 @@ def redirect_logs(client, execution_id, instance_ctx=None):
                 event.get('message', "")
             )
             message = message.encode('utf-8')
-
-            ctx.logger.info(
-                'Message {0} for Event {1} for execution_id {1}'.format(
-                    message, event))
-
             level = event.get('level')
-            if level in PREDEFINED_LOG_LEVELS:
-                ctx.logger.log(PREDEFINED_LOG_LEVELS[level], message)
-            else:
-                ctx.logger.log(20, message)
+
+            # If the event dict had a 'level' key, then the value is
+            # a string. In that case, convert it to uppercase and get
+            # the matching Python logging constant.
+            if isinstance(level, basestring):
+                level = logging.getLevelName(level.upper())
+
+            # In the (very) odd case that the level is still not an int
+            # (can happen if the original level value wasn't recognized
+            # by Python's logging library), then use 'INFO'.
+            if not isinstance(level, int):
+                level = logging.INFO
+
+            ctx.logger.log(level, message)
 
         last_event += len(events)
 
         if len(events) == 0:
-            ctx.logger.log(20, "Returned nothing, let's get logs next time.")
+            ctx.logger.info(
+                "Waiting for log messages (execution: %s)...", execution_id)
             break
 
     instance_ctx.runtime_properties[count_events][execution_id] = last_event
@@ -175,7 +174,7 @@ def is_deployment_execution_at_state(client,
 
     if not execution_id:
         raise NonRecoverableError(
-            'Execution id was not found for "{0}" deployment.'.format(
+            'Execution id was not found for deployment "{0}"'.format(
                 dep_id))
 
     execution_get_args = ['status', 'workflow_id',
@@ -184,20 +183,19 @@ def is_deployment_execution_at_state(client,
     execution = client.executions.get(execution_id=execution_id,
                                       _include=execution_get_args)
     ctx.logger.info(
-        'Execution "{0}" of component "{1}" state is {2}'.format(execution_id,
-                                                                 dep_id,
-                                                                 execution))
+        'Execution "%s" of component "%s" state is %s',
+        execution_id, dep_id, execution)
 
     if log_redirect:
         ctx.logger.debug(
-            'Execution info with log_redirect is {0}'.format(execution))
+            'Execution info with log_redirect is %s', execution)
         redirect_logs(client, execution_id, instance_ctx)
 
     execution_status = execution.get('status')
     if execution_status == state:
         ctx.logger.debug(
             'The status for execution'
-            ' "{0}" is {1}.'.format(execution_id, state))
+            ' "%s" is %s', execution_id, state)
 
         return True
     elif execution_status == 'failed':
@@ -226,8 +224,7 @@ def verify_execution_state(client,
         'instance_ctx': instance_ctx
     }
 
-    ctx.logger.debug('Polling execution state with: {0}'.format(
-        pollster_args))
+    ctx.logger.debug('Polling execution state with: %s', pollster_args)
     result = poll_with_timeout(
         lambda: is_deployment_execution_at_state(**pollster_args),
         timeout=timeout,
--- cloudify_types/cloudify_types/component/tests/base_test_suite.py
+++ cloudify_types/cloudify_types/component/tests/base_test_suite.py
@@ -48,6 +48,7 @@ class ComponentTestBase(testtools.TestCase):
         super(ComponentTestBase, self).setUp()
         self._ctx = self.get_mock_ctx('test', COMPONENT_PROPS)
         self._ctx.logger.log = mock.MagicMock(return_value=None)
+        self._ctx.logger.info = mock.MagicMock(return_value=None)
         current_ctx.set(self._ctx)
         self.cfy_mock_client = MockCloudifyRestClient()
 
