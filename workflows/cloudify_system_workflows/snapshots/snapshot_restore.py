b02a18c04687a0d2a028c11431fb562631294917
Madda <nobody@nowhere.local>
2019-08-26 10:05:01 +0000

    CY-1650 Only restore license if there isn't one

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -530,12 +530,17 @@ class SnapshotRestore(object):
                     ctx.tenant_name,
                 )
         finally:
-            postgres.restore_license_from_dump(self._tempdir)
+            if not self._license_exists(postgres):
+                postgres.restore_license_from_dump(self._tempdir)
 
         ctx.logger.info('Successfully restored database')
         # This is returned so that we can decide whether to restore the admin
         # user depending on whether we have the hash salt
         return admin_user_update_command
+
+    def _license_exists(self, postgres):
+        result = postgres.run_query('SELECT * FROM licenses;')
+        return False if '0' in result['status'] else True
 
     def _encrypt_secrets(self, postgres):
         # The secrets are encrypted
