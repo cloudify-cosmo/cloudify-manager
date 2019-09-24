7ca912267c9492ff6154561b868002a2a119759e
davidginzbourg <davidginzbourg@gmail.com>
2019-09-24 11:21:45 -0700

    CY-1632 Fix wagon files not being deleted after plugin installation fails (#1933)
    - Note: in the
    tests/integration_tests/tests/agentless_tests/test_plugins.py
    test_installing_corrupted_plugin_doesnt_affect_system_integrity test;
    the wagon file count assertion takes into account the fact that
    wagon==0.6.3 leaves behind files at /tmp when executing
    wagon.get_source(..), in some cases where the wagon file is corrupted
    (e.g. missing its main directories).

--- tests/integration_tests/tests/agentless_tests/test_plugins.py
+++ tests/integration_tests/tests/agentless_tests/test_plugins.py
@@ -13,13 +13,17 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
+from cloudify.plugins.install_utils import INSTALLING_PREFIX
+
 from cloudify_rest_client.exceptions import CloudifyClientError
 
-from integration_tests.tests import utils as test_utils
 from integration_tests import AgentlessTestCase
+from integration_tests.tests import utils as test_utils
 
-TEST_PACKAGE_NAME = 'cloudify-script-plugin'
-TEST_PACKAGE_VERSION = '1.2'
+TEST_PACKAGE_NAME = 'cloudify-aria-plugin'
+TEST_PACKAGE_VERSION = '1.1'
+TEST_PACKAGE2_NAME = 'cloudify-diamond-plugin'
+TEST_PACKAGE2_VERSION = '1.3'
 
 
 class TestPlugins(AgentlessTestCase):
@@ -30,6 +34,13 @@ class TestPlugins(AgentlessTestCase):
             put_plugin_response = test_utils.upload_mock_plugin(
                     TEST_PACKAGE_NAME,
                     TEST_PACKAGE_VERSION)
+            execution = self._get_latest_execution('install_plugin')
+            self.wait_for_execution_to_end(execution)
+            put_plugin_response['archive_name'] = \
+                put_plugin_response['archive_name'][len(INSTALLING_PREFIX):]
+
+            self.logger.info('Plugin test execution ID is '
+                             '{0}.'.format(execution.id))
             plugin_id = put_plugin_response.get('id')
             self.assertIsNotNone(plugin_id)
             self.assertEquals(put_plugin_response.get('package_name'),
@@ -83,28 +94,77 @@ class TestPlugins(AgentlessTestCase):
             )
             self.assertEquals(404, e.status_code)
 
-    def _get_execution(self, workflow_id):
-        executions = self.client.executions.list(include_system_workflows=True)
-        ex_list = [e for e in executions if e.workflow_id == workflow_id]
-        self.assertEqual(len(ex_list), 1,
-                         msg='Expected to find 1 execution with workflow_id '
-                             '`{workflow_id}`, but found: '
-                             '{ex_list}'.format(
-                             workflow_id=workflow_id, ex_list=ex_list)
-                         )
-        return ex_list[0]
-
     def test_install_uninstall_workflows_execution(self):
         test_utils.upload_mock_plugin(TEST_PACKAGE_NAME, TEST_PACKAGE_VERSION)
 
-        ex = self._get_execution('install_plugin')
-        self.wait_for_execution_to_end(ex)
+        execution = self._get_latest_execution('install_plugin')
+        self.wait_for_execution_to_end(execution)
 
         plugin = self.client.plugins.list()[0]
         self.client.plugins.delete(plugin.id)
 
-        ex = self._get_execution('uninstall_plugin')
-        self.wait_for_execution_to_end(ex)
+        execution = self._get_latest_execution('uninstall_plugin')
+        self.wait_for_execution_to_end(execution)
 
         plugins = self.client.plugins.list()
         self.assertEqual(len(plugins), 0)
+
+
+class TestPluginsSystemState(AgentlessTestCase):
+    def test_installing_corrupted_plugin_doesnt_affect_system_integrity(self):
+        self._upload_plugin_and_assert_values(TEST_PACKAGE_NAME,
+                                              TEST_PACKAGE_VERSION,
+                                              plugins_count=0,
+                                              wagon_files_count=1,
+                                              corrupt_plugin=True)
+        plugin = self._upload_plugin_and_assert_values(TEST_PACKAGE_NAME,
+                                                       TEST_PACKAGE_VERSION,
+                                                       plugins_count=1,
+                                                       wagon_files_count=3)
+        plugin2 = self._upload_plugin_and_assert_values(TEST_PACKAGE2_NAME,
+                                                        TEST_PACKAGE2_VERSION,
+                                                        plugins_count=2,
+                                                        wagon_files_count=2)
+        self._uninstall_plugin_and_assert_values(plugin, 1)
+        self._uninstall_plugin_and_assert_values(plugin2, 0)
+
+    def _upload_plugin_and_assert_values(self,
+                                         package_name,
+                                         package_version,
+                                         plugins_count,
+                                         wagon_files_count,
+                                         corrupt_plugin=False):
+        plugin = test_utils.upload_mock_plugin(
+            package_name,
+            package_version,
+            corrupt_plugin=corrupt_plugin)
+        wagon_file = plugin.archive_name[len(INSTALLING_PREFIX):]
+
+        execution = self._get_latest_execution('install_plugin')
+        if corrupt_plugin:
+            with self.assertRaisesRegexp(
+                    RuntimeError, 'Workflow execution failed: .*'):
+                self.wait_for_execution_to_end(execution)
+            execution = self._get_latest_execution('uninstall_plugin')
+        self.wait_for_execution_to_end(execution)
+
+        find_output = self._find_file_on_manager(wagon_file)
+        self.assertEqual(len(find_output), wagon_files_count)
+        plugins = self.client.plugins.list()
+        self.assertEqual(len(plugins), plugins_count)
+        return plugin
+
+    def _find_file_on_manager(self, wagon_file):
+        find_output = self.execute_on_manager(
+            "find / -name '{0}'".format(wagon_file)).stdout.split('\n')
+        if find_output and not find_output[-1]:
+            find_output.pop()
+        return find_output
+
+    def _uninstall_plugin_and_assert_values(self, plugin, plugins_count):
+        self.client.plugins.delete(plugin.id)
+        execution = self._get_latest_execution('uninstall_plugin')
+        self.wait_for_execution_to_end(execution)
+        plugins = self.client.plugins.list()
+        self.assertEqual(len(plugins), plugins_count)
+        self.assertNotIn(plugin.id, plugins)
