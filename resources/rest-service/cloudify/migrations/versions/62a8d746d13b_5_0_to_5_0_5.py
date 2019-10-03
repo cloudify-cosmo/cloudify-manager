bb733c01d4cb2c31b95749c38593b59365ca7cce
Madda <nobody@nowhere.local>
2019-10-03 11:14:54 +0000

    CY-1705 Verify LDAPS certs

--- resources/rest-service/cloudify/migrations/versions/62a8d746d13b_5_0_to_5_0_5.py
+++ resources/rest-service/cloudify/migrations/versions/62a8d746d13b_5_0_to_5_0_5.py
@@ -9,13 +9,37 @@ Create Date: 2019-08-23 13:36:03.985636
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import orm
+from sqlalchemy.ext.declarative import declarative_base
 
+from manager_rest.storage.models import User
+from manager_rest.storage.models_base import JSONString, UTCDateTime
 
 # revision identifiers, used by Alembic.
 revision = '62a8d746d13b'
 down_revision = '423a1643f365'
 branch_labels = None
 depends_on = None
+
+Base = declarative_base()
+
+
+class Config(Base):
+    __tablename__ = 'config'
+
+    name = sa.Column(sa.Text, primary_key=True)
+    value = sa.Column(JSONString(), nullable=False)
+    schema = sa.Column(JSONString(), nullable=True)
+    is_editable = sa.Column(sa.Boolean, default=True)
+    updated_at = sa.Column(UTCDateTime())
+    scope = sa.Column(sa.Text, primary_key=True)
+    _updater_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey(User.id, ondelete='SET NULL'),
+        nullable=True,
+        index=False,
+        primary_key=False,
+    )
 
 
 def upgrade():
@@ -29,8 +53,29 @@ def upgrade():
         'deployment_updates',
         sa.Column('runtime_only_evaluation', sa.Boolean(), nullable=True))
 
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+    session.add(
+        Config(
+            name='ldap_ca_path',
+            value=None,
+            scope='rest',
+            schema={'type': 'string'},
+            is_editable=True
+        )
+    )
+
 
 def downgrade():
     op.drop_column('deployment_updates', 'runtime_only_evaluation')
     op.drop_column('deployments', 'runtime_only_evaluation')
     op.drop_column('executions', 'blueprint_id')
+
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+
+    ldap_ca_path = session.query(Config).filter_by(
+        name='ldap_ca_path',
+        scope='rest',
+    ).one()
+    session.delete(ldap_ca_path)
