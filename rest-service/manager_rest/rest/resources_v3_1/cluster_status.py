44adc27b4fd929e75aad01611230cd274fb1e05b
tyacbovi <tyacbovi@gmail.com>
2019-11-21 23:28:43 +0200

    cluster-status: small refactor - added missing edge case - small style refactors

--- rest-service/manager_rest/rest/resources_v3_1/cluster_status.py
+++ rest-service/manager_rest/rest/resources_v3_1/cluster_status.py
@@ -20,6 +20,8 @@ from datetime import datetime
 
 from flask import current_app
 
+from cloudify.cluster_status import CloudifyNodeType
+
 from manager_rest import manager_exceptions
 from manager_rest.security.authorization import authorize
 from manager_rest.storage import models, get_storage_manager
@@ -42,6 +44,9 @@ class ClusterStatus(SecuredResourceReadonlyMode):
 
     @staticmethod
     def _verify_report_newer_than_current(report_time, path):
+        if not (os.path.exists(path) and os.path.isfile(path)):
+            # Nothing to do if the file does not exists
+            return
         with open(path) as current_report_file:
             current_report = json.load(current_report_file)
         if report_time < parse_datetime_string(current_report['timestamp']):
@@ -53,44 +58,59 @@ class ClusterStatus(SecuredResourceReadonlyMode):
     def _verify_timestamp(report_time):
         if report_time > datetime.utcnow():
             raise manager_exceptions.BadParametersError(
-                'The report timestamp `{}` is in the future'.
+                'The report timestamp `{0}` is in the future'.
                 format(report_time))
 
     @staticmethod
-    def _node_id_exists(node_id, model):
-        return get_storage_manager().exists(model,
-                                            filters={'node_id': node_id})
-
-    def put(self, node_id, model, node_type):
-        report_dict = self._get_request_dict()
-        if not self._node_id_exists(node_id, model):
+    def _verify_node_exists(node_id, model):
+        if not get_storage_manager().exists(model,
+                                            filters={'node_id': node_id}):
             raise manager_exceptions.BadParametersError(
-                'The given node id {} is invalid'.format(node_id))
+                'The given node id {0} is invalid'.format(node_id))
+
+    @staticmethod
+    def _create_statues_folder_if_needed():
+        if not os.path.exists(STATUSES_PATH):
+            os.makedirs(STATUSES_PATH)
+
+    @staticmethod
+    def _save_report(path, report_dict):
+        with open(path, 'w') as report_file:
+            json.dump(report_dict, report_file)
+
+    def _write_report(self, node_id, model, node_type):
+        self._create_statues_folder_if_needed()
+        self._verify_node_exists(node_id, model)
+        report_dict = self._get_request_dict()
         report_time = parse_datetime_string(report_dict['timestamp'])
         self._verify_timestamp(report_time)
         path = '{status_path}/{node_type}_{node_id}.json'.format(
             status_path=STATUSES_PATH, node_type=node_type, node_id=node_id)
-        if os.path.exists(STATUSES_PATH):
-            self._verify_report_newer_than_current(report_time, path)
-        else:
-            os.makedirs(STATUSES_PATH)
-        with open(path, 'w') as report_file:
-            json.dump(report_dict, report_file)
+        self._verify_report_newer_than_current(report_time, path)
+        self._save_report(path, report_dict)
+        current_app.logger.info('Received new status report for '
+                                '{0} of type {1}'.format(node_id, node_type))
 
 
 class ManagerClusterStatus(ClusterStatus):
     @authorize('manager_cluster_status_put')
-    def put(self, node_id, model=models.Manager, node_type='manager'):
-        super(ManagerClusterStatus, self).put(node_id, model, node_type)
+    def put(self, node_id):
+        self._write_report(node_id,
+                           models.Manager,
+                           CloudifyNodeType.MANAGER)
 
 
 class DBClusterStatus(ClusterStatus):
     @authorize('db_cluster_status_put')
-    def put(self, node_id, model=models.DBNodes, node_type='db'):
-        super(DBClusterStatus, self).put(node_id, model, node_type)
+    def put(self, node_id):
+        super(DBClusterStatus, self).put(node_id,
+                                         models.DBNodes,
+                                         CloudifyNodeType.DB)
 
 
 class BrokerClusterStatus(ClusterStatus):
     @authorize('broker_cluster_status_put')
-    def put(self, node_id, model=models.RabbitMQBroker, node_type='broker'):
-        super(BrokerClusterStatus, self).put(node_id, model, node_type)
+    def put(self, node_id):
+        super(BrokerClusterStatus, self).put(node_id,
+                                             models.RabbitMQBroker,
+                                             CloudifyNodeType.BROKER)
