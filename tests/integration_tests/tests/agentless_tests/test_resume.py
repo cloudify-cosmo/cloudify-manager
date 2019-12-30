3b310fc16fda23ca39d2077da35826d8274956a4
asdf <tehasdf@gmail.com>
2019-12-30 11:55:34 +0100

    Move the sleep into _unlock_operation
    it was already in one test, too

--- tests/integration_tests/tests/agentless_tests/test_resume.py
+++ tests/integration_tests/tests/agentless_tests/test_resume.py
@@ -109,6 +109,9 @@ class TestResumeMgmtworker(AgentlessTestCase):
                             node_instance.id)
                     else:
                         break
+        # and now wait for the task to finish. It polls every 1 second, so
+        # allow a 3 seconds wait.
+        time.sleep(3)
 
     def _start_mgmtworker(self):
         self.logger.info('Restarting mgmtworker')
@@ -135,9 +138,6 @@ class TestResumeMgmtworker(AgentlessTestCase):
 
         self._unlock_operation('interface1.op_resumable', node_ids=['node1'])
 
-        # and now wait for the task to finish. It polls every 1 second, so
-        # allow a 3 seconds wait.
-        time.sleep(3)
         task = self._find_remote_operation(graphs[0].id)
         self.assertEqual(task.state, tasks.TASK_SUCCEEDED)
 
@@ -335,7 +335,6 @@ class TestResumeMgmtworker(AgentlessTestCase):
         self.assertEqual(execution.status, 'cancelled')
 
         self._unlock_operation('interface1.op_nonresumable')
-        time.sleep(5)
         execution = self.client.executions.resume(execution.id, force=True)
         execution = self.wait_for_execution_to_end(execution)
         self.assertEqual(execution.status, 'terminated')
