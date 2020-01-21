bf1e2f16bd2bae2a56d4b9cc1ba6c961e402334c
tyacbovi <3802463+tyacbovi@users.noreply.github.com>
2020-01-21 12:16:44 +0200

    CY-2213: small refactor

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -547,14 +547,13 @@ class SnapshotRestore(object):
 
         config_dump_path = postgres.dump_config_tables(self._tempdir)
         status_reporter_roles_path, status_reporter_users_path = \
-            postgres.dump_status_reporters(postgres, self._tempdir)
+            postgres.dump_status_reporters(self._tempdir)
         with utils.db_schema(schema_revision, config=self._config):
             admin_user_update_command = postgres.restore(
                 self._tempdir, premium_enabled=self._premium_enabled)
         postgres.restore_config_tables(config_dump_path)
         postgres.restore_current_execution()
-        postgres.restore_status_reporters(postgres,
-                                          status_reporter_roles_path,
+        postgres.restore_status_reporters(status_reporter_roles_path,
                                           status_reporter_users_path)
         try:
             self._restore_stage(postgres, self._tempdir, stage_revision)
