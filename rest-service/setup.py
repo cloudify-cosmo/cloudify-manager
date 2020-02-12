7155beba51b64cbd5bc376f2003293c725a28d33
Inbal Amrani <inbala@cloudify.co>
2020-02-12 12:22:09 +0200

    Pin flask-login version to 0.4.1

--- rest-service/setup.py
+++ rest-service/setup.py
@@ -21,6 +21,7 @@ install_requires = [
     'flask-restful-swagger==0.20.1',
     'flask-sqlalchemy==2.3.2',
     'flask-security==3.0.0',
+    'flask-login==0.4.1',
     'flask-migrate==2.2.1',
     'cloudify-common==5.1.0.dev1',
     'requests>=2.7.0,<3.0.0',
