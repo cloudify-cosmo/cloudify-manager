198f02cff8fafba00b616cf208d35dda9012f840
Yoni Itzhak <yoni@cloudify.co>
2020-02-27 11:45:12 +0200

    CY-2364-5.0.5-build-set-default-is-cascading-to-false (#2118)
    * CY-2364-set-default-is-cascading-to-false
    - Changing the `is_cascading` parameter to false on all default workflows in types.yaml.
    - In case the `is_cascading` parameter is not specified, it would be false by default.

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -718,7 +718,7 @@ class ResourceManager(object):
             wait_after_fail=wait_after_fail,
             scheduled_time=scheduled_time)
 
-        is_cascading_workflow = workflow.get('is_cascading', True)
+        is_cascading_workflow = workflow.get('is_cascading', False)
         if is_cascading_workflow:
             components_dep_ids = self._find_all_components_deployment_id(
                 deployment_id)
