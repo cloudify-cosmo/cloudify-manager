f4eb700c086655c0fc71a3c3b80d41eee4b10e7a
Mateusz Neumann <mateumann@gmail.com>
2020-03-12 11:04:45 +0100

    CY-2435 Restore scheduled tasks from snapshot (#2124)
    Properly restore scheduled executions from snapshots.
    
    Set status of overdue executions as FAILED, requeue executions that are
    due in the future.

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -73,6 +73,7 @@ from .constants import (
     SECURITY_FILE_LOCATION,
     SECURITY_FILENAME
 )
+from .utils import is_later_than_now
 
 
 class SnapshotRestore(object):
@@ -145,6 +146,7 @@ class SnapshotRestore(object):
                 self._restore_agents()
                 self._restore_amqp_vhosts_and_users()
                 self._restore_deployment_envs(postgres)
+                self._restore_scheduled_executions()
 
                 if self._premium_enabled:
                     self._reconfigure_status_reporter(postgres)
@@ -927,6 +929,22 @@ class SnapshotRestore(object):
             '--user_id',
             str(user_id),
         ]).aggr_stdout.strip()
+
+    def _restore_scheduled_executions(self):
+        """Restore executions scheduled for a time after snapshot creation."""
+        for execution in self._client.executions.list(
+                _get_all_results=True, status=Execution.SCHEDULED):
+            if is_later_than_now(execution.scheduled_for):
+                ctx.logger.debug("Restoring execution {} (at {})".format(
+                    execution.workflow_id, execution.scheduled_for))
+                self._client.executions.requeue(execution.id)
+            else:
+                self._client.executions.update(execution.id,
+                                               Execution.FAILED)
+                ctx.logger.info(
+                    "Execution {} scheduled for {} is overdue. "
+                    "Marking as FAILED.".format(execution.id,
+                                                execution.scheduled_for))
 
     @staticmethod
     def _mark_manager_restoring():
