320f3ac40cc55d17e9a3547a8df0767f75d68902
Mateusz Neumann <mateumann@gmail.com>
2020-03-12 11:04:45 +0100

    CY-2435 Restore scheduled tasks from snapshot (#2124)
    Properly restore scheduled executions from snapshots.
    
    Set status of overdue executions as FAILED, requeue executions that are
    due in the future.

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -47,6 +47,7 @@ from manager_rest.utils import (send_event,
                                 validate_deployment_and_site_visibility,
                                 extract_host_agent_plugins_from_plan)
 from manager_rest.plugins_update.constants import PLUGIN_UPDATE_WORKFLOW
+from manager_rest.rest.rest_utils import parse_datetime_string
 from manager_rest.storage import (db,
                                   get_storage_manager,
                                   models,
@@ -618,6 +619,39 @@ class ResourceManager(object):
             execution.deployment_id)
         for exec_id in components_executions:
             self.resume_execution(exec_id, force)
+
+        return execution
+
+    def requeue_execution(self, execution_id, force=False):
+        execution = self.sm.get(models.Execution, execution_id)
+        if execution.status != ExecutionState.SCHEDULED:
+            raise manager_exceptions.ConflictError(
+                'Cannot requeue execution: `{0}` in state: `{1}`'
+                .format(execution.id, execution.status))
+
+        workflow_id = execution.workflow_id
+        deployment = execution.deployment
+        blueprint = deployment.blueprint
+        workflow_plugins = blueprint.plan[
+            constants.WORKFLOW_PLUGINS_TO_INSTALL]
+        workflow = deployment.workflows[workflow_id]
+        execution_token = generate_execution_token(execution_id)
+
+        workflow_executor.execute_workflow(
+            workflow_id,
+            workflow,
+            workflow_plugins=workflow_plugins,
+            blueprint_id=deployment.blueprint_id,
+            deployment=deployment,
+            execution_id=execution_id,
+            execution_parameters=execution.parameters,
+            bypass_maintenance=False,
+            dry_run=False,
+            resume=False,
+            scheduled_time=parse_datetime_string(execution.scheduled_for),
+            execution_creator=execution.creator,
+            execution_token=execution_token
+        )
 
         return execution
 
--- rest-service/manager_rest/rest/resources_v1/executions.py
+++ rest-service/manager_rest/rest/resources_v1/executions.py
@@ -180,14 +180,16 @@ class ExecutionsId(SecuredResource):
         action = request_dict['action']
 
         valid_actions = ['cancel', 'force-cancel', 'kill', 'resume',
-                         'force-resume']
+                         'force-resume', 'requeue']
 
         if action not in valid_actions:
             raise manager_exceptions.BadParametersError(
                 'Invalid action: {0}, Valid action values are: {1}'.format(
                     action, valid_actions))
 
-        if action in ('resume', 'force-resume'):
+        if action == 'requeue':
+            return get_resource_manager().requeue_execution(execution_id)
+        elif action in ('resume', 'force-resume'):
             return get_resource_manager().resume_execution(
                 execution_id, force=action == 'force-resume')
         return get_resource_manager().cancel_execution(
