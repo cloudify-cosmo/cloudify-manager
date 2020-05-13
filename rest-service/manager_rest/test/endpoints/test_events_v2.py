70b2aa4e6826a6b7ecf0e62d4b36861ed21efc98
Mateusz Neumann <mateumann@gmail.com>
2020-05-13 14:50:36 +0200

    CY-2725 Filtering events delete by timestamp range (#2175)
    Added ability to remove events for specified timestamp range.
    
    This patch is accompanied by the other one in cloudify-cli.
    
    * Add simple test_delte_events_timestamp_range
    
    * Save deleted logs to a file
    
    Store deleted logs in /opt/manager/logs/<FILENAME>.log
    
    * Add test_delete_events_store_before
    
    * Resolve reviewer's remarks

--- rest-service/manager_rest/test/endpoints/test_events_v2.py
+++ rest-service/manager_rest/test/endpoints/test_events_v2.py
@@ -12,6 +12,8 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
+from mock import patch
+
 from manager_rest.test.attribute import attr
 
 from manager_rest.test import base_test
@@ -43,4 +45,27 @@ class EventsTest(base_test.BaseServerTestCase):
     def test_delete_events(self):
         response = self.client.events.delete(
             '<deployment_id>', include_logs=True)
+        self.assertEqual(response.items, [0])
+
+    @attr(client_min_version=3,
+          client_max_version=base_test.LATEST_API_VERSION)
+    def test_delete_events_timestamp_range(self):
+        response = self.client.events.delete(
+            '<deployment_id>', include_logs=True,
+            from_datetime='2020-01-01', to_datetime='2020-02-02')
+        self.assertEqual(response.items, [0])
+
+    @attr(client_min_version=3,
+          client_max_version=base_test.LATEST_API_VERSION)
+    @patch('manager_rest.rest.resources_v2.events.Events._store_log_entries')
+    def test_delete_events_store_before(self, store_log_entries):
+        response = self.client.events.delete(
+            '<deployment_id>', include_logs=False,
+            store_before='true')
+        self.assertEqual(store_log_entries.call_count, 1)
+        self.assertEqual(response.items, [0])
+        response = self.client.events.delete(
+            '<deployment_id>', include_logs=True,
+            store_before='true')
+        self.assertEqual(store_log_entries.call_count, 3)
         self.assertEqual(response.items, [0])
