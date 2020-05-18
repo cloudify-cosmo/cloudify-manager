b1c3554597d28faa4036252f4fd1e861ef5aae2c
Madda <nobody@nowhere.local>
2020-05-18 14:47:01 +0100

    CYBL-1017 Correctly store pre-scale state

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -1211,6 +1211,17 @@ class ResourceManager(object):
                     'started deployment modifications: {0}'
                     .format(active_modifications))
 
+        # We need to store the pre-modification state here so that it can be
+        # used to roll back correctly on error.
+        # We have to deepcopy it because it contains a lot of mutable children
+        # which will then (sometimes) be modified by the other methods and
+        # result in a rollback that breaks the deployment and snapshots.
+        pre_modification = [
+            deepcopy(instance.to_dict()) for instance in
+            self.sm.list(models.NodeInstance,
+                         filters=deployment_id_filter,
+                         get_all_results=True)]
+
         nodes = [node.to_dict() for node
                  in self.sm.list(models.Node, filters=deployment_id_filter,
                                  get_all_results=True)]
@@ -1218,18 +1229,14 @@ class ResourceManager(object):
                           in self.sm.list(models.NodeInstance,
                                           filters=deployment_id_filter,
                                           get_all_results=True)]
+
         node_instances_modification = tasks.modify_deployment(
             nodes=nodes,
             previous_nodes=nodes,
             previous_node_instances=node_instances,
             modified_nodes=modified_nodes,
             scaling_groups=deployment.scaling_groups)
-
-        node_instances_modification['before_modification'] = [
-            instance.to_dict() for instance in
-            self.sm.list(models.NodeInstance,
-                         filters=deployment_id_filter,
-                         get_all_results=True)]
+        node_instances_modification['before_modification'] = pre_modification
 
         now = utils.get_formatted_timestamp()
         modification_id = str(uuid.uuid4())
