b532f9cdf23ac2420b4f20390f5acb415778c853
Mateusz Neumann <mateumann@gmail.com>
2020-05-18 12:00:01 +0200

    CY-2774 Add `title` descriptive field for plugins (#2182)

--- rest-service/manager_rest/test/endpoints/test_plugins.py
+++ rest-service/manager_rest/test/endpoints/test_plugins.py
@@ -348,3 +348,17 @@ class PluginsTest(BaseServerTestCase):
             self.client.plugins.upload(zip_path)
 
         self.quiet_delete(tmp_file_path)
+
+    @attr(client_min_version=3.1,
+          client_max_version=base_test.LATEST_API_VERSION)
+    def test_plugin_upload_with_title(self):
+        tmp_file_path = self.create_wheel(
+            TEST_PACKAGE_NAME,
+            TEST_PACKAGE_VERSION)
+        yaml_path = self.get_full_path(TEST_PACKAGE_YAML_FILE)
+        zip_path = self.zip_files([tmp_file_path, yaml_path])
+        uploaded_plugin = self.client.plugins.upload(zip_path,
+                                                     plugin_title='test')
+        get_plugin_by_id_response = self.client.plugins.get(uploaded_plugin.id)
+        self.assertIsNotNone(get_plugin_by_id_response)
+        self.assertEqual(get_plugin_by_id_response.title, 'test')
