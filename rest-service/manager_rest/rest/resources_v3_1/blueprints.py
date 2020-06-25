02d9e83df33cb51dd6ad2e15daa4c5cb9269e378
glukhman <michaelg@cloudify.co>
2020-06-25 11:54:03 +0300

    CY-2822 Detect duplicate blueprint ID in endpoint

--- rest-service/manager_rest/rest/resources_v3_1/blueprints.py
+++ rest-service/manager_rest/rest/resources_v3_1/blueprints.py
@@ -13,13 +13,14 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
+from flask import request
+
 from flask_restful.inputs import boolean
 from flask_restful.reqparse import Argument
 from flask_restful_swagger import swagger
 
 from cloudify.models_states import VisibilityState
 
-from manager_rest.storage import models
 from manager_rest.security import SecuredResource
 from manager_rest.security.authorization import authorize
 from manager_rest.resource_manager import get_resource_manager
@@ -28,7 +29,9 @@ from manager_rest.upload_manager import (UploadedBlueprintsManager,
 from manager_rest.rest import (rest_utils,
                                resources_v2,
                                rest_decorators)
+from manager_rest.storage import models, get_storage_manager
 from manager_rest.rest.rest_utils import get_args_and_verify_arguments
+from manager_rest.manager_exceptions import ConflictError, IllegalActionError
 
 
 class BlueprintsSetGlobal(SecuredResource):
@@ -73,6 +76,25 @@ class BlueprintsId(resources_v2.BlueprintsId):
             is_argument=True,
             valid_values=VisibilityState.STATES
         )
+        # Fail fast if trying to upload a duplicate blueprint
+        current_tenant = request.headers.get('tenant')
+        if visibility == VisibilityState.GLOBAL:
+            existing_duplicates = get_storage_manager().list(
+                models.Blueprint, filters={'id': blueprint_id})
+            if existing_duplicates:
+                raise IllegalActionError(
+                    "Can't set or create the resource `{0}`, it's visibility "
+                    "can't be global because it also exists in other "
+                    "tenants".format(blueprint_id))
+        else:
+            existing_duplicates = get_storage_manager().list(
+                models.Blueprint, filters={'id': blueprint_id,
+                                           'tenant_name': current_tenant})
+            if existing_duplicates:
+                raise ConflictError(
+                    'blueprint with id={0} already exists on tenant {1} or '
+                    'with global visibility'.format(blueprint_id,
+                                                    current_tenant))
         return UploadedBlueprintsManager().\
             receive_uploaded_data(data_id=blueprint_id,
                                   visibility=visibility)
