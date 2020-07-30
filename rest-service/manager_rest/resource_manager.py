01e29fae90d010d35dc7437bee3ad9eb269033e0
glukhman <michaelg@cloudify.co>
2020-07-30 16:14:52 +0300

    CY-3147 Fix agent plugin installer

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -27,7 +27,8 @@ from flask_security import current_user
 
 from cloudify.cryptography_utils import encrypt
 from cloudify.workflows import tasks as cloudify_tasks
-from cloudify.plugins.install_utils import INSTALLING_PREFIX
+from cloudify.plugins.install_utils import (INSTALLING_PREFIX,
+                                            remove_status_prefix)
 from cloudify.models_states import (SnapshotState,
                                     ExecutionState,
                                     VisibilityState,
@@ -280,6 +281,10 @@ class ResourceManager(object):
             self._validate_plugin_yaml(plugin)
 
         if not utils.plugin_installable_on_current_platform(plugin):
+            plugin_entry = self.sm.get(models.Plugin, plugin.id)
+            plugin_entry = remove_status_prefix(plugin_entry)
+            if plugin_entry:
+                self.sm.update(plugin_entry)
             return
 
         self._execute_system_workflow(
