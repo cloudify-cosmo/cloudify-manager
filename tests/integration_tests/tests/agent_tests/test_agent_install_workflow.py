c0acb50c0aaaddbe26770fac5d05cf7d0126ef23
asdf <tehasdf@gmail.com>
2020-08-05 13:36:14 +0200

    CY-3216 Integration test checking for amqp queue leaks

--- tests/integration_tests/tests/agent_tests/test_agent_install_workflow.py
+++ tests/integration_tests/tests/agent_tests/test_agent_install_workflow.py
@@ -23,6 +23,69 @@ from integration_tests.tests.utils import get_resource as resource
 @pytest.mark.usefixtures('dockercompute_plugin')
 @pytest.mark.usefixtures('allow_agent')
 class TestWorkflow(AgentTestWithPlugins):
+    def _get_queues(self, vhost=None):
+        cmd = ['rabbitmqctl', 'list_queues', '-s']
+        if vhost:
+            cmd += ['-p', vhost]
+        output = self.execute_on_manager(cmd)
+        return {line.split()[0] for line in output.splitlines()}
+
+    def _get_exchanges(self, vhost=None):
+        cmd = ['rabbitmqctl', 'list_exchanges', '-s']
+        if vhost:
+            cmd += ['-p', vhost]
+        output = self.execute_on_manager(cmd)
+        return {line.split()[0] for line in output.splitlines()}
+
+    def test_amqp_queues_list(self):
+        """There's no additional queues after uninstalling the agent.
+
+        We've seen queue leaks in the past, where queues or exchanges
+        were not deleted. Check that uninstalling the agent, also removes
+        its AMQP resources.
+        """
+        vhost = 'rabbitmq_vhost_default_tenant'
+        deployment_id = 'd{0}'.format(uuid.uuid4())
+
+        main_queues = self._get_queues()
+        main_exchanges = self._get_exchanges()
+        tenant_queues = self._get_queues(vhost)
+        tenant_exchanges = self._get_exchanges(vhost)
+
+        self.deploy_application(
+            resource('dsl/agent_tests/with_agent.yaml'),
+            deployment_id=deployment_id,
+            timeout_seconds=120
+        )
+        # installing the agent does nothing for the / vhost
+        assert self._get_queues() == main_queues
+        assert self._get_exchanges() == main_exchanges
+
+        # after installing the agent, there's 2 new queues and at least
+        # 1 new exchange
+        agent_queues = self._get_queues(vhost) - tenant_queues
+        agent_exchanges = self._get_exchanges(vhost) - tenant_exchanges
+        assert len(agent_queues) == 2
+        assert any(queue.endswith('_service') for queue in agent_queues)
+        assert any(queue.endswith('_operation') for queue in agent_queues)
+        assert any(exc.startswith('agent_host') for exc in agent_exchanges)
+        # we already checked that there's an agent exchange, but there
+        # might also exist a logs exchange and an events exchange, depending
+        # if any events or logs were sent or not
+        assert len(agent_exchanges) in (1, 2, 3)
+
+        self.undeploy_application(deployment_id)
+
+        # after uninstalling the agent, there's still no new queues on
+        # the / vhost
+        assert self._get_queues() == main_queues
+        assert self._get_exchanges() == main_exchanges
+        # there's no queues left over
+        assert self._get_queues(vhost) == tenant_queues
+        # the logs and events exchanges will still exist, but the agent
+        # exchange must have been deleted
+        assert not any(exc.startswith('agent_host') for exc in agent_exchanges)
+
     def test_deploy_with_agent_worker(self):
         # In 4.2, the default (remote) agent installation path only requires
         # the "create" operation
