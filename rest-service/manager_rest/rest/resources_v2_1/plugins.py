38732329dc90679126551c0a30a1365f7ef9b6e7
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2020-08-11 09:53:23 +0200

    CY-3302 Plugin uninstall fixes (#2302)
    * Plugin.installation_state: declare cascade=delete
    
    Tell the ORM that this FK is indeed cascading on deletes, and that
    it doesn't need to do anything when deleting a plugin, because
    the DB will take care of it.
    
    * Plugin uninstall: skip useless check
    
    plugin_installable_on_current_platform is not used when installing,
    so it shouldn't used when uninstalling either
    
    * delete plugin_installable_on_current_platform
    
    not used anymore
    
    * 2.1 plugins: drop useless try/excepts
    
    - `except ManagerException: raise` is obviously useless
    - `except Exception: reraise` is bullshit, just show the original
      exception instead
    - `except ExecutionTimeout` is just a misunderstanding of how this
      works, the restservice doesn't wait for executions to finish,
      so it will never get a timeout. It only runs the execution.
      Well, used to, now it's a service task and not an execution anyway
    
    * drop test_uninstall_timeout
    
    you gotta love that patches a function to throw, and then asserts
    that it did throw. Anyway, 2 commits ago I removed that except,
    and so removing the test now
    
    * drop test_uninstall_failure
    
    no, i removed the reraise-as-PluginInstallationError.
    Unhandled/unexpected exceptions are a 500. That's it.
    
    Plugin remove is supposed to not throw anything, if it does, it's
    a 500. Amen.

--- rest-service/manager_rest/rest/resources_v2_1/plugins.py
+++ rest-service/manager_rest/rest/resources_v2_1/plugins.py
@@ -13,14 +13,9 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
-import sys
-
 from flask_restful_swagger import swagger
 
-from cloudify._compat import reraise
-
 from manager_rest.storage import models
-from manager_rest import manager_exceptions
 from manager_rest.security.authorization import authorize
 from manager_rest.resource_manager import get_resource_manager
 
@@ -45,26 +40,5 @@ class PluginsId(resources_v2.PluginsId):
         force = verify_and_convert_bool(
             'force', request_dict.get('force', False)
         )
-        try:
-            return get_resource_manager().remove_plugin(plugin_id=plugin_id,
-                                                        force=force)
-        except manager_exceptions.ManagerException:
-            raise
-        except manager_exceptions.ExecutionTimeout:
-            tp, ex, tb = sys.exc_info()
-            reraise(
-                manager_exceptions.PluginInstallationTimeout,
-                manager_exceptions.PluginInstallationTimeout(
-                    'Timed out during plugin un-installation. '
-                    '({0}: {1})'.format(tp.__name__, ex)
-                ),
-                tb)
-        except Exception:
-            tp, ex, tb = sys.exc_info()
-            reraise(
-                manager_exceptions.PluginInstallationError,
-                manager_exceptions.PluginInstallationError(
-                    'Failed during plugin un-installation. '
-                    '({0}: {1})'.format(tp.__name__, ex)
-                ),
-                tb)
+        return get_resource_manager().remove_plugin(plugin_id=plugin_id,
+                                                    force=force)
