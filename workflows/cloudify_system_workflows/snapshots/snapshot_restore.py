a3dc77dab1844617a967e7d2b08967d1a0844f65
asdf <tehasdf@gmail.com>
2020-08-19 13:10:21 +0200

    snapshot_restore: remove the plugin bits
    we're now not installing plugins up-front, so remove all the
    things related to plugins

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -20,13 +20,10 @@ import time
 import base64
 import shutil
 import zipfile
-import platform
 import tempfile
 import threading
 import subprocess
-from contextlib import closing, contextmanager
-
-import wagon
+from contextlib import contextmanager
 
 from cloudify._compat import queue
 from cloudify.workflows import ctx
@@ -91,8 +88,7 @@ class SnapshotRestore(object):
                  premium_enabled,
                  user_is_bootstrap_admin,
                  restore_certificates,
-                 no_reboot,
-                 ignore_plugin_failure):
+                 no_reboot):
         self._config = utils.DictToAttributes(config)
         self._snapshot_id = snapshot_id
         self._force = force
@@ -101,8 +97,6 @@ class SnapshotRestore(object):
         self._no_reboot = no_reboot
         self._premium_enabled = premium_enabled
         self._user_is_bootstrap_admin = user_is_bootstrap_admin
-        self._ignore_plugin_failure = \
-            ignore_plugin_failure
         self._post_restore_commands = []
 
         self._tempdir = None
@@ -132,7 +126,6 @@ class SnapshotRestore(object):
                 stage_revision = re.sub(r".*\n", '', stage_revision)
             self._validate_snapshot()
 
-            existing_plugins = self._get_existing_plugin_names()
             with Postgres(self._config) as postgres:
                 utils.sudo(ALLOW_DB_CLIENT_CERTS_SCRIPT)
                 self._restore_files_to_manager()
@@ -148,7 +141,6 @@ class SnapshotRestore(object):
                 self._generate_new_rest_token()
                 self._restart_rest_service()
                 self._restart_stage_service()
-                self._restore_plugins(existing_plugins)
                 self._restore_credentials(postgres)
                 self._restore_agents()
                 self._restore_amqp_vhosts_and_users()
@@ -258,16 +250,9 @@ class SnapshotRestore(object):
             new_token = f.read()
         return new_token
 
-    def _should_ignore_plugin_failure(self,
-                                      message):
-        return 'cloudify_agent.operations.install_plugins' in \
-               message and self._ignore_plugin_failure
-
-    def _get_and_execute_task_graph(self, token_info, deployment_id,
-                                    dep_ctx, tenant, tenant_client,
-                                    workflow_ctx, ctx_params,
-                                    deps_with_failed_plugins,
-                                    failed_deployments):
+    def _get_and_execute_task_graph(
+            self, deployment_id, dep_ctx, tenant, workflow_ctx, ctx_params,
+            failed_deployments):
         """
         While in the correct workflow context, this method
         creates a `create_deployment_env` task graph and executes it.
@@ -280,34 +265,12 @@ class SnapshotRestore(object):
         try:
             with current_workflow_ctx.push(workflow_ctx, ctx_params):
                 try:
-                    api_token = self._get_api_token(
-                        token_info[tenant][deployment_id]
-                    )
-                    dep = tenant_client.deployments.get(deployment_id)
-                    blueprint = tenant_client.blueprints.get(
-                        dep_ctx.blueprint.id,
-                    )
-                    tasks_graph = self._get_tasks_graph(
-                        dep_ctx,
-                        blueprint,
-                        dep,
-                        api_token,
-                    )
+                    tasks_graph = generate_create_dep_tasks_graph(dep_ctx)
                     with dep_ctx:
                         tasks_graph.execute()
                 except RuntimeError as re:
-                    if self._should_ignore_plugin_failure(re.message):
-                        ctx.logger.warning('Failed to install plugins for '
-                                           'deployment `{0}` under tenant '
-                                           '`{1}`.  Proceeding since '
-                                           '`ignore_plugin_failure` flag was'
-                                           ' used.'
-                                           .format(deployment_id, tenant))
-                        ctx.logger.debug(re.message)
-                        deps_with_failed_plugins.put((deployment_id, tenant))
-                    else:
-                        failed_deployments.put((deployment_id, tenant))
-                        ctx.logger.info(re)
+                    failed_deployments.put((deployment_id, tenant))
+                    ctx.logger.info(re)
         finally:
             self._semaphore.release()
 
@@ -327,8 +290,6 @@ class SnapshotRestore(object):
 
     def _restore_deployment_envs(self, postgres):
         deps = utils.get_dep_contexts(self._snapshot_version)
-        token_info = postgres.get_deployment_creator_ids_and_tokens()
-        deps_with_failed_plugins = queue.Queue()
         failed_deployments = queue.Queue()
         threads = list()
 
@@ -338,7 +299,6 @@ class SnapshotRestore(object):
                     tenant=tenant,
                 )
             )
-            tenant_client = get_rest_client(tenant=tenant)
 
             for deployment_id, dep_ctx in deployments.items():
                 # Task graph is created and executed by threads to
@@ -347,10 +307,9 @@ class SnapshotRestore(object):
                 wf_parameters = current_workflow_ctx.get_parameters()
                 self._semaphore.acquire()
                 t = threading.Thread(target=self._get_and_execute_task_graph,
-                                     args=(token_info, deployment_id, dep_ctx,
-                                           tenant, tenant_client, wf_ctx,
+                                     args=(deployment_id, dep_ctx,
+                                           tenant, wf_ctx,
                                            wf_parameters,
-                                           deps_with_failed_plugins,
                                            failed_deployments)
                                      )
                 t.setDaemon(True)
@@ -368,7 +327,7 @@ class SnapshotRestore(object):
                                       ' tracebacks logged above for more'
                                       ' details.'.format(deployments))
 
-        self._log_final_information(deps_with_failed_plugins)
+        ctx.logger.info('Successfully restored deployment environments.')
 
     def _restore_inter_deployment_dependencies(self):
         # managers older than 4.6.0 didn't have the support get_capability.
@@ -439,22 +398,6 @@ class SnapshotRestore(object):
                                     'dependencies for deployment %s from '
                                     'tenant %s. %s',
                                     deployment_id, tenant, err)
-
-    @staticmethod
-    def _log_final_information(deps_with_failed_plugins):
-        if deps_with_failed_plugins.empty():
-            ctx.logger.info('Successfully restored deployment environments.')
-
-        # Alert users that some deployments were restored but their
-        # plugins were not installed.
-        else:
-            deployments = list(deps_with_failed_plugins.queue)
-            ctx.logger.warning('Finished restoring deployment evnironments.'
-                               ' Please note that the following deployment '
-                               'plugins were not installed and need to be'
-                               ' installed manually on each deployment`s '
-                               'virtual env: {0}'.
-                               format(deployments))
 
     def _restore_amqp_vhosts_and_users(self):
         subprocess.check_call(
@@ -745,180 +688,6 @@ class SnapshotRestore(object):
             '{0}.zip'.format(self._snapshot_id)
         )
 
-    def _get_existing_plugin_names(self):
-        ctx.logger.debug('Collecting existing plugins')
-        existing_plugins = self._client.plugins.list(_all_tenants=True,
-                                                     _get_all_results=True)
-        return [self._get_plugin_info(p) for p in existing_plugins]
-
-    def _get_plugin_info(self, plugin):
-        return {
-            'path': os.path.join(
-                '/opt/manager/resources/plugins',
-                plugin['id'],
-                plugin.archive_name
-            ),
-            'id': plugin['id'],
-            'tenant': plugin['tenant_name'],
-            'visibility': plugin['visibility'],
-        }
-
-    def _get_plugins_to_install(self, existing_plugins):
-        """Return a list of plugins that need to be installed (meaning, they
-        weren't installed on the manager before the restore and they *can* be
-        installed on the manager)
-
-        :param existing_plugins: Names of already installed plugins
-        """
-
-        def should_install(plugin):
-            # Can't just do 'not in' as plugin is a dict
-            hashable_existing = (frozenset(p) for p in existing_plugins)
-            return frozenset(plugin.items()) not in hashable_existing
-
-        ctx.logger.debug('Looking for plugins to install')
-        all_plugins = self._client.plugins.list(_all_tenants=True,
-                                                _get_all_results=True)
-        installable_plugins = [
-            self._get_plugin_info(plugin) for plugin in all_plugins
-            if self._plugin_installable_on_current_platform(plugin)
-        ]
-        ctx.logger.debug('Found {0} plugins in total'
-                         .format(len(all_plugins)))
-        plugins_to_install = {}
-        for plugin in installable_plugins:
-            if should_install(plugin):
-                tenant = plugin['tenant']
-                plugins_to_install.setdefault(tenant, []).append(plugin)
-        ctx.logger.info('Found plugins to install for tenant: {p}'.format(
-            p=plugins_to_install,
-        ))
-        return plugins_to_install
-
-    @staticmethod
-    def _restore_plugin(client, tenant, plugin, plugins_tmp):
-        plugin_dir = os.path.dirname(plugin['path'])
-        wagon_name = os.path.basename(plugin['path'])
-        ctx.logger.info(
-            'Installing plugin {plugin} for {tenant}'.format(
-                plugin=wagon_name,
-                tenant=tenant,
-            )
-        )
-
-        temp_plugin = os.path.join(plugins_tmp, wagon_name)
-        # in case there are both wagon and yaml files
-        if len(os.listdir(plugin_dir)) > 1:
-            temp_plugin += '.zip'
-            # creating a zip containing the wagon and plugin.yaml
-            with closing(zipfile.ZipFile(temp_plugin, 'w')) as zip_file:
-                for root, _, files in os.walk(plugin_dir):
-                    for filename in files:
-                        file_path = os.path.join(root, filename)
-                        zip_file.write(file_path, os.path.relpath(file_path,
-                                                                  plugin_dir))
-        else:
-            # support case of no plugin.yaml - copy only wagon
-            shutil.copyfile(plugin['path'], temp_plugin)
-
-        client.plugins.delete(plugin['id'], force=True)
-        try:
-            client.plugins.upload(temp_plugin,
-                                  visibility=plugin['visibility'])
-        finally:
-            # In any case, failure or success, delete tmp* folder
-            os.remove(temp_plugin)
-
-    def _wait_for_plugin_executions(self, client):
-        while True:
-            executions = client.executions.list(
-                include_system_workflows=True,
-                _all_tenants=True,
-                _get_all_results=True
-            )
-            waiting = []
-            for execution in executions:
-                if execution.workflow_id == 'install_plugin':
-                    if execution.status not in Execution.END_STATES:
-                        waiting.append(execution)
-            if not waiting:
-                break
-            else:
-                ctx.logger.info(
-                    'Waiting for plugin install executions to finish: {0} '
-                    '(state: {1})'
-                    .format(execution.id, execution.status))
-                time.sleep(3)
-
-    @staticmethod
-    def __remove_failed_plugins_footprints(client, failed_plugins):
-        for failed_plugin in failed_plugins:
-            # Removing failed plugins from the database and file server
-            try:
-                ctx.logger.info('Removing failed plugin footprints')
-                client.plugins.delete(failed_plugin['id'], force=True)
-            except Exception as ex:
-                ctx.logger.warning('Failed to delete plugin footprints %s '
-                                   'with error: %s. Proceeding...',
-                                   failed_plugin, ex)
-
-    @staticmethod
-    def __log_message_for_plugin_restore(failed_plugins):
-        if not failed_plugins:
-            ctx.logger.info('Successfully restored plugins')
-        else:
-            plugin_installation_log_message = \
-                'Plugins: {0} have not been installed ' \
-                'successfully'.format(failed_plugins)
-            ctx.logger.warning(plugin_installation_log_message)
-
-    def _restore_plugins(self, existing_plugins):
-        """Install any plugins that weren't installed prior to the restore
-
-        :param existing_plugins: Names of already installed plugins
-        """
-        ctx.logger.info('Restoring plugins')
-        plugins_to_install = self._get_plugins_to_install(existing_plugins)
-        failed_plugins = []
-        for tenant, plugins in plugins_to_install.items():
-            client = get_rest_client(tenant=tenant)
-            plugins_tmp = tempfile.mkdtemp()
-            try:
-                for plugin in plugins:
-                    try:
-                        self._restore_plugin(client,
-                                             tenant,
-                                             plugin,
-                                             plugins_tmp)
-                    except Exception as ex:
-                        if self._ignore_plugin_failure:
-                            ctx.logger.warning(
-                                'Failed to restore plugin: %s, '
-                                'ignore-plugin-failure flag '
-                                'used. Proceeding...', plugin)
-                            ctx.logger.debug(
-                                'Restore plugin failure error: %s', ex)
-                            failed_plugins.append(plugin)
-                        else:
-                            raise ex
-                self._wait_for_plugin_executions(client)
-                SnapshotRestore.__remove_failed_plugins_footprints(
-                    client, failed_plugins)
-            finally:
-                os.rmdir(plugins_tmp)
-        SnapshotRestore.__log_message_for_plugin_restore(failed_plugins)
-
-    @staticmethod
-    def _plugin_installable_on_current_platform(plugin):
-        dist, _, release = platform.linux_distribution(
-            full_distribution_name=False)
-        dist, release = dist.lower(), release.lower()
-        return (plugin['supported_platform'] == 'any' or all([
-            plugin['supported_platform'] == wagon.get_platform(),
-            plugin['distribution'] == dist,
-            plugin['distribution_release'] == release
-        ]))
-
     def _restore_credentials(self, postgres):
         ctx.logger.info('Restoring credentials')
         restore_credentials(self._tempdir, postgres, self._snapshot_version)
@@ -928,24 +697,6 @@ class SnapshotRestore(object):
         ctx.logger.info('Restoring cloudify agent data')
         Agents().restore(self._tempdir, self._snapshot_version)
         ctx.logger.info('Successfully restored cloudify agent data')
-
-    def _get_tasks_graph(self, dep_ctx, blueprint, deployment, token):
-        """Create a deployment creation tasks graph
-        """
-        blueprint_plan = blueprint['plan']
-        return generate_create_dep_tasks_graph(
-            dep_ctx,
-            deployment_plugins_to_install=blueprint_plan[
-                'deployment_plugins_to_install'],
-            workflow_plugins_to_install=blueprint_plan[
-                'workflow_plugins_to_install'],
-            policy_configuration={
-                'api_token': token,
-                'policy_types': deployment['policy_types'],
-                'policy_triggers': deployment['policy_triggers'],
-                'groups': deployment['groups']
-            }
-        )
 
     def _load_hash_salt(self):
         if self._snapshot_version >= V_4_4_0:
