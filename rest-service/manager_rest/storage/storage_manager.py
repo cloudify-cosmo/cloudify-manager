a2637fb09460fdbb8abfbea472cfef85c6a38f13
asdf <tehasdf@gmail.com>
2020-09-03 01:06:30 +0200

    CY-3435 get_all_results: omit the limit/offset clauses
    In `_paginate`, make `get_all_results` just omit limit & offset.
    Before, it was just doing nothing.
    
    Also, the test was creating 1001 items, and then checking
    that with `get_all_results` it returns 1000 (not all ie. 1001).
    WHAT THE HELL.
    
    Anyway, fixed the test, and also made it be 10 & 20 instead of 1001,
    because inserting 1000 items via the storage managers takes on
    the order of 10 seconds.

--- rest-service/manager_rest/storage/storage_manager.py
+++ rest-service/manager_rest/storage/storage_manager.py
@@ -400,7 +400,11 @@ class SQLStorageManager(object):
             offset = 0
 
         total = query.order_by(None).count()  # Fastest way to count
-        results = query.limit(size).offset(offset).all()
+        if get_all_results:
+            results = query.all()
+        else:
+            results = query.limit(size).offset(offset).all()
+
         return results, total, size, offset
 
     @staticmethod
