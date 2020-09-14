437c20852595556f297014b22dd1e56f92ae9aed
asdf <tehasdf@gmail.com>
2020-09-14 13:28:28 +0200

    Bring storage_manager in line with patches

--- rest-service/manager_rest/storage/storage_manager.py
+++ rest-service/manager_rest/storage/storage_manager.py
@@ -113,7 +113,9 @@ class SQLStorageManager(object):
     def _add_value_filter(self, query, filters):
         for column, value in filters.iteritems():
             column, value = self._update_case_insensitive(column, value)
-            if isinstance(value, (list, tuple)):
+            if callable(value):
+                query = query.filter(value(column))
+            elif isinstance(value, (list, tuple)):
                 query = query.filter(column.in_(value))
             else:
                 query = query.filter(column == value)
