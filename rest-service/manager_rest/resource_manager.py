3634b770f5baadef6d656f9058f78a62780753af
asdf <tehasdf@gmail.com>
2020-09-14 12:53:22 +0200

    CY-3420 Lightweight deployments delete
    Deleting deployments can lead to fetching an unbounded amount of
    items - executions and node instances - to delete it ORM-side, and to
    return them as part of the response.
    
    Instead of deleting them ORM-side, delete them DB-side: we already
    have the CASCADE set up on the DB side, we just need to say
    passive_deletes so that sqlalchemy knows not to touch the
    relationship.
    
    Instead of returning the whole deployment object, with embedded
    relationships, just return a mock object with just the id.
    We can't avoid returning _something_ because the 4.6 CLI expects us
    to return a dict.
    We'll only do it for deployments, because this is the most affected
    endpoint, and other endpoints aren't having problems in practice
    (and this is a 4.6 fix, on an as-needed basis)
    
    Also, don't fetch all of the node-instances. Only look for the
    ones that are actually not uninitialized/deleted. And only the
    first page of those, we don't need to spew out 100k node instance ids
    if we are going to fail anyway.

--- rest-service/manager_rest/resource_manager.py
+++ rest-service/manager_rest/resource_manager.py
@@ -440,11 +440,12 @@ class ResourceManager(object):
                               in ExecutionState.END_STATES])))
         if not ignore_live_nodes:
             deplyment_id_filter = self.create_filters_dict(
-                deployment_id=deployment_id)
+                deployment_id=deployment_id,
+                state=lambda col: ~col.in_(['uninitialized', 'deleted'])
+            )
             node_instances = self.sm.list(
                 models.NodeInstance,
                 filters=deplyment_id_filter,
-                get_all_results=True
             )
             # validate either all nodes for this deployment are still
             # uninitialized or have been deleted
@@ -467,7 +468,7 @@ class ResourceManager(object):
         # Delete deployment data  DB (should only happen AFTER the workflow
         # finished successfully, hence the delete_db_mode flag)
         else:
-            return self.sm.delete(deployment)
+            return self.sm.delete(deployment, load_relationships=False)
 
     def _reset_operations(self, execution, from_states=None):
         """Force-resume the execution: restart failed operations.
--- rest-service/manager_rest/storage/storage_manager.py
+++ rest-service/manager_rest/storage/storage_manager.py
@@ -581,11 +581,12 @@ class SQLStorageManager(object):
         self._validate_unique_resource_id_per_tenant(instance)
         return instance
 
-    def delete(self, instance):
+    def delete(self, instance, load_relationships=True):
         """Delete the passed instance
         """
         current_app.logger.debug('Delete {0}'.format(instance))
-        self._load_relationships(instance)
+        if load_relationships:
+            self._load_relationships(instance)
         db.session.delete(instance)
         self._safe_commit()
         return instance
