2496dc616e40cd7e29dbb2b4fb68545d7b3058cd
glukhman <michaelg@cloudify.co>
2020-09-22 12:31:19 +0300

    CY-3519 Restore IDDs only on relevant deployments, subprocess in manager env

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -287,39 +287,22 @@ class SnapshotRestore(object):
         ctx.logger.info('Restoring inter deployment dependencies')
         update_service_composition = (self._snapshot_version == V_5_0_5)
 
-        deployment_contexts = utils.get_dep_contexts(self._snapshot_version)
-        deployments_queue = queue.Queue()
-        failed_deployments_queue = queue.Queue()
-        for tenant_name, deployments in deployment_contexts:
-            for dep_id in deployments:
-                deployments_queue.put((tenant_name, dep_id))
+        script_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'restore_idd_script.py'
+        )
+        cmd = ['/opt/manager/env/bin/python',
+               script_path,
+               ctx.tenant_name,
+               str(update_service_composition)]
+        restore_idd_script = subprocess.run(cmd)
 
-        wf_context = current_workflow_ctx.get_ctx()
-        context_params = current_workflow_ctx.get_parameters()
-
-        threads = []
-        for i in range(min(self._config.snapshot_restore_threads,
-                           deployments_queue.qsize())):
-            t = threading.Thread(
-                target=self._create_inter_deployment_dependencies,
-                args=(deployments_queue, failed_deployments_queue, wf_context,
-                      context_params, update_service_composition)
-            )
-            threads.append(t)
-            t.start()
-
-        for t in threads:
-            t.join()
-
-        if not failed_deployments_queue.empty():
-            deployments = list(failed_deployments_queue.queue)
+        if restore_idd_script.returncode:
+            restore_idd_log_path = 'mgmtworker/logs/restore_idd.log'
             raise NonRecoverableError('Failed to restore snapshot, could not '
                                       'create the inter deployment '
-                                      'dependencies from the following '
-                                      'deployments {0}. See exception '
-                                      'tracebacks logged above for more '
-                                      'details'.format(deployments))
-
+                                      'dependencies. See log {0} for more '
+                                      'details'.format(restore_idd_log_path))
         ctx.logger.info('Successfully restored inter deployment dependencies.')
 
     @staticmethod
@@ -345,6 +328,7 @@ class SnapshotRestore(object):
                                     'dependencies for deployment %s from '
                                     'tenant %s. %s',
                                     deployment_id, tenant, err)
+            deployments_queue.task_done()
 
     def _restore_amqp_vhosts_and_users(self):
         subprocess.check_call(
