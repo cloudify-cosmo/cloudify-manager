f4af8380dd6d6f4a800176e93dcb9b4408d28512
glukhman <47591609+glukhman@users.noreply.github.com>
2020-10-05 17:01:09 +0300

    bump gunicorn to 20.0.4 (#2381)

--- rest-service/setup.py
+++ rest-service/setup.py
@@ -25,7 +25,7 @@ install_requires = [
     'flask-migrate>2.2.0,<2.6.0',
     'cloudify-common==5.1.0.dev1',
     'requests>=2.7.0,<3.0.0',
-    'gunicorn==18.0',
+    'gunicorn==20.0.4',
     'pyyaml==5.3.1',
     'psutil==5.7.2',
     'virtualenv==15.1.0',
