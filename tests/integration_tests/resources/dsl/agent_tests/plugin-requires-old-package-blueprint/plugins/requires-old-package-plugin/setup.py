cea3f865271b50a4d1e861bd72ea96bd843b4630
Mohammed AbuAisha <mohammeda@cloudify.co>
2020-10-15 12:57:26 +0300

    Revert "Bump version"
    This reverts commit 5a00006e5bd4344bdd8bd40f5918df3a7c057585.

--- tests/integration_tests/resources/dsl/agent_tests/plugin-requires-old-package-blueprint/plugins/requires-old-package-plugin/setup.py
+++ tests/integration_tests/resources/dsl/agent_tests/plugin-requires-old-package-blueprint/plugins/requires-old-package-plugin/setup.py
@@ -22,6 +22,6 @@ setup(
     packages=['requires_old_package_plugin'],
     install_requires=[
         # The old repo/package before we merged it to cloudify-common
-        "cloudify-common==20.12.15.dev1"
+        "cloudify-common==5.2.0.dev1"
     ]
 )
