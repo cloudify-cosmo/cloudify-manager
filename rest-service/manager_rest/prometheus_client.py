cf9d3fa449cfe605a66fe6f5c9aee898de388952
geokala <geokala@users.noreply.github.com>
2020-10-15 11:18:00 +0100

    CY-3523 Log problems fetching prometheus info

--- rest-service/manager_rest/prometheus_client.py
+++ rest-service/manager_rest/prometheus_client.py
@@ -1,33 +1,42 @@
-#########
-# Copyright (c) 2020 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 import requests
+QUERY_URL_TEMPLATE = 'https://{address}:8009/monitoring/api/v1/query'
 
 
-def query(root_uri, query_string, auth=None, ca_path=None, timeout=None):
-    query_uri = '{0}api/v1/query'.format(root_uri)
+def query(address, query_string, logger, auth=None, ca_path=None,
+          timeout=None):
+    query_url = QUERY_URL_TEMPLATE.format(address=address)
+    url_with_query_string = (
+        query_url + '?query=' + requests.utils.quote(query_string)
+    )
     params = {'query': query_string}
     try:
-        r = requests.get(query_uri,
+        r = requests.get(query_url,
                          params=params,
                          auth=auth,
                          verify=ca_path if ca_path else True,
                          timeout=timeout)
-    except requests.exceptions.ConnectionError:
-        return []
-    return _format_prometheus_response(r) or []
+    except Exception as err:
+        logger.error(
+            "Error retrieving prometheus results from '%s' with ca '%s': "
+            "(%s) %s",
+            url_with_query_string,
+            ca_path,
+            type(err),
+            err,
+        )
+        result = []
+    else:
+        result = _format_prometheus_response(r) or []
+
+        if not result:
+            logger.error(
+                "Could not get prometheus results from '%s' with ca '%s'. "
+                'Response code %s.',
+                url_with_query_string,
+                ca_path,
+                r.status_code,
+            )
+    return result
 
 
 def _format_prometheus_response(response):
