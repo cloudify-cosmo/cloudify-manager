1d1e1413ab444d2de83fd073edb5dc942aeade94
Mateusz Neumann <mateumann@gmail.com>
2020-10-22 14:55:26 +0200

    RD-209 Update Prometheus host label (#2406)
    * Update host label in Prometheus target files
    
    (not in the prometheus.yml)
    
    * No need to restart Prometheus
    
    ... because service discovery mechanism is used, not static targets.

--- packaging/ip_setter/files/opt/cloudify/manager-ip-setter/manager-ip-setter.sh
+++ packaging/ip_setter/files/opt/cloudify/manager-ip-setter/manager-ip-setter.sh
@@ -76,17 +76,11 @@ function set_prometheus_ip() {
     return
   fi
 
-  if [ "$1" == "supervisord" ]; then
-    /usr/bin/supervisorctl -c /etc/supervisord.conf stop prometheus
-  fi
-
   echo "Updating the host label in the Prometheus configuration..."
-  /usr/bin/sed -ri "s/^(\s+host:\s*).*$/\1${ip}/" ${prometheus_yml}
-
-  if [ "$1" == "supervisord" ]; then
-    echo "Starting Prometheus service"
-    /usr/bin/supervisorctl -c /etc/supervisord.conf restart prometheus
-  fi
+  prometheus_targets="/etc/prometheus/targets/local_*.yml"
+  for target_file in ${prometheus_targets} ; do
+    /usr/bin/sed -ri "s/^(\s+labels:.+\"?host\"?:.*)127\.0\.0\.1(.*)$/\1${ip}\2/" "${target_file}"
+  done
 }
 
 
