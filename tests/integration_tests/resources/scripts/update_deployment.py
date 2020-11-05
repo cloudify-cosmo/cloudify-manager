bb3cfb6b18611ec287eec71869fbc397e3180537
mabuaisha <mohammeda@cloudify.co>
2020-11-05 17:44:45 +0200

    RD-417 Set the rest configuration path for test runtime function (#2432)

--- tests/integration_tests/resources/scripts/update_deployment.py
+++ tests/integration_tests/resources/scripts/update_deployment.py
@@ -1,9 +1,14 @@
+import os
 import argparse
+
 from manager_rest import config, storage
 from manager_rest.flask_utils import setup_flask_app
 
 
 def main(deployment_id):
+    # We need to set the environment variable for rest config path as its
+    # only available on the context of restservice
+    os.environ['MANAGER_REST_CONFIG_PATH'] = '/opt/manager/cloudify-rest.conf'
     setup_flask_app()
     config.instance.load_configuration(from_db=False)
     sm = storage.get_storage_manager()
