995b2e9b189aac9cda8af45b7d27e49db9cd0936
glukhman <michaelg@cloudify.co>
2020-11-08 17:25:17 +0200

    RD-397 Allow test backwards api run in a Py3 env

--- tests/integration_tests/resources/scripts/test_old_rest_client.sh
+++ tests/integration_tests/resources/scripts/test_old_rest_client.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 
-virtualenv old_cfy_env
+virtualenv --python=/usr/bin/python2.7 old_cfy_env
 source old_cfy_env/bin/activate
 pip install cloudify==${client_version}
 python ${python_script_path}
