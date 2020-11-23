078fc0b480b94f8db41567826f572871db620cb9
Yoni Itzhak <yoni@cloudify.co>
2020-11-23 14:02:48 +0200

    RD-478 Create labels endpoint (#2458)
    * RD-478 Create labels endpoint

---
+++ rest-service/manager_rest/rest/resources_v3_1/labels.py
@@ -0,0 +1,30 @@
+from flask import request
+
+from manager_rest.security import SecuredResource
+from manager_rest.security.authorization import authorize
+from manager_rest.storage import models, get_storage_manager
+
+from .. import rest_decorators, rest_utils
+
+
+class DeploymentsLabels(SecuredResource):
+    @authorize('labels_list')
+    @rest_decorators.marshal_list_response
+    @rest_decorators.paginate
+    def get(self, pagination=None):
+        """Get all deployments labels' keys in the current tenant"""
+        get_all_results = rest_utils.verify_and_convert_bool(
+            '_get_all_results',
+            request.args.get('_get_all_results', False)
+        )
+        results = get_storage_manager().list(
+            models.DeploymentLabel,
+            include=['key'],
+            pagination=pagination,
+            filters={'_deployment_fk': models.Deployment._storage_id},
+            get_all_results=get_all_results,
+            distinct=['key']
+        )
+
+        results.items = [label.key for label in results]
+        return results
