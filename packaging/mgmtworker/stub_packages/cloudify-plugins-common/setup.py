e1f5ff987496e62318e9eda2f52422bc573adb3a
opencm <limor@gigaspaces.com>
2020-12-28 14:02:57 +0000

    Bump version

--- packaging/mgmtworker/stub_packages/cloudify-plugins-common/setup.py
+++ packaging/mgmtworker/stub_packages/cloudify-plugins-common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-plugins-common',
-    version='5.1.1',
+    version='5.2.0.dev1',
     packages=[],
     description='[DEPRECATED] A stub for the old '
                 'cloudify-plugins-common package',
