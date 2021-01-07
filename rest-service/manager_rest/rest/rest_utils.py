2251581ab23de2db938024a1d6a449ca57540be2
Yoni Itzhak <yoni@cloudify.co>
2021-01-07 13:12:50 +0200

    RD-827 Fix delete deployment with component fails (#2519)
    * RD-827 Fix delete deployment with component fails

--- rest-service/manager_rest/rest/rest_utils.py
+++ rest-service/manager_rest/rest/rest_utils.py
@@ -548,8 +548,7 @@ class RecursiveDeploymentDependencies(object):
 
 
 def update_inter_deployment_dependencies(sm):
-    dependencies_list = sm.list(models.InterDeploymentDependencies,
-                                filters={'target_deployment': None})
+    dependencies_list = sm.list(models.InterDeploymentDependencies)
     for dependency in dependencies_list:
         if (dependency.target_deployment_func and
                 not dependency.external_target):
@@ -557,27 +556,34 @@ def update_inter_deployment_dependencies(sm):
 
 
 def _update_dependency_target_deployment(sm, dependency):
-    target_deployment_id = _evaluate_target_func(dependency)
-    if target_deployment_id:
-        target_deployment_instance = sm.get(models.Deployment,
-                                            target_deployment_id)
-        dependency.target_deployment = target_deployment_instance
+    eval_target_deployment = _get_deployment_from_target_func(
+        sm, dependency.target_deployment_func, dependency.source_deployment_id)
+    if (eval_target_deployment and
+            eval_target_deployment != dependency.target_deployment):
+        dependency.target_deployment = eval_target_deployment
 
         # check for cyclic dependencies
         dep_graph = RecursiveDeploymentDependencies(sm)
         source_id = str(dependency.source_deployment_id)
-        target_id = str(target_deployment_id)
+        target_id = str(eval_target_deployment.id)
         dep_graph.create_dependencies_graph()
         dep_graph.assert_no_cyclic_dependencies(source_id, target_id)
 
         sm.update(dependency)
 
 
-def _evaluate_target_func(dependency):
-    if is_function(dependency.target_deployment_func):
+def _evaluate_target_func(target_dep_func, source_dep_id):
+    if is_function(target_dep_func):
         evaluated_func = evaluate_intrinsic_functions(
-            {'target_deployment': dependency.target_deployment_func},
-            dependency.source_deployment_id)
+            {'target_deployment': target_dep_func}, source_dep_id)
         return evaluated_func.get('target_deployment')
 
-    return dependency.target_deployment_func
+    return target_dep_func
+
+
+def _get_deployment_from_target_func(sm, target_dep_func, source_dep_id):
+    target_dep_id = _evaluate_target_func(target_dep_func, source_dep_id)
+    if target_dep_id:
+        return sm.get(models.Deployment, target_dep_id, fail_silently=True)
+
+    return None
