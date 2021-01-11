144109f6e51d81bee7ab3e598a7fe6c564b40347
Yoni Itzhak <yoni@cloudify.co>
2021-01-11 18:06:33 +0200

    Add more commands to the filters endpoint

--- rest-service/manager_rest/rest/resources_v3_1/filters.py
+++ rest-service/manager_rest/rest/resources_v3_1/filters.py
@@ -1,6 +1,7 @@
 import re
-import json
+
 from flask import request
+from flask_security import current_user
 
 from cloudify.models_states import VisibilityState
 
@@ -9,6 +10,7 @@ from manager_rest.security import SecuredResource
 from manager_rest.utils import get_formatted_timestamp
 from manager_rest.rest import rest_decorators, rest_utils
 from manager_rest.security.authorization import authorize
+from manager_rest.resource_manager import get_resource_manager
 from manager_rest.storage import models, get_storage_manager, filters
 
 from .deployments import LABEL_LEN
@@ -51,21 +53,84 @@ class FiltersId(SecuredResource):
         Currently, this function only supports the creation of a labels filter
         """
         rest_utils.validate_inputs({'filter_id': filter_id})
-        request_dict = rest_utils.get_json_and_verify_params(
-            {'filter_rules': {'type': list}})
-        labels_filters = request_dict['filter_rules']
-        parsed_labels_filters = _parse_labels_filters(labels_filters)
-        visibility = rest_utils.get_visibility_parameter(
-            optional=True, valid_values=VisibilityState.STATES)
-
+        parsed_labels_filters, visibility = _get_filter_rules_and_visibility()
+        now = get_formatted_timestamp()
         new_filter = models.Filter(
             id=filter_id,
-            value=json.dumps({'labels': parsed_labels_filters}),
-            created_at=get_formatted_timestamp(),
+            value={'labels': parsed_labels_filters},
+            created_at=now,
+            updated_at=now,
             visibility=visibility
         )
 
         return get_storage_manager().put(new_filter)
+
+    @authorize('filters_get')
+    @rest_decorators.marshal_with(models.Filter)
+    def get(self, filter_id, _include=None):
+        """
+        Get a filter by ID
+        """
+        return get_storage_manager().get(
+            models.Filter, filter_id, include=_include)
+
+    @authorize('filters_delete')
+    def delete(self, filter_id):
+        """
+        Delete a filter by ID
+        """
+        storage_manager = get_storage_manager()
+        filter_elem = storage_manager.get(models.Filter, filter_id)
+        _validate_filter_modification_permitted(filter_elem)
+        storage_manager.delete(filter_elem, validate_global=True)
+        return None, 204
+
+    @authorize('filters_update')
+    @rest_decorators.marshal_with(models.Filter)
+    def patch(self, filter_id):
+        """Update a filter by its ID
+
+        This function updates the filter rules and visibility
+        """
+        if not request.json:
+            raise manager_exceptions.IllegalActionError(
+                'Update a filter request must include at least one parameter '
+                'to update')
+
+        labels_filters, visibility = _get_filter_rules_and_visibility(True)
+        storage_manager = get_storage_manager()
+        filter_elem = storage_manager.get(models.Filter, filter_id)
+        _validate_filter_modification_permitted(filter_elem)
+        if visibility:
+            get_resource_manager().validate_visibility_value(
+                models.Filter, filter_elem, visibility)
+            filter_elem.visibility = visibility
+        if labels_filters:
+            filter_elem.value = {'labels': labels_filters}
+        filter_elem.updated_at = get_formatted_timestamp()
+
+        return storage_manager.update(filter_elem)
+
+
+def _get_filter_rules_and_visibility(filter_rules_optional=False):
+    request_dict = rest_utils.get_json_and_verify_params(
+        {'filter_rules': {'type': list, 'optional': filter_rules_optional}})
+
+    labels_filters = (_parse_labels_filters(request_dict['filter_rules'])
+                      if 'filter_rules' in request_dict else None)
+    visibility = rest_utils.get_visibility_parameter(
+        optional=True, valid_values=VisibilityState.STATES)
+
+    return labels_filters, visibility
+
+
+def _validate_filter_modification_permitted(filter_elem):
+    if not (rest_utils.is_administrator(filter_elem.tenant) or
+            filter_elem.created_by == current_user.username):
+        raise manager_exceptions.ForbiddenError(
+            'User `{0}` is not permitted to modify the filter `{1}`'.format(
+                current_user.username, filter_elem.id)
+        )
 
 
 def _parse_labels_filters(labels_filters_list):
