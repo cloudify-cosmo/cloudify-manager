c6fecf2413f43cdaf59686a8f6daf5c56166a564
Yoni Itzhak <yoni@cloudify.co>
2021-01-14 14:19:27 +0200

    RD-704 Filters integration tests

--- tests/integration_tests/tests/agentless_tests/test_labels.py
+++ tests/integration_tests/tests/agentless_tests/test_labels.py
@@ -1,4 +1,3 @@
-import uuid
 import pytest
 
 from cloudify.models_states import VisibilityState
@@ -6,15 +5,15 @@ from cloudify.models_states import VisibilityState
 from integration_tests.tests import utils
 from integration_tests import AgentlessTestCase
 
+LABELS = [{'key1': 'val1'}, {'key1': 'val2'}, {'key2': 'val3'}]
+LABELS_2 = [{'key1': 'val1'}, {'key1': 'val3'}, {'key3': 'val3'}]
+
 
 @pytest.mark.usefixtures('cloudmock_plugin')
 class DeploymentsLabelsTest(AgentlessTestCase):
-    LABELS = [{'key1': 'val1'}, {'key1': 'val2'}, {'key2': 'val3'}]
-    LABELS_2 = [{'key1': 'val1'}, {'key1': 'val3'}, {'key3': 'val3'}]
-
     def test_viewer_labels_permissions(self):
         """A "viewer" is allowed to list the labels' keys and values."""
-        self._put_deployment_with_labels(self.client, self.LABELS)
+        self.put_deployment_with_labels(self.client, LABELS)
         self.client.users.create('user', 'password', role='default')
         self.client.tenants.add_user('user', 'default_tenant', role='viewer')
         viewer_client = utils.create_rest_client(host=self.env.container_ip,
@@ -34,10 +33,10 @@ class DeploymentsLabelsTest(AgentlessTestCase):
         self.client.tenants.create('new_tenant')
         new_tenant_client = utils.create_rest_client(
             host=self.env.container_ip, tenant='new_tenant')
-        self._put_deployment_with_labels(self.client, self.LABELS)
-        self._put_deployment_with_labels(new_tenant_client,
-                                         self.LABELS_2,
-                                         VisibilityState.GLOBAL)
+        self.put_deployment_with_labels(self.client, LABELS)
+        self.put_deployment_with_labels(new_tenant_client,
+                                        LABELS_2,
+                                        visibility=VisibilityState.GLOBAL)
 
         self._assert_keys_and_values(self.client,
                                      {'key1': {'val1', 'val2', 'val3'},
@@ -51,20 +50,3 @@ class DeploymentsLabelsTest(AgentlessTestCase):
         for key, values in keys_values_dict.items():
             values_list = self.client.deployments_labels.list_key_values(key)
             self.assertEqual(set(values_list.items), values)
-
-    def _put_deployment_with_labels(self,
-                                    client,
-                                    labels,
-                                    visibility=VisibilityState.TENANT):
-        dsl_path = utils.get_resource('dsl/basic.yaml')
-        blueprint_id = deployment_id = 'd{0}'.format(uuid.uuid4())
-        client.blueprints.upload(dsl_path, blueprint_id, visibility=visibility)
-        utils.wait_for_blueprint_upload(blueprint_id, client)
-        deployment = client.deployments.create(blueprint_id,
-                                               deployment_id,
-                                               visibility=visibility,
-                                               labels=labels)
-        utils.wait_for_deployment_creation_to_complete(self.env.container_id,
-                                                       deployment_id,
-                                                       self.client)
-        return deployment
