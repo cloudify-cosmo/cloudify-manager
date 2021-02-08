1f543875b1bbf2b0d9b2ce53b23b21006acfd8f2
glukhman <michaelg@cloudify.co>
2021-02-08 14:02:19 +0200

    RD-1204 Add unittests for the scheduled service

--- tests/integration_tests/tests/agentless_tests/test_execution_schedules.py
+++ tests/integration_tests/tests/agentless_tests/test_execution_schedules.py
@@ -1,41 +1,22 @@
-import datetime
+from time import sleep
+from datetime import datetime
 
 from integration_tests import AgentlessTestCase
-from integration_tests.framework import utils
-from integration_tests.tests.utils import (
-    do_retries,
-    get_resource as resource)
+from integration_tests.tests.utils import get_resource as resource
 
 
 class ExecutionsSchedulesTest(AgentlessTestCase):
 
     def test_schedule_execution(self):
-        deployment = self.deploy(resource("dsl/basic.yaml"))
-
-        # schedule 3 runs of `install`
-        schedule = self.client.execution_schedules.create(
-            'sched-1', deployment.id, 'install', since=datetime.now(),
-            frequency='1 min', count=3)
-
-        import pydevd
-        pydevd.settrace('192.168.43.135', port=53100, stdoutToServer=True,
-                        stderrToServer=True)
-
-    def test_schedule_execution_queue_simultaneous_execs(self):
-        pass
-
-    def test_schedule_execution_stop_on_fail(self):
-        pass
-
-    def test_schedule_execution_continue_on_fail(self):
-        pass
-
-    def test_schedule_execution_slip_shorter_than_downtime(self):
-        pass
-
-    def test_schedule_execution_slip_longer_than_downtime(self):
-        pass
-
-    def test_schedule_executions_disable_and_enable(self):
-        # TODO:: add when we have this option
-        pass
+        deployment = self.deploy(resource("dsl/empty_blueprint.yaml"))
+        self.client.execution_schedules.create(
+            'install-every-minute',
+            deployment.id,
+            'install',
+            since=datetime.utcnow().replace(second=0, microsecond=0),
+            frequency='1 min')  # run each HH:MM:00.0
+        sleep(60)  # the scheduler should fire the execution within 1 min.
+        executions = self.client.executions.list(deployment_id=deployment.id,
+                                                 workflow_id='install')
+        self.assertEqual(1, len(executions))
+        self.client.execution_schedules.delete('install-every-minute')
