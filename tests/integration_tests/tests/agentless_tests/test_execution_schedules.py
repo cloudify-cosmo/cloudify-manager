5ad9074695a91b30afa8ccfd72587531c121d4ea
glukhman <47591609+glukhman@users.noreply.github.com>
2021-02-25 14:54:39 +0200

    RD-1185 Parse schedules from DSL in dep. create (#2699)

--- tests/integration_tests/tests/agentless_tests/test_execution_schedules.py
+++ tests/integration_tests/tests/agentless_tests/test_execution_schedules.py
@@ -3,7 +3,9 @@ from datetime import datetime
 from retrying import retry
 
 from integration_tests import AgentlessTestCase
-from integration_tests.tests.utils import get_resource as resource
+from integration_tests.tests.utils import (
+    get_resource as resource,
+    wait_for_deployment_creation_to_complete)
 
 
 class ExecutionsSchedulesTest(AgentlessTestCase):
@@ -18,6 +20,16 @@ class ExecutionsSchedulesTest(AgentlessTestCase):
             frequency='1 min')  # run each HH:MM:00.0
         self.verify_execution_fired(deployment)
         self.client.execution_schedules.delete('install-every-minute')
+
+    def test_upload_blueprint_with_default_schedules(self):
+        deployment = self.deploy(resource(
+            "dsl/blueprint_with_default_schedules.yaml"))
+        wait_for_deployment_creation_to_complete(self.env.container_id,
+                                                 deployment.id,
+                                                 self.client)
+        schedule_from_bp = self.client.execution_schedules.get('sc1')
+        self.assertEquals(schedule_from_bp['rule']['frequency'], '1w')
+        self.assertEquals(len(schedule_from_bp['all_next_occurrences']), 5)
 
     @retry(wait_fixed=1000, stop_max_attempt_number=120)
     def verify_execution_fired(self, deployment):
