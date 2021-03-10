958174ec41dd130cbeda9b59975cc9d7bc5eef2b
Yoni Itzhak <yoni@cloudify.co>
2021-03-10 17:31:46 +0200

    Adding pagination and deopping decorator

--- rest-service/manager_rest/rest/resources_v3_1/labels.py
+++ rest-service/manager_rest/rest/resources_v3_1/labels.py
@@ -3,10 +3,10 @@ from flask import request
 from manager_rest.constants import CFY_LABELS
 from manager_rest.security import SecuredResource
 from manager_rest.security.authorization import authorize
+from manager_rest.storage.storage_manager import ListResult
 from manager_rest.storage import models, get_storage_manager
 
 from .. import rest_decorators, rest_utils
-from ..responses_v2 import ListResponse
 
 
 class DeploymentsLabels(SecuredResource):
@@ -14,11 +14,10 @@ class DeploymentsLabels(SecuredResource):
     @rest_decorators.marshal_list_response
     @rest_decorators.paginate
     @rest_decorators.search('key')
-    @rest_decorators.get_reserved_labels_keys
-    def get(self, get_reserved=None, pagination=None, search=None):
+    def get(self, pagination=None, search=None):
         """Get all deployments' labels' keys"""
-        if get_reserved:
-            return _return_reserved_labels_keys()
+        if _is_reserved_labels_keys_in_request():
+            return ListResult.from_list(items=CFY_LABELS)
         return get_labels_keys(models.Deployment,
                                models.DeploymentLabel,
                                pagination,
@@ -44,11 +43,10 @@ class BlueprintsLabels(SecuredResource):
     @rest_decorators.marshal_list_response
     @rest_decorators.paginate
     @rest_decorators.search('key')
-    @rest_decorators.get_reserved_labels_keys
-    def get(self, get_reserved=None,  pagination=None, search=None):
+    def get(self, pagination=None, search=None):
         """Get all blueprints' labels' keys"""
-        if get_reserved:
-            return _return_reserved_labels_keys()
+        if _is_reserved_labels_keys_in_request():
+            return ListResult.from_list(items=CFY_LABELS)
         return get_labels_keys(models.Blueprint,
                                models.BlueprintLabel,
                                pagination,
@@ -114,5 +112,6 @@ def get_labels_key_values(key, resource_model, resource_labels_model,
     return results
 
 
-def _return_reserved_labels_keys():
-    return ListResponse(items=CFY_LABELS, metadata={'total': len(CFY_LABELS)})
+def _is_reserved_labels_keys_in_request():
+    return rest_utils.verify_and_convert_bool(
+        'reserved', request.args.get('_reserved', False))
