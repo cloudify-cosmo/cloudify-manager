ff4ac75be45e9b852baea904da50162418950178
Yoni Itzhak <yoni@cloudify.co>
2021-04-07 14:14:25 +0300

    RD-1834 Refurbish labels tests

--- rest-service/manager_rest/test/endpoints/test_labels.py
+++ rest-service/manager_rest/test/endpoints/test_labels.py
@@ -13,25 +13,145 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
+import uuid
+
+from cloudify_rest_client.exceptions import CloudifyClientError
+
 from manager_rest.test import base_test
 from manager_rest.test.attribute import attr
 from manager_rest.constants import RESERVED_LABELS
 
 
-@attr(client_min_version=3.1, client_max_version=base_test.LATEST_API_VERSION)
-class LabelsTestCase(base_test.BaseServerTestCase):
+class LabelsBaseTestCase(base_test.BaseServerTestCase):
+    __test__ = False
+
     LABELS = [{'env': 'aws'}, {'arch': 'k8s'}]
     LABELS_2 = [{'env': 'gcp'}, {'arch': 'k8s'}]
+    UPDATED_LABELS = [{'env': 'gcp'}, {'arch': 'k8s'}]
+    UPDATED_UPPERCASE_LABELS = [{'env': 'GCp'}, {'ArCh': 'k8s'}]
+    UPPERCASE_LABELS = [{'EnV': 'aWs'}, {'aRcH': 'k8s'}]
+    DUPLICATE_LABELS = [{'env': 'aws'}, {'env': 'aws'}]
+    INVALID_LABELS_LIST = [{'env': 'aws', 'aRcH': 'k8s'}]
 
-    def setUp(self, labels_client):
+    def setUp(self, resource, put_resource_with_labels_cmd):
         super().setUp()
-        self.labels_client = labels_client
+        self.resource = resource
+        self.labels_client = getattr(self.client, f'{resource}_labels')
+        self.put_resource_with_labels = put_resource_with_labels_cmd
 
-    def _test_list_resource_labels_keys(self):
+    def update_resource_labels(self, resource_id, labels):
+        if self.resource == 'deployments':
+            return self.client.deployments.update_labels(resource_id, labels)
+        elif self.resource == 'blueprints':
+            return self.client.blueprints.update(resource_id,
+                                                 {'labels': labels})
+        else:
+            raise Exception('Resource unknown')
+
+    def test_resource_creation_success_with_labels(self):
+        resource = self.put_resource_with_labels(self.LABELS)
+        self.assert_resource_labels(resource.labels, self.LABELS)
+
+    def test_uppercase_labels_to_lowercase(self):
+        resource = self.put_resource_with_labels(self.UPPERCASE_LABELS)
+        self.assert_resource_labels(resource.labels, self.LABELS)
+
+    def test_creation_success_with_special_labels(self):
+        labels = [{'k.e-y': '&val\xf3e'}, {'key': ' va=l ue'},
+                  {'ke_y': 'val=\\,u:e'}]
+        resource = self.put_resource_with_labels(labels)
+        self.assert_resource_labels(resource.labels, labels)
+
+    def test_creation_success_with_normalized_label_value(self):
+        # Testing that the value Ó is being normalized and lowercased
+        labels = [{'key': '\u004f\u0301'}]
+        resource = self.put_resource_with_labels(labels)
+        self.assert_resource_labels(resource.labels, [{'key': '\u00f3'}])
+
+    def test_update_resource_labels(self):
+        resource = self.put_resource_with_labels(self.LABELS)
+        updated_res = self.update_resource_labels(resource.id,
+                                                  self.UPDATED_LABELS)
+        self.assert_resource_labels(updated_res.labels, self.UPDATED_LABELS)
+
+    def test_update_uppercase_resource_labels(self):
+        resource = self.put_resource_with_labels(self.LABELS)
+        updated_res = self.update_resource_labels(
+            resource.id, self.UPDATED_UPPERCASE_LABELS)
+        self.assert_resource_labels(updated_res.labels, self.UPDATED_LABELS)
+
+    def test_remove_resource_labels(self):
+        resource = self.put_resource_with_labels(self.LABELS)
+        self.assert_resource_labels(resource.labels, self.LABELS)
+        updated_res = self.update_resource_labels(resource.id, [])
+        self.assertEmpty(updated_res.labels)
+
+    def test_creation_failure_with_invalid_label_key(self):
+        err_label = [{'k ey': 'value'}]
+        error_msg = '`k ey`.*illegal characters'
+        self.assertRaisesRegex(CloudifyClientError,
+                               error_msg,
+                               self.put_resource_with_labels,
+                               labels=err_label)
+
+    def test_invalid_use_of_system_prefix_in_labels(self):
+        error_msg = '400: .*reserved for internal use'
+        self.assertRaisesRegex(CloudifyClientError,
+                               error_msg,
+                               self.put_resource_with_labels,
+                               labels=[{'csys-blah': 'val1'}])
+
+    def test_creation_failure_with_invalid_label_value(self):
+        err_labels = [{'key': 'test\n'}, {'key': 'test\t'}, {'key': 't,es t"'}]
+        error_msg = '{0}.*illegal characters'
+        for err_label in err_labels:
+            self.assertRaisesRegex(CloudifyClientError,
+                                   error_msg.format(err_label['key']),
+                                   self.put_resource_with_labels,
+                                   resource_id='i{0}'.format(uuid.uuid4()),
+                                   labels=[err_label])
+
+    def test_creation_failure_with_invalid_labels_list(self):
+        error_msg = 'Labels must be a list of 1-entry dictionaries'
+        self.assertRaisesRegex(CloudifyClientError,
+                               error_msg,
+                               self.put_resource_with_labels,
+                               labels=self.INVALID_LABELS_LIST)
+
+    def test_creation_failure_with_duplicate_labels(self):
+        error_msg = 'You cannot define the same label twice'
+        self.assertRaisesRegex(CloudifyClientError,
+                               error_msg,
+                               self.put_resource_with_labels,
+                               labels=self.DUPLICATE_LABELS)
+
+    def test_update_failure_with_invalid_labels_list(self):
+        resource = self.put_deployment_with_labels(self.LABELS)
+        error_msg = 'Labels must be a list of 1-entry dictionaries'
+        self.assertRaisesRegex(CloudifyClientError,
+                               error_msg,
+                               self.update_resource_labels,
+                               resource_id=resource.id,
+                               labels=self.INVALID_LABELS_LIST)
+
+    def test_update_failure_with_duplicate_labels(self):
+        resource = self.put_deployment_with_labels(self.LABELS)
+        error_msg = 'You cannot define the same label twice'
+        self.assertRaisesRegex(CloudifyClientError,
+                               error_msg,
+                               self.update_resource_labels,
+                               resource_id=resource.id,
+                               labels=self.DUPLICATE_LABELS)
+
+    def test_list_resource_labels_keys(self):
+        self.put_resource_with_labels(self.LABELS, resource_id='res1')
+        self.put_resource_with_labels(self.LABELS_2, resource_id='res2')
         keys_list = self.labels_client.list_keys()
         self.assertEqual(set(keys_list.items), {'env', 'arch'})
 
-    def _test_list_resource_labels_key_values(self):
+    def test_list_resource_labels_key_values(self):
+        self.put_resource_with_labels(self.LABELS, resource_id='res1')
+        self.put_resource_with_labels(self.LABELS_2, resource_id='res2')
         env_values = self.labels_client.list_key_values('env')
         arch_values = self.labels_client.list_key_values('arch')
         self.assertEqual(set(env_values.items), {'aws', 'gcp'})
@@ -50,35 +170,66 @@ class LabelsTestCase(base_test.BaseServerTestCase):
         self.assertEqual(reserved_labels.items, list(RESERVED_LABELS))
 
 
-class DeploymentsLabelsTestCase(LabelsTestCase):
+@attr(client_min_version=3.1, client_max_version=base_test.LATEST_API_VERSION)
+class DeploymentsLabelsTestCase(LabelsBaseTestCase):
+    __test__ = True
+
     def setUp(self):
-        super().setUp(self.client.deployments_labels)
+        super().setUp('deployments', self.put_deployment_with_labels)
 
-    def test_list_deployment_labels_keys(self):
-        self.put_deployment_with_labels(self.LABELS)
-        self.put_deployment_with_labels(self.LABELS_2)
-        self._test_list_resource_labels_keys()
+    def test_deployment_creation_success_without_labels(self):
+        _, _, _, deployment = self.put_deployment()
+        self.assertEmpty(deployment.labels)
 
-    def test_list_deployment_labels_key_values(self):
-        self.put_deployment_with_labels(self.LABELS)
-        self.put_deployment_with_labels(self.LABELS_2)
-        self._test_list_resource_labels_key_values()
+    def test_update_empty_deployments_labels(self):
+        _, _, _, deployment = self.put_deployment()
+        self.assertEqual(deployment.labels, [])
+        updated_dep = self.client.deployments.update_labels(deployment.id,
+                                                            self.LABELS)
+        self.assert_resource_labels(updated_dep.labels, self.LABELS)
+
+    def test_create_deployment_labels_from_blueprint(self):
+        self.put_blueprint(blueprint_id='bp1',
+                           blueprint_file_name='blueprint_with_labels_1.yaml')
+
+        deployment = self.client.deployments.create(
+            blueprint_id='bp1', deployment_id='dep1',
+            labels=[{'key1': 'key1_val1'}, {'new_key': 'new_value'}])
+        self.create_deployment_environment(deployment)
+        deployment = self.client.deployments.get(deployment.id)
+
+        expected_dep_labels = [
+            {'key1': 'key1_val1'}, {'key2': 'key2_val1'}, {'key2': 'va l:u,e'},
+            {'key2': 'key2_val2'}, {'new_key': 'new_value'}
+        ]
+        self.assert_resource_labels(deployment.labels, expected_dep_labels)
 
 
-class BlueprintsLabelsTestCase(LabelsTestCase):
+@attr(client_min_version=3.1, client_max_version=base_test.LATEST_API_VERSION)
+class BlueprintsLabelsTestCase(LabelsBaseTestCase):
+    __test__ = True
+
     def setUp(self):
-        super().setUp(self.client.blueprints_labels)
+        super().setUp('blueprints', self.put_blueprint_with_labels)
 
-    def test_list_blueprint_labels_keys(self):
-        self.put_blueprint_with_labels(self.LABELS, blueprint_id='bp1')
-        self.put_blueprint_with_labels(self.LABELS_2, blueprint_id='bp2')
-        self._test_list_resource_labels_keys()
+    def test_blueprint_creation_success_without_labels(self):
+        blueprint = self.put_blueprint()
+        self.assertEmpty(blueprint.labels)
 
-    def test_list_blueprint_labels_key_values(self):
-        self.put_blueprint_with_labels(self.LABELS, blueprint_id='bp1')
-        self.put_blueprint_with_labels(self.LABELS_2, blueprint_id='bp2')
-        self._test_list_resource_labels_key_values()
+    def test_update_empty_blueprint_labels(self):
+        blueprint = self.put_blueprint_with_labels(self.LABELS)
+        updated_bp = self.client.blueprints.update(blueprint.id,
+                                                   {'labels': []})
+        self.assert_resource_labels(updated_bp.labels, [])
 
-
-# This way we avoid rom running this class too
-del LabelsTestCase
+    def test_create_blueprint_labels_from_blueprint(self):
+        blueprint = self.put_blueprint(
+            blueprint_id='bp1',
+            blueprint_file_name='blueprint_with_labels_1.yaml',
+            labels=[{'bp_key1': 'bp_key1_val1'},
+                    {'new_bp_key': 'new_bp_value'}])
+        expected_bp_labels = [
+            {'bp_key1': 'bp_key1_val1'}, {'bp_key2': 'bp_key2_val1'},
+            {'bp_key2': 'bp_key2_val2'}, {'new_bp_key': 'new_bp_value'}
+        ]
+        self.assert_resource_labels(blueprint.labels, expected_bp_labels)
