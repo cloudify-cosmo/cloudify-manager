209235533519bc23bac173d3167e3e3460186376
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2021-04-08 12:23:36 +0200

    cloudify-types: bump requests version (#2881)
    this is the same version as in cloudify-common.
    Otherwise, we get a lot of version conflicts, eg. when running
    the ctx executable in mgmtworker (`/opt/mgmtworker/env/bin/ctx`)

--- cloudify_types/setup.py
+++ cloudify_types/setup.py
@@ -30,7 +30,7 @@ setup(
     description='Various special Cloudify types implementation.',
     install_requires=[
         'cloudify-common==5.3.0.dev1',
-        'requests==2.21.0',
-        'PyYAML==5.4.1'
+        'requests>=2.25.0,<3.0.0',
+        'PyYAML==5.4.1',
     ]
 )
