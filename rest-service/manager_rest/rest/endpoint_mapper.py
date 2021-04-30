6ee99cc3e00797154836b176003584a8dd896d3a
geokala <geokala@users.noreply.github.com>
2021-04-30 15:42:10 +0100

    RD-2147 Add unauthenticated short-status endpoint (#2969)

--- rest-service/manager_rest/rest/endpoint_mapper.py
+++ rest-service/manager_rest/rest/endpoint_mapper.py
@@ -1,18 +1,3 @@
-#########
-# Copyright (c) 2020 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 from . import (swagger, resources_v1, resources_v2,
                resources_v2_1, resources_v3, resources_v3_1)
 
@@ -64,6 +49,7 @@ def setup_resources(api):
         'NodeInstancesId': 'node-instances/<string:node_instance_id>',
         'Events': 'events',
         'Status': 'status',
+        'OK': 'ok',
         'ProviderContext': 'provider/context',
         'Version': 'version',
         'EvaluateFunctions': 'evaluate/functions',
