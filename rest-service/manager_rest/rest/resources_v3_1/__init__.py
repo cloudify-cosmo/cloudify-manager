c278942fa206f6e3f3c5ff3baf1f861f34c809e4
Madda <nobody@nowhere.local>
2021-04-30 14:59:49 +0100

    RD-2147 Add unauthenticated short-status endpoint

--- rest-service/manager_rest/rest/resources_v3_1/__init__.py
+++ rest-service/manager_rest/rest/resources_v3_1/__init__.py
@@ -1,18 +1,3 @@
-#########
-# Copyright (c) 2020 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 from .license import License, LicenseCheck       # NOQA
 from .tokens import UserTokens                   # NOQA
 from .sites import Sites, SitesName              # NOQA
@@ -95,7 +80,7 @@ from .operations import (                        # NOQA
     TasksGraphsId,
 )
 
-from .status import Status                       # NOQA
+from .status import OK, Status                   # NOQA
 
 from .cluster_status import (                    # NOQA
     ClusterStatus
