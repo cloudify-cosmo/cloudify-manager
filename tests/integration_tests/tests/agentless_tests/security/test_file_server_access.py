db5010853a60892f1de61bbbb66c0215486ff7a4
asdf <tehasdf@gmail.com>
2021-05-05 13:09:45 +0200

    fix test_file_server_access.py
    with the various changes we've been making

--- tests/integration_tests/tests/agentless_tests/security/test_file_server_access.py
+++ tests/integration_tests/tests/agentless_tests/security/test_file_server_access.py
@@ -1,28 +1,11 @@
-########
-# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import requests
-import requests.status_codes
 
 from cloudify_cli.env import get_auth_header
 from manager_rest.constants import DEFAULT_TENANT_ROLE
 
 from integration_tests import AgentlessTestCase
 from integration_tests.tests.constants import USER_ROLE
-from integration_tests.tests.utils import (
-    create_rest_client, get_resource as resource)
+from integration_tests.tests.utils import get_resource as resource
 
 
 class ResourcesAvailableTest(AgentlessTestCase):
@@ -37,7 +20,7 @@ class ResourcesAvailableTest(AgentlessTestCase):
         self.client.tenants.add_user(username='u',
                                      tenant_name='t',
                                      role=DEFAULT_TENANT_ROLE)
-        tenant_t_client = create_rest_client(tenant='t')
+        tenant_t_client = self.create_rest_client(tenant='t')
         tenant_t_client.blueprints.upload(resource('dsl/empty_blueprint.yaml'),
                                           entity_id='blu')
 
@@ -46,12 +29,12 @@ class ResourcesAvailableTest(AgentlessTestCase):
         self._assert_request_status_code(
             headers=admin_headers,
             path='/blueprints/default_tenant/blu/empty_blueprint.yaml',
-            expected_status_code=requests.status_codes.codes.ok)
+            expected_status_code=200)
 
         self._assert_request_status_code(
             headers=admin_headers,
             path='/blueprints/t/blu/empty_blueprint.yaml',
-            expected_status_code=requests.status_codes.codes.ok)
+            expected_status_code=200)
 
         # user u can only access blueprints in tenant t
         user_headers = get_auth_header('u', 'password')
@@ -59,7 +42,8 @@ class ResourcesAvailableTest(AgentlessTestCase):
         self._assert_request_status_code(
             headers=user_headers,
             path='/blueprints/t/blu/empty_blueprint.yaml',
-            expected_status_code=requests.status_codes.codes.ok)
+            expected_status_code=200)
+
         # invalid - trying to access unauthorized tenant
         self._assert_request_status_code(
             headers=user_headers,
@@ -76,12 +60,10 @@ class ResourcesAvailableTest(AgentlessTestCase):
                                     headers,
                                     path,
                                     expected_status_code):
-        self.assertEquals(
-            expected_status_code,
-            requests.get(
-                'https://{0}:53333/resources{1}'.format(self.get_manager_ip(),
-                                                        path),
-                headers=headers,
-                verify=False
-            ).status_code
+        response = requests.get(
+            'https://{0}:53333/resources{1}'.format(self.get_manager_ip(),
+                                                    path),
+            headers=headers,
+            verify=False
         )
+        assert expected_status_code == response.status_code
