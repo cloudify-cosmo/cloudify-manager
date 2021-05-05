849aaa6633d3927ce8820375774b27a32c7cf145
asdf <tehasdf@gmail.com>
2021-05-05 12:26:57 +0200

    style
    just free -lines

--- tests/integration_tests/tests/agentless_tests/multi_tenancy/test_list_tenant_filter.py
+++ tests/integration_tests/tests/agentless_tests/multi_tenancy/test_list_tenant_filter.py
@@ -1,17 +1,3 @@
-########
-# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    * See the License for the specific language governing permissions and
-#    * limitations under the License.
 from integration_tests.framework import utils
 from integration_tests import AgentlessTestCase
 from integration_tests.tests.constants import USER_ROLE
@@ -23,10 +9,6 @@ from cloudify_rest_client.exceptions import CloudifyClientError
 
 
 class ListResourcesTest(AgentlessTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(ListResourcesTest, cls).setUpClass()
-
     def setUp(self):
         super(ListResourcesTest, self).setUp()
         self.client.tenants.create('TEST_TENANT')
