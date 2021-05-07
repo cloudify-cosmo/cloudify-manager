14bdbb22c50e6be40b4e41b48ff1fdffc162f6da
asdf <tehasdf@gmail.com>
2021-05-07 10:55:59 +0200

    Store the manager_config in the prepare script
    It has to be hardcoded somewhere, might as well be in the prepare
    script, not in the separate json file.
    Then, we dont need to copy the json file from host, we entirely
    prepare it container-side.

--- tests/integration_tests/resources/scripts/prepare_reset_storage.py
+++ tests/integration_tests/resources/scripts/prepare_reset_storage.py
@@ -7,6 +7,15 @@ from manager_rest.storage import models
 from manager_rest.flask_utils import setup_flask_app
 
 
+MANAGER_CONFIG = {
+    'workflow': {
+        'task_retries': 0,
+        'task_retry_interval': 0,
+        'subgraph_retries': 0
+    },
+}
+
+
 def get_password_hash():
     setup_flask_app()
     return models.User.query.get(0).password
@@ -16,11 +25,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', dest='config')
     args = parser.parse_args()
-    with open(args.config) as f:
-        script_config = json.load(f)
     config.instance.load_from_file('/opt/manager/cloudify-rest.conf')
 
-    script_config['password_hash'] = get_password_hash()
+    script_config = {
+        'manager_config': MANAGER_CONFIG,
+        'password_hash': get_password_hash(),
+    }
 
     with open(args.config, 'w') as f:
         json.dump(script_config, f)
--- tests/integration_tests/tests/constants.py
+++ tests/integration_tests/tests/constants.py
@@ -1,10 +1,3 @@
-MANAGER_CONFIG = {
-    'workflow': {
-        'task_retries': 0,
-        'task_retry_interval': 0,
-        'subgraph_retries': 0
-    },
-}
 USER_ROLE = 'default'
 ADMIN_ROLE = 'sys_admin'
 USER_IN_TENANT_ROLE = 'user'
