46d4d6038667f54b260b0d884172eeb63f8fc3b7
asdf <tehasdf@gmail.com>
2021-05-17 11:58:07 +0200

    remember to commit!

--- workflows/cloudify_system_workflows/snapshots/populate_deployment_statuses.py
+++ workflows/cloudify_system_workflows/snapshots/populate_deployment_statuses.py
@@ -3,6 +3,7 @@ from manager_rest import config
 from manager_rest.flask_utils import setup_flask_app
 from manager_rest.constants import SECURITY_FILE_LOCATION
 from manager_rest.snapshot_utils import populate_deployment_statuses
+from manager_rest.storage import db
 
 
 os.environ['MANAGER_REST_CONFIG_PATH'] = '/opt/manager/cloudify-rest.conf'
@@ -12,3 +13,4 @@ if __name__ == '__main__':
     with setup_flask_app().app_context():
         config.instance.load_configuration()
         populate_deployment_statuses()
+        db.session.commit()
