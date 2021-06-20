23e7b5063c8ae9cffda4a8b56f76583a4bf5ef34
opencm <limor@gigaspaces.com>
2021-06-20 08:35:01 +0000

    Bump version

--- rest-service/setup.py
+++ rest-service/setup.py
@@ -23,7 +23,7 @@ install_requires = [
     'flask-security==3.0.0',
     'flask-login==0.4.1',
     'flask-migrate>2.2.0,<2.6.0',
-    'cloudify-common==6.1.0',
+    'cloudify-common==6.2.0.dev1',
     'requests>=2.7.0,<3.0.0',
     'gunicorn==20.0.4',
     'pyyaml==5.4.1',
@@ -48,7 +48,7 @@ install_requires = [
 
 setup(
     name='cloudify-rest-service',
-    version='6.1.0',
+    version='6.2.0.dev1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(
