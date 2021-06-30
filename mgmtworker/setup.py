49dbec357ee42f74d42efdd2f638fccbdcad469a
opencm <limor@gigaspaces.com>
2021-06-30 10:30:05 +0000

    Bump version

--- mgmtworker/setup.py
+++ mgmtworker/setup.py
@@ -16,15 +16,15 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'cloudify-common==5.2.3',
-    'cloudify-agent==5.2.3',
+    'cloudify-common==5.2.4.dev1',
+    'cloudify-agent==5.2.4.dev1',
     'packaging==17.1',
 ]
 
 
 setup(
     name='cloudify-mgmtworker',
-    version='5.2.3',
+    version='5.2.4.dev1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(include='mgmtworker*'),
