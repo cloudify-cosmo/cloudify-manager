01f9c0a4ae7f0a1c99607b1851498f5b4b1dab91
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2021-07-16 11:24:44 +0200

    fix the kwarg name (#3164)
    ...whoops

--- workflows/cloudify_system_workflows/snapshots/agents.py
+++ workflows/cloudify_system_workflows/snapshots/agents.py
@@ -53,7 +53,7 @@ class Agents(object):
         agents = client.agents.list(_all_tenants=True)
         agent_ids = [agent.id for agent in agents.items]
         node_instances = client.node_instances.search(
-            agent_ids, _all_tenants=True)
+            agent_ids, all_tenants=True)
         for instance in node_instances:
             self._add_node_instance_to_result(instance, result)
         self._dump_result_to_file(tempdir, result)
