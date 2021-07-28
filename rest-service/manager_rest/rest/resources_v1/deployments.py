1d893327a29bea3c182f8ba8cf8f4cd04838bc2b
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2021-07-28 16:51:15 +0200

    RD-2846 Drop useless .asdict calls (#3176)
    The orm now does return actual orm objects from this, not keyedtuples,
    so this quip is unnecessary.

--- rest-service/manager_rest/rest/resources_v1/deployments.py
+++ rest-service/manager_rest/rest/resources_v1/deployments.py
@@ -74,11 +74,6 @@ class DeploymentsId(SecuredResource):
             deployment_id,
             include=_include
         )
-        if _include and 'workflows' in _include:  # Similar to CY-760
-            result = result._asdict()
-            result['workflows'] = models.Deployment._list_workflows(
-                result['workflows'],
-            )
         return result
 
     @swagger.operation(
