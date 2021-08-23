30851bcd79c1e210ade1214a5f5e1963aa0903c2
Mateusz Neumann <mateumann@gmail.com>
2021-08-23 11:56:17 +0200

    Clean up the imports

--- rest-service/manager_rest/security/audit.py
+++ rest-service/manager_rest/security/audit.py
@@ -1,22 +1,21 @@
 import typing
 
 import flask
-from flask import g
 from manager_rest.storage.models_base import db
 
 
 def set_audit_method(method: str):
-    g.audit_auth_method = method
+    flask.g.audit_auth_method = method
 
 
 def set_username(username: str):
-    g.audit_username = username
+    flask.g.audit_username = username
     db.session.execute("SET SESSION audit.username = :name",
                        params={'name': username})
 
 
 def set_tenant(tenant: str):
-    g.audit_tenant = tenant
+    flask.g.audit_tenant = tenant
 
 
 def extend_headers(response: flask.Response) -> flask.Response:
@@ -27,10 +26,13 @@ def extend_headers(response: flask.Response) -> flask.Response:
 
 def _prepare_headers() -> typing.List[typing.Tuple[str, str]]:
     headers = []
-    if 'audit_auth_method' in g:
-        headers.append(('X-Cloudify-Audit-Auth-Method', g.audit_auth_method))
-    if 'audit_tenant' in g:
-        headers.append(('X-Cloudify-Audit-Tenant', g.audit_tenant))
-    if 'audit_username' in g:
-        headers.append(('X-Cloudify-Audit-Username', g.audit_username))
+    if 'audit_auth_method' in flask.g:
+        headers.append(('X-Cloudify-Audit-Auth-Method',
+                        flask.g.audit_auth_method))
+    if 'audit_tenant' in flask.g:
+        headers.append(('X-Cloudify-Audit-Tenant',
+                        flask.g.audit_tenant))
+    if 'audit_username' in flask.g:
+        headers.append(('X-Cloudify-Audit-Username',
+                        flask.g.audit_username))
     return headers
