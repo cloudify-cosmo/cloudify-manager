f9a778d00e5cdacacc5824aaae79699a89668256
asdf <tehasdf@gmail.com>
2021-08-24 00:36:09 +0200

    RD-3031 Call step-extractor from the workflow
    A separate operation will extract the steps and store them in
    the dep-update object.

--- workflows/cloudify_system_workflows/deployment_update/workflow.py
+++ workflows/cloudify_system_workflows/deployment_update/workflow.py
@@ -2,6 +2,10 @@ from cloudify.decorators import workflow
 from cloudify.state import workflow_ctx
 from cloudify.manager import get_rest_client
 
+from dsl_parser import tasks
+
+from .step_extractor import extract_steps
+
 
 def create_update(*, update_id, new_inputs):
     client = get_rest_client()
@@ -17,11 +21,58 @@ def create_update(*, update_id, new_inputs):
     )
 
 
+def prepare_plan(*, blueprint_id, update_id):
+    """Prepare the new deployment plan for a deployment update"""
+    client = get_rest_client()
+    bp = client.blueprints.get(blueprint_id)
+    dep_up = client.deployment_updates.get(update_id)
+
+    deployment_plan = tasks.prepare_deployment_plan(
+        bp.plan,
+        client.secrets.get,
+        dep_up.new_inputs,
+        runtime_only_evaluation=dep_up.runtime_only_evaluation
+    )
+    client.deployment_updates.set_attributes(update_id, plan=deployment_plan)
+
+
+def create_steps(*, update_id):
+    """Given a deployment update, extracts the steps for it"""
+    client = get_rest_client()
+    dep_up = client.deployment_updates.get(update_id)
+    deployment = client.deployments.get(dep_up.deployment_id)
+    nodes = client.nodes.list(deployment_id=dep_up.deployment_id)
+
+    # step-extractor expects workflows in this format - this is the same format
+    # as retuend by prepare_deployment_plan
+    deployment['workflows'] = {
+        workflow.id: dict(workflow) for workflow in deployment['workflows']
+    }
+    for deployment_workflow in deployment['workflows'].values():
+        deployment_workflow.pop('name')
+
+    supported_steps, unsupported_steps = extract_steps(
+        nodes,
+        deployment,
+        dep_up.deployment_plan
+    )
+    if unsupported_steps:
+        for step in unsupported_steps:
+            workflow_ctx.logger.error('Unsupported step: %s', step)
+        raise RuntimeError('Cannot update: unsupported steps found')
+
+    client.deployment_updates.set_attributes(
+        update_id,
+        steps=[step.as_dict() for step in supported_steps]
+    )
+
+
 def _prepare_update_graph(
         ctx,
         update_id,
         *,
         inputs=None,
+        blueprint_id=None,
         **kwargs):
     graph = ctx.graph_mode()
     seq = graph.sequence()
@@ -29,6 +80,13 @@ def _prepare_update_graph(
         ctx.local_task(create_update, kwargs={
             'update_id': update_id,
             'new_inputs': inputs or {},
+        }, total_retries=0),
+        ctx.local_task(prepare_plan, kwargs={
+            'update_id': update_id,
+            'blueprint_id': blueprint_id,
+        }, total_retries=0),
+        ctx.local_task(create_steps, kwargs={
+            'update_id': update_id,
         }, total_retries=0),
     )
     return graph
