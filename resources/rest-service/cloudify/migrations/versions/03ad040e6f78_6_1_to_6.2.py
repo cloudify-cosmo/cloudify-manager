6df336b3b6912436ee17365bfb00525e15d29d25
Mateusz Neumann <mateumann@gmail.com>
2021-08-23 12:51:17 +0200

    Split write_audit_log into simpler functions
    Implement write_audit_log() functionality in few more less clunky DB
    functions.

--- resources/rest-service/cloudify/migrations/versions/03ad040e6f78_6_1_to_6.2.py
+++ resources/rest-service/cloudify/migrations/versions/03ad040e6f78_6_1_to_6.2.py
@@ -22,65 +22,54 @@ audit_operation = sa.Enum(
 )
 
 
-TABLES_TO_AUDIT = ['agents',
-                   'blueprints',
-                   'blueprints_filter',
-                   'blueprints_labels',
-                   'certificates',
-                   'config',
-                   'db_nodes',
-                   'deployment_groups',
-                   'deployment_groups_deployments',
-                   'deployment_groups_labels',
-                   'deployment_labels_dependencies',
-                   'deployment_modifications',
-                   'deployment_update_steps',
-                   'deployment_updates',
-                   'deployments',
-                   'deployments_filters',
-                   'deployments_labels',
-                   'execution_groups',
-                   'execution_groups_executions',
-                   'execution_schedules',
-                   'executions',
-                   'groups',
-                   'groups_roles',
-                   'groups_tenants',
-                   'inter_deployment_dependencies',
-                   'licenses',
-                   'maintenance_mode',
-                   'managers',
-                   'node_instances',
-                   'nodes',
-                   'operations',
-                   'permissions',
-                   'plugins',
-                   'plugins_states',
-                   'plugins_updates',
-                   'provider_context',
-                   'rabbitmq_brokers',
-                   'roles',
-                   'secretes',
-                   'sites',
-                   'snapshots',
-                   'tasks_graphs',
-                   'tenants',
-                   'usage_collector',
-                   'users',
-                   'users_groups',
-                   'users_roles',
-                   'users_tenants']
+TABLES_TO_AUDIT = [('agents', 'storage_id'),
+                   ('blueprints', 'storage_id'),
+                   ('blueprints_filters', 'storage_id'),
+                   ('blueprints_labels', 'id'),
+                   ('certificates', 'id'),
+                   ('deployment_groups', 'storage_id'),
+                   ('deployment_groups_labels', 'id'),
+                   ('deployment_labels_dependencies', 'storage_id'),
+                   ('deployment_modifications', 'storage_id'),
+                   ('deployment_update_steps', 'storage_id'),
+                   ('deployment_updates', 'storage_id'),
+                   ('deployments', 'storage_id'),
+                   ('deployments_filters', 'storage_id'),
+                   ('deployments_labels', 'id'),
+                   ('execution_groups', 'storage_id'),
+                   ('execution_schedules', 'storage_id'),
+                   ('executions', 'storage_id'),
+                   ('groups', 'id'),
+                   ('inter_deployment_dependencies', 'storage_id'),
+                   ('licenses', 'id'),
+                   ('maintenance_mode', 'id'),
+                   ('managers', 'id'),
+                   ('node_instances', 'storage_id'),
+                   ('nodes', 'storage_id'),
+                   ('operations', 'storage_id'),
+                   ('permissions', 'id'),
+                   ('plugins', 'storage_id'),
+                   ('plugins_states', 'storage_id'),
+                   ('plugins_updates', 'storage_id'),
+                   ('roles', 'id'),
+                   ('secrets', 'storage_id'),
+                   ('sites', 'storage_id'),
+                   ('snapshots', 'storage_id'),
+                   ('tasks_graphs', 'storage_id'),
+                   ('tenants', 'id'),
+                   ('usage_collector', 'id'),
+                   ('users', 'id')]
 
 
 def upgrade():
     _create_audit_log_table()
-    _create_function_write_audit_log()
+    _create_functions_write_audit_log()
     _create_audit_triggers()
 
 
 def downgrade():
     _drop_audit_triggers()
-    _drop_function_write_audit_log()
+    _drop_functions_write_audit_log()
     _drop_audit_log_table()
 
 
@@ -139,67 +128,89 @@ def _drop_audit_log_table():
 
 
 # flake8: noqa
-def _create_function_write_audit_log():
+def _create_functions_write_audit_log():
     op.execute("""
-        CREATE OR REPLACE FUNCTION write_audit_log() RETURNS TRIGGER AS $func$
-        DECLARE
-            _table TEXT := TG_ARGV[0]::TEXT;
-            _id INTEGER;`
-            _user TEXT;
-            _execution TEXT;
+    CREATE OR REPLACE FUNCTION audit_username() RETURNS TEXT AS $$
         BEGIN
-            BEGIN
-                SELECT current_setting('audit.username') INTO _user;
-            EXCEPTION WHEN syntax_error_or_access_rule_violation THEN
-                _user = NULL;
-            END;
-            BEGIN
-                SELECT current_setting('audit.execution_id') INTO _execution;
-            EXCEPTION WHEN syntax_error_or_access_rule_violation THEN
-                _execution = NULL;
-            END;
-            IF (TG_OP = 'DELETE') THEN
-                IF (_table = 'blueprints_labels') THEN
-                    _id = OLD.id;
-                ELSE
-                    _id = OLD._storage_id;
-                END IF;
-                INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
-                    VALUES (_table, _id, 'delete', _user, _execution, now());
-                RETURN OLD;
-            ELSE
-                IF (_table = 'blueprints_labels') THEN
-                    _id = NEW.id;
-                ELSE
-                    _id = NEW._storage_id;
-                END IF;
-                IF (TG_OP = 'UPDATE') THEN
-                    INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
-                        VALUES (_table, _id, 'update', _user, _execution, now());
-                    RETURN NEW;
-                ELSIF (TG_OP = 'INSERT') THEN
-                    INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
-                        VALUES (_table, _id, 'create', _user, _execution, now());
-                    RETURN NEW;
-                END IF;
+            RETURN current_setting('audit.username');
+        EXCEPTION WHEN syntax_error_or_access_rule_violation THEN
+            RETURN NULL;
+        END;
+    $$ LANGUAGE plpgsql;
+
+
+    CREATE OR REPLACE FUNCTION audit_execution_id() RETURNS TEXT AS $$
+        BEGIN
+            RETURN current_setting('audit.execution_id');
+        EXCEPTION WHEN syntax_error_or_access_rule_violation THEN
+            IF (SELECT audit_username() IS NULL) THEN
+                RETURN 'administrative task';
             END IF;
             RETURN NULL;
         END;
-        $func$ LANGUAGE plpgsql;""")
+    $$ LANGUAGE plpgsql;
 
 
-def _drop_function_write_audit_log():
+    CREATE OR REPLACE FUNCTION write_audit_log_storage_id() RETURNS TRIGGER AS $$
+        DECLARE
+            _table TEXT := TG_ARGV[0]::TEXT;
+            _user TEXT := audit_username();
+            _execution_id TEXT := audit_execution_id();
+        BEGIN
+            IF (TG_OP = 'INSERT') THEN
+                INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
+                    VALUES (_table, NEW._storage_id, 'create', _user, _execution_id, now());
+                RETURN NEW;
+            ELSEIF (TG_OP = 'UPDATE') THEN
+                INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
+                    VALUES (_table, NEW._storage_id, 'update', _user, _execution_id, now());
+                RETURN NEW;
+            ELSEIF (TG_OP = 'DELETE') THEN
+                INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
+                    VALUES (_table, OLD._storage_id, 'delete', _user, _execution_id, now());
+                RETURN OLD;
+            END IF;
+            RETURN NULL;
+        END;
+    $$ LANGUAGE plpgsql;
+
+
+    CREATE OR REPLACE FUNCTION write_audit_log_id() RETURNS TRIGGER AS $$
+        DECLARE
+            _table TEXT := TG_ARGV[0]::TEXT;
+            _user TEXT := audit_username();
+            _execution_id TEXT := audit_execution_id();
+        BEGIN
+            IF (TG_OP = 'INSERT') THEN
+                INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
+                    VALUES (_table, NEW.id, 'create', _user, _execution_id, now());
+                RETURN NEW;
+            ELSEIF (TG_OP = 'UPDATE') THEN
+                INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
+                    VALUES (_table, NEW.id, 'update', _user, _execution_id, now());
+                RETURN NEW;
+            ELSEIF (TG_OP = 'DELETE') THEN
+                INSERT INTO audit_log (ref_table, ref_id, operation, creator_name, execution_id, created_at)
+                    VALUES (_table, OLD.id, 'delete', _user, _execution_id, now());
+                RETURN OLD;
+            END IF;
+            RETURN NULL;
+        END;
+    $$ LANGUAGE plpgsql;""")
+
+
+def _drop_functions_write_audit_log():
     op.execute("""DROP FUNCTION write_audit_log();""")
 
 
 def _create_audit_triggers():
-    for table_name in TABLES_TO_AUDIT:
+    for table_name, id_field in TABLES_TO_AUDIT:
         op.execute(f"""
             CREATE TRIGGER audit_{table_name}
-            AFTER INSERT OR UPDATE OR DELETE ON {table_name}
-            FOR EACH ROW EXECUTE PROCEDURE write_audit_log('{table_name}');""")
+            AFTER INSERT OR UPDATE OR DELETE ON {table_name} FOR EACH ROW
+            EXECUTE PROCEDURE write_audit_log_{id_field}('{table_name}');""")
 
 
 def _drop_audit_triggers():
-    for table_name in TABLES_TO_AUDIT:
+    for table_name, _ in TABLES_TO_AUDIT:
         op.execute(f"""DROP TRIGGER audit_{table_name} ON {table_name};""")
