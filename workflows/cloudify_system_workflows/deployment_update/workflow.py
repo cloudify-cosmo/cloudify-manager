bf1f484929c5bc6d95de0b630df206ef645981e4
asdf <tehasdf@gmail.com>
2021-08-25 13:39:12 +0200

    RD-3039 Create new nodes in the dep-update workflow
    Based on the steps and the plan, do create new nodes,
    and node-instances.
    
    I'm making a separate graph for the destructive actions.

--- workflows/cloudify_system_workflows/deployment_update/workflow.py
+++ workflows/cloudify_system_workflows/deployment_update/workflow.py
@@ -68,6 +68,34 @@ def create_steps(*, update_id):
     )
 
 
+def prepare_update_nodes(*, update_id):
+    client = get_rest_client()
+    dep_up = client.deployment_updates.get(update_id)
+    deployment = client.deployments.get(dep_up.deployment_id)
+    old_nodes = client.nodes.list(deployment_id=dep_up.deployment_id)
+    new_nodes = dep_up.deployment_plan['nodes'].copy()
+    old_instances = client.node_instances.list(
+        deployment_id=dep_up.deployment_id)
+    changed_instances = tasks.modify_deployment(
+        nodes=new_nodes,
+        previous_nodes=old_nodes,
+        previous_node_instances=old_instances,
+        scaling_groups=deployment.scaling_groups,
+        modified_nodes=()
+    )
+    workflow_ctx.logger.info('changed_instances %s', changed_instances)
+    update_nodes = {}
+    for step in dep_up.steps:
+        if step['entity_type'] != 'node':
+            continue
+        update_nodes.setdefault(step['action'], []).append(step['entity_id'])
+    client.deployment_updates.set_attributes(
+        update_id,
+        nodes=update_nodes,
+        node_instances=changed_instances
+    )
+
+
 def _prepare_update_graph(
         ctx,
         update_id,
@@ -89,16 +117,77 @@ def _prepare_update_graph(
         ctx.local_task(create_steps, kwargs={
             'update_id': update_id,
         }, total_retries=0),
+        ctx.local_task(prepare_update_nodes, kwargs={
+            'update_id': update_id,
+        }, total_retries=0),
     )
     return graph
 
 
+def create_new_nodes(*, update_id):
+    client = get_rest_client()
+    dep_up = client.deployment_updates.get(update_id)
+    update_nodes = dep_up['deployment_update_nodes'] or {}
+    plan_nodes = dep_up.deployment_plan['nodes']
+    create_nodes = []
+    for node_path in update_nodes.get('add', []):
+        node_name = node_path.split(':')[-1]
+        for plan_node in plan_nodes:
+            if plan_node['name'] == node_name:
+                create_nodes.append(plan_node)
+                new_node = plan_node
+                break
+        else:
+            raise RuntimeError(f'New node {node_name} not found in the plan')
+    client.nodes.create_many(
+        dep_up.deployment_id,
+        create_nodes
+    )
+
+
+def create_new_instances(*, update_id):
+    client = get_rest_client()
+    dep_up = client.deployment_updates.get(update_id)
+
+    update_instances = dep_up['deployment_update_node_instances']
+    if update_instances.get('added_and_related'):
+        client.node_instances.create_many(
+            dep_up.deployment_id,
+            update_instances['added_and_related']
+        )
+
+
+def _perform_update_graph(ctx, update_id, **kwargs):
+    graph = ctx.graph_mode()
+    seq = graph.sequence()
+    seq.add(
+        ctx.local_task(create_new_nodes, kwargs={
+            'update_id': update_id,
+        }, total_retries=0),
+        ctx.local_task(create_new_instances, kwargs={
+            'update_id': update_id,
+        }, total_retries=0),
+    )
+    return graph
+
+
+def _clear_graph(graph):
+    for task in graph.tasks:
+        graph.remove_task(task)
+
+
 @workflow
-def update_deployment(ctx, **kwargs):
+def update_deployment(ctx, *, preview=False, **kwargs):
     client = get_rest_client()
     update_id = '{0}_{1}'.format(ctx.deployment.id, ctx.execution_id)
     graph = _prepare_update_graph(ctx, update_id, **kwargs)
     graph.execute()
+
+
+    if not preview:
+        _clear_graph(graph)
+        graph = _perform_update_graph(ctx, update_id)
+        graph.execute()
 
     client.deployment_updates.set_attributes(
         update_id,
