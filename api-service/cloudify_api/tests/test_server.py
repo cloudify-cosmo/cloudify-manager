6c9452a0ebc6fae4d73b8fcdc28c425345abd5c5
Mateusz Neumann <mateumann@gmail.com>
2021-08-30 18:05:02 +0200

    Update tests

--- api-service/cloudify_api/tests/test_server.py
+++ api-service/cloudify_api/tests/test_server.py
@@ -2,13 +2,14 @@ import logging
 from typing import List
 
 import mock
+import pytest
 import unittest
 
 from cloudify_api.server import CloudifyAPI
 
 
 class MockConfig:
-    db_url = 'postgresql://user:secret@localhost/database'
+    async_dsn = 'postgresql+asyncpg://user:secret@localhost/database'
     postgresql_host = 'localhost'
     api_service_log_path = '/dev/null'
     api_service_log_level = 'WARNING'
@@ -17,9 +18,11 @@ class MockConfig:
 
 class CloudifyManagerServiceTest(unittest.TestCase):
     def test_server_init(self):
-        with mock.patch('manager_rest.config.instance', MockConfig()):
+        with mock.patch('manager_rest.config.instance', MockConfig()) as c:
             s = CloudifyAPI(load_config=False)
-            self.assertEqual(
-                'postgresql+asyncpg://user:secret@localhost/database',
-                s.settings.sqlalchemy_database_dsn)
+            self.assertEqual(c.async_dsn, s.settings.sqlalchemy_database_dsn)
             self.assertEqual(logging.WARNING, s.logger.level)
+
+    def test_server_load_config(self):
+        # it's either FileNotFoundError or sqlalchemy.exc.OperationalError
+        pytest.raises(Exception, CloudifyAPI, load_config=True)
