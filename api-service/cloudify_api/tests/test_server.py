dbe45797419534e249a63a6d6e2ab600739ae9cf
Mateusz Neumann <mateumann@gmail.com>
2021-09-01 13:19:21 +0200

    Update tests

--- api-service/cloudify_api/tests/test_server.py
+++ api-service/cloudify_api/tests/test_server.py
@@ -1,4 +1,3 @@
-import logging
 import unittest
 
 import mock
@@ -6,23 +5,16 @@ import mock
 from cloudify_api.server import CloudifyAPI
 
 
-class MockConfig:
-    async_dsn = 'postgresql+asyncpg://user:secret@localhost/database'
-    postgresql_host = 'localhost'
-    api_service_log_path = '/dev/null'
-    api_service_log_level = 'WARNING'
-    warnings: list = []
-
-
 class CloudifyManagerServiceTest(unittest.TestCase):
     def test_server_init(self):
-        with mock.patch('manager_rest.config.instance', MockConfig()) as c:
-            s = CloudifyAPI(load_config=False)
-            self.assertEqual(c.async_dsn, s.settings.sqlalchemy_database_dsn)
-            self.assertEqual(logging.WARNING, s.logger.level)
+        app = CloudifyAPI()
+        assert app.settings.cloudify_rest_config_file
+        assert app.settings.sqlalchemy_database_dsn is not None
+        assert app.logger
 
     def test_server_load_config_raises(self):
         with mock.patch('manager_rest.config.Config.load_from_file') as m:
             # TypeError when checking if postgresql_host address is IPv6
-            self.assertRaises(TypeError, CloudifyAPI, load_config=True)
+            s = CloudifyAPI()
+            self.assertRaises(TypeError, s.configure)
             m.assert_called_with('/opt/manager/cloudify-rest.conf')
