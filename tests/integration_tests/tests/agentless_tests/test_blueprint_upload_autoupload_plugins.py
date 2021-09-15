bf73ad2cfa05594f3a2de56468c3f5215e4ccdda
Michael Glukhman <michaelg@cloudify.co>
2021-09-15 11:05:41 +0300

    RD-2984 Move test with network failore to restservice unittests

--- tests/integration_tests/tests/agentless_tests/test_blueprint_upload_autoupload_plugins.py
+++ tests/integration_tests/tests/agentless_tests/test_blueprint_upload_autoupload_plugins.py
@@ -1,18 +1,3 @@
-########
-# Copyright (c) 2020 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    * See the License for the specific language governing permissions and
-#    * limitations under the License.
-
 import pytest
 import requests
 
@@ -125,64 +110,8 @@ class BlueprintUploadAutouploadPluginsTest(AgentlessTestCase):
         self.assertGreater(parse_version(plugin_versions[1]),
                            parse_version("3"))
 
-    def test_blueprint_upload_autoupload_plugins_network_error(self):
-        # block github in /etc/hosts
-        docker.execute(
-            self.env.container_id,
-            'sh -c "sudo cat /etc/hosts > /tmp/old_hosts"')
-        docker.execute(
-            self.env.container_id,
-            'sh -c "echo 127.0.0.1  github.com > /tmp/new_hosts"')
-        docker.execute(
-            self.env.container_id,
-            'sh -c "cat /tmp/new_hosts > /etc/hosts"')
-
-        blueprint_id = 'bp_os3_no_github'
-        blueprint_filename = 'blueprint_with_plugins_from_catalog_os3.yaml'
-        self.assertRaisesRegexp(
-            CloudifyClientError,
-            'Couldn\'t download plugin from',
-            self.client.blueprints.upload,
-            resource('dsl/{}'.format(blueprint_filename)),
-            entity_id=blueprint_id)
-        blueprint = self.client.blueprints.get(blueprint_id)
-        self.assertEqual(blueprint['state'], BlueprintUploadState.INVALID)
-
-        docker.execute(
-            self.env.container_id,
-            'sh -c "cat /tmp/old_hosts > /etc/hosts"')
-
     def _upload_and_verify_blueprint(self, blueprint_id, blueprint_filename):
         blueprint = self.client.blueprints.upload(
             resource('dsl/{}'.format(blueprint_filename)),
             entity_id=blueprint_id)
         self.assertEqual(blueprint['state'], BlueprintUploadState.UPLOADED)
-
-    def _verify_blueprint_uploaded(self, blueprint, blueprint_filename):
-        self.assertEqual(blueprint.state, BlueprintUploadState.UPLOADED)
-        self.assertEqual(blueprint.main_file_name, blueprint_filename)
-        self.assertNotEqual(blueprint.plan, None)
-        self._verify_blueprint_files(blueprint.id, blueprint_filename)
-
-    def _verify_blueprint_files(self, blueprint_id, blueprint_filename):
-        # blueprint available in manager resources
-        admin_headers = self.client._client.headers
-        resp = requests.get(
-            'https://{0}:53333/resources/blueprints/default_tenant/'
-            '{1}/{2}'.format(ipv6_url_compat(self.get_manager_ip()),
-                             blueprint_id,
-                             blueprint_filename),
-            headers=admin_headers,
-            verify=False
-        )
-        self.assertEqual(resp.status_code, requests.status_codes.codes.ok)
-        # blueprint archive available in uploaded blueprints
-        resp = requests.get(
-            'https://{0}:53333/uploaded-blueprints/default_tenant/'
-            '{1}/{1}.tar.gz'.format(
-                ipv6_url_compat(self.get_manager_ip()),
-                blueprint_id),
-            headers=admin_headers,
-            verify=False
-        )
-        self.assertEqual(resp.status_code, requests.status_codes.codes.ok)
