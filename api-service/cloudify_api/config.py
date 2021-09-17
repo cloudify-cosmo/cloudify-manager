46cd2fc3792a3b08ed570ce038a65c1929a5609d
Mateusz Neumann <mateumann@gmail.com>
2021-09-17 16:09:18 +0200

    Make Settings.*_dsn not empty str but None by default

--- api-service/cloudify_api/config.py
+++ api-service/cloudify_api/config.py
@@ -1,11 +1,13 @@
+from typing import Optional
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     cloudify_rest_config_file: str = '/opt/manager/cloudify-rest.conf'
-    sqlalchemy_dsn: str = ''
+    sqlalchemy_dsn: Optional[str]
     sqlalchemy_engine_options: dict = {}
-    asyncpg_dsn: str = ''
+    asyncpg_dsn: Optional[str]
 
 
 settings = Settings()
