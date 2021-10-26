d86ffece3c1e2e150ce9d23a1039e4cf17cd38de
asdf <tehasdf@gmail.com>
2021-10-26 16:14:52 +0200

    Bump this test
    As usual.

--- rest-service/manager_rest/test/endpoints/test_storage_manager.py
+++ rest-service/manager_rest/test/endpoints/test_storage_manager.py
@@ -157,7 +157,7 @@ class StorageManagerTests(base_test.BaseServerTestCase):
         self.sm.put(dep)
 
         serialized_dep = dep.to_response()
-        self.assertEqual(37, len(serialized_dep))
+        self.assertEqual(38, len(serialized_dep))
         self.assertEqual(dep.id, serialized_dep['id'])
         self.assertEqual(dep.created_at, serialized_dep['created_at'])
         self.assertEqual(dep.updated_at, serialized_dep['updated_at'])
@@ -177,6 +177,7 @@ class StorageManagerTests(base_test.BaseServerTestCase):
         serialized_dep.pop('latest_execution_finished_operations')
         serialized_dep.pop('has_sub_deployments')
         serialized_dep.pop('create_execution')
+        serialized_dep.pop('latest_execution')
 
         # Deprecated columns, for backwards compatibility -
         # was added to the response
