f7fc0d4f3f1b5af238288564eedd95546684c547
geokala <geokala@users.noreply.github.com>
2021-11-12 13:49:15 +0000

    RD-3502 Support owner/timestamp for sites (#3322)

--- rest-service/manager_rest/rest/resources_v3_1/sites.py
+++ rest-service/manager_rest/rest/resources_v3_1/sites.py
@@ -1,27 +1,15 @@
-#########
-# Copyright (c) 2013-2019 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 from flask import request
 
 from cloudify._compat import text_type
 from cloudify.models_states import VisibilityState
 
-from manager_rest.rest import rest_decorators
+from manager_rest.rest import rest_decorators, rest_utils
 from manager_rest.security import SecuredResource
 from manager_rest import utils, manager_exceptions
-from manager_rest.security.authorization import authorize
+from manager_rest.security.authorization import (
+    authorize,
+    check_user_action_allowed,
+)
 from manager_rest.storage import models, get_storage_manager
 from manager_rest.resource_manager import get_resource_manager
 from manager_rest.rest.rest_utils import (validate_inputs,
@@ -54,7 +42,16 @@ class SitesName(SecuredResource):
         new_site.longitude = request_dict.get('longitude')
         new_site.visibility = (request_dict['visibility'] or
                                VisibilityState.TENANT)
-        new_site.created_at = utils.get_formatted_timestamp()
+
+        if 'created_at' in request_dict:
+            check_user_action_allowed('set_timestamp')
+            new_site.created_at = request_dict['created_at']
+        else:
+            new_site.created_at = utils.get_formatted_timestamp()
+
+        if 'creator' in request_dict:
+            check_user_action_allowed('set_owner')
+            new_site.creator = rest_utils.valid_user(request_dict['creator'])
         return get_storage_manager().put(new_site)
 
     @rest_decorators.marshal_with(models.Site)
