b17477fccf4b819e728643daa5cb9fe477a77260
geokala <geokala@users.noreply.github.com>
2021-11-30 13:33:09 +0000

    RD-3507 Support not parsing a blueprint+set state (#3362)
    * RD-3507 Support not parsing a blueprint+set state
    
    * Do not announce cake
    
    While cake is good, we must bear in mind there are a variety of desserts,
    and to only dump debugging output to something cake related is not
    sufficiently impartial.
    
    * RD-3507 Don't skip bp upload execution by default

--- workflows/cloudify_system_workflows/blueprint.py
+++ workflows/cloudify_system_workflows/blueprint.py
@@ -1,18 +1,3 @@
-########
-# Copyright (c) 2020 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    * See the License for the specific language governing permissions and
-#    * limitations under the License.
-
 import os
 import shutil
 import tempfile
@@ -21,7 +6,6 @@ import traceback
 
 from setuptools import archive_util
 
-from cloudify._compat import unquote
 from cloudify.decorators import workflow
 from cloudify.manager import get_rest_client
 from cloudify.models_states import BlueprintUploadState
@@ -115,7 +99,7 @@ def upload(ctx, **kwargs):
 
     # get actual app file name
     if app_file_name:
-        app_file_name = unquote(app_file_name)
+        app_file_name = app_file_name
         application_file = os.path.join(app_dir, app_file_name)
         if not os.path.isfile(application_file):
             error_msg = '{0} does not exist in the application ' \
