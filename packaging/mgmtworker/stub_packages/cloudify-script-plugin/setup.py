873a6314d0abd6dc6e1d0ccb17db781a4d3ce839
opencm <limor@gigaspaces.com>
2021-12-07 13:59:11 +0000

    Bump version

--- packaging/mgmtworker/stub_packages/cloudify-script-plugin/setup.py
+++ packaging/mgmtworker/stub_packages/cloudify-script-plugin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-script-plugin',
-    version='6.0.0',
+    version='6.0.1.dev1',
     packages=[],
     description='[DEPRECATED] A stub for the '
                 'old cloudify-script-plugin package',
