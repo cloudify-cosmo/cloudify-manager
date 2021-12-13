32e7509abc6eb32e122e11d16accb00ab29ac6dd
Madda <nobody@nowhere.local>
2021-12-13 09:50:11 +0100

    RD-3507 Move zip archive creation to common

--- workflows/cloudify_system_workflows/snapshots/snapshot_create.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_create.py
@@ -1,18 +1,3 @@
-########
-# Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    * See the License for the specific language governing permissions and
-#    * limitations under the License.
-
 import os
 import json
 import shutil
@@ -21,6 +6,7 @@ import tempfile
 from cloudify.workflows import ctx
 from cloudify.manager import get_rest_client
 from cloudify.constants import FILE_SERVER_SNAPSHOTS_FOLDER
+from cloudify.zip_utils import make_zip64_archive
 
 from . import constants, networks, utils
 from .agents import Agents
@@ -144,7 +130,7 @@ class SnapshotCreate(object):
         snapshot_archive_name = self._get_snapshot_archive_name()
         ctx.logger.info(
             'Creating snapshot archive: {0}'.format(snapshot_archive_name))
-        utils.make_zip64_archive(snapshot_archive_name, self._tempdir)
+        make_zip64_archive(snapshot_archive_name, self._tempdir)
 
     def _get_snapshot_archive_name(self):
         """Return the base name for the snapshot archive
