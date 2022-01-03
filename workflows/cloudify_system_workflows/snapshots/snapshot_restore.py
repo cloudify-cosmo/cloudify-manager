e83523f5a0bd98f1fd362cfc75ecc17b760bd633
Madda <nobody@nowhere.local>
2022-01-03 11:09:57 +0100

    RD-3781 Preserve zip permissions

--- workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
+++ workflows/cloudify_system_workflows/snapshots/snapshot_restore.py
@@ -67,6 +67,53 @@ from .constants import (
 from .utils import is_later_than_now, parse_datetime_string, get_tenants_list
 
 
+# Reproduced/modified from patch for https://bugs.python.org/issue15795
+class ZipFile(zipfile.ZipFile):
+    def _extract_member(self, member, targetpath, pwd):
+        """Extract the ZipInfo object 'member' to a physical
+           file on the path targetpath.
+        """
+        if not isinstance(member, zipfile.ZipInfo):
+            member = self.getinfo(member)
+
+        # build the destination pathname, replacing
+        # forward slashes to platform specific separators.
+        arcname = member.filename.replace('/', os.path.sep)
+
+        if os.path.altsep:
+            arcname = arcname.replace(os.path.altsep, os.path.sep)
+        # interpret absolute pathname as relative, remove drive letter or
+        # UNC path, redundant separators, "." and ".." components.
+        arcname = os.path.splitdrive(arcname)[1]
+        invalid_path_parts = ('', os.path.curdir, os.path.pardir)
+        arcname = os.path.sep.join(x for x in arcname.split(os.path.sep)
+                                   if x not in invalid_path_parts)
+        if os.path.sep == '\\':
+            # filter illegal characters on Windows
+            arcname = self._sanitize_windows_name(arcname, os.path.sep)
+
+        targetpath = os.path.join(targetpath, arcname)
+        targetpath = os.path.normpath(targetpath)
+
+        # Create all upper directories if necessary.
+        upperdirs = os.path.dirname(targetpath)
+        if upperdirs and not os.path.exists(upperdirs):
+            os.makedirs(upperdirs)
+
+        if member.is_dir():
+            if not os.path.isdir(targetpath):
+                os.mkdir(targetpath)
+            return targetpath
+
+        with self.open(member, pwd=pwd) as source, \
+                open(targetpath, "wb") as target:
+            shutil.copyfileobj(source, target)
+
+        mode = member.external_attr >> 16 & 0xFFF
+        os.chmod(targetpath, mode)
+        return targetpath
+
+
 class BufferLogger(object):
     def __init__(self):
         self._buffer = []
@@ -679,7 +726,7 @@ class SnapshotRestore(object):
         :return: A dict representing the metadata json file
         """
         ctx.logger.debug('Extracting snapshot: {0}'.format(snapshot_path))
-        with zipfile.ZipFile(snapshot_path, 'r') as zipf:
+        with ZipFile(snapshot_path, 'r') as zipf:
             zipf.extractall(self._tempdir)
         with open(os.path.join(self._tempdir, METADATA_FILENAME), 'r') as f:
             metadata = json.load(f)
