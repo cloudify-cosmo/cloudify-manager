f3ca9be7ad0c5617ced3890908c8bed1a936d771
opencm <limor@gigaspaces.com>
2022-02-07 13:49:44 +0000

    Bump version

--- packaging/mgmtworker/stub_packages/cloudify-rest-client/setup.py
+++ packaging/mgmtworker/stub_packages/cloudify-rest-client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-rest-client',
-    version='6.3.0',
+    version='6.3.1.dev1',
     packages=[],
     description='[DEPRECATED] A stub for the old cloudify-rest-client package',
 )
