0ae3645fdbe4cd36968d87afe38cda20e5423cfc
Michael Glukhman <michaelg@cloudify.co>
2022-02-07 14:41:58 +0200

    RD-3950 Add POST Community Contacts EP

---
+++ rest-service/manager_rest/rest/resources_v3_1/community_contacts.py
@@ -0,0 +1,47 @@
+from flask import request
+from requests import post
+
+from cloudify._compat import text_type
+from manager_rest.security import SecuredResource
+from manager_rest.security.authorization import authorize
+from manager_rest.rest.rest_utils import get_json_and_verify_params
+from manager_rest import config, premium_enabled, manager_exceptions
+
+CREATE_CONTACT_URL = "https://api.cloudify.co/cloudifyCommunityCreateContact"
+
+class CommunityContacts(SecuredResource):
+    @authorize('community_contact_create')
+    def post(self, **kwargs):
+        if premium_enabled:
+            raise manager_exceptions.CommunityOnly()
+        request_dict = get_json_and_verify_params({
+            'first_name': {'type': text_type},
+            'last_name': {'type': text_type},
+            'email': {'type': text_type},
+            'phone': {'type': text_type},
+            'is_eula': {'type': bool},
+            'is_send_services_details': {'optional': True, 'type': bool}
+        })
+        if not request_dict['is_eula']:
+            raise manager_exceptions.BadParametersError(
+                "EULA must be confirmed by user")
+        r = post(CREATE_CONTACT_URL, json={
+            "firstname": request_dict['first_name'],
+            "lastname": request_dict['last_name'],
+            "email": request_dict['email'],
+            "phone": request_dict['phone']
+        })
+        generic_error_msg = 'There was a problem while submiting the form, ' \
+                            'please try later'
+        if not r.ok:
+            raise manager_exceptions.UnknownAction(generic_error_msg)
+        r_json = r.json()
+        r_status = r_json['status']
+        if r_status == 200:
+            return { 'customer_id': 'COM-{}-{}'.format(
+                r_json['company_name'], r_json['contact_id']) }
+        elif r_status == 409:
+            raise manager_exceptions.ConflictError(
+                'There was a conflict while submiting your details')
+        else:
+            raise manager_exceptions.UnknownAction(generic_error_msg)
