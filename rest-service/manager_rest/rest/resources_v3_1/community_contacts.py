810be30002f57f57c7a240acf376aef78615a86b
Michael Glukhman <michaelg@cloudify.co>
2022-02-08 14:57:39 +0200

    RD-3950 Error logging

--- rest-service/manager_rest/rest/resources_v3_1/community_contacts.py
+++ rest-service/manager_rest/rest/resources_v3_1/community_contacts.py
@@ -1,13 +1,15 @@
-from flask import request
 from requests import post
+from flask import current_app
+from json import JSONDecodeError
 
 from cloudify._compat import text_type
 from manager_rest.security import SecuredResource
 from manager_rest.security.authorization import authorize
+from manager_rest import premium_enabled, manager_exceptions
 from manager_rest.rest.rest_utils import get_json_and_verify_params
-from manager_rest import config, premium_enabled, manager_exceptions
 
 CREATE_CONTACT_URL = "https://api.cloudify.co/cloudifyCommunityCreateContact"
+
 
 class CommunityContacts(SecuredResource):
     @authorize('community_contact_create')
@@ -29,19 +31,42 @@ class CommunityContacts(SecuredResource):
             "firstname": request_dict['first_name'],
             "lastname": request_dict['last_name'],
             "email": request_dict['email'],
-            "phone": request_dict['phone']
+            "phone": request_dict['phone'],
+            "send_services_details":
+                request_dict.get('is_send_services_details')
         })
         generic_error_msg = 'There was a problem while submiting the form, ' \
                             'please try later'
+        error_log_msg = 'Error creating contact "%s" - %s: %s'
         if not r.ok:
+            current_app.logger.error(error_log_msg,
+                                     request_dict['email'],
+                                     r.status_code,
+                                     r.text)
             raise manager_exceptions.UnknownAction(generic_error_msg)
-        r_json = r.json()
+        try:
+            r_json = r.json()
+        except JSONDecodeError:
+            current_app.logger.error(error_log_msg,
+                                     request_dict['email'],
+                                     500,
+                                     "Could not decode response JSON")
+            raise manager_exceptions.UnknownAction(generic_error_msg)
+
         r_status = r_json['status']
         if r_status == 200:
-            return { 'customer_id': 'COM-{}-{}'.format(
-                r_json['company_name'], r_json['contact_id']) }
+            return {'customer_id': 'COM-{}-{}'.format(r_json['company_name'],
+                                                      r_json['contact_id'])}
         elif r_status == 409:
+            current_app.logger.error(error_log_msg,
+                                     request_dict['email'],
+                                     r_status,
+                                     r_json['message'])
             raise manager_exceptions.ConflictError(
                 'There was a conflict while submiting your details')
         else:
+            current_app.logger.error(error_log_msg,
+                                     request_dict['email'],
+                                     r_status,
+                                     r_json['message'])
             raise manager_exceptions.UnknownAction(generic_error_msg)
