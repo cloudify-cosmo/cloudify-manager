e3d50bcaf99554434e789e1aefea3a8babed0771
Mateusz Neumann <mateumann@gmail.com>
2022-02-16 17:03:58 +0100

    Allow to search deployments by display_name and tenant_name

--- rest-service/manager_rest/storage/resource_models.py
+++ rest-service/manager_rest/storage/resource_models.py
@@ -505,7 +505,8 @@ class Deployment(CreatedAtMixin, SQLResourceBase):
 
     @classproperty
     def allowed_filter_attrs(cls):
-        return ['blueprint_id', 'created_by', 'site_name', 'schedules']
+        return ['blueprint_id', 'created_by', 'site_name', 'schedules',
+                'tenant_name', 'display_name']
 
     def to_response(self, include=None, **kwargs):
         include = include or self.response_fields
