9ce3c656adc9218a79e657043b6d6508819fd360
Michael Glukhman <michaelg@cloudify.co>
2022-03-24 11:50:19 +0200

    RD-4341 No actual need to check consumers list for ODSR. Just install

--- cloudify_types/cloudify_types/shared_resource/operations.py
+++ cloudify_types/cloudify_types/shared_resource/operations.py
@@ -43,17 +43,15 @@ def _mark_verified_shared_resource_node(deployment_id):
 def _checkin_resource_consumer(client, deployment_id):
     deployment = client.deployments.get(deployment_id)
     type_labels = _get_label_values(deployment.labels, 'csys-obj-type')
-    consumers_list = _get_label_values(deployment.labels, 'csys-consumer-id')
     if deployment.installation_status == 'inactive':
-        if 'on-demand-resource' in type_labels and not consumers_list:
+        if 'on-demand-resource' in type_labels:
             execute_workflow_base(client, 'install', deployment_id)
-        elif 'on-demand-resource' not in type_labels:
-            error_msg = \
-                f'SharedResource\'s deployment "{deployment_id}" is in an '\
-                f'inactive state and isn\'t labeled as ' \
-                f'`csys-obj-type: on-demand-resource`. Please install the '\
-                f'deployment first'
-            raise NonRecoverableError(error_msg)
+        else:
+            raise NonRecoverableError(
+                f'SharedResource\'s deployment "{deployment_id}" is in an '
+                f'inactive state and isn\'t labeled as '
+                f'`csys-obj-type: on-demand-resource`. Please install the '
+                f'deployment first')
 
 
 def _checkout_resource_consumer(client, deployment_id):
