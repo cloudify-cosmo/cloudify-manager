d15bc3bc18e9248515357e84af03536e70440681
asdf <tehasdf@gmail.com>
2022-07-05 14:16:45 +0200

    tests: factor out `deploy`
    and also add a test too

--- workflows/cloudify_system_workflows/tests/deployment_update/test_workflow.py
+++ workflows/cloudify_system_workflows/tests/deployment_update/test_workflow.py
@@ -7,27 +7,52 @@ def dsl_path_base():
     return os.path.join(os.path.dirname(__file__), 'blueprints')
 
 
+def deploy(blueprint_filename, resource_id='d1', storage=None, inputs=None):
+    if storage is None:
+        storage = local.InMemoryStorage()
+    blueprint_path = os.path.join(dsl_path_base(), blueprint_filename)
+    storage.create_blueprint(resource_id, blueprint_path)
+    storage.create_deployment(resource_id, resource_id, inputs=inputs)
+    return storage
+
+
+def test_empty_update():
+    storage = deploy('empty.yaml', resource_id='d1')
+    dep_env = local.load_env('d1', storage)
+    storage.create_deployment_update('d1', 'update1', {})
+    dep_env.execute('update', parameters={'update_id': 'update1'})
+    executions = dep_env.storage.get_executions()
+    assert len(executions) == 1
+    assert executions[0]['workflow_id'] == 'update'
+    assert executions[0]['status'] == 'terminated'
+
+
 def test_run_update_workflow():
-    storage = local.InMemoryStorage()
-    storage.create_blueprint(
-        'bp1',
-        os.path.join(dsl_path_base(), 'empty.yaml'),
-    )
+    storage = deploy('empty.yaml', resource_id='d1')
     storage.create_blueprint(
         'bp2',
         os.path.join(dsl_path_base(), 'description.yaml'),
     )
-    storage.create_deployment('dep1', 'bp1')
-    original_description = storage.get_deployment('dep1').description
-    dep_env = local.load_env('dep1', storage)
-    storage.create_deployment_update('dep1', 'update1', {
-        'id': 'update1',
+    original_description = storage.get_deployment('d1').description
+    dep_env = local.load_env('d1', storage)
+    storage.create_deployment_update('d1', 'update1', {
         'new_blueprint_id': 'bp2',
-        'old_blueprint_id': 'bp1',
-        'new_inputs': {},
-        'runtime_only_evaluation': False,
-        'deployment_id': 'dep1',
     })
     dep_env.execute('update', parameters={'update_id': 'update1'})
-    changed_description = storage.get_deployment('dep1').description
+    changed_description = storage.get_deployment('d1').description
     assert changed_description != original_description
+
+
+def test_change_property():
+    storage = deploy('property_input.yaml', resource_id='d1', inputs={
+        'inp1': 'value1',
+    })
+    dep_env = local.load_env('d1', storage)
+    storage.create_deployment_update('d1', 'update1', {
+        'new_inputs': {'inp1': 'value2'}
+    })
+    dep_env.execute('update', parameters={'update_id': 'update1'})
+    # reload the storage to get the updated deployment + node
+    dep_env = local.load_env('d1', storage)
+    node = dep_env.storage.get_node('n1', evaluate_functions=True)
+    assert node.properties['prop1'] == 'value2'
