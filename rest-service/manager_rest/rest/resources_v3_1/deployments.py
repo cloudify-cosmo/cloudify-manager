c7a14e6f4814918f65dfacb2e3f477b40eac3a59
Madda <nobody@nowhere.local>
2022-08-16 10:17:16 +0200

    Pass workflows through even if null
    RM will handle it.

--- rest-service/manager_rest/rest/resources_v3_1/deployments.py
+++ rest-service/manager_rest/rest/resources_v3_1/deployments.py
@@ -236,20 +236,18 @@ class DeploymentsId(resources_v1.DeploymentsId):
         with sm.transaction():
             if not skip_plugins_validation:
                 rm.check_blueprint_plugins_installed(blueprint.plan)
-            dep_args = {
-                "private_resource": args.private_resource,
-                "visibility": visibility,
-                "site": site,
-                "runtime_only_evaluation": request_dict.get(
+            deployment = rm.create_deployment(
+                blueprint,
+                deployment_id,
+                private_resource=args.private_resource,
+                visibility=visibility,
+                site=site,
+                runtime_only_evaluation=request_dict.get(
                     'runtime_only_evaluation', False),
-                "created_at": created_at,
-                "created_by": owner,
-            }
-            workflows = request_dict.get('workflows')
-            if workflows:
-                dep_args['workflows'] = workflows
-            deployment = rm.create_deployment(blueprint, deployment_id,
-                                              **dep_args)
+                created_at=created_at,
+                created_by=owner,
+                workflows=request_dict.get('workflows'),
+            )
             if request_dict.get('workdir_zip'):
                 tmpdir_path = mkdtemp()
                 try:
