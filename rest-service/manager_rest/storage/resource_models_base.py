9b4ddb038fb85177b54340280d5f32e4de19e7b7
Madda <nobody@nowhere.local>
2022-09-05 13:28:10 +0200

    RD-2873 Return created_by in labels

--- rest-service/manager_rest/storage/resource_models_base.py
+++ rest-service/manager_rest/storage/resource_models_base.py
@@ -1,18 +1,3 @@
-#########
-# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 from typing import Dict, List
 
 from flask_restful import fields
@@ -141,7 +126,7 @@ class SQLResourceBase(SQLModelBase):
         return [Label(key=label.key,
                       value=label.value,
                       created_at=label.created_at,
-                      creator_id=label.creator.id)
+                      created_by=label.creator.username)
                 for label in labels]
 
     def check_unique_query(self):
