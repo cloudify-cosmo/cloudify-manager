963718267da5615f6b7fe66dab754067e42a60ff
asdf <tehasdf@gmail.com>
2022-09-07 10:46:54 +0200

    rewrite other tests the same way!

--- rest-service/manager_rest/test/infrastructure/test_resolver_with_catalog_support.py
+++ rest-service/manager_rest/test/infrastructure/test_resolver_with_catalog_support.py
@@ -14,7 +14,11 @@
 #  * limitations under the License.
 
 import os
+import shutil
+from datetime import datetime
 
+from manager_rest.constants import FILE_SERVER_PLUGINS_FOLDER
+from manager_rest.storage import models
 from manager_rest.test.base_test import BaseServerTestCase
 from manager_rest.resolver_with_catalog_support import (
     ResolverWithCatalogSupport,
@@ -36,32 +40,61 @@ class TestPluginParseWithResolver(BaseServerTestCase):
     def setUp(self):
         super(TestPluginParseWithResolver, self).setUp()
         self.resolver = ResolverWithCatalogSupport(storage_manager=self.sm)
+        self.plugin1 = self._plugin(
+            yaml='# this_is_plugin_1',
+            id='plugin1',
+        )
+        self.plugin2 = self._plugin(
+            yaml='# this_is_plugin_2',
+            id='plugin2',
+            package_version=TEST_PLUGIN_VERSION2,
+        )
+
+    def _plugin_dir(self, plugin):
+        return os.path.join(
+            self.server_configuration.file_server_root,
+            FILE_SERVER_PLUGINS_FOLDER,
+            plugin.id,
+        )
+
+    def _plugin(self, yaml, **kwargs):
+        plugin_kwargs = {
+            'package_name': TEST_PACKAGE_NAME,
+            'package_version': TEST_PLUGIN_VERSION1,
+            'archive_name': 'archive.wgn',
+            'uploaded_at': datetime.utcnow(),
+            'wheels': [],
+            'creator': self.user,
+            'tenant': self.tenant,
+        }
+        plugin_kwargs.update(kwargs)
+        plugin = models.Plugin(**plugin_kwargs)
+        plugin_dir = self._plugin_dir(plugin)
+        os.makedirs(plugin_dir)
+        self.addCleanup(shutil.rmtree, plugin_dir)
+        with open(os.path.join(plugin_dir, 'plugin.yaml'), 'w') as f:
+            f.write(yaml)
+        return plugin
 
     def test_successful_plugin_import_resolver(self):
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION1)
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION2,
-                           package_yaml='mock_blueprint/plugin2.yaml')
         import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             TEST_PLUGIN_VERSION1)
         plugin_str = self.resolver.fetch_import(import_url)
-        self.assertTrue('this_is_plugin_1' in plugin_str)
+        assert 'this_is_plugin_1' in plugin_str
 
     def test_not_existing_plugin_import_resolver(self):
         with self.assertRaisesRegex(
                 InvalidPluginError, r'Plugin .+ not found'):
             self.resolver.fetch_import(
-                import_url=PLUGIN_PREFIX + TEST_PACKAGE_NAME)
+                import_url=PLUGIN_PREFIX + 'other-plugin')
 
     def test_fetches_max_in_range(self):
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION1)
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION2,
-                           package_yaml='mock_blueprint/plugin2.yaml')
         import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             '>={0},<={1}'.format(TEST_PLUGIN_VERSION1, TEST_PLUGIN_VERSION2))
         plugin_str = self.resolver.fetch_import(import_url)
-        self.assertTrue('this_is_plugin_2' in plugin_str)
+        assert 'this_is_plugin_2' in plugin_str
 
         import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
@@ -70,19 +103,13 @@ class TestPluginParseWithResolver(BaseServerTestCase):
         self.assertTrue('this_is_plugin_1' in plugin_str)
 
     def test_fetches_max_in_range_with_constraint(self):
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION1)
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION2,
-                           package_yaml='mock_blueprint/plugin2.yaml')
         import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             '>={0},!={1}'.format(TEST_PLUGIN_VERSION1, TEST_PLUGIN_VERSION2))
         plugin_str = self.resolver.fetch_import(import_url)
-        self.assertTrue('this_is_plugin_1' in plugin_str)
+        assert 'this_is_plugin_1' in plugin_str
 
     def test_no_version_found(self):
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION1)
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION2,
-                           package_yaml='mock_blueprint/plugin2.yaml')
         import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             '!={0},!={1}'.format(TEST_PLUGIN_VERSION1, TEST_PLUGIN_VERSION2))
@@ -97,7 +124,6 @@ class TestPluginParseWithResolver(BaseServerTestCase):
             self.resolver.fetch_import(import_url)
 
     def test_bad_version_format(self):
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION1)
         import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             ',{0}'.format(TEST_PLUGIN_VERSION1))
@@ -107,20 +133,13 @@ class TestPluginParseWithResolver(BaseServerTestCase):
             self.resolver.fetch_import(import_url)
 
     def test_fetches_max_with_general_range(self):
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION1)
-        self.upload_plugin(TEST_PACKAGE_NAME, TEST_PLUGIN_VERSION2,
-                           package_yaml='mock_blueprint/plugin2.yaml')
         import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             '')
         plugin_str = self.resolver.fetch_import(import_url)
-        self.assertTrue('this_is_plugin_2' in plugin_str)
+        assert 'this_is_plugin_2' in plugin_str
 
     def test_single_plugin_yaml_compatibility(self):
-        self.upload_plugin_multiple_yamls(
-            TEST_PACKAGE_NAME,
-            TEST_PLUGIN_VERSION1,
-            package_yamls=['mock_blueprint/plugin.yaml'])
         import_url = PLUGIN_IMPORT_FORMAT.format(TEST_PACKAGE_NAME, '')
         plugin_str = self.resolver.fetch_import(import_url, '')
         assert plugin_str
@@ -130,61 +149,38 @@ class TestPluginParseWithResolver(BaseServerTestCase):
         assert plugin_str
 
     def test_fetches_proper_plugin_yaml(self):
-        self.upload_plugin_multiple_yamls(
+        plugin1_dir = self._plugin_dir(self.plugin1)
+        with open(os.path.join(plugin1_dir, 'plugin_1_3.yaml'), 'w') as f:
+            f.write('# this_is_plugin_1, DSL version 1_3')
+
+        import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             TEST_PLUGIN_VERSION1,
-            package_yamls=['mock_blueprint/plugin.yaml',
-                           'mock_blueprint/plugin_1_3.yaml'])
-        import_url = PLUGIN_IMPORT_FORMAT.format(TEST_PACKAGE_NAME, '')
+        )
         plugin_str = self.resolver.fetch_import(import_url, '1_3')
         assert plugin_str.startswith('# this_is_plugin_1, DSL version 1_3')
         plugin_str = self.resolver.fetch_import(import_url)
         assert plugin_str.startswith('# this_is_plugin_1')
         plugin_str = self.resolver.fetch_import(import_url, '1_4')
-        assert plugin_str.startswith('# this_is_plugin_1\n')
+        assert plugin_str.startswith('# this_is_plugin_1')
 
     def test_fetch_plugin_yaml_matching_dsl_version(self):
-        self.upload_plugin_multiple_yamls(
+        plugin1_dir = self._plugin_dir(self.plugin1)
+        with open(os.path.join(plugin1_dir, 'plugin_1_3.yaml'), 'w') as f:
+            f.write('# this_is_plugin_1, DSL version 1_3')
+        with open(os.path.join(plugin1_dir, 'plugin_1_4.yaml'), 'w') as f:
+            f.write('# this_is_plugin_1, DSL version 1_4')
+
+        import_url = PLUGIN_IMPORT_FORMAT.format(
             TEST_PACKAGE_NAME,
             TEST_PLUGIN_VERSION1,
-            package_yamls=['mock_blueprint/plugin_1_3.yaml',
-                           'mock_blueprint/plugin_1_4.yaml'])
-        import_url = PLUGIN_IMPORT_FORMAT.format(TEST_PACKAGE_NAME, '')
+        )
         plugin_str = self.resolver.fetch_import(import_url)
         assert plugin_str.startswith('# this_is_plugin_1')
         plugin_str = self.resolver.fetch_import(import_url, '1_3')
         assert plugin_str.startswith('# this_is_plugin_1, DSL version 1_3')
         plugin_str = self.resolver.fetch_import(import_url, '1_4')
         assert plugin_str.startswith('# this_is_plugin_1, DSL version 1_4')
-
-    @classmethod
-    def upload_plugin_multiple_yamls(
-            cls,
-            package_name,
-            package_version,
-            package_yamls=None):
-        if package_yamls is None:
-            package_yamls = ['mock_blueprint/plugin.yaml']
-        wgn_path, yaml_paths = cls._create_wagon_and_yamls(
-            package_name,
-            package_version,
-            package_yamls
-        )
-        zip_path = cls.zip_files([wgn_path] + yaml_paths)
-        response = cls.post_file('/plugins', zip_path)
-        os.remove(wgn_path)
-        return response
-
-    @staticmethod
-    def _create_wagon_and_yamls(
-            package_name,
-            package_version,
-            package_yaml_files=['mock_blueprint/plugin.yaml']):
-        temp_file_path = BaseServerTestCase.create_wheel(package_name,
-                                                         package_version)
-        yaml_paths = [BaseServerTestCase.get_full_path(package_yaml_file)
-                      for package_yaml_file in package_yaml_files]
-        return temp_file_path, yaml_paths
 
 
 class TestBlueprintParseWithResolver(BaseServerTestCase):
