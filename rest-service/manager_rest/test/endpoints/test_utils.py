e76be063c86ab99433a7ffac653a0846e26e1261
Madda <nobody@nowhere.local>
2022-09-13 16:23:21 +0200

    Revert changes to generate_progress_func asserter

--- rest-service/manager_rest/test/endpoints/test_utils.py
+++ rest-service/manager_rest/test/endpoints/test_utils.py
@@ -1,6 +1,5 @@
 def generate_progress_func(total_size, buffer_size=8192):
     """Generate a function that helps test upload/download progress
-
     :param total_size: Total size of the file to upload/download
     :param buffer_size: Size of chunk
     :return: A function that receives 2 ints - number of bytes read so far,
@@ -10,15 +9,15 @@ def generate_progress_func(total_size, buffer_size=8192):
     iteration = [0]
     max_iterations = total_size // buffer_size
 
-    def print_progress(read_bytes, total_bytes):
+    def print_progress(read, total):
         i = iteration[0]
-        assert read_bytes == total_bytes
+        assert total == total_size
 
         expected_read_value = buffer_size * (i + 1)
         if i < max_iterations:
-            assert read_bytes == expected_read_value
+            assert read == expected_read_value
         else:
-            assert read_bytes == total_bytes
+            assert read == total_size
 
         iteration[0] += 1
 
