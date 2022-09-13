b2502d127f0c3030de98765dd4e3e9a990522c0d
Madda <nobody@nowhere.local>
2022-09-13 14:46:08 +0200

    Assert the right thing

--- rest-service/manager_rest/test/endpoints/test_utils.py
+++ rest-service/manager_rest/test/endpoints/test_utils.py
@@ -1,19 +1,3 @@
-#########
-# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
-
 def generate_progress_func(total_size, buffer_size=8192):
     """Generate a function that helps test upload/download progress
 
@@ -30,7 +14,7 @@ def generate_progress_func(total_size, buffer_size=8192):
         read_bytes, total_bytes = watcher.bytes_read, watcher.len
 
         i = iteration[0]
-        assert read_bytes == total_bytes
+        assert read_size == total_bytes
 
         expected_read_value = buffer_size * (i + 1)
         if i < max_iterations:
