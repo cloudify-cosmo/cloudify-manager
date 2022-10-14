2636acd9574717e2b4636a6a2d259b1393f56995
Mateusz Neumann <mateumann@gmail.com>
2022-10-14 14:18:44 +0200

    Migration scripts (on snapshot restore)

--- mgmtworker/cloudify_system_workflows/snapshots/constants.py
+++ mgmtworker/cloudify_system_workflows/snapshots/constants.py
@@ -71,6 +71,7 @@ V_5_1_0 = ManagerVersion('5.1.0')
 V_5_2_0 = ManagerVersion('5.2.0')
 V_5_3_0 = ManagerVersion('5.3.0')
 V_6_0_0 = ManagerVersion('6.0.0')
+V_7_0_0 = ManagerVersion('7.0.0')
 
 
 class VisibilityState(object):
