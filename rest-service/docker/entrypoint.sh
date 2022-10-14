a955c4635c2b4fc4978c79bd797d3502f5baaac4
Adrian Polańczyk <adrian.polanczyk@codilime.com>
2022-10-14 10:54:43 +0200

    RD-6102 Changes requested by reviewers v3

--- rest-service/docker/entrypoint.sh
+++ rest-service/docker/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env -S bash -eux
 
-exec \
-  /opt/rest-service/docker/prepare_secrets.sh \
-  & \
-  gunicorn \
-    --pid /run/cloudify-restservice/pid \
-    --chdir / \
-    --workers $WORKER_COUNT \
-    --max-requests $MAX_REQUESTS \
-    --bind 0.0.0.0:$PORT \
-    --timeout 300 \
-    --access-logfile /var/log/cloudify/rest/audit.log \
-    --access-logformat '%(t)s %(h)s %({X-Cloudify-Audit-Username}o)s %({X-Cloudify-Audit-Tenant}o)s %({X-Cloudify-Audit-Auth-Method}o)s "%(r)s" %(s)s %(b)s "%(a)s" took %(M)sms' \
-    manager_rest.wsgi:app
+/opt/rest-service/docker/prepare_secrets.sh
+
+exec gunicorn \
+  --pid /run/cloudify-restservice/pid \
+  --chdir / \
+  --workers $WORKER_COUNT \
+  --max-requests $MAX_REQUESTS \
+  --bind 0.0.0.0:$PORT \
+  --timeout 300 \
+  --access-logfile /var/log/cloudify/rest/audit.log \
+  --access-logformat '%(t)s %(h)s %({X-Cloudify-Audit-Username}o)s %({X-Cloudify-Audit-Tenant}o)s %({X-Cloudify-Audit-Auth-Method}o)s "%(r)s" %(s)s %(b)s "%(a)s" took %(M)sms' \
+  manager_rest.wsgi:app
--- rest-service/docker/entrypoint_config.sh
+++ rest-service/docker/entrypoint_config.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env -S bash -eux
 
-exec \
-  /opt/rest-service/docker/prepare_secrets.sh \
-  & \
-  python -m manager_rest.configure_manager
+/opt/rest-service/docker/prepare_secrets.sh
+
+exec python -m manager_rest.configure_manager
