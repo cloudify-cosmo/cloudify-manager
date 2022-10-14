d345096da86b18212eaf9c6a8905d5f1765d272e
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2022-10-14 11:13:21 +0200

    RD-6116 Install system-workflows in mgmtworker (#3860)
    * Move cloudify_system_workflows/ to under mgmtworker/
    
    Those are only used in the mgmtworker anyway. No need for those to
    be a separate package. But I'm leaving import paths the same
    (so mgmtworker will now install two top-level packages)
    
    * mgmtworker requirements
    
    Now include the workflows' requirements as well. Create a reqs.in,
    reqs.txt, all that jazz.
    
    * remove refeferences to the old package

--- mgmtworker/setup.py
+++ mgmtworker/setup.py
@@ -19,6 +19,11 @@ install_requires = [
     'cloudify-common==7.0.0.dev1',
     'cloudify-agent==7.0.0.dev1',
     'packaging==17.1',
+    'retrying>1,<2',
+    'psycopg2==2.9.3',
+    'cryptography>=37,<40',
+    'python-dateutil==2.8.2',
+    'pytz',
 ]
 
 
@@ -27,7 +32,7 @@ setup(
     version='7.0.0.dev1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
-    packages=find_packages(include='mgmtworker*'),
+    packages=find_packages(),
     license='LICENSE',
     description='Cloudify Management Worker',
     install_requires=install_requires
