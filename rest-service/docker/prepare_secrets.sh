057e56a0de5728d3c38443ec9c9f576f4ad652db
asdf <tehasdf@gmail.com>
2022-10-21 15:06:46 +0200

    prepare_secrets.sh: include encryption_key

--- rest-service/docker/prepare_secrets.sh
+++ rest-service/docker/prepare_secrets.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env -S bash -eux
 
-echo "
+if [ ! -e "/opt/manager/cloudify-rest.conf" ]; then
+    echo "
 postgresql_db_name: ${POSTGRES_DB}
 postgresql_host: ${POSTGRES_HOST}
 postgresql_username: ${POSTGRES_USER}
 postgresql_password: ${POSTGRES_PASSWORD}
 " > /opt/manager/cloudify-rest.conf
+fi
 
-echo "
-secret_key: ${SECRET_KEY}
-hash_salt: ${HASH_SALT}
+if [ ! -e "/opt/manager/rest-security.conf" ]; then
+    # TODO: generate the encryption key, don't just hardcode it
+    echo "
+{
+    \"secret_key\": \"${SECRET_KEY}\",
+    \"hash_salt\": \"${HASH_SALT}\",
+    \"encryption_key\": \"Fm89X7nuj-FvNB21jf5znIJylGXunva-RvK30756UPEBj0Bns0u-XY3XVGs6H8CAnGsiAI0tzcM0CbfbLcTk8Q==\"
+}
 " > /opt/manager/rest-security.conf
+fi
