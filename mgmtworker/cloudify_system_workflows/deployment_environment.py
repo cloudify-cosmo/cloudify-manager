ef01516943bd7622f3b7325d7338756c9b13cec8
Madda <nobody@nowhere.local>
2022-11-07 11:55:54 +0000

    Handle new datatype feeding into create_dep_env

--- mgmtworker/cloudify_system_workflows/deployment_environment.py
+++ mgmtworker/cloudify_system_workflows/deployment_environment.py
@@ -91,7 +91,7 @@ def _join_groups(client, deployment_id, groups):
 
 
 def _get_deployment_labels(new_labels, plan_labels):
-    labels = {tuple(label) for label in new_labels}
+    labels = {(label['key'], label['value']) for label in new_labels}
     for name, label_spec in plan_labels.items():
         labels |= {(name.lower(), value) for value in
                    label_spec.get('values', [])}
