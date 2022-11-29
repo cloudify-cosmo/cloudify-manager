c8adaa49967ed289feec076bb0b5d7636edb236b
Mateusz Neumann <mateumann@gmail.com>
2022-11-29 09:39:26 +0100

    Refactor (part 1)

---
+++ rest-service/manager_rest/upload_manager/__init__.py
@@ -0,0 +1,17 @@
+from manager_rest.upload_manager.manager import (
+    cleanup_blueprint_archive_from_file_server,
+    extract_blueprint_archive_to_file_server,
+    remove_blueprint_icon_file,
+    update_blueprint_icon_file,
+    upload_blueprint_archive_to_file_server,
+    upload_plugin,
+    upload_snapshot,
+)
+
+__all__ = ('cleanup_blueprint_archive_from_file_server',
+           'extract_blueprint_archive_to_file_server',
+           'remove_blueprint_icon_file',
+           'update_blueprint_icon_file',
+           'upload_blueprint_archive_to_file_server',
+           'upload_plugin',
+           'upload_snapshot')
