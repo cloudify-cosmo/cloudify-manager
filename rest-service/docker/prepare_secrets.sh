41feb6ac0420eca82bc000d833189549cf8e9f2d
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2022-11-30 00:26:16 +0100

    prepare_secrets.sh: hide output
    No need to show the `echo` in the log - it might (and does) contain
    secrets/credentials!

--- rest-service/docker/prepare_secrets.sh
+++ rest-service/docker/prepare_secrets.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S bash -eux
 
+echo "Provisioning config files..."
+
+set +x
 if [ ! -e "/opt/manager/cloudify-rest.conf" ]; then
     echo "
 postgresql_db_name: ${POSTGRES_DB}
@@ -20,3 +23,6 @@ if [ ! -e "/opt/manager/rest-security.conf" ]; then
 }
 " > /opt/manager/rest-security.conf
 fi
+set -x
+
+echo "Config files provisioned!"
