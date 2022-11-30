522da450676481c7e4d5c3ba46111ac944e51350
Mateusz Neumann <mateumann@gmail.com>
2022-11-30 18:23:40 +0100

    S3 storage client implementation

--- rest-service/manager_rest/upload_manager/storage.py
+++ rest-service/manager_rest/upload_manager/storage.py
@@ -1,6 +1,8 @@
 import os
 import shutil
+from xml.etree import ElementTree
 
+import requests
 from flask import current_app
 
 from manager_rest import manager_exceptions
@@ -49,10 +51,64 @@ class LocalStorageClient(StorageClient):
             os.remove(full_path)
 
 
+class S3StorageClient(StorageClient):
+    """S3StorageClient implements storage methods for S3-compatible storage"""
+    XML_NS = {'s3': 'http://s3.amazonaws.com/doc/2006-03-01/'}
+
+    def __init__(self,
+                 base_uri: str,
+                 bucket_name: str,
+                 req_timeout: float):
+        super().__init__(base_uri)
+        self.bucket_name = bucket_name
+        self.req_timeout = req_timeout
+
+    def files(self, path: str):
+        params = {}
+        if path:
+            params.update({'prefix': path})
+        response = requests.get(
+            self.server_url,
+            params=params,
+            timeout=self.req_timeout
+        )
+
+        xml_et = ElementTree.fromstring(response.content)
+
+        for contents in xml_et.findall('s3:Contents', S3StorageClient.XML_NS):
+            key = contents.find('s3:Key', S3StorageClient.XML_NS)
+            yield key.text
+
+    def put(self, src_path: str, dst_path: str):
+        with open(src_path, 'rb') as data:
+            # do we need a multipart upload maybe?
+            requests.put(
+                f'{self.server_url}/{dst_path}',
+                data=data,
+                timeout=self.req_timeout,
+            )
+
+    def delete(self, path: str):
+        requests.delete(
+            f'{self.server_url}/{path}',
+            timeout=self.req_timeout,
+        )
+
+    @property
+    def server_url(self):
+        return f'{self.base_uri}/{self.bucket_name}'.rstrip('/')
+
+
 def init_storage_client(config):
     match config.file_server_type.lower():
         case 'local':
             client = LocalStorageClient(config.file_server_root)
+        case 's3':
+            client = S3StorageClient(
+                config.s3_server_url,
+                config.s3_resources_bucket,
+                config.s3_client_timeout,
+            )
         case _:
             raise manager_exceptions.UnsupportedFileServerType(
                 f'Unsupported file server type: {config.file_server_type}'
