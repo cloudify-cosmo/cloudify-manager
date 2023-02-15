e62642f1fac5780aa101d87f3dc03a85aa8c2422
Mateusz Neumann <mateumann@gmail.com>
2023-02-15 10:12:27 +0100

    RD-7015 api-service container configuration (#4058)
    * Update requirements
    
    * api-service - Dockerfile and entrypoint scripts
    
    * Revert to /var/log/cloudify/rest/ for rest-service's logs
      It's more consistent this way (with AIO and other Cloudify environments)

--- api-service/docker/entrypoint.sh
+++ api-service/docker/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S bash -eux
 
-/opt/rest-service/docker/prepare_secrets.sh
+/opt/api-service/docker/prepare_secrets.sh
 
 python -m manager_rest.configure_manager --db-wait postgresql
-python -m manager_rest.configure_manager --rabbitmq-wait rabbitmq
 
 exec gunicorn \
-  --pid /run/cloudify-restservice/pid \
+  --pid /run/cloudify-api/pid \
   --chdir / \
   --workers $WORKER_COUNT \
   --max-requests $MAX_REQUESTS \
@@ -14,4 +13,4 @@ exec gunicorn \
   --timeout 300 \
   --access-logfile /var/log/cloudify/rest/audit.log \
   --access-logformat '%(t)s %(h)s %({X-Cloudify-Audit-Username}o)s %({X-Cloudify-Audit-Tenant}o)s %({X-Cloudify-Audit-Auth-Method}o)s "%(r)s" %(s)s %(b)s "%(a)s" took %(M)sms' \
-  manager_rest.wsgi:app
+  cloudify_api.main:app
