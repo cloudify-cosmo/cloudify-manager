24b80d27f087e4f20f0729d0add450682cb28990
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2023-02-23 14:53:07 +0100

    RD-7061 dep-update inte-tests: use `get` in scripts
    All the scripts used something like:
    ```bash
    a_ops_counter=$(ctx instance runtime_properties 'a_ops_counter')
    ```
    On the first run of the script, that errors out, spewing an error
    message into the logs - because that is a KeyError, the property
    doesn't exist.
    
    The script continues anyway, because there's no `set -e`, and the test
    can pass.
    
    This commit replaces it with a `get` in all the scripts, so that
    there's no unnecessary error printout.
    
    (and now the scripts COULD use `set -e` if we wanted to)

--- tests/integration_tests/resources/dsl/deployment_update/add_relationships_between_added_nodes/modification/increment_d.sh
+++ tests/integration_tests/resources/dsl/deployment_update/add_relationships_between_added_nodes/modification/increment_d.sh
@@ -2,7 +2,7 @@
 
 # load current ops count (or default to 0)
 
-d_ops_counter=$(ctx instance runtime_properties 'd_ops_counter')
+d_ops_counter=$(ctx instance runtime_properties get 'd_ops_counter')
 d_ops_counter=${d_ops_counter:-0}
 # increment ops count
 d_ops_counter=$(expr $d_ops_counter + 1)
