08ca10376ca032be3364ca3df950f24b0e52fce8
Adrian Polanczyk <61688882+adrian-polanczyk-codilime@users.noreply.github.com>
2023-03-14 11:41:19 +0100

    RND-176 - Configurable Prometheus URL (#4100)

--- rest-service/migrations/versions/edd6d829a209_6_4_to_7_0.py
+++ rest-service/migrations/versions/edd6d829a209_6_4_to_7_0.py
@@ -51,9 +51,11 @@ def upgrade():
     add_file_server_type_config()
     add_default_agents_rest_port_config()
     add_blueprint_requirements_column()
+    add_prometheus_url_config()
 
 
 def downgrade():
+    drop_prometheus_url_config()
     drop_blueprint_requirements_column()
     drop_default_agents_rest_port_config()
     drop_file_server_type_config()
@@ -822,3 +824,27 @@ def add_blueprint_requirements_column():
 
 def drop_blueprint_requirements_column():
     op.drop_column('blueprints', 'requirements')
+
+
+def add_prometheus_url_config():
+    op.bulk_insert(
+        config_table,
+        [
+            {
+                'name': 'prometheus_url',
+                'value': 'http://127.0.0.1:9090',
+                'scope': 'rest',
+                'schema': {'type': 'string'},
+                'is_editable': True,
+            },
+        ],
+    )
+
+
+def drop_prometheus_url_config():
+    op.execute(
+        config_table.delete().where(
+            (config_table.c.name == op.inline_literal('prometheus_url'))
+            & (config_table.c.scope == op.inline_literal('rest'))
+        )
+    )
