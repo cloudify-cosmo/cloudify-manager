a56b85fb27407565e107efab266b9fa3ac4c26b4
Mateusz Neumann <mateumann@gmail.com>
2023-03-28 12:19:43 +0200

    Refactor
    Get rid of `SELECT` query builder

--- api-service/cloudify_api/routers/audit.py
+++ api-service/cloudify_api/routers/audit.py
@@ -2,14 +2,13 @@ import asyncio
 from datetime import datetime
 from typing import Sequence
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, Field, parse_obj_as, root_validator
-from sqlalchemy import delete
-from sqlalchemy.future import select
-from sqlalchemy.sql.selectable import Select
+from sqlalchemy import delete, select
+from sqlalchemy.sql.expression import BinaryExpression
 
-from cloudify_api import models
+from cloudify_api import models, CloudifyAPI
 from cloudify_api.common import common_parameters, get_app, make_db_session
 from cloudify_api.listener import NOTIFICATION_CHANNEL
 from cloudify_api.results import DeletedResult, Paginated
@@ -128,6 +127,19 @@ class TruncateParams(BaseModel):
 router = APIRouter(prefix="/audit", tags=["Audit Log"])
 
 
+def audit_log_filters(creator_name: str | None = None,
+                      execution_id: str | None = None,
+                      since: datetime | None = None) -> list[BinaryExpression]:
+    filters = []
+    if creator_name is not None:
+        filters.append(models.AuditLog.creator_name == creator_name)
+    if execution_id is not None:
+        filters.append(models.AuditLog.execution_id == execution_id)
+    if since is not None:
+        filters.append(models.AuditLog.created_at >= since)
+    return filters
+
+
 @router.post("", status_code=204)
 async def inject_audit_logs(logs: list[InsertLog],
                             session=Depends(make_db_session),
@@ -143,31 +155,28 @@ async def inject_audit_logs(logs: list[InsertLog],
 
 
 @router.get("", response_model=PaginatedAuditLog)
-async def list_audit_log(creator_name: str | None = None,
-                         execution_id: str | None = None,
-                         since: datetime | None = None,
-                         session=Depends(make_db_session),
+async def list_audit_log(session=Depends(make_db_session),
                          app=Depends(get_app),
+                         filters=Depends(audit_log_filters),
                          p=Depends(common_parameters)) -> PaginatedAuditLog:
-    app.logger.debug("list_audit_log, creator_name=%s, "
-                     "execution_id=%s, since=%s",
-                     creator_name, execution_id, since)
-    query = select_audit_log_query(creator_name, execution_id, since)
+    app.logger.debug("list_audit_log with filters=%s",
+                     [str(f) for f in filters])
+    query = select(models.AuditLog).order_by('created_at').where(*filters)
     return await PaginatedAuditLog.paginated(session, query, p)
 
 
 @router.get("/stream")
-async def stream_audit_log(request: Request,
-                           creator_name: str | None = None,
-                           execution_id: str | None = None,
-                           since: datetime | None = None
-                           ) -> StreamingResponse:
-    request.app.logger.debug("Handling stream_audit_log request")
+async def stream_audit_log(
+        app=Depends(get_app),
+        filters=Depends(audit_log_filters),
+) -> StreamingResponse:
+    app.logger.debug("Handling stream_audit_log request with filters=%s",
+                     [str(f) for f in filters])
     queue = asyncio.Queue()
-    request.app.listener.attach_queue(NOTIFICATION_CHANNEL, queue)
+    app.listener.attach_queue(NOTIFICATION_CHANNEL, queue)
     headers = {"Content-Type": "text/event-stream"}
     return StreamingResponse(
-        audit_log_streamer(request, queue, creator_name, execution_id, since),
+        audit_log_streamer(app, filters, queue),
         headers=headers)
 
 
@@ -185,31 +194,25 @@ async def truncate_audit_log(p=Depends(TruncateParams),
     return await DeletedResult.executed(session, stmt)
 
 
-def select_audit_log_query(creator_name: str | None = None,
-                           execution_id: str | None = None,
-                           since: datetime | None = None) -> Select:
-    stmt = select(models.AuditLog).order_by('created_at')
-    if creator_name is not None:
-        stmt = stmt.where(models.AuditLog.creator_name == creator_name)
-    if execution_id is not None:
-        stmt = stmt.where(models.AuditLog.execution_id == execution_id)
-    if since is not None:
-        stmt = stmt.where(models.AuditLog.created_at >= since)
-    return stmt
+def get_parameters_from_filters(
+        filters: list[BinaryExpression]
+) -> dict[str: str | datetime]:
+    params = {}
+    for f in filters:
+        params[f.left.table.name + '.' + f.left.name] = f.right.value
+    return params
 
 
-async def audit_log_streamer(request: Request,
+async def audit_log_streamer(app: CloudifyAPI,
+                             filters: list[BinaryExpression],
                              queue: asyncio.Queue,
-                             creator_name: str | None = None,
-                             execution_id: str | None = None,
-                             since: datetime | None = None
                              ) -> Sequence[bytes]:
+    params = get_parameters_from_filters(filters)
     streamed_ids = set()
-    if since is not None:
-        query = select_audit_log_query(creator_name=creator_name,
-                                       execution_id=execution_id,
-                                       since=since)
-        async with request.app.db_session_maker() as session:
+
+    if params.get('audit_log.created_at') is not None:
+        query = select(models.AuditLog).order_by('created_at').where(*filters)
+        async with app.db_session_maker() as session:
             db_records = await session.execute(query)
         for db_record in db_records.scalars().all():
             record = AuditLog.from_orm(db_record)
@@ -219,13 +222,17 @@ async def audit_log_streamer(request: Request,
         try:
             data = await queue.get()
         except asyncio.CancelledError:
-            request.app.listener.remove_queue(NOTIFICATION_CHANNEL, queue)
+            app.listener.remove_queue(NOTIFICATION_CHANNEL, queue)
             break
         record: AuditLog = parse_obj_as(AuditLog, data)
-        if not record.matches(creator_name, execution_id, since):
+        if not record.matches(
+                params.get('audit_log.creator_name'),
+                params.get('audit_log.execution_id'),
+                params.get('audit_log.created_at'),
+        ):
             continue
         await record.update_ref_identifier_tenant_name(
-                request.app.get_tenant_name)
+                app.get_tenant_name)
 
         yield make_streaming_response(record.json(exclude_none=True))
         if not queue.empty():
