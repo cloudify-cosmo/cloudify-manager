ee3608b5fa6cb7cf057604c4503c038079b384b7
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2023-03-30 17:05:08 +0200

    skip some inte-tests when on k8s
    those can't succeed there, or at least, not yet

--- tests/integration_tests/tests/agentless_tests/component/test_component_basics.py
+++ tests/integration_tests/tests/agentless_tests/component/test_component_basics.py
@@ -351,6 +351,10 @@ node_templates:
         self.assertEqual(len(self.client.plugins.list()), 1)
         self.client.plugins.delete(mock_id)
 
+    @pytest.mark.skipif(
+        "config.getoption('--k8s-namespace')",
+        reason='Cannot use direct plugin filenames in the k8s manager',
+    )
     def test_distro_behaviour(self):
         self.env.copy_file_to_manager(resource('dsl/plugins'), '/tmp')
         basic_blueprint_path = resource('dsl/empty_blueprint.yaml')
