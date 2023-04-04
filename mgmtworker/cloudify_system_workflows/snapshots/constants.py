95aec29c50a97efd4a41a86aa340326843098273
Mateusz Neumann <mateumann@gmail.com>
2023-04-04 16:25:55 +0200

    RND-373 Restore legacy snapshot code
    * Restore legacy snapshot_create code
    
    * Enable legacy snapshot creation
    
    * Enable legacy snapshot restore
    
    --
    
    This is a cherry-pick of https://github.com/cloudify-cosmo/cloudify-manager/pull/4157

--- mgmtworker/cloudify_system_workflows/snapshots/constants.py
+++ mgmtworker/cloudify_system_workflows/snapshots/constants.py
@@ -72,6 +72,7 @@ V_5_2_0 = ManagerVersion('5.2.0')
 V_5_3_0 = ManagerVersion('5.3.0')
 V_6_0_0 = ManagerVersion('6.0.0')
 V_7_0_0 = ManagerVersion('7.0.0')
+V_7_1_0 = ManagerVersion('7.1.0')
 
 
 class VisibilityState(object):
