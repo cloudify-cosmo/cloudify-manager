ba7b284f394681c0b9231b58a670b2d67b539d89
Mateusz Neumann <mateumann@gmail.com>
2023-04-17 09:51:33 +0200

    Refactor test_auditlog_listener test

--- mgmtworker/cloudify_system_workflows/tests/snapshots/test_auditlog_listener.py
+++ mgmtworker/cloudify_system_workflows/tests/snapshots/test_auditlog_listener.py
@@ -13,66 +13,61 @@ from cloudify_system_workflows.snapshots.snapshot_create import SnapshotCreate
 FAKE_MANAGER_VERSION = 'THIS_MANAGER_VERSION'
 
 
+class AuditLogResponse:
+    def __init__(self, content: list[bytes]):
+        self.content = self.async_iterator(content)
+
+    @staticmethod
+    async def async_iterator(iterable):
+        for i in iterable:
+            yield i
+
+
 @pytest.fixture(scope='function')
 def auditlog_listener() -> AuditLogListener:
     queue = Queue()
     client = MockClient()
-    listener = AuditLogListener(client.auditlog, queue)
+    listener = AuditLogListener(client, queue)
     yield listener
     listener.stop()
 
 
-def _get_rest_client():
-    class AuditLogResponse:
-        def __init__(self, content: list[bytes]):
-            self.content = self.async_iterator(content)
-
-        @staticmethod
-        async def async_iterator(iterable):
-            for i in iterable:
-                yield i
-
+def _get_rest_client(side_effect):
     mock_client = mock.Mock()
-    mock_client.auditlog.stream = mock.AsyncMock(
-            side_effect=[
-                AuditLogResponse([
-                    b'{"id": "1192", "ref_table": "users", "ref_id": "0", '
-                    b'"ref_identifier": {"username": "admin"}, '
-                    b'"operation": "update", "creator_name": "", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T09:27:57.926"}',
-                ]),
-                Exception('breaking connection'),
-                AuditLogResponse([
-                    b'{"id": "1193", "ref_table": "usage_collector", '
-                    b'"ref_id": "0", "ref_identifier": {"id": "0", '
-                    b'"manager_id": "manager_id"}, '
-                    b'"operation": "update", "creator_name": "", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T09:27:57.927"}',
-                ]),
-            ]
-    )
+    mock_client.auditlog.stream = mock.AsyncMock(side_effect=side_effect)
     return mock_client
 
 
 @pytest.fixture
-def mock_get_client():
+def mock_get_breaking_client(**_):
     with mock.patch(
-        'cloudify_system_workflows.snapshots.snapshot_create'
-        '.get_rest_client',
-        side_effect=_get_rest_client,
+        'cloudify_system_workflows.snapshots.snapshot_create.get_rest_client',
+        side_effect=lambda **_: _get_rest_client([
+            AuditLogResponse([
+                b'{"id": "1192", "ref_table": "users", "ref_id": "0", '
+                b'"ref_identifier": {"username": "admin"}, '
+                b'"operation": "update", "creator_name": "", '
+                b'"execution_id": "", '
+                b'"created_at": "2023-04-05T09:27:57.926"}',
+            ]),
+            Exception('breaking connection'),
+            AuditLogResponse([
+                b'{"id": "1193", "ref_table": "usage_collector", '
+                b'"ref_id": "0", "ref_identifier": {"id": "0", '
+                b'"manager_id": "manager_id"}, '
+                b'"operation": "update", "creator_name": "", '
+                b'"execution_id": "", '
+                b'"created_at": "2023-04-05T09:27:57.927"}',
+            ]),
+        ]),
     ):
         yield
 
 
-# @pytest.fixture(scope='module')
 @pytest.fixture()
 def mock_ctx():
     with mock.patch(
-        'cloudify_system_workflows.snapshots.snapshot_create'
-        '.ctx',
-        # Don't try to magicmock the context or we need a context
+        'cloudify_system_workflows.snapshots.snapshot_create.ctx',
         new=mock.Mock(),
     ):
         yield
@@ -81,8 +76,8 @@ def mock_ctx():
 @pytest.fixture
 def mock_get_manager_version():
     with mock.patch(
-        'cloudify_system_workflows.snapshots.snapshot_create'
-        '.utils.get_manager_version',
+        'cloudify_system_workflows.snapshots.snapshot_create.utils'
+        '.get_manager_version',
         return_value=FAKE_MANAGER_VERSION
     ):
         yield
@@ -91,20 +86,23 @@ def mock_get_manager_version():
 def test_reconnect(
         auditlog_listener,
         mock_ctx,
-        mock_get_client,
+        mock_get_breaking_client,
         mock_get_manager_version
 ):
     snap_id = 'testsnapshot'
     tempdir = tempfile.mkdtemp(prefix='snap-cre-auditlog-reconnect-test')
     snap_cre = SnapshotCreate(
-            snapshot_id=snap_id,
-            config={
-                'created_status': 'created',
-                'failed_status': 'failed',
-                'file_server_root': tempdir,
-            },
+        snapshot_id=snap_id,
+        config={
+            'created_status': 'created',
+            'failed_status': 'failed',
+            'file_server_root': tempdir,
+        },
     )
-    snap_cre._get_tenants = mock.Mock(return_value=['tenant1', 'tenant2'])
+    snap_cre._get_tenants = \
+        mock.Mock(return_value={'tenant1': {}, 'tenant2': {}})
+    snap_cre._blueprints_list = mock.Mock(return_value=[('tenant1', 'bp1')])
+    snap_cre._plugins_list = mock.Mock(return_value=[])
     snap_cre._dump_management = mock.Mock()
     snap_cre._dump_composer = mock.Mock()
     snap_cre._dump_stage = mock.Mock()
@@ -127,10 +125,7 @@ def test_reconnect(
             'id': '1193',
             'ref_table': 'usage_collector',
             'ref_id': '0',
-            'ref_identifier': {
-                'id': '0',
-                'manager_id': 'manager_id'
-            },
+            'ref_identifier': {'id': '0', 'manager_id': 'manager_id'},
             'operation': 'update',
             'creator_name': '',
             'execution_id': '',
