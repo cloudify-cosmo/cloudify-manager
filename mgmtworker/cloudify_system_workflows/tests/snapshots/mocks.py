255be6706b8a2e4f7e6c66d65e50dd634c867455
Mateusz Neumann <mateumann@gmail.com>
2023-04-27 07:28:18 +0200

    Refactor snapshot tests (mostly mocks)

---
+++ mgmtworker/cloudify_system_workflows/tests/snapshots/mocks.py
@@ -0,0 +1,204 @@
+import json
+import shutil
+import tempfile
+from collections import namedtuple
+from contextlib import contextmanager
+from typing import Type
+from unittest import mock
+
+from cloudify import constants
+from cloudify_rest_client import CloudifyClient
+from cloudify_rest_client.responses import ListResponse
+from cloudify_system_workflows.snapshots.snapshot_create import SnapshotCreate
+
+FAKE_MANAGER_VERSION = 'THIS_MANAGER_VERSION'
+FAKE_MGMTWORKER_TOKEN = 'FAKE_TOKEN'
+
+EMPTY_TENANTS_LIST_SE = [
+    ListResponse(
+        items=[],
+        metadata={'pagination': {'offset': 0, 'size': 0, 'total': 0}}
+    )
+]
+TWO_TENANTS_LIST_SE = [
+    ListResponse(
+        items=[{'name': 'tenant1'}, {'name': 'tenant2'}],
+        metadata={'pagination': {'offset': 0, 'size': 2, 'total': 2}}
+    ),
+]
+ONE_BLUEPRINT_LIST_SE = [
+    ListResponse(
+            items=[{'id': 'bp1', 'tenant_name': 'tenant1'}],
+            metadata={'pagination': {'offset': 0, 'size': 1, 'total': 1}}
+    )
+]
+TWO_BLUEPRINTS_LIST_SE = [
+    ListResponse(
+        items=[
+            {'id': 'bp1', 'tenant_name': 'tenant1'},
+            {'id': 'bp2', 'tenant_name': 'tenant1'}
+        ],
+        metadata={'pagination': {'offset': 0, 'size': 2, 'total': 2}}
+    )
+]
+
+
+class MockHTTPClient(CloudifyClient.client_class):
+    """This is a carbon-copy of cloudify_rest_client.tests.MockHTTPClient
+     which is not shipped as part of cloudify-common."""
+
+    def __init__(self, *args, **kwargs):
+        super(MockHTTPClient, self).__init__(*args, **kwargs)
+        self._do_request = mock.Mock()
+
+
+class MockClient(CloudifyClient):
+    """This is a carbon-copy of cloudify_rest_client.tests.MockClient which
+     is not shipped as part of cloudify-common."""
+    client_class = MockHTTPClient
+
+    def __init__(self, **kwargs):
+        params = {
+            'host': '192.0.2.4',
+        }
+        params.update(kwargs)
+        super(MockClient, self).__init__(**params)
+        # Default to make calls have a chance of working
+        self.mock_do_request.return_value = {}
+
+    @property
+    def mock_do_request(self):
+        return self._client._do_request
+
+    def assert_last_mock_call(self, endpoint, data=None, params=None,
+                              expected_status_code=200, stream=False,
+                              expected_method='get'):
+        if not params:
+            params = {}
+
+        _, kwargs = self.mock_do_request.call_args_list[-1]
+
+        called_endpoint = kwargs['request_url'].rpartition('v3.1')[2]
+        assert endpoint == called_endpoint
+
+        assert data == kwargs['body']
+        assert params == kwargs['params']
+        assert expected_status_code == kwargs['expected_status_code']
+        assert stream == kwargs['stream']
+
+        assert expected_method == kwargs['requests_method'].__name__
+
+    @property
+    def last_mock_call_headers(self):
+        return self.mock_do_request.call_args_list[-1][1]['headers']
+
+    def check_last_auth_headers(self, auth=None, token=None):
+        expected = {
+            constants.CLOUDIFY_EXECUTION_TOKEN_HEADER: None,
+            constants.CLOUDIFY_AUTHENTICATION_HEADER: auth,
+            constants.CLOUDIFY_TOKEN_AUTHENTICATION_HEADER: token,
+        }
+
+        # We don't just do a set because content-type (and other headers) may
+        # be on valid requests
+        for header in expected:
+            if expected[header] is not None:
+                assert self.last_mock_call_headers[header] == expected[header]
+            else:
+                assert header not in self.last_mock_call_headers
+
+
+class AuditLogResponse:
+    def __init__(self, elements: list[dict]):
+        content_iterable = []
+        for element in elements:
+            content_iterable.append(json.dumps(element).encode('utf-8'))
+        self.content = self.async_iterator(content_iterable)
+
+    @staticmethod
+    async def async_iterator(iterable):
+        for i in iterable:
+            yield i
+
+
+DeploymentResponse = namedtuple('Deployment', 'id blueprint_id')
+
+
+def get_mocked_client(
+        side_effects: list[tuple[Type[mock.Mock], tuple, list]]
+):
+    client = mock.Mock()
+    for _mock, attrs, side_effect in side_effects:
+        # Set up mock with a side_effect.  E.g. for _mock = mock.AsyncMock and
+        # attrs = ('x', 'y', 'z'), the assignment is going to be like:
+        # client.x.y.z = mock.AsyncMock(side_effect=side_effect)
+        obj = client
+        for attr in attrs[:-1]:
+            obj = getattr(obj, attr)
+        setattr(obj, attrs[-1], _mock(side_effect=side_effect))
+    return client
+
+
+def get_mocked_ui_client(
+        side_effects: list[tuple[tuple, list]]
+):
+    return get_mocked_client([
+        (mock.Mock, attrs, side_effect)
+        for attrs, side_effect in side_effects
+    ])
+
+
+@contextmanager
+def prepare_snapshot_create_with_mocks(
+        snapshot_id: str,
+        rest_mocks: list[tuple[Type[mock.Mock], tuple, list]] | None = None,
+        composer_mocks: list[tuple[tuple, list]] | None = None,
+        stage_mocks: list[tuple[tuple, list]] | None = None,
+        **kwargs
+):
+    with mock.patch(
+        'cloudify_system_workflows.snapshots.snapshot_create.ctx',
+        new=mock.Mock(),
+    ):
+        with mock.patch(
+            'cloudify_system_workflows.snapshots.snapshot_create'
+            '.get_manager_version',
+            return_value=FAKE_MANAGER_VERSION
+        ):
+            with mock.patch(
+                'cloudify_system_workflows.snapshots.ui_clients'
+                '.get_admin_api_token',
+                return_value=FAKE_MGMTWORKER_TOKEN
+            ):
+                with mock.patch(
+                    'cloudify_system_workflows.snapshots'
+                    '.snapshot_create.get_composer_client',
+                    side_effect=lambda **_:
+                        get_mocked_ui_client(composer_mocks or [])
+                ):
+                    with mock.patch(
+                        'cloudify_system_workflows.snapshots'
+                        '.snapshot_create.get_stage_client',
+                        side_effect=lambda **_:
+                            get_mocked_ui_client(stage_mocks or [])
+                    ):
+                        with mock.patch(
+                            'cloudify_system_workflows.snapshots'
+                            '.snapshot_create.get_rest_client',
+                            side_effect=lambda **_:
+                                get_mocked_client(rest_mocks)
+                        ):
+                            temp_dir = tempfile.mkdtemp(suffix=snapshot_id)
+                            snapshot_create = SnapshotCreate(
+                                snapshot_id=snapshot_id,
+                                config={
+                                    'created_status': 'created',
+                                    'failed_status': 'failed',
+                                    'file_server_root': temp_dir,
+                                },
+                                **kwargs,
+                            )
+                            yield snapshot_create
+                            shutil.rmtree(snapshot_create._temp_dir,
+                                          ignore_errors=True)
+                            shutil.rmtree(temp_dir, ignore_errors=True)
--- mgmtworker/cloudify_system_workflows/tests/snapshots/test_auditlog_listener.py
+++ mgmtworker/cloudify_system_workflows/tests/snapshots/test_auditlog_listener.py
@@ -1,31 +1,18 @@
-import shutil
-import tempfile
-from collections import namedtuple
 from queue import Queue
 from unittest import mock
 
 import pytest
 
-from cloudify_rest_client.responses import ListResponse
-from cloudify_system_workflows.tests.snapshots.mock_client import MockClient
 from cloudify_system_workflows.snapshots.audit_listener import AuditLogListener
-from cloudify_system_workflows.snapshots.snapshot_create import SnapshotCreate
-
-
-FAKE_MANAGER_VERSION = 'THIS_MANAGER_VERSION'
-
-
-class AuditLogResponse:
-    def __init__(self, content: list[bytes]):
-        self.content = self.async_iterator(content)
-
-    @staticmethod
-    async def async_iterator(iterable):
-        for i in iterable:
-            yield i
-
-
-DeploymentResponse = namedtuple('Deployment', 'id blueprint_id')
+from cloudify_system_workflows.tests.snapshots.mocks import (
+    AuditLogResponse,
+    DeploymentResponse,
+    MockClient,
+    prepare_snapshot_create_with_mocks,
+    TWO_TENANTS_LIST_SE,
+    ONE_BLUEPRINT_LIST_SE,
+    TWO_BLUEPRINTS_LIST_SE,
+)
 
 
 @pytest.fixture(scope='function')
@@ -37,229 +24,149 @@ def auditlog_listener() -> AuditLogListener:
     listener.stop()
 
 
-def _get_rest_client(
-        auditlog_stream_se=None,
-        deployments_get_se=None,
-):
-    client = mock.Mock()
-    if auditlog_stream_se:
-        client.auditlog.stream = mock.AsyncMock(side_effect=auditlog_stream_se)
-    if deployments_get_se:
-        client.deployments.get = mock.Mock(side_effect=deployments_get_se)
-
-    client.tenants.list = mock.Mock(return_value=ListResponse(
-            items=[{'name': 'tenant1'}, {'name': 'tenant2'}],
-            metadata={'pagination': {'offset': 0, 'size': 2, 'total': 2}}
-    ))
-    return client
-
-
-@pytest.fixture
-def mock_get_breaking_client(**_):
-    with mock.patch(
-        'cloudify_system_workflows.snapshots.snapshot_create.get_rest_client',
-        side_effect=lambda **_: _get_rest_client(
-            auditlog_stream_se=[
-                AuditLogResponse([
-                    b'{"id": "1192", "ref_table": "blueprints", '
-                    b'"ref_id": "0", "ref_identifier": {"id": "bp1", '
-                    b'"tenant_name": "tenant1"}, '
-                    b'"operation": "update", "creator_name": "", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T09:27:57.926"}',
-                ]),
-                Exception('breaking connection'),
-                AuditLogResponse([
-                    b'{"id": "1193", "ref_table": "blueprints", '
-                    b'"ref_id": "0", "ref_identifier": {"id": "bp1", '
-                    b'"tenant_name": "tenant1"}, '
-                    b'"operation": "update", "creator_name": "", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T09:27:57.927"}',
-                ]),
-            ],
-        ),
-    ):
-        yield
-
-
-@pytest.fixture
-def mock_get_standard_client(**_):
-    with mock.patch(
-        'cloudify_system_workflows.snapshots.snapshot_create.get_rest_client',
-        side_effect=lambda **_: _get_rest_client(
-            auditlog_stream_se=[
-                AuditLogResponse([
-                    b'{"id": "1292", "ref_table": "deployments", '
-                    b'"ref_id": "1", "ref_identifier": {"id": "d1", '
-                    b'"_tenant_id": "0", "tenant_name": "tenant1"}, '
-                    b'"operation": "create", "creator_name": "admin", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T10:27:57.926"}',
-                ]),
-                AuditLogResponse([
-                    b'{"id": "1293", "ref_table": "blueprints", '
-                    b'"ref_id": "2", "ref_identifier": {"id": "bp2", '
-                    b'"_tenant_id": "0", "tenant_name": "tenant1"}, '
-                    b'"operation": "create", "creator_name": "admin", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T10:27:57.927"}',
-                ]),
-                AuditLogResponse([
-                    b'{"id": "1294", "ref_table": "blueprints", '
-                    b'"ref_id": "1", "ref_identifier": {"id": "bp1", '
-                    b'"_tenant_id": "0", "tenant_name": "tenant1"}, '
-                    b'"operation": "update", "creator_name": "admin", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T10:27:57.928"}',
-                ]),
-                AuditLogResponse([
-                    b'{"id": "1295", "ref_table": "deployments", '
-                    b'"ref_id": "2", "ref_identifier": {"id": "d2", '
-                    b'"_tenant_id": "0", "tenant_name": "tenant1"}, '
-                    b'"operation": "create", "creator_name": "admin", '
-                    b'"execution_id": "", '
-                    b'"created_at": "2023-04-05T10:27:57.929"}',
-                ]),
-            ],
-            deployments_get_se=[
-                DeploymentResponse('d1', 'bp1'),
-                DeploymentResponse('d2', 'bp2'),
-            ],
-        )
-    ):
-        yield
-
-
-@pytest.fixture()
-def mock_ctx():
-    with mock.patch(
-        'cloudify_system_workflows.snapshots.snapshot_create.ctx',
-        new=mock.Mock(),
-    ):
-        yield
-
-
-@pytest.fixture
-def mock_get_manager_version():
-    with mock.patch(
-        'cloudify_system_workflows.snapshots.snapshot_create'
-        '.get_manager_version',
-        return_value=FAKE_MANAGER_VERSION
-    ):
-        yield
-
-
-def test_reconnect(
-        auditlog_listener,
-        mock_ctx,
-        mock_get_breaking_client,
-        mock_get_manager_version
-):
-    snap_id = 'testreconnectsnapshot'
-    tempdir = tempfile.mkdtemp(prefix='snap-cre-auditlog-reconnect-test')
-    snap_cre = SnapshotCreate(
-        snapshot_id=snap_id,
-        config={
-            'created_status': 'created',
-            'failed_status': 'failed',
-            'file_server_root': tempdir,
-        },
-    )
-    snap_cre._blueprints_list = mock.Mock(return_value=[('tenant1', 'bp1')])
-    snap_cre._plugins_list = mock.Mock(return_value=[])
-    snap_cre._dump_management = mock.Mock()
-    snap_cre._dump_composer = mock.Mock()
-    snap_cre._dump_stage = mock.Mock()
-    snap_cre._dump_tenant = mock.Mock()
-    snap_cre._append_new_object_from_auditlog = mock.Mock()
-
-    snap_cre.create(timeout=1)
-
-    snap_cre._append_new_object_from_auditlog.assert_has_calls([
-        mock.call({
-            'id': '1192',
+def test_reconnect(auditlog_listener):
+    auditlog_stream_se = [
+        AuditLogResponse([{
+            'id': 1192,
             'ref_table': 'blueprints',
-            'ref_id': '0',
+            'ref_id': 0,
             'ref_identifier': {'id': 'bp1', 'tenant_name': 'tenant1'},
             'operation': 'update',
             'creator_name': '',
             'execution_id': '',
             'created_at': '2023-04-05T09:27:57.926'
-        }),
-        mock.call({
-            'id': '1193',
+        }]),
+        Exception('breaking connection'),
+        AuditLogResponse([{
+            'id': 1193,
             'ref_table': 'blueprints',
-            'ref_id': '0',
+            'ref_id': 0,
             'ref_identifier': {'id': 'bp1', 'tenant_name': 'tenant1'},
             'operation': 'update',
             'creator_name': '',
             'execution_id': '',
             'created_at': '2023-04-05T09:27:57.927'
-        }),
-    ])
+        }]),
+    ]
+    with prepare_snapshot_create_with_mocks(
+        'test-reconnect-snapshot',
+        rest_mocks=[
+            (mock.AsyncMock, ('auditlog', 'stream'), auditlog_stream_se),
+            (mock.Mock, ('tenants', 'list'), TWO_TENANTS_LIST_SE),
+            (mock.Mock, ('blueprints', 'list'), TWO_BLUEPRINTS_LIST_SE),
+        ],
+    ) as snap_cre:
+        snap_cre._append_new_object_from_auditlog = mock.Mock()
+        snap_cre.create(timeout=0.2)
+        snap_cre._append_new_object_from_auditlog.assert_has_calls([
+            mock.call({
+                'id': 1192,
+                'ref_table': 'blueprints',
+                'ref_id': 0,
+                'ref_identifier': {'id': 'bp1', 'tenant_name': 'tenant1'},
+                'operation': 'update',
+                'creator_name': '',
+                'execution_id': '',
+                'created_at': '2023-04-05T09:27:57.926'
+            }),
+            mock.call({
+                'id': 1193,
+                'ref_table': 'blueprints',
+                'ref_id': 0,
+                'ref_identifier': {'id': 'bp1', 'tenant_name': 'tenant1'},
+                'operation': 'update',
+                'creator_name': '',
+                'execution_id': '',
+                'created_at': '2023-04-05T09:27:57.927'
+            }),
+        ])
 
-    shutil.rmtree(tempdir)
 
-
-def test_dont_append_new_blueprints(
-        auditlog_listener,
-        mock_ctx,
-        mock_get_standard_client,
-        mock_get_manager_version
-):
-    snap_id = 'testsnapshot'
-    tempdir = tempfile.mkdtemp(prefix='snap-cre-auditlog-no-new-bps-test')
-    snap_cre = SnapshotCreate(
-        snapshot_id=snap_id,
-        config={
-            'created_status': 'created',
-            'failed_status': 'failed',
-            'file_server_root': tempdir,
-        },
-    )
-    snap_cre._get_tenants = \
-        mock.Mock(return_value={'tenant1': {}, 'tenant2': {}})
-    snap_cre._blueprints_list = mock.Mock(return_value=[('tenant1', 'bp1')])
-
-    snap_cre._plugins_list = mock.Mock(return_value=[])
-    snap_cre._dump_management = mock.Mock()
-    snap_cre._dump_composer = mock.Mock()
-    snap_cre._dump_stage = mock.Mock()
-    snap_cre._dump_tenant = mock.Mock()
-    snap_cre._append_new_object_from_auditlog = mock.Mock()
-
-    snap_cre.create(timeout=1)
-
-    snap_cre._append_new_object_from_auditlog.assert_has_calls([
-        mock.call({
-            'id': '1292',
+def test_dont_append_new_blueprints(auditlog_listener):
+    auditlog_stream_se = [
+        AuditLogResponse([{
+            'id': 1292,
             'ref_table': 'deployments',
-            'ref_id': '1',
-            'ref_identifier': {
-                'id': 'd1',
-                '_tenant_id': '0',
-                'tenant_name': 'tenant1'
-            },
+            'ref_id': 1,
+            'ref_identifier': {'id': 'd1',
+                               'tenant_name': 'tenant1'},
             'operation': 'create',
             'creator_name': 'admin',
             'execution_id': '',
-            'created_at': '2023-04-05T10:27:57.926'
-        }),
-        mock.call({
-            'id': '1294',
+            'created_at': '2023-04-05T10:27:57.926',
+        }]),
+        AuditLogResponse([{
+            'id': 1293,
             'ref_table': 'blueprints',
-            'ref_id': '1',
-            'ref_identifier': {
-                'id': 'bp1',
-                '_tenant_id': '0',
-                'tenant_name': 'tenant1'
-            },
+            'ref_id': 2,
+            'ref_identifier': {'id': 'bp2',
+                               'tenant_name': 'tenant1'},
+            'operation': 'create',
+            'creator_name': 'admin',
+            'execution_id': '',
+            'created_at': '2023-04-05T10:27:57.927',
+        }]),
+        AuditLogResponse([{
+            'id': 1294,
+            'ref_table': 'blueprints',
+            'ref_id': 1,
+            'ref_identifier': {'id': 'bp1', 'tenant_name': 'tenant1'},
             'operation': 'update',
             'creator_name': 'admin',
             'execution_id': '',
-            'created_at': '2023-04-05T10:27:57.928'
-        }),
-    ])
-
-    shutil.rmtree(tempdir)
+            'created_at': '2023-04-05T10:27:57.928',
+        }]),
+        AuditLogResponse([{
+            'id': 1295,
+            'ref_table': 'deployments',
+            'ref_id': 2,
+            'ref_identifier': {'id': 'd2', 'tenant_name': 'tenant1'},
+            'operation': 'create',
+            'creator_name': 'admin',
+            'execution_id': '',
+            'created_at': '2023-04-05T10:27:57.929',
+        }]),
+    ]
+    deployments_get_se = [
+        DeploymentResponse(id='d1', blueprint_id='bp1'),
+        DeploymentResponse(id='d2', blueprint_id='bp2'),
+    ]
+    with prepare_snapshot_create_with_mocks(
+        'test-snapshot-dont-append-new-blueprints',
+        rest_mocks=[
+            (mock.AsyncMock, ('auditlog', 'stream'), auditlog_stream_se),
+            (mock.Mock, ('tenants', 'list'), TWO_TENANTS_LIST_SE),
+            (mock.Mock, ('blueprints', 'list'), ONE_BLUEPRINT_LIST_SE),
+            (mock.Mock, ('deployments', 'get'), deployments_get_se),
+        ],
+    ) as snap_cre:
+        snap_cre._append_new_object_from_auditlog = mock.Mock()
+        snap_cre.create(timeout=0.2)
+        snap_cre._append_new_object_from_auditlog.assert_has_calls([
+            mock.call({
+                'id': 1292,
+                'ref_table': 'deployments',
+                'ref_id': 1,
+                'ref_identifier': {
+                    'id': 'd1',
+                    'tenant_name': 'tenant1'
+                },
+                'operation': 'create',
+                'creator_name': 'admin',
+                'execution_id': '',
+                'created_at': '2023-04-05T10:27:57.926'
+            }),
+            mock.call({
+                'id': 1294,
+                'ref_table': 'blueprints',
+                'ref_id': 1,
+                'ref_identifier': {
+                    'id': 'bp1',
+                    'tenant_name': 'tenant1'
+                },
+                'operation': 'update',
+                'creator_name': 'admin',
+                'execution_id': '',
+                'created_at': '2023-04-05T10:27:57.928'
+            }),
+        ])
