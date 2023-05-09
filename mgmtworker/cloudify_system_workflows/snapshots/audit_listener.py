8aededab0b9c4e45be2d98b546343dbc19617932
Mateusz Neumann <mateumann@gmail.com>
2023-05-09 11:15:24 +0200

    Create snapshot files in snapshot_create workflow
    It's complementary to removing that functionality from the rest client.

--- mgmtworker/cloudify_system_workflows/snapshots/audit_listener.py
+++ mgmtworker/cloudify_system_workflows/snapshots/audit_listener.py
@@ -1,6 +1,5 @@
 import asyncio
 import json
-from collections import defaultdict
 from datetime import datetime
 from queue import Queue
 from threading import Event, Thread
@@ -34,8 +33,7 @@ class AuditLogListener(Thread):
         self._stopped = Event()
         self._tenant_clients: dict[str: CloudifyClient] = {}
         self._stream_timeout = stream_timeout
-        self.__snapshot_entities: dict[tuple[str | None, str], set[str]] = \
-            defaultdict(set)
+        self.__snapshot_entities: dict[tuple[str | None, str], set[str]] = {}
 
     def start(self, tenant_clients: dict[str, CloudifyClient] | None = None):
         self._tenant_clients = tenant_clients or {}
@@ -49,15 +47,18 @@ class AuditLogListener(Thread):
         self._loop.call_soon_threadsafe(self._loop.stop)
         self._loop.call_soon_threadsafe(self._loop.close)
 
-    def append_entities(
+    def append_entity(
             self,
             tenant_name: str | None,
             entity_type: str,
-            identifiers: set[str],
+            identifier: str,
     ):
         key = (tenant_name, entity_type)
-        self.__snapshot_entities[key] = \
-            self.__snapshot_entities[key].union(identifiers)
+        if (key not in self.__snapshot_entities or
+                not self.__snapshot_entities[key]):
+            self.__snapshot_entities[key] = {identifier}
+        else:
+            self.__snapshot_entities[key].add(identifier)
 
     async def _stream_logs(self):
         """Keep putting logs in a queue, reconnect in case of any errors."""
@@ -84,17 +85,14 @@ class AuditLogListener(Thread):
         tenant_name = ref_identifier.get('tenant_name')
         ref_table = audit_log['ref_table']
 
-        if (tenant_name, ref_table) not in self.__snapshot_entities:
+        if (tenant_name, ref_table) not in self.__snapshot_entities or \
+                not self.__snapshot_entities[(tenant_name, ref_table)]:
             return False
 
         key_id = '_storage_id' if ref_table in ['events', 'logs'] else 'id'
-        entity_id = ref_identifier.get(key_id, None)
-
-        if entity_id:
-            return (
-                    entity_id in
-                    self.__snapshot_entities[(tenant_name, ref_table)]
-            )
+        if entity_id := ref_identifier.get(key_id, None):
+            return entity_id in \
+                self.__snapshot_entities[(tenant_name, ref_table)]
         return False
 
     def _related_to_snapshot(self, audit_log: dict) -> bool:
