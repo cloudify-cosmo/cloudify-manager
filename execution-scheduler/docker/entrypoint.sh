fb2dd101f99fe5e28bc769359ef055fd1a5e3f70
Łukasz Maksymczuk <tehasdf@users.noreply.github.com>
2023-10-02 12:29:57 +0200

    NE-6694 execution-scheduler: declare HASH_SALT (#4300)
    * NE-6694 execution-scheduler: declare HASH_SALT
    
    The execution scheduler needs to know the hash salt so that it
    can generate rest tokens when sending executions. We declare it the
    same way we do in the restservice.
    
    Note: currently, this is not configurable in the helm chart,
    but it should be.
    
    * pin werkzeug to <3

--- execution-scheduler/docker/entrypoint.sh
+++ execution-scheduler/docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S bash -eux
 
+set +x
 echo "
 postgresql_db_name: ${POSTGRES_DB}
 postgresql_host: ${POSTGRES_HOST}
@@ -7,10 +8,15 @@ postgresql_username: ${POSTGRES_USER}
 postgresql_password: ${POSTGRES_PASSWORD}
 " > /opt/manager/cloudify-rest.conf
 
-echo "
-secret_key: ${SECRET_KEY}
+if [ ! -e "/opt/manager/rest-security.conf" ]; then
+    echo "
+{
+    \"secret_key\": \"${SECRET_KEY}\",
+    \"hash_salt\": \"${HASH_SALT}\"
+}
 " > /opt/manager/rest-security.conf
-
+fi
+set -x
 
 python -m manager_rest.configure_manager --db-wait postgresql
 python -m manager_rest.configure_manager --rabbitmq-wait rabbitmq
