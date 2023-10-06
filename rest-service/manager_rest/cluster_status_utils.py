2db0d2bcf5de1eef309841b664ea1e5c3bab89fe
Mateusz Neumann <mateumann@gmail.com>
2023-10-06 12:42:09 +0200

    CYBL-2091 Implement cluster status for k8s environment

---
+++ rest-service/manager_rest/cluster_status_utils.py
@@ -0,0 +1,205 @@
+from datetime import datetime
+from enum import StrEnum
+from typing import Any
+
+from cloudify.cluster_status import CloudifyNodeType
+from flask import current_app
+from pydantic import BaseModel
+
+from manager_rest.config import instance as config
+from manager_rest.prometheus_client import query as prometheus_query
+
+QUERY_STRINGS = {
+    CloudifyNodeType.DB: "(postgres_healthy) or (postgres_service)",
+    CloudifyNodeType.BROKER: "(rabbitmq_healthy) or (rabbitmq_service)",
+    CloudifyNodeType.MANAGER: "(manager_healthy) or (manager_service)",
+}
+
+SERVICE_ASSIGNMENTS = {
+    CloudifyNodeType.DB: [
+        "postgresql-14",
+    ],
+    CloudifyNodeType.BROKER: [
+        "cloudify-rabbitmq",
+    ],
+    CloudifyNodeType.MANAGER: [
+        "cloudify-composer",
+        "cloudify-mgmtworker",
+        "cloudify-restservice",
+        "cloudify-stage",
+        "cloudify-api",
+        "cloudify-execution-scheduler",
+        "nginx",
+        "prometheus",
+    ],
+}
+
+STATUS = "status"
+
+
+class Status(StrEnum):
+    FAIL = "Fail"
+    OK = "OK"
+
+
+class Metric(BaseModel):
+    healthy: bool
+    last_check: str | None
+    metric_name: str | None
+    metric_type: str | None
+
+
+class Node(BaseModel):
+    private_ip: str | None
+    public_ip: str | None
+    version: str | None
+    metrics: list[Metric] | None
+    failures: list[str] | None = None
+    status: Status
+
+    def calc_status(self):
+        self.status = (
+            Status.OK
+            if all(m.healthy for m in self.metrics) and not self.failures
+            else Status.FAIL
+        )
+
+
+class Service(BaseModel):
+    is_external: bool = False
+    nodes: dict[str, Node] = {}
+    failures: list[str] | None = None
+    status: Status
+
+    def calc_status(self, in_depth=True):
+        if in_depth:
+            for node_name in self.nodes:
+                self.nodes[node_name].calc_status()
+        self.status = (
+            Status.OK
+            if all(n.status for n in self.nodes.values()) and not self.failures
+            else Status.FAIL
+        )
+
+
+class ClusterStatus(BaseModel):
+    status: Status = Status.FAIL
+    services: dict[str, Service] = {}
+
+    def calc_status(self, in_depth=True):
+        if in_depth:
+            for service_name in self.services:
+                self.services[service_name].calc_status(in_depth)
+        self.status = (
+            Status.OK
+            if all(s.status for s in self.services.values())
+            else Status.FAIL
+        )
+        return self
+
+    def populate_prometheus_metrics(self, prometheus_results: list[dict]):
+        for result in prometheus_results:
+            service_assignment, instance, node = parse_metrics(result)
+            if not service_assignment:
+                continue
+            if service_assignment not in self.services:
+                self.services[service_assignment] = Service(
+                    is_external=False,
+                    nodes={instance: node},
+                    status=Status.OK,
+                )
+            else:
+                self.services[service_assignment].nodes[instance] = node
+        return self
+
+    def fill_missing_services(self, service_assignments: dict[str, list[str]]):
+        for service_assignment, service_names in service_assignments.items():
+            if service_assignment not in self.services:
+                self.services[service_assignment] = Service(
+                    is_external=False,
+                    nodes={},
+                    status=Status.FAIL,
+                    failures=[
+                        f"Missing metrics for {service_name}"
+                        for service_name in service_names
+                    ],
+                )
+            else:
+                nodes = self.services[service_assignment].nodes
+                for service_name in service_names:
+                    found = False
+                    for node_name, node in nodes.items():
+                        for metric in node.metrics:
+                            if metric.metric_name == service_name:
+                                found = True
+                                break
+                        if found:
+                            break
+                    if not found:
+                        if self.services[service_assignment].failures is None:
+                            self.services[service_assignment].failures = []
+                        self.services[service_assignment].failures.append(
+                            f"Missing metrics for service {service_name}"
+                        )
+                        self.services[service_assignment].status = Status.FAIL
+        return self
+
+
+def get_cluster_status() -> dict[str, Any]:
+    query_string = ' or '.join(QUERY_STRINGS.values())
+    prometheus_results = prometheus_query(
+        query_string=query_string,
+        logger=current_app.logger,
+        timeout=config.monitoring_timeout,
+    )
+    status = ClusterStatus()\
+        .populate_prometheus_metrics(prometheus_results)\
+        .fill_missing_services(SERVICE_ASSIGNMENTS)\
+        .calc_status()
+    return status.dict(exclude_none=True)
+
+
+def service_node_type(service_name: str) -> str | None:
+    for node_type, service_names in SERVICE_ASSIGNMENTS.items():
+        if service_name in service_names:
+            return node_type
+
+
+def parse_metrics(result: dict) -> (str | None, str | None, Node | None):
+    raw_metric = result.get("metric", {})
+    instance = raw_metric.get("instance")
+    service_name = raw_metric.get("name")
+
+    service_assignment = service_node_type(service_name)
+    if not service_assignment:
+        return None, None, None
+
+    timestamp, healthy = result.get("value", [0, ""])
+    healthy = bool(int(healthy)) if healthy else False
+    return (
+        service_assignment,
+        instance,
+        Node(
+            private_ip=instance,
+            public_ip=instance,
+            metrics=[
+                Metric(
+                    healthy=healthy,
+                    last_check=last_check_from_timestamp(timestamp),
+                    metric_name=service_name,
+                    metric_type=raw_metric.get('job') + " "
+                    + raw_metric.get('__name__'),
+                )
+            ],
+            failures=(
+                [f"Unhealthy service {service_name}"] if not healthy else None
+            ),
+            status=Status.OK if healthy else Status.FAIL,
+        ),
+    )
+
+
+def last_check_from_timestamp(ts: float | None) -> str:
+    if ts:
+        return datetime.fromtimestamp(ts).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return "unknown"
--- rest-service/manager_rest/rest/resources_v3_1/cluster_status.py
+++ rest-service/manager_rest/rest/resources_v3_1/cluster_status.py
@@ -1,18 +1,4 @@
-#########
-# Copyright (c) 2019 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
+import os
 
 from flask import request
 
@@ -22,8 +8,10 @@ from manager_rest.rest import responses, swagger
 from manager_rest.security.authorization import authorize
 from manager_rest.rest.rest_decorators import marshal_with
 from manager_rest.security import SecuredResource
-from manager_rest.cluster_status_manager import (STATUS,
-                                                 get_cluster_status)
+from manager_rest.cluster_status_manager import (
+    get_cluster_status as get_cluster_status_legacy,
+)
+from manager_rest.cluster_status_utils import STATUS, get_cluster_status
 from manager_rest.rest.rest_utils import (verify_and_convert_bool,
                                           get_json_and_verify_params)
 
@@ -51,12 +39,18 @@ class ClusterStatus(SecuredResource):
             'summary',
             request.args.get('summary', False)
         )
-        cluster_status = get_cluster_status(detailed=not summary_response)
+        if os.environ.get('RUNTIME_ENVIRONMENT', 'legacy').lower() in [
+            'k8s',
+            'kubernetes',
+        ]:
+            status = get_cluster_status()
+        else:
+            status = get_cluster_status_legacy(detailed=not summary_response)
 
         # If the response should be only the summary
         if summary_response:
-            short_status = cluster_status.get(STATUS)
+            short_status = status.get(STATUS)
             status_code = 500 if short_status == ServiceStatus.FAIL else 200
             return {'status': short_status, 'services': {}}, status_code
 
-        return cluster_status
+        return status
