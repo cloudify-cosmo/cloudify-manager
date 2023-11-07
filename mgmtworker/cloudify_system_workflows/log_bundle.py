d0d1a365eae71c026ab6c7cc25520f827230ef31
Mateusz Neumann <mateumann@gmail.com>
2023-11-07 20:54:11 +0100

    Implement two versions of fetch-logs script
    ... and run them accoriding to environment mgmtworker is running in

--- mgmtworker/cloudify_system_workflows/log_bundle.py
+++ mgmtworker/cloudify_system_workflows/log_bundle.py
@@ -1,44 +1,26 @@
 import os
-import shutil
+import pathlib
 import subprocess
 
 from cloudify.manager import get_rest_client
 from cloudify.workflows import ctx
+from cloudify_rest_client import CloudifyClient
 
 
 def create(log_bundle_id, **kwargs):
     ctx.logger.info('Creating log bundle `{0}`'.format(log_bundle_id))
     try:
         client = get_rest_client(tenant=ctx.tenant_name)
-
-        monitoring_username = client.manager.get_config(
-            name='log_fetch_username')['value']
-        monitoring_password = client.manager.get_config(
-            name='log_fetch_password')['value']
-
-        manager_nodes = {node['private_ip']
-                         for node in client.manager.get_managers()}
-        db_nodes = {node['host']
-                    for node in client.manager.get_db_nodes()}
-        broker_nodes = {node['management_host']
-                        for node in client.manager.get_brokers()}
-        addresses = manager_nodes | db_nodes | broker_nodes
+        script_cmd, script_env = prepare_script(client)
 
         log_bundle_tmp_path = subprocess.check_output(
-            [
-                '/opt/mgmtworker/scripts/fetch-logs',
-                '-a', ','.join(addresses),
-            ],
-            env={
-                'MONITORING_USERNAME': monitoring_username,
-                'MONITORING_PASSWORD': monitoring_password,
-            },
+            script_cmd,
+            env=script_env,
             stderr=subprocess.STDOUT,
         ).strip()
 
-        dest = f'/opt/manager/resources/log_bundles/{log_bundle_id}.zip'
-        shutil.move(log_bundle_tmp_path, dest)
-        os.chmod(dest, 0o440)
+        client.log_bundles.upload_archive(log_bundle_id, log_bundle_tmp_path)
+        os.remove(log_bundle_tmp_path)
     except Exception as err:
         client.log_bundles.update_status(
             log_bundle_id,
@@ -47,3 +29,65 @@ def create(log_bundle_id, **kwargs):
         )
         raise
     client.log_bundles.update_status(log_bundle_id, 'created')
+
+
+def installation_method():
+    match python_cmd():
+        case "/opt/mgmtworker/env/bin/python3":
+            return "rpm"
+        case "/usr/local/bin/python":
+            return "helm"
+        case _:
+            return "unknown"
+
+
+def prepare_script(client: CloudifyClient) -> (list[str], dict[str, str]):
+    manager_nodes = {
+        node['private_ip'] for node in client.manager.get_managers()
+    }
+    db_nodes = {node['host'] for node in client.manager.get_db_nodes()}
+    broker_nodes = {
+        node['management_host'] for node in client.manager.get_brokers()
+    }
+    addresses = manager_nodes | db_nodes | broker_nodes
+
+    match os.environ.get("RUNTIME_ENVIRONMENT").lower():
+        case "k8s":
+            installed_from = "helm"
+        case "cluster" | "aio":
+            installed_from = "rpm"
+        case _:
+            installed_from = installation_method()
+
+    match installed_from:
+        case "helm":
+            script_cmd = ["/opt/mgmtworker/scripts/fetch-logs-local"]
+            script_env = {}
+        case "rpm":
+            script_cmd = [
+                "/opt/mgmtworker/scripts/fetch-logs-cluster",
+                "-a",
+                ",".join(addresses),
+            ]
+            script_env = {
+                "MONITORING_USERNAME": client.manager.get_config(
+                    name="log_fetch_username"
+                )["value"],
+                "MONITORING_PASSWORD": client.manager.get_config(
+                    name="log_fetch_password"
+                )["value"],
+            }
+        case _:
+            raise RuntimeError(
+                f"Unsupported installation method: {installed_from}"
+            )
+    return script_cmd, script_env
+
+
+def python_cmd() -> str:
+    for location in [
+        "/opt/mgmtworker/env/bin/python3",
+        "/usr/local/bin/python",
+    ]:
+        if pathlib.Path(location).is_file():
+            return location
