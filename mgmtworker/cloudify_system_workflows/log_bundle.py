c693bdce3dfe047437259bc2b883660e9c09fa85
Mateusz Neumann <mateumann@gmail.com>
2023-11-08 06:31:43 +0100

    Get rid of fetch-logs scripts
    Instead run the Python code directly from the mgmtworker's workflow code

--- mgmtworker/cloudify_system_workflows/log_bundle.py
+++ mgmtworker/cloudify_system_workflows/log_bundle.py
@@ -1,88 +1,147 @@
 import os
-import subprocess
-import sys
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+
+import requests
 
 from cloudify.manager import get_rest_client
 from cloudify.workflows import ctx
+from cloudify.zip_utils import make_zip64_archive
 from cloudify_rest_client import CloudifyClient
 
+LOG_BASE_URL_LEGACY = "https://{server}:8009/cfylogs/"
+LOG_CA_CERT_PATH_LEGACY = "/etc/cloudify/ssl/monitoring_ca_cert.pem"
+LOG_PATH_K8S = "/opt/logs"
 
-def create(log_bundle_id, **kwargs):
-    ctx.logger.info('Creating log bundle `{0}`'.format(log_bundle_id))
+
+def create(log_bundle_id, **_):
+    ctx.logger.info("Creating log bundle `{0}`".format(log_bundle_id))
+    client = get_rest_client(tenant=ctx.tenant_name)
     try:
-        client = get_rest_client(tenant=ctx.tenant_name)
-        script_cmd, script_env = prepare_script(client)
-
-        log_bundle_tmp_path = subprocess.check_output(
-            script_cmd,
-            env=script_env,
-            stderr=subprocess.STDOUT,
-        ).strip()
-
+        log_bundle_tmp_path = do_create(client)
         client.log_bundles.upload_archive(log_bundle_id, log_bundle_tmp_path)
         os.remove(log_bundle_tmp_path)
     except Exception as err:
         client.log_bundles.update_status(
             log_bundle_id,
-            'failed',
+            "failed",
             str(err),
         )
         raise
-    client.log_bundles.update_status(log_bundle_id, 'created')
+    client.log_bundles.update_status(log_bundle_id, "created")
 
 
-def installation_method():
-    match sys.executable:
-        case "/opt/mgmtworker/env/bin/python3":
-            return "rpm"
-        case "/usr/local/bin/python":
-            return "helm"
-        case _:
-            return "unknown"
-
-
-def prepare_script(client: CloudifyClient) -> (list[str], dict[str, str]):
+def do_create(client: CloudifyClient) -> str:
     manager_nodes = {
-        node['private_ip'] for node in client.manager.get_managers()
+        node["private_ip"] for node in client.manager.get_managers()
     }
-    db_nodes = {node['host'] for node in client.manager.get_db_nodes()}
+    db_nodes = {node["host"] for node in client.manager.get_db_nodes()}
     broker_nodes = {
-        node['management_host'] for node in client.manager.get_brokers()
+        node["management_host"] for node in client.manager.get_brokers()
     }
-    addresses = manager_nodes | db_nodes | broker_nodes
 
-    match os.environ.get("RUNTIME_ENVIRONMENT").lower():
-        case "k8s":
-            installed_from = "helm"
-        case "cluster" | "aio":
-            installed_from = "rpm"
-        case _:
-            installed_from = installation_method()
+    if os.environ.get("RUNTIME_ENVIRONMENT", "").lower() == "k8s":
+        return fetch_k8s_logs()
+    else:
+        return fetch_legacy_logs(
+            manager_nodes | db_nodes | broker_nodes,
+            client.manager.get_config(name="log_fetch_username")["value"],
+            client.manager.get_config(name="log_fetch_password")["value"],
+        )
 
-    match installed_from:
-        case "helm":
-            script_cmd = [
-                sys.executable,
-                "/opt/mgmtworker/scripts/fetch-logs-local",
-            ]
-            script_env = {}
-        case "rpm":
-            script_cmd = [
-                sys.executable,
-                "/opt/mgmtworker/scripts/fetch-logs-cluster",
-                "-a",
-                ",".join(addresses),
-            ]
-            script_env = {
-                "MONITORING_USERNAME": client.manager.get_config(
-                    name="log_fetch_username"
-                )["value"],
-                "MONITORING_PASSWORD": client.manager.get_config(
-                    name="log_fetch_password"
-                )["value"],
-            }
-        case _:
-            raise RuntimeError(
-                f"Unsupported installation method: {installed_from}"
+
+def fetch_k8s_logs() -> str:
+    zip_file = NamedTemporaryFile(
+        prefix="cfylogs", suffix=".zip", delete=False
+    )
+    zip_file.close()
+    zip_path = zip_file.name
+    make_zip64_archive(zip_path, LOG_PATH_K8S)
+    return zip_path
+
+
+def fetch_legacy_logs(
+    addresses: set[str], username: str, password: str
+) -> str:
+    session = requests.Session()
+    session.auth = (username, password)
+    session.verify = LOG_CA_CERT_PATH_LEGACY
+
+    with TemporaryDirectory(prefix="cfylogs") as temp_path:
+        zip_file = NamedTemporaryFile(
+            prefix="cfylogs", suffix=".zip", delete=False
+        )
+        zip_file.close()
+        zip_path = zip_file.name
+
+        for server in addresses:
+            with open(
+                os.path.join(temp_path, server + ".log"), "w"
+            ) as log_handle:
+                fetch_legacy_logs_dir(server, temp_path, log_handle, session)
+
+        make_zip64_archive(zip_path, temp_path)
+        return zip_path
+
+
+def fetch_legacy_logs_dir(server, data_dir, log_handle, session):
+    save_path = os.path.join(data_dir, server)
+    url = LOG_BASE_URL_LEGACY.format(server=server)
+
+    dirs = [""]
+
+    while dirs:
+        dir_path = dirs.pop()
+        os.makedirs(os.path.join(save_path, dir_path))
+
+        try:
+            contents = session.get(url + dir_path, timeout=(10, None))
+        except requests.exceptions.ConnectionError as err:
+            log_handle.write(f"Failed to retrieve dir {dir_path}: {err}\n")
+            continue
+
+        if contents.status_code != 200:
+            log_handle.write(
+                f"Failed to retrieve dir {dir_path}: "
+                f"{contents.status_code}- {contents.reason}\n"
             )
-    return script_cmd, script_env
+        files = sorted(
+            [
+                dir_path + entry["name"]
+                for entry in contents.json()
+                if entry["type"] == "file"
+            ]
+        )
+
+        dirs.extend(
+            sorted(
+                [
+                    dir_path + entry["name"] + "/"
+                    for entry in contents.json()
+                    if entry["type"] == "directory"
+                ]
+            )
+        )
+
+        for file_path in files:
+            try:
+                downloaded_file = session.get(
+                    url + file_path, timeout=(10, None)
+                )
+            except requests.exceptions.ConnectionError as err:
+                log_handle.write(
+                    f"Failed to retrieve dir {file_path}: {err}\n"
+                )
+                continue
+
+            if downloaded_file.status_code == 200:
+                log_handle.write(f"Retrieved file {file_path}\n")
+                with open(
+                    os.path.join(save_path, file_path), "wb"
+                ) as file_handle:
+                    file_handle.write(downloaded_file.content)
+            else:
+                log_handle.write(
+                    f"Failed to retrieve file {file_path}: "
+                    f"{downloaded_file.status_code}- "
+                    f"{downloaded_file.reason}\n"
+                )
