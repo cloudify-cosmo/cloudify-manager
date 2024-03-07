793c4defaf5bd5eee2bb3352db1140468a46bac4
opencm <limor@gigaspaces.com>
2024-03-07 15:20:17 +0000

    Bump version

--- execution-scheduler/setup.py
+++ execution-scheduler/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudify-execution-scheduler',
-    version='7.0.4.dev1',
+    version='7.0.4',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(),
--- mgmtworker/setup.py
+++ mgmtworker/setup.py
@@ -38,7 +38,7 @@ else:
 
 setup(
     name='cloudify-mgmtworker',
-    version='7.0.4.dev1',
+    version='7.0.4',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(),
